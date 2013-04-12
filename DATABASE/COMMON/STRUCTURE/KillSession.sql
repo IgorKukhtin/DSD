dbd288f164c03706a516c1997412ba6528953d48
Kukhtin Igor <kukhtinigor@hotmail.com>
2013-04-12 09:49:54 +0300

    KillSession

--- DATABASE/COMMON/STRUCTURE/KillSession.sql
+++ DATABASE/COMMON/STRUCTURE/KillSession.sql
@@ -1,3 +1,17 @@
-SELECT pg_terminate_backend(pg_stat_activity.pid)
-FROM pg_stat_activity
-WHERE pg_stat_activity.datname = 'dsd';
+DO $$
+
+BEGIN
+   
+   PERFORM pg_terminate_backend(pg_stat_activity.procpid)
+   FROM pg_stat_activity
+   WHERE pg_stat_activity.datname = 'dsd';
+
+   EXCEPTION WHEN OTHERS THEN
+
+      PERFORM pg_terminate_backend(pg_stat_activity.pid)
+      FROM pg_stat_activity
+      WHERE pg_stat_activity.datname = 'dsd';
+
+END $$;
+
+
