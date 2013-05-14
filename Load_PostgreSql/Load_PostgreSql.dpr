2a555839047f1270d686a407b9b11b63e56742db
Konstantin <ashtu777@gmail.com>
2013-05-14 16:14:03 +0300

    new Projects

---
+++ Load_PostgreSql/Load_PostgreSql.dpr
@@ -0,0 +1,13 @@
+program Load_PostgreSql;
+
+uses
+  Forms,
+  Main in 'Main.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
