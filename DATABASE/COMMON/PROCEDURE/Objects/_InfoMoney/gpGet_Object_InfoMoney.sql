d72815bb65733daf63cf72aa1e88d61f08bbc64c
innaf <innaf@ua.fm>
2013-06-21 23:20:54 +0300

    1

--- DATABASE/COMMON/PROCEDURE/Objects/_InfoMoney/gpGet_Object_InfoMoney.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/_InfoMoney/gpGet_Object_InfoMoney.sql
@@ -53,11 +53,12 @@ $BODY$BEGIN
  LEFT JOIN ObjectLink AS ObjectLink_InfoMoney_InfoMoneyDestination
         ON ObjectLink_InfoMoney_InfoMoneyDestination.ObjectId = Object_InfoMoney.Id
        AND ObjectLink_InfoMoney_InfoMoneyDestination.DescId = zc_ObjectLink_InfoMoney_InfoMoneyDestination()
+ LEFT JOIN Object AS Object_InfoMoneyDestination ON Object_InfoMoneyDestination.Id = ObjectLink_InfoMoney_InfoMoneyDestination.ChildObjectId
+ 
  LEFT JOIN ObjectLink AS ObjectLink_InfoMoney_InfoMoneyGroup
         ON ObjectLink_InfoMoney_InfoMoneyGroup.ObjectId = Object_InfoMoney.Id
        AND ObjectLink_InfoMoney_InfoMoneyGroup.DescId = zc_ObjectLink_InfoMoney_InfoMoneyGroup()
- LEFT JOIN Object AS Object_InfoMoneyDestination ON Object_InfoMoneyDestination.Id = ObjectLink_InfoMoney_InfoMoneyDestination.ChildObjectId
- LEFT JOIN Object AS ObjectLink_InfoMoney_InfoMoneyGroup ON Object_InfoMoneyGroup.Id = ObjectLink_InfoMoney_InfoMoneyGroup.ChildObjectId
+ LEFT JOIN Object AS Object_InfoMoneyGroup ON Object_InfoMoneyGroup.Id = ObjectLink_InfoMoney_InfoMoneyGroup.ChildObjectId
      WHERE Object_InfoMoney.Id = inId;
    END IF;
   
--- DATABASE/COMMON/PROCEDURE/Objects/_InfoMoney/gpSelect_Object_InfoMoney.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/_InfoMoney/gpSelect_Object_InfoMoney.sql
@@ -32,11 +32,13 @@ $BODY$BEGIN
  LEFT JOIN ObjectLink AS ObjectLink_InfoMoney_InfoMoneyDestination
         ON ObjectLink_InfoMoney_InfoMoneyDestination.ObjectId = Object_InfoMoney.Id
        AND ObjectLink_InfoMoney_InfoMoneyDestination.DescId = zc_ObjectLink_InfoMoney_InfoMoneyDestination()
+ LEFT JOIN Object AS Object_InfoMoneyDestination ON Object_InfoMoneyDestination.Id = ObjectLink_InfoMoney_InfoMoneyDestination.ChildObjectId
+ 
  LEFT JOIN ObjectLink AS ObjectLink_InfoMoney_InfoMoneyGroup
         ON ObjectLink_InfoMoney_InfoMoneyGroup.ObjectId = Object_InfoMoney.Id
        AND ObjectLink_InfoMoney_InfoMoneyGroup.DescId = zc_ObjectLink_InfoMoney_InfoMoneyGroup()
- LEFT JOIN Object AS Object_InfoMoneyDestination ON Object_InfoMoneyDestination.Id = ObjectLink_InfoMoney_InfoMoneyDestination.ChildObjectId
- LEFT JOIN Object AS ObjectLink_InfoMoney_InfoMoneyGroup ON Object_InfoMoneyGroup.Id = ObjectLink_InfoMoney_InfoMoneyGroup.ChildObjectId
+ LEFT JOIN Object AS Object_InfoMoneyGroup ON Object_InfoMoneyGroup.Id = ObjectLink_InfoMoney_InfoMoneyGroup.ChildObjectId
+
      WHERE Object_InfoMoney.DescId = zc_Object_InfoMoney();
   
 END;$BODY$
