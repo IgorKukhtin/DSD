43207e6f19261caf85a729051d49974c6c111fd6
Konstantin <ashtu777@gmail.com>
2013-07-09 00:45:05 +0300

    ok

--- DATABASE/COMMON/PROCEDURE/MovementItemContainer/gpUnComplete_Movement.sql
+++ DATABASE/COMMON/PROCEDURE/MovementItemContainer/gpUnComplete_Movement.sql
@@ -14,10 +14,16 @@ $BODY$BEGIN
   PERFORM lpDelete_MovementItemContainer(inMovementId);
 
   -- Обязательно меняем статус документа
-  UPDATE Movement SET StatusId = zc_Object_Status_UnComplete() WHERE Id = inMovementId;
+  UPDATE Movement SET StatusId = zc_Enum_Status_UnComplete() WHERE Id = inMovementId;
 
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-  
-                            
+
+
+/*-------------------------------------------------------------------------------
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+
+ 08.07.13                                        * rename to zc_Enum_Status_UnComplete
+*/
