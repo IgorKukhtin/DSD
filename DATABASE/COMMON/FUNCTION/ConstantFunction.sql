e0f910c4e5d9526163e1186ab1b6b720a115cbcd
Konstantin <ashtu777@gmail.com>
2013-07-16 18:50:36 +0300

    ok

--- DATABASE/COMMON/FUNCTION/ConstantFunction.sql
+++ DATABASE/COMMON/FUNCTION/ConstantFunction.sql
@@ -2,9 +2,11 @@ CREATE OR REPLACE FUNCTION zc_DateEnd() RETURNS TDateTime AS $BODY$BEGIN RETURN 
 
 CREATE OR REPLACE FUNCTION zc_DateStart_PartionGoods() RETURNS TDateTime AS $BODY$BEGIN RETURN ('18.03.2013'); END; $BODY$ LANGUAGE PLPGSQL IMMUTABLE;
 
+CREATE OR REPLACE FUNCTION zc_DateStart_ObjectCostOnUnit() RETURNS TDateTime AS $BODY$BEGIN RETURN ('01.09.2013'); END; $BODY$ LANGUAGE PLPGSQL IMMUTABLE;
 
 /*-------------------------------------------------------------------------------
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 16.07.13                                        *
  12.07.13                                        *
 */
