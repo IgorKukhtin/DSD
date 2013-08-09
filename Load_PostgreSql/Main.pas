e2deeb4ffb0b3ff290f55445db657363d79282cd
K0sta <Ashtu777@gmail.com>
2013-08-09 13:58:31 +0300

    ok

--- Load_PostgreSql/Main.pas
+++ Load_PostgreSql/Main.pas
@@ -5596,6 +5596,5 @@ alter table dba.PriceListItems_byHistory add Id_Postgres integer null;
 alter table dba.Bill add Id_Postgres integer null;
 alter table dba.BillItems add Id_Postgres integer null;
 alter table dba.BillItemsReceipt add Id_Postgres integer null;
-
 ok
 }
