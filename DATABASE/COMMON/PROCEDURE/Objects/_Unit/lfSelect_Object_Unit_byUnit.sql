a10c6888ae45357910ad1a8dfd9b0c6f4563ef33
innaf <innaf@ua.fm>
2013-09-12 13:25:20 +0300

    1

--- DATABASE/COMMON/PROCEDURE/Objects/_Unit/lfSelect_Object_Unit_byUnit.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/_Unit/lfSelect_Object_Unit_byUnit.sql
@@ -15,13 +15,13 @@ BEGIN
     	UNION ALL
     	SELECT sc.ObjectId, sc.ChildObjectId FROM ObjectLink sc 
     		INNER JOIN RecurObjectLink rs ON rs.ChildObjectId = sc.ObjectId
-    	WHERE sc.DescId = zc_Object_Unit()
+    	WHERE sc.DescId = zc_ObjectLink_Unit_Parent()
     )
      SELECT ObjectLink_0.ObjectId AS UnitId 
      FROM ObjectLink 
         LEFT JOIN RecurObjectLink ON ObjectLink.ChildObjectId = RecurObjectLink.ObjectId  
                                  AND RecurObjectLink.ChildObjectId = inUnitId
-     WHERE ObjectLink.DescId = zc_Object_Unit();
+     WHERE ObjectLink.DescId = zc_ObjectLink_Unit_Parent();
 
 END;
 $BODY$
