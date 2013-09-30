3b341d726ea6000dc4c35598668ebf7d4be6b3cd
K0sta <Ashtu777@gmail.com>
2013-09-30 18:18:04 +0300

    personal

---
+++ DATABASE/COMMON/PROCEDURE/Movement/_PersonalSendCash/gpInsertUpdate_Movement_PersonalSendCash.sql
@@ -0,0 +1,46 @@
+-- Function: gpInsertUpdate_Movement_PersonalSendCash()
+
+-- DROP FUNCTION gpInsertUpdate_Movement_PersonalSendCash();
+
+CREATE OR REPLACE FUNCTION gpInsertUpdate_Movement_PersonalSendCash(
+ INOUT ioId                  Integer   , -- Ключ объекта <Документ>
+    IN inInvNumber           TVarChar  , -- Номер документа
+    IN inOperDate            TDateTime , -- Дата документа
+    IN inPersonalId          Integer   , -- Сотрудник
+    IN inSession             TVarChar    -- сессия пользователя
+)                              
+RETURNS Integer AS
+$BODY$
+   DECLARE vbUserId Integer;
+BEGIN
+
+
+     -- проверка прав пользователя на вызов процедуры
+     -- PERFORM lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_Movement_PersonalSendCash());
+     vbUserId := inSession;
+
+     -- сохранили <Документ>
+     ioId := lpInsertUpdate_Movement (ioId, zc_Movement_PersonalSendCash(), inInvNumber, inOperDate, NULL);
+
+     -- сохранили связь с <Сотрудник>
+     PERFORM lpInsertUpdate_MovementLinkObject (zc_MovementLinkObject_Personal(), ioId, inPersonalId);
+     
+     -- пересчитали Итоговые суммы по накладной
+     PERFORM lpInsertUpdate_MovementFloat_TotalSumm (ioId);
+
+     -- сохранили протокол
+     -- PERFORM lpInsert_MovementProtocol (ioId, vbUserId);
+
+END;
+$BODY$
+  LANGUAGE PLPGSQL VOLATILE;
+
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 30.09.13                                        *
+*/
+
+-- тест
+-- SELECT * FROM gpInsertUpdate_Movement_PersonalSendCash (ioId:= 0, inInvNumber:= '-1', inOperDate:= '01.01.2013', inAmount:= 20, inFromId:= 1, inToId:= 1, inPaidKindId:= 1,  inInfoMoneyId:= 0, inUnitId:= 0, inServiceDate:= '01.01.2013', inSession:= '2')
