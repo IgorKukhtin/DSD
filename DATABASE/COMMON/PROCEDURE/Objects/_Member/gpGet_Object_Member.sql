311497ad47c9479d7834391c021b9229f0bd4d01
innaf <innaf@ua.fm>
2013-10-01 15:44:26 +0300

    WorkTimeKind, Member

--- DATABASE/COMMON/PROCEDURE/Objects/_Member/gpGet_Object_Member.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/_Member/gpGet_Object_Member.sql
@@ -6,7 +6,9 @@ CREATE OR REPLACE FUNCTION gpGet_Object_Member(
     IN inId          Integer,        -- Физические лица 
     IN inSession     TVarChar        -- сессия пользователя
 )
-RETURNS TABLE (Id Integer, Code Integer, Name TVarChar, INN TVarChar, isErased boolean) AS
+RETURNS TABLE (Id Integer, Code Integer, Name TVarChar
+            , INN TVarChar, DriverCertificate TVarChar, Comment TVarChar
+            , isErased boolean) AS
 $BODY$
 BEGIN
 
@@ -20,21 +22,35 @@ BEGIN
              CAST (0 as Integer)    AS Id
            , lfGet_ObjectCode(0, zc_Object_Member()) AS Code
            , CAST ('' as TVarChar)  AS NAME
+           
            , CAST ('' as TVarChar)  AS INN
+           , CAST ('' as TVarChar)  AS DriverCertificate
+           , CAST ('' as TVarChar)  AS Comment
+           
            , CAST (NULL AS Boolean) AS isErased;
    ELSE
        RETURN QUERY 
      SELECT 
-           Object_Member.Id             AS Id
-         , Object_Member.ObjectCode     AS Code
-         , Object_Member.ValueData      AS Name
+           Object_Member.Id         AS Id
+         , Object_Member.ObjectCode AS Code
+         , Object_Member.ValueData  AS Name
          
-         , ObjectString_INN.ValueData   AS INN
-         , Object_Member.isErased       AS isErased
+         , ObjectString_INN.ValueData               AS INN
+         , ObjectString_DriverCertificate.ValueData AS DriverCertificate
+         , ObjectString_Comment.ValueData           AS Comment
+         
+         , Object_Member.isErased  AS isErased
          
      FROM OBJECT AS Object_Member
           LEFT JOIN ObjectString AS ObjectString_INN ON ObjectString_INN.ObjectId = Object_Member.Id 
                 AND ObjectString_INN.DescId = zc_ObjectString_Member_INN()
+ 
+          LEFT JOIN ObjectString AS ObjectString_DriverCertificate ON ObjectString_DriverCertificate.ObjectId = Object_Member.Id 
+                AND ObjectString_DriverCertificate.DescId = zc_ObjectString_Member_DriverCertificate()
+
+          LEFT JOIN ObjectString AS ObjectString_Comment ON ObjectString_Comment.ObjectId = Object_Member.Id 
+                AND ObjectString_Comment.DescId = zc_ObjectString_Member_Comment()
+
      WHERE Object_Member.Id = inId;
      
    END IF;
@@ -49,7 +65,8 @@ ALTER FUNCTION gpGet_Object_Member(integer, TVarChar) OWNER TO postgres;
 /*-------------------------------------------------------------------------------
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
- 01.07.13          *
+ 01.10.13         *  add DriverCertificate, Comment                            
+ 01.07.13         *
  19.07.13                        *
 
 */
