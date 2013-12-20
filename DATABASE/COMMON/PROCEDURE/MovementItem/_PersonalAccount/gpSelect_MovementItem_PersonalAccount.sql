043207338d0c6e5590369b26d13f9fcb13eee97e
innaf <innaf@ua.fm>
2013-12-20 22:23:36 +0200

    1

--- DATABASE/COMMON/PROCEDURE/MovementItem/_PersonalAccount/gpSelect_MovementItem_PersonalAccount.sql
+++ DATABASE/COMMON/PROCEDURE/MovementItem/_PersonalAccount/gpSelect_MovementItem_PersonalAccount.sql
@@ -8,12 +8,12 @@ CREATE OR REPLACE FUNCTION gpSelect_MovementItem_PersonalAccount(
     IN inIsErased    Boolean      , -- 
     IN inSession     TVarChar       -- сессия пользователя
 )
-RETURNS TABLE (MIId_20401 Integer, MIId_21201 Integer
+RETURNS TABLE (Id Integer, OperDate TDateTime
+             , InfoMoneyId Integer, InfoMoneyName TVarChar
              , PersonalId Integer, PersonalCode Integer, PersonalName TVarChar
-             , Amount_20401 TFloat, Amount_21201 TFloat
              , RouteId Integer, RouteName TVarChar
              , CarId Integer, CarName TVarChar, CarModelName TVarChar
-             , OperDate TDateTime
+             , Amount TFloat
              , isErased Boolean
               )
 AS
@@ -24,64 +24,57 @@ BEGIN
      -- PERFORM lpCheckRight (inSession, zc_Enum_Process_Select_MI_PersonalAccount());
 
      RETURN QUERY 
-       SELECT
-             tmpMovementItem.MIId_20401
-           , tmpMovementItem.MIId_21201
-           , tmpMovementItem.PersonalId
-           , View_Personal.PersonalCode
-           , View_Personal.PersonalName
-         
-           , CAST (tmpMovementItem.Amount_20401 AS TFloat) AS Amount_20401
-           , CAST (tmpMovementItem.Amount_21201 AS TFloat) AS Amount_21201
+       SELECT MovementItem.Id
+            , MIDate_OperDate.ValueData   AS OperDate
            
-           , Object_Route.Id         AS RouteId
-           , Object_Route.ValueData  AS RouteName
+            , View_InfoMoney.InfoMoneyId
+            , View_InfoMoney.InfoMoneyName
+            
+            , MovementItem.ObjectId       AS PersonalId
+            , View_Personal.PersonalCode
+            , View_Personal.PersonalName
+                  
+            , Object_Route.Id             AS RouteId
+            , Object_Route.ValueData      AS RouteName
 
-           , Object_Car.Id              AS CarId
-           , Object_Car.ValueData       AS CarName
-           , Object_CarModel.ValueData  AS CarModelName
+            , Object_Car.Id               AS CarId
+            , Object_Car.ValueData        AS CarName
+            , Object_CarModel.ValueData   AS CarModelName
 
-           , tmpMovementItem.OperDate
-           , tmpMovementItem.isErased
+            
+            , MovementItem.Amount         AS Amount
+            , MovementItem.isErased       AS isErased
+                  
+       FROM (SELECT FALSE AS isErased UNION ALL SELECT inIsErased AS isErased WHERE inIsErased = TRUE) AS tmpIsErased
+       
+             JOIN MovementItem ON MovementItem.MovementId = inMovementId
+                              AND MovementItem.DescId     = zc_MI_Master()
+                              AND MovementItem.isErased   = tmpIsErased.isErased
+                 
+             LEFT JOIN MovementItemDate AS MIDate_OperDate
+                                        ON MIDate_OperDate.MovementItemId = MovementItem.Id
+                                       AND MIDate_OperDate.DescId = zc_MIDate_OperDate()
+                 
+             LEFT JOIN MovementItemLinkObject AS MILinkObject_InfoMoney
+                                              ON MILinkObject_InfoMoney.MovementItemId = MovementItem.Id
+                                             AND MILinkObject_InfoMoney.DescId = zc_MILinkObject_InfoMoney()
+             LEFT JOIN Object_InfoMoney_View AS View_InfoMoney ON View_InfoMoney.InfoMoneyId = MILinkObject_InfoMoney.ObjectId
+                 
+             LEFT JOIN MovementItemLinkObject AS MILinkObject_Route
+                                              ON MILinkObject_Route.MovementItemId = MovementItem.Id
+                                             AND MILinkObject_Route.DescId = zc_MILinkObject_Route()
+             LEFT JOIN Object AS Object_Route ON Object_Route.Id = MILinkObject_Route.ObjectId
+                 
+             LEFT JOIN MovementItemLinkObject AS MILinkObject_Car
+                                              ON MILinkObject_Car.MovementItemId = MovementItem.Id
+                                             AND MILinkObject_Car.DescId = zc_MILinkObject_Car()
+             LEFT JOIN Object AS Object_Car ON Object_Car.Id = MILinkObject_Car.ObjectId
+                                                  
+             LEFT JOIN Object_Personal_View AS View_Personal ON View_Personal.PersonalId =MovementItem.ObjectId
 
-       FROM (SELECT MovementItem.ObjectId       AS PersonalId
-                  , MovementItem.isErased       AS isErased
-                  , MILinkObject_Route.ObjectId AS RouteId
-                  , MILinkObject_Car.ObjectId   AS CarId
-                  , MIDate_OperDate.ValueData   AS OperDate
-                  , MAX (CASE WHEN MILinkObject_InfoMoney.ObjectId = zc_Enum_InfoMoney_20401() THEN MovementItem.Id ELSE 0 END) AS MIId_20401
-                  , MAX (CASE WHEN MILinkObject_InfoMoney.ObjectId = zc_Enum_InfoMoney_21201() THEN MovementItem.Id ELSE 0 END) AS MIId_21201
-                  , SUM (CASE WHEN MILinkObject_InfoMoney.ObjectId = zc_Enum_InfoMoney_20401() THEN MovementItem.Amount ELSE 0 END) AS Amount_20401
-                  , SUM (CASE WHEN MILinkObject_InfoMoney.ObjectId = zc_Enum_InfoMoney_21201() THEN MovementItem.Amount ELSE 0 END) AS Amount_21201
-             FROM (SELECT FALSE AS isErased UNION ALL SELECT inIsErased AS isErased WHERE inIsErased = TRUE) AS tmpIsErased
-                  JOIN MovementItem ON MovementItem.MovementId = inMovementId
-                                   AND MovementItem.DescId     = zc_MI_Master()
-                                   AND MovementItem.isErased   = tmpIsErased.isErased
-                  LEFT JOIN MovementItemDate AS MIDate_OperDate
-                                             ON MIDate_OperDate.MovementItemId = MovementItem.Id
-                                            AND MIDate_OperDate.DescId = zc_MIDate_OperDate()
-                  LEFT JOIN MovementItemLinkObject AS MILinkObject_InfoMoney
-                                                   ON MILinkObject_InfoMoney.MovementItemId = MovementItem.Id
-                                                  AND MILinkObject_InfoMoney.DescId = zc_MILinkObject_InfoMoney()
-                  LEFT JOIN MovementItemLinkObject AS MILinkObject_Route
-                                                   ON MILinkObject_Route.MovementItemId = MovementItem.Id
-                                                  AND MILinkObject_Route.DescId = zc_MILinkObject_Route()
-                  LEFT JOIN MovementItemLinkObject AS MILinkObject_Car
-                                                   ON MILinkObject_Car.MovementItemId = MovementItem.Id
-                                                  AND MILinkObject_Car.DescId = zc_MILinkObject_Car()
-             GROUP BY MovementItem.ObjectId
-                    , MovementItem.isErased
-                    , MILinkObject_Route.ObjectId
-                    , MILinkObject_Car.ObjectId
-                    , MIDate_OperDate.ValueData
-            ) AS tmpMovementItem
-            LEFT JOIN Object_Personal_View AS View_Personal ON View_Personal.PersonalId = tmpMovementItem.PersonalId
-            LEFT JOIN Object AS Object_Route ON Object_Route.Id = tmpMovementItem.RouteId
-            LEFT JOIN Object AS Object_Car ON Object_Car.Id = tmpMovementItem.CarId
-
-            LEFT JOIN ObjectLink AS Car_CarModel ON Car_CarModel.ObjectId = Object_Car.Id
-                                                AND Car_CarModel.DescId = zc_ObjectLink_Car_CarModel()
-            LEFT JOIN Object AS Object_CarModel ON Object_CarModel.Id = Car_CarModel.ChildObjectId
+             LEFT JOIN ObjectLink AS Car_CarModel ON Car_CarModel.ObjectId = Object_Car.Id
+                                                 AND Car_CarModel.DescId = zc_ObjectLink_Car_CarModel()
+             LEFT JOIN Object AS Object_CarModel ON Object_CarModel.Id = Car_CarModel.ChildObjectId
 
       ;
 
