60b407da79672fb25aec1729e0d7df02221873e5
K0sta <Ashtu777@gmail.com>
2014-01-14 23:04:23 +0200

    zc_Movement_LossDebt

---
+++ DATABASE/COMMON/PROCEDURE/MovementItem/_LossDebt/gpInsertUpdate_MovementItem_LossDebt.sql
@@ -0,0 +1,106 @@
+-- Function: gpInsertUpdate_MovementItem_LossDebt ()
+
+DROP FUNCTION IF EXISTS gpInsertUpdate_MovementItem_LossDebt (Integer, Integer, Integer, TFloat, TDateTime, Integer, Integer, Integer, Integer, TVarChar);
+
+
+CREATE OR REPLACE FUNCTION gpInsertUpdate_MovementItem_LossDebt(
+ INOUT ioId                  Integer   , -- Ключ объекта <Элемент документа>
+    IN inMovementId          Integer   , -- ключ Документа
+    IN inJuridicalId         Integer   , -- Юр.лицо
+ INOUT ioAmountDebet         TFloat    , -- Сумма
+ INOUT ioAmountKredit        TFloat    , -- Сумма
+ INOUT ioSummDebet           TFloat    , -- Сумма остатка (долг)
+ INOUT ioSummKredit          TFloat    , -- Сумма остатка (долг)
+ INOUT ioIsCalculated        Boolean   , -- Сумма рассчитывается по остатку (да/нет)
+    IN inContractId          Integer   , -- Договор
+    IN inPaidKindId          Integer   , -- Вид форм оплаты
+    IN inInfoMoneyId         Integer   , -- Статьи назначения
+    IN inUnitId              Integer   , -- Подразделение
+    IN inSession             TVarChar    -- сессия пользователя
+)                              
+RETURNS RECORD AS
+$BODY$
+   DECLARE vbUserId Integer;
+   DECLARE vbAmount TFloat;
+   DECLARE vbSumm TFloat;
+BEGIN
+     -- проверка прав пользователя на вызов процедуры
+     -- vbUserId := PERFORM lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_MI_LossDebt());
+     vbUserId := inSession;
+
+     -- проверка
+     IF COALESCE (inJuridicalId, 0) = 0
+     THEN
+         RAISE EXCEPTION 'Ошибка. Не установлено <Юр.лицо>.';
+     END IF;
+
+     -- проверка
+     IF (COALESCE (ioAmountDebet, 0) <> 0) AND (COALESCE (ioAmountKredit, 0) <> 0) THEN
+        RAISE EXCEPTION 'Должна быть введена только одна сумма - или Дебет или Кредит.';
+     END IF;
+
+     -- проверка
+     IF (COALESCE (ioSummDebet, 0) <> 0) AND (COALESCE (ioSummKredit, 0) <> 0) THEN
+        RAISE EXCEPTION 'Должна быть введена только одна сумма - или Долг дебет Долг кредит.';
+     END IF;
+
+     -- расчет
+     IF ioAmountDebet <> 0 THEN
+        vbAmount := ioAmountDebet;
+     ELSE
+        vbAmount := -1 * ioAmountKredit;
+     END IF;
+     -- расчет
+     IF ioSummDebet <> 0 THEN
+        vbSumm := ioSummDebet;
+     ELSE
+        vbSumm := -1 * ioSummKredit;
+     END IF;
+
+     -- расчет
+     ioIsCalculated:= (vbSumm <> 0 OR vbAmount = 0)
+     -- 
+     IF vbSumm <> 0 THEN ioAmountDebet: = 0; ioAmountKredit: = 0; END IF;
+
+     -- сохранили <Элемент документа>
+     ioId := lpInsertUpdate_MovementItem (ioId, zc_MI_Master(), inJuridicalId, inMovementId, vbAmount, NULL);
+
+     -- сохранили свойство <Сумма рассчитывается по остатку (да/нет)>
+     PERFORM lpInsertUpdate_MovementItemBoolean (zc_MIBoolean_MasterFuel(), ioId, inIsMasterFuel);
+
+     -- сохранили свойство <Сумма остатка (долг)>
+     PERFORM lpInsertUpdate_MovementItemFloat (zc_MIFloat_Summ(), ioId, vbSumm);
+
+     -- сохранили свойство < Сумма рассчитывается по остатку (да/нет)>
+     PERFORM lpInsertUpdate_MovementItemBoolean (zc_MIBoolean_MasterFuel(), ioId, inIsMasterFuel);
+
+     -- сохранили связь с <Договор>
+     PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_Contract(), ioId, inContractId);
+
+     -- сохранили связь с <Вид форм оплаты>
+     PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_PaidKind(), ioId, inPaidKindId);
+
+     -- сохранили связь с <Статьи назначения>
+     PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_InfoMoney(), ioId, inInfoMoneyId);
+
+     -- сохранили связь с <Подразделение>
+     PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_Unit(), ioId, inUnitId);
+
+     -- пересчитали Итоговые суммы по документу
+     PERFORM lpInsertUpdate_MovementFloat_TotalSumm (inMovementId);
+
+     -- сохранили протокол
+     -- PERFORM lpInsert_MovementItemProtocol (ioId, vbUserId);
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.
+ 14.01.14                                        *
+*/
+
+-- тест
+-- SELECT * FROM gpInsertUpdate_MovementItem_LossDebt (ioId:= 0, inMovementId:= 10, inGoodsId:= 1, inAmount:= 0, inAmountPartner:= 0, inPrice:= 1, inCountForPrice:= 1, inLiveWeight:= 0, inHeadCount:= 0, inPartionGoods:= '', inGoodsKindId:= 0, inSession:= '2')
---
+++ DATABASE/COMMON/VIEW/Object/00_Object_Contract_InvNumber_View.sql
@@ -0,0 +1,31 @@
+-- View: Object_Contract_InvNumber_View
+
+DROP VIEW IF EXISTS Object_Contract_InvNumber_View CASCADE;
+
+CREATE OR REPLACE VIEW Object_Contract_InvNumber_View AS
+  SELECT Object_Contract.Id                          AS ContractId
+       , Object_Contract.ObjectCode                  AS ContractCode  
+       , CASE WHEN Object_Contract.ValueData <> '' THEN Object_Contract.ValueData ELSE '**уп' || Object_InfoMoney.ObjectCode :: TVarChar END :: TVarChar AS InvNumber
+       , ObjectLink_Contract_InfoMoney.ChildObjectId         AS InfoMoneyId
+       , Object_Contract.isErased                    AS isErased
+  FROM Object AS Object_Contract
+       LEFT JOIN ObjectLink AS ObjectLink_Contract_InfoMoney
+                            ON ObjectLink_Contract_InfoMoney.ObjectId = Object_Contract.Id 
+                           AND ObjectLink_Contract_InfoMoney.DescId = zc_ObjectLink_Contract_InfoMoney()
+                           
+       LEFT JOIN Object AS Object_InfoMoney ON Object_InfoMoney.Id = ObjectLink_Contract_InfoMoney.ChildObjectId
+  WHERE Object_Contract.DescId = zc_Object_Contract();
+
+
+ALTER TABLE Object_Contract_InvNumber_View  OWNER TO postgres;
+
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 14.01.14                                        * 
+*/
+
+-- тест
+-- SELECT * FROM Object_Contract_InvNumber_View
