07fe9380fd9aefad2a398095d272ced256749f1d
DmytroManko <dnepro.ua@gmail.com>
2014-01-28 01:25:02 +0200

    +

---
+++ DATABASE/COMMON/PROCEDURE/Movement/_ReturnIn/Movement_ReturnIn_seq.sql
@@ -0,0 +1,21 @@
+DO $$
+BEGIN
+  IF (SELECT COUNT(*) from pg_statio_all_sequences where relname = 'movement_returnin_seq') = 0 THEN
+    CREATE SEQUENCE movement_returnin_seq
+    INCREMENT 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1
+    CACHE 1;
+    ALTER TABLE movement_returnin_seq
+      OWNER TO postgres;
+  END IF;
+END $$;
+
+
+/*-------------------------------------------------------------------------------
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.    Манько Д.А.
+27.01.14                                                            *
+
+*/
