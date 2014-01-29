81b44637d62a37b8bce698141835a4c97a9e7e45
DmytroManko <dnepro.ua@gmail.com>
2014-01-29 18:25:11 +0200

    +

--- DATABASE/COMMON/PROCEDURE/Movement/_Sale/gpGet_Movement_Sale.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/_Sale/gpGet_Movement_Sale.sql
@@ -33,32 +33,29 @@ BEGIN
          SELECT
                0 AS Id
              , CAST (NEXTVAL ('movement_sale_seq') AS TVarChar) AS InvNumber
-             , inOperDate						 AS OperDate
-             , Object_Status.Code               AS StatusCode
-             , Object_Status.Name               AS StatusName
-             , CAST (False as Boolean)          AS Checked
-             , inOperDate				      		 AS	OperDatePartner
-             , CAST (False as Boolean)               AS PriceWithVAT
-             , CAST (20 as TFloat)                   AS VATPercent
-             , CAST (0 as TFloat)                    AS ChangePercent
-             , CAST (0 as TFloat)                    AS TotalCount
-             , CAST (0 as TFloat)                    AS TotalSummMVAT
-             , CAST (0 as TFloat)                    AS TotalSummPVAT
-             , CAST (0 as TFloat)                    AS TotalSumm
-
-
-             , 0                     AS FromId
-             , CAST ('' as TVarChar) AS FromName
-             , 0                     AS ToId
-             , CAST ('' as TVarChar) AS ToName
-
-             , 0                     AS PaidKindId
-             , CAST ('' as TVarChar) AS PaidKindName
-             , 0                     AS ContractId
-             , CAST ('' as TVarChar) AS ContractName
-             , 0                     AS PersonalPackerId
-             , CAST ('' as TVarChar) AS PersonalPackerName
-             , CAST ('' as TVarChar) AS InvNumberOrder
+             , inOperDate						 	AS OperDate
+             , Object_Status.Code               	AS StatusCode
+             , Object_Status.Name              		AS StatusName
+             , CAST (False as Boolean)         		AS Checked
+             , inOperDate				      		AS OperDatePartner
+             , CAST (False as Boolean)              AS PriceWithVAT
+             , CAST (20 as TFloat)                  AS VATPercent
+             , CAST (0 as TFloat)                   AS ChangePercent
+             , CAST (0 as TFloat)                   AS TotalCount
+             , CAST (0 as TFloat)                   AS TotalSummMVAT
+             , CAST (0 as TFloat)                   AS TotalSummPVAT
+             , CAST (0 as TFloat)                   AS TotalSumm
+             , 0                     				AS FromId
+             , CAST ('' as TVarChar) 				AS FromName
+             , 0                     				AS ToId
+             , CAST ('' as TVarChar) 				AS ToName
+             , 0                     				AS PaidKindId
+             , CAST ('' as TVarChar) 				AS PaidKindName
+             , 0                     				AS ContractId
+             , CAST ('' as TVarChar) 				AS ContractName
+             , 0                     				AS PersonalPackerId
+             , CAST ('' as TVarChar) 				AS PersonalPackerName
+             , CAST ('' as TVarChar) 				AS InvNumberOrder
           FROM lfGet_Object_Status(zc_Enum_Status_UnComplete()) AS Object_Status;
      ELSE
 
@@ -67,28 +64,28 @@ BEGIN
              Movement.Id
            , Movement.InvNumber
            , Movement.OperDate
-           , Object_Status.ObjectCode    AS StatusCode
-           , Object_Status.ValueData     AS StatusName
-           , MovementBoolean_Checked.ValueData           AS Checked
-           , MovementDate_OperDatePartner.ValueData      AS OperDatePartner
-           , MovementBoolean_PriceWithVAT.ValueData      AS PriceWithVAT
-           , MovementFloat_VATPercent.ValueData          AS VATPercent
-           , MovementFloat_ChangePercent.ValueData       AS ChangePercent
-           , MovementFloat_TotalCount.ValueData          AS TotalCount
-           , MovementFloat_TotalSummMVAT.ValueData       AS TotalSummMVAT
-           , MovementFloat_TotalSummPVAT.ValueData       AS TotalSummPVAT
-           , MovementFloat_TotalSumm.ValueData           AS TotalSumm
-           , Object_From.Id                    AS FromId
-           , Object_From.ValueData             AS FromName
-           , Object_To.Id                      AS ToId
-           , Object_To.ValueData               AS ToName
-           , Object_PaidKind.Id                AS PaidKindId
-           , Object_PaidKind.ValueData         AS PaidKindName
-           , Object_Contract.Id                AS ContractId
-           , Object_Contract.ValueData         AS ContractName
-           , Object_RouteSorting.Id        AS RouteSortingId
-           , Object_RouteSorting.ValueData AS RouteSortingName
-           , MovementString_InvNumberOrder.ValueData     AS InvNumberOrder
+           , Object_Status.ObjectCode    				AS StatusCode
+           , Object_Status.ValueData     				AS StatusName
+           , MovementBoolean_Checked.ValueData          AS Checked
+           , MovementDate_OperDatePartner.ValueData     AS OperDatePartner
+           , MovementBoolean_PriceWithVAT.ValueData     AS PriceWithVAT
+           , MovementFloat_VATPercent.ValueData         AS VATPercent
+           , MovementFloat_ChangePercent.ValueData      AS ChangePercent
+           , MovementFloat_TotalCount.ValueData         AS TotalCount
+           , MovementFloat_TotalSummMVAT.ValueData      AS TotalSummMVAT
+           , MovementFloat_TotalSummPVAT.ValueData      AS TotalSummPVAT
+           , MovementFloat_TotalSumm.ValueData          AS TotalSumm
+           , Object_From.Id                    			AS FromId
+           , Object_From.ValueData             			AS FromName
+           , Object_To.Id                      			AS ToId
+           , Object_To.ValueData               			AS ToName
+           , Object_PaidKind.Id                			AS PaidKindId
+           , Object_PaidKind.ValueData         			AS PaidKindName
+           , Object_Contract.ContractId        			AS ContractId
+           , Object_Contract.invnumber         			AS ContractName
+           , Object_RouteSorting.Id        				AS RouteSortingId
+           , Object_RouteSorting.ValueData 				AS RouteSortingName
+           , MovementString_InvNumberOrder.ValueData    AS InvNumberOrder
 
        FROM Movement
             LEFT JOIN Object AS Object_Status ON Object_Status.Id = Movement.StatusId
@@ -124,9 +121,11 @@ BEGIN
             LEFT JOIN MovementFloat AS MovementFloat_TotalSummMVAT
                                     ON MovementFloat_TotalSummMVAT.MovementId =  Movement.Id
                                    AND MovementFloat_TotalSummMVAT.DescId = zc_MovementFloat_TotalSummMVAT()
+
             LEFT JOIN MovementFloat AS MovementFloat_TotalSummPVAT
                                     ON MovementFloat_TotalSummPVAT.MovementId =  Movement.Id
                                    AND MovementFloat_TotalSummPVAT.DescId = zc_MovementFloat_TotalSummPVAT()
+
             LEFT JOIN MovementFloat AS MovementFloat_TotalSumm
                                     ON MovementFloat_TotalSumm.MovementId =  Movement.Id
                                    AND MovementFloat_TotalSumm.DescId = zc_MovementFloat_TotalSumm()
@@ -134,27 +133,31 @@ BEGIN
             LEFT JOIN MovementLinkObject AS MovementLinkObject_From
                                          ON MovementLinkObject_From.MovementId = Movement.Id
                                         AND MovementLinkObject_From.DescId = zc_MovementLinkObject_From()
+
             LEFT JOIN Object AS Object_From ON Object_From.Id = MovementLinkObject_From.ObjectId
 
             LEFT JOIN MovementLinkObject AS MovementLinkObject_To
                                          ON MovementLinkObject_To.MovementId = Movement.Id
                                         AND MovementLinkObject_To.DescId = zc_MovementLinkObject_To()
+
             LEFT JOIN Object AS Object_To ON Object_To.Id = MovementLinkObject_To.ObjectId
 
             LEFT JOIN MovementLinkObject AS MovementLinkObject_PaidKind
                                          ON MovementLinkObject_PaidKind.MovementId = Movement.Id
                                         AND MovementLinkObject_PaidKind.DescId = zc_MovementLinkObject_PaidKind()
+
             LEFT JOIN Object AS Object_PaidKind ON Object_PaidKind.Id = MovementLinkObject_PaidKind.ObjectId
 
             LEFT JOIN MovementLinkObject AS MovementLinkObject_Contract
                                          ON MovementLinkObject_Contract.MovementId = Movement.Id
                                         AND MovementLinkObject_Contract.DescId = zc_MovementLinkObject_Contract()
-            LEFT JOIN Object AS Object_Contract ON Object_Contract.Id = MovementLinkObject_Contract.ObjectId
 
+            LEFT JOIN object_contract_invnumber_view AS Object_Contract ON Object_Contract.contractid = MovementLinkObject_Contract.ObjectId
 
             LEFT JOIN MovementLinkObject AS MovementLinkObject_RouteSorting
                                          ON MovementLinkObject_RouteSorting.MovementId = Movement.Id
                                         AND MovementLinkObject_RouteSorting.DescId = zc_MovementLinkObject_RouteSorting()
+
             LEFT JOIN Object AS Object_RouteSorting ON Object_RouteSorting.Id = MovementLinkObject_RouteSorting.ObjectId
 
        WHERE Movement.Id =  inMovementId
@@ -170,6 +173,7 @@ ALTER FUNCTION gpGet_Movement_Sale (Integer, TDateTime, TVarChar) OWNER TO postg
 /*
  »—“Œ–»ﬂ –¿«–¿¡Œ“ »: ƒ¿“¿, ¿¬“Œ–
                ‘ÂÎÓÌ˛Í ».¬.    ÛıÚËÌ ».¬.    ÎËÏÂÌÚ¸Â‚  .».   Ã‡Ì¸ÍÓ ƒ.
+ 29.01.14                                                        * fix ContractName if empty
  16.01.14                                                        * MovementBoolean_Checked
  29.07.13         * add zc_MovementLinkObject_Personal
  14.07.13         *
