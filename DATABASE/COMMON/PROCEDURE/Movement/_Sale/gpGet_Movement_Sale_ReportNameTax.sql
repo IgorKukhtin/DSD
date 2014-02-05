ca70edda58e502f5a8eb49c467bba66060a41a90
DmytroManko <dnepro.ua@gmail.com>
2014-02-05 23:06:42 +0200

    +++

---
+++ DATABASE/COMMON/PROCEDURE/Movement/_Sale/gpGet_Movement_Sale_ReportNameTax.sql
@@ -0,0 +1,52 @@
+-- Function: gpGet_Movement_Sale_ReportNameTax()
+
+DROP FUNCTION IF EXISTS gpGet_Movement_Sale_ReportNameTax (Integer, TVarChar);
+
+
+CREATE OR REPLACE FUNCTION gpGet_Movement_Sale_ReportNameTax (
+    IN inMovementId         Integer  , -- ключ Документа
+    IN inSession            TVarChar   -- сессия пользователя
+)
+RETURNS TVarChar
+AS
+$BODY$
+   DECLARE vbOperDate TDateTime;
+   DECLARE vbPartnerId Integer;
+   DECLARE vbPrintFormName TVarChar;
+BEGIN
+
+     -- проверка прав пользователя на вызов процедуры
+     -- PERFORM lpCheckRight (inSession, zc_Enum_Process_Get_Movement_Sale());
+
+       SELECT
+            COALESCE (PrintForms_View.PrintFormName, 'PrintMovement_SaleTax')
+       INTO vbPrintFormName
+       FROM Movement
+
+       LEFT JOIN PrintForms_View
+              ON Movement.OperDate BETWEEN PrintForms_View.StartDate AND PrintForms_View.EndDate
+             AND PrintForms_View.ReportType = 'SaleTax'
+
+       WHERE Movement.Id =  inMovementId
+         AND Movement.DescId = zc_Movement_Sale();
+
+
+     RETURN (vbPrintFormName);
+
+
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+ALTER FUNCTION gpGet_Movement_Sale_ReportNameTax (Integer, TVarChar) OWNER TO postgres;
+
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.
+ 05.02.14                                                        *
+*/
+
+-- тест
+--SELECT gpGet_Movement_Sale_ReportNameTax FROM gpGet_Movement_Sale_ReportNameTax(inMovementId := 40874,  inSession := '5'); -- все
+--SELECT * FROM gpGet_Movement_Sale_ReportNameTax(inMovementId := 40869,  inSession := '5'); -- Метро
