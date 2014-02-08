09de3b3a6f9ba3d07884bd5e9e6687e495292cf2
innaf <innaf@ua.fm>
2014-02-08 20:56:38 +0200

    простите

---
+++ Forms/Report/Report_GoodsMI_Income.pas
@@ -0,0 +1,49 @@
+unit Report_GoodsMI_Income;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, AncestorReport, cxGraphics, cxControls,
+  cxLookAndFeels, cxLookAndFeelPainters, dxSkinsCore, dxSkinsDefaultPainters,
+  dxSkinscxPCPainter, cxPCdxBarPopupMenu, cxStyles, cxCustomData, cxFilter,
+  cxData, cxDataStorage, cxEdit, Data.DB, cxDBData, cxContainer, Vcl.ComCtrls,
+  dxCore, cxDateUtils, dxSkinsdxBarPainter, dsdAddOn, ChoicePeriod,
+  dxBarExtItems, dxBar, cxClasses, dsdDB, Datasnap.DBClient, dsdAction,
+  Vcl.ActnList, cxPropertiesStore, cxLabel, cxTextEdit, cxMaskEdit,
+  cxDropDownEdit, cxCalendar, Vcl.ExtCtrls, cxGridLevel, cxGridCustomView,
+  cxGridCustomTableView, cxGridTableView, cxGridDBTableView, cxGrid, cxPC,
+  dsdGuides, cxButtonEdit, cxCurrencyEdit, Vcl.Menus;
+
+type
+  TReport_GoodsMI_IncomeForm = class(TAncestorReportForm)
+    clTradeMarkName: TcxGridDBColumn;
+    clGoodsGroupName: TcxGridDBColumn;
+    clGoodsCode: TcxGridDBColumn;
+    clGoodsName: TcxGridDBColumn;
+    clSumm: TcxGridDBColumn;
+    clAmount_Weight: TcxGridDBColumn;
+    cxLabel4: TcxLabel;
+    edGoodsGroup: TcxButtonEdit;
+    GoodsGroupGuides: TdsdGuides;
+    FormParams: TdsdFormParams;
+    clAmount_Sh: TcxGridDBColumn;
+    edInDescName: TcxTextEdit;
+    clAmountPartner_Weight: TcxGridDBColumn;
+    clAmountPartner_Sh: TcxGridDBColumn;
+    clGoodsKindName: TcxGridDBColumn;
+    clPaidKindName: TcxGridDBColumn;
+    clFuelKindName: TcxGridDBColumn;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+initialization
+  RegisterClass(TReport_GoodsMI_IncomeForm);
+
+end.
