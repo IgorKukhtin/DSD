4ee898cbf66c1742a2b577ea0da564c0a86dfeca
DmytroManko <dnepro.ua@gmail.com>
2014-02-10 01:45:12 +0200

    +++

---
+++ DATABASE/COMMON/PROCEDURE/MovementItem/_Tax/gpInsertUpdate_MovementItem_Tax.sql
@@ -0,0 +1,54 @@
+-- Function: gpInsertUpdate_MovementItem_Tax()
+
+DROP FUNCTION IF EXISTS gpInsertUpdate_MovementItem_Tax (integer, integer, integer, tfloat, tfloat, tfloat, tfloat, integer, TVarChar);
+
+
+CREATE OR REPLACE FUNCTION gpInsertUpdate_MovementItem_Tax(
+ INOUT ioId                  Integer   , -- Ключ объекта <Элемент документа>
+    IN inMovementId          Integer   , -- Ключ объекта <Документ>
+    IN inGoodsId             Integer   , -- Товары
+    IN inAmount              TFloat    , -- Количество
+    IN inPrice               TFloat    , -- Цена
+ INOUT ioCountForPrice       TFloat    , -- Цена за количество
+   OUT outAmountSumm         TFloat    , -- Сумма расчетная
+    IN inGoodsKindId         Integer   , -- Виды товаров
+    IN inSession             TVarChar    -- сессия пользователя
+)
+RETURNS RECORD
+AS
+$BODY$
+   DECLARE vbUserId Integer;
+BEGIN
+     -- проверка прав пользователя на вызов процедуры
+--     vbUserId := lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_MI_Tax());
+
+     -- проверка - удаленный элемент документа не может корректироваться
+     IF ioId <> 0 AND EXISTS (SELECT Id FROM MovementItem WHERE Id = ioId AND isErased = TRUE)
+     THEN
+         RAISE EXCEPTION 'Ошибка.Элемент не может корректироваться т.к. он <Удален>.';
+     END IF;
+
+     SELECT tmp.ioId, tmp.ioCountForPrice, tmp.outAmountSumm
+            INTO ioId, ioCountForPrice, outAmountSumm
+     FROM lpInsertUpdate_MovementItem_Tax (ioId                 := ioId
+                                          , inMovementId         := inMovementId
+                                          , inGoodsId            := inGoodsId
+                                          , inAmount             := inAmount
+                                          , inPrice              := inPrice
+                                          , ioCountForPrice      := ioCountForPrice
+                                          , inGoodsKindId        := inGoodsKindId
+                                          , inUserId             := vbUserId
+                                           ) AS tmp;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.
+ 10.02.14                                                        *
+*/
+
+-- тест
+-- SELECT * FROM gpInsertUpdate_MovementItem_Tax (ioId:= 0, inMovementId:= 10, inGoodsId:= 1, inAmount:= 0, inPrice:= 1, ioCountForPrice:= 1, inGoodsKindId:= 0, inSession:= '2')
