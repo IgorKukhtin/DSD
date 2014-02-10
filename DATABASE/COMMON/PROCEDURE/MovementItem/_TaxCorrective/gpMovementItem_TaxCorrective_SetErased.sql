7da2e4ae104f7e7e79cc50ad6d5ce92eb8deb60f
DmytroManko <dnepro.ua@gmail.com>
2014-02-10 08:52:31 +0200

    +++

---
+++ DATABASE/COMMON/PROCEDURE/MovementItem/_TaxCorrective/gpMovementItem_TaxCorrective_SetErased.sql
@@ -0,0 +1,54 @@
+-- Function: gpMovementItem_TaxCorrective_SetErased (Integer, TVarChar)
+
+DROP FUNCTION IF EXISTS gpMovementItem_TaxCorrective_SetErased (Integer, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpMovementItem_TaxCorrective_SetErased(
+    IN inMovementItemId      Integer              , -- ключ объекта <Элемент документа>
+   OUT outIsErased           Boolean              , -- новое значение
+    IN inSession             TVarChar               -- текущий пользователь
+)
+  RETURNS Boolean
+AS
+$BODY$
+   DECLARE vbMovementId Integer;
+   DECLARE vbStatusId Integer;
+   DECLARE vbUserId Integer;
+BEGIN
+  vbUserId:= lpCheckRight(inSession, zc_Enum_Process_SetErased_MI_TaxCorrective());
+
+  -- устанавливаем новое значение
+  outIsErased := TRUE;
+
+  -- Обязательно меняем
+  UPDATE MovementItem SET isErased = TRUE WHERE Id = inMovementItemId
+         RETURNING MovementId INTO vbMovementId;
+
+  -- проверка - связанные документы Изменять нельзя
+  -- PERFORM lfCheck_Movement_Parent (inMovementId:= vbMovementId, inComment:= 'изменение');
+
+  -- определяем <Статус>
+  vbStatusId := (SELECT StatusId FROM Movement WHERE Id = vbMovementId);
+  -- проверка - проведенные/удаленные документы Изменять нельзя
+  IF vbStatusId <> zc_Enum_Status_UnComplete()
+  THEN
+      RAISE EXCEPTION 'Ошибка.Изменение документа в статусе <%> не возможно.', lfGet_Object_ValueData (vbStatusId);
+  END IF;
+
+  -- пересчитали Итоговые суммы по накладной
+  PERFORM lpInsertUpdate_MovementFloat_TotalSumm (vbMovementId);
+
+  -- outIsErased := FALSE;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+ALTER FUNCTION gpMovementItem_TaxCorrective_SetErased (Integer, TVarChar) OWNER TO postgres;
+
+/*-------------------------------------------------------------------------------
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.
+10.02.14                                                          *
+*/
+
+-- тест
+-- SELECT * FROM gpMovementItem_TaxCorrective_SetErased (inMovementId:= 55, outIsErased:=false , inSession:= '2')
---
+++ DATABASE/COMMON/PROCEDURE/MovementItem/_TaxCorrective/gpSelect_MovementItem_TaxCorrective.sql
@@ -0,0 +1,173 @@
+-- Function: gpSelect_MovementItem_TaxCorrective()
+
+
+ DROP FUNCTION IF EXISTS gpSelect_MovementItem_TaxCorrective (Integer, Boolean, Boolean, TVarChar);
+
+
+CREATE OR REPLACE FUNCTION gpSelect_MovementItem_TaxCorrective(
+    IN inMovementId  Integer      , -- ключ Документа
+    IN inShowAll     Boolean      , --
+    IN inisErased    Boolean      , --
+    IN inSession     TVarChar       -- сессия пользователя
+)
+RETURNS TABLE (Id Integer, GoodsId Integer, GoodsCode Integer, GoodsName TVarChar, Amount TFloat
+             , Price TFloat, CountForPrice TFloat
+             , GoodsKindId Integer, GoodsKindName  TVarChar
+             , AmountSumm TFloat, isErased Boolean
+              )
+AS
+$BODY$
+  DECLARE vbOperDate TDateTime;
+BEGIN
+
+     -- проверка прав пользователя на вызов процедуры
+     -- PERFORM lpCheckRight (inSession, zc_Enum_Process_Select_MovementItem_TaxCorrective());
+
+     -- inShowAll:= TRUE;
+
+     IF inShowAll THEN
+
+     vbOperDate := (SELECT Movement.OperDate FROM Movement WHERE Movement.Id = inMovementId);
+
+     RETURN QUERY
+       SELECT
+             0                                      AS Id
+           , tmpGoods.GoodsId                       AS GoodsId
+           , tmpGoods.GoodsCode                     AS GoodsCode
+           , tmpGoods.GoodsName                     AS GoodsName
+           , CAST (NULL AS TFloat)                  AS Amount
+--           , CAST (lfObjectHistory_PriceListItem.ValuePrice AS TFloat) AS Price
+           , CAST (NULL AS TFloat)                  AS Price
+           , CAST (1 AS TFloat)                     AS CountForPrice
+           , Object_GoodsKind.Id                    AS GoodsKindId
+           , Object_GoodsKind.ValueData             AS GoodsKindName
+           , CAST (NULL AS TFloat)                  AS AmountSumm
+           , FALSE                                  AS isErased
+
+       FROM (SELECT Object_Goods.Id                                                   AS GoodsId
+                  , Object_Goods.ObjectCode                                           AS GoodsCode
+                  , Object_Goods.ValueData                                            AS GoodsName
+                  , COALESCE (ObjectLink_GoodsByGoodsKind_GoodsKind.ChildObjectId, 0) AS GoodsKindId
+             FROM Object AS Object_Goods
+                  LEFT JOIN ObjectLink AS ObjectLink_GoodsByGoodsKind_Goods
+                                       ON ObjectLink_GoodsByGoodsKind_Goods.ChildObjectId = Object_Goods.Id
+                                      AND ObjectLink_GoodsByGoodsKind_Goods.DescId = zc_ObjectLink_GoodsByGoodsKind_Goods()
+                  LEFT JOIN ObjectLink AS ObjectLink_GoodsByGoodsKind_GoodsKind
+                                       ON ObjectLink_GoodsByGoodsKind_GoodsKind.ObjectId = ObjectLink_GoodsByGoodsKind_Goods.ObjectId
+                                      AND ObjectLink_GoodsByGoodsKind_GoodsKind.DescId = zc_ObjectLink_GoodsByGoodsKind_GoodsKind()
+             WHERE Object_Goods.DescId = zc_Object_Goods()
+            ) AS tmpGoods
+            LEFT JOIN (SELECT MovementItem.ObjectId                         AS GoodsId
+                            , COALESCE (MILinkObject_GoodsKind.ObjectId, 0) AS GoodsKindId
+                       FROM (SELECT FALSE AS isErased UNION ALL SELECT inIsErased AS isErased WHERE inIsErased = TRUE) AS tmpIsErased
+                            JOIN MovementItem ON MovementItem.MovementId = inMovementId
+                                             AND MovementItem.DescId     = zc_MI_Master()
+                                             AND MovementItem.isErased   = tmpIsErased.isErased
+                            LEFT JOIN MovementItemLinkObject AS MILinkObject_GoodsKind
+                                                             ON MILinkObject_GoodsKind.MovementItemId = MovementItem.Id
+                                                            AND MILinkObject_GoodsKind.DescId = zc_MILinkObject_GoodsKind()
+
+                      ) AS tmpMI ON tmpMI.GoodsId     = tmpGoods.GoodsId
+                                AND tmpMI.GoodsKindId = tmpGoods.GoodsKindId
+            LEFT JOIN Object AS Object_GoodsKind ON Object_GoodsKind.Id = tmpGoods.GoodsKindId
+--            LEFT JOIN lfSelect_ObjectHistory_PriceListItem (inPriceListId:= inPriceListId, inOperDate:= vbOperDate)
+--                   AS lfObjectHistory_PriceListItem ON lfObjectHistory_PriceListItem.GoodsId = tmpGoods.GoodsId
+
+       WHERE tmpMI.GoodsId IS NULL
+
+      UNION ALL
+       SELECT
+             MovementItem.Id                        AS Id
+           , Object_Goods.Id                        AS GoodsId
+           , Object_Goods.ObjectCode                AS GoodsCode
+           , Object_Goods.ValueData                 AS GoodsName
+           , MovementItem.Amount                    AS Amount
+           , MIFloat_Price.ValueData                AS Price
+           , MIFloat_CountForPrice.ValueData        AS CountForPrice
+           , Object_GoodsKind.Id                    AS GoodsKindId
+           , Object_GoodsKind.ValueData             AS GoodsKindName
+
+           , CAST (CASE WHEN MIFloat_CountForPrice.ValueData > 0
+                           THEN CAST ( (COALESCE (MovementItem.Amount, 0)) * MIFloat_Price.ValueData / MIFloat_CountForPrice.ValueData AS NUMERIC (16, 2))
+                           ELSE CAST ( (COALESCE (MovementItem.Amount, 0)) * MIFloat_Price.ValueData AS NUMERIC (16, 2))
+                   END AS TFloat)                   AS AmountSumm
+           , MovementItem.isErased                  AS isErased
+
+       FROM (SELECT FALSE AS isErased UNION ALL SELECT inIsErased AS isErased WHERE inIsErased = TRUE) AS tmpIsErased
+            JOIN MovementItem ON MovementItem.MovementId = inMovementId
+                             AND MovementItem.DescId     = zc_MI_Master()
+                             AND MovementItem.isErased   = tmpIsErased.isErased
+
+            LEFT JOIN Object AS Object_Goods ON Object_Goods.Id = MovementItem.ObjectId
+
+            LEFT JOIN MovementItemFloat AS MIFloat_Price
+                                        ON MIFloat_Price.MovementItemId = MovementItem.Id
+                                       AND MIFloat_Price.DescId = zc_MIFloat_Price()
+            LEFT JOIN MovementItemFloat AS MIFloat_CountForPrice
+                                        ON MIFloat_CountForPrice.MovementItemId = MovementItem.Id
+                                       AND MIFloat_CountForPrice.DescId = zc_MIFloat_CountForPrice()
+
+            LEFT JOIN MovementItemLinkObject AS MILinkObject_GoodsKind
+                                             ON MILinkObject_GoodsKind.MovementItemId = MovementItem.Id
+                                            AND MILinkObject_GoodsKind.DescId = zc_MILinkObject_GoodsKind()
+            LEFT JOIN Object AS Object_GoodsKind ON Object_GoodsKind.Id = MILinkObject_GoodsKind.ObjectId
+
+            ;
+     ELSE
+
+     RETURN QUERY
+       SELECT
+             MovementItem.Id
+           , Object_Goods.Id                        AS GoodsId
+           , Object_Goods.ObjectCode                AS GoodsCode
+           , Object_Goods.ValueData                 AS GoodsName
+           , MovementItem.Amount                    AS Amount
+           , MIFloat_Price.ValueData                AS Price
+           , MIFloat_CountForPrice.ValueData        AS CountForPrice
+           , Object_GoodsKind.Id                    AS GoodsKindId
+           , Object_GoodsKind.ValueData             AS GoodsKindName
+           , CAST (CASE WHEN MIFloat_CountForPrice.ValueData > 0
+                           THEN CAST ( (COALESCE (MovementItem.Amount, 0)) * MIFloat_Price.ValueData / MIFloat_CountForPrice.ValueData AS NUMERIC (16, 2))
+                           ELSE CAST ( (COALESCE (MovementItem.Amount, 0)) * MIFloat_Price.ValueData AS NUMERIC (16, 2))
+                   END AS TFloat)                   AS AmountSumm
+           , MovementItem.isErased                  AS isErased
+
+       FROM (SELECT FALSE AS isErased UNION ALL SELECT inIsErased AS isErased WHERE inIsErased = TRUE) AS tmpIsErased
+            JOIN MovementItem ON MovementItem.MovementId = inMovementId
+                             AND MovementItem.DescId     = zc_MI_Master()
+                             AND MovementItem.isErased   = tmpIsErased.isErased
+            LEFT JOIN Object AS Object_Goods ON Object_Goods.Id = MovementItem.ObjectId
+
+            LEFT JOIN MovementItemFloat AS MIFloat_Price
+                                        ON MIFloat_Price.MovementItemId = MovementItem.Id
+                                       AND MIFloat_Price.DescId = zc_MIFloat_Price()
+            LEFT JOIN MovementItemFloat AS MIFloat_CountForPrice
+                                        ON MIFloat_CountForPrice.MovementItemId = MovementItem.Id
+                                       AND MIFloat_CountForPrice.DescId = zc_MIFloat_CountForPrice()
+
+
+            LEFT JOIN MovementItemLinkObject AS MILinkObject_GoodsKind
+                                             ON MILinkObject_GoodsKind.MovementItemId = MovementItem.Id
+                                            AND MILinkObject_GoodsKind.DescId = zc_MILinkObject_GoodsKind()
+            LEFT JOIN Object AS Object_GoodsKind ON Object_GoodsKind.Id = MILinkObject_GoodsKind.ObjectId
+
+            ;
+
+     END IF;
+
+END;
+$BODY$
+LANGUAGE PLPGSQL VOLATILE;
+ALTER FUNCTION gpSelect_MovementItem_TaxCorrective (Integer, Boolean, Boolean, TVarChar) OWNER TO postgres;
+
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.
+ 10.02.14                                                        *
+*/
+
+-- тест
+
+--SELECT * FROM gpSelect_MovementItem_TaxCorrective (inMovementId:= 4229, inPriceListId:=0, inShowAll:= True, inisErased:= True, inSession:= '2')
+--SELECT * FROM gpSelect_MovementItem_TaxCorrective (inMovementId:= 4229, inShowAll:= True, inisErased:= True, inSession:= '2')
