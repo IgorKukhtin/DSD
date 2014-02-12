924a4a8b3c3d992e37e78d133bc14c405dee0ec9
innaf <innaf@ua.fm>
2014-02-12 17:40:10 +0200

    6

--- Forms/Guides/Country.pas
+++ Forms/Guides/Country.pas
@@ -4,71 +4,28 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, cxGraphics, cxControls, cxLookAndFeels,
-  cxLookAndFeelPainters, cxStyles, cxCustomData, cxFilter, cxData,
-  cxDataStorage, cxEdit, Data.DB, cxDBData, cxGridLevel, cxClasses,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, AncestorGuides, cxGraphics, cxControls,
+  cxLookAndFeels, cxLookAndFeelPainters, dxSkinsCore, dxSkinsDefaultPainters,
+  dxSkinscxPCPainter, cxPCdxBarPopupMenu, cxStyles, cxCustomData, cxFilter,
+  cxData, cxDataStorage, cxEdit, Data.DB, cxDBData, dxSkinsdxBarPainter,
+  Vcl.Menus, dsdAddOn, dxBarExtItems, dxBar, cxClasses, dsdDB,
+  Datasnap.DBClient, dsdAction, Vcl.ActnList, cxPropertiesStore, cxGridLevel,
   cxGridCustomView, cxGridCustomTableView, cxGridTableView, cxGridDBTableView,
-  cxGrid, Datasnap.DBClient, cxPropertiesStore, dxBar,
-  Vcl.ActnList, DataModul, ParentForm, dsdDB, dsdAction, dsdAddOn, dxBarExtItems,
-  cxGridBandedTableView, cxGridDBBandedTableView, cxCheckBox, dxSkinsCore,
-  dxSkinBlack, dxSkinBlue, dxSkinBlueprint, dxSkinCaramel, dxSkinCoffee,
-  dxSkinDarkRoom, dxSkinDarkSide, dxSkinDevExpressDarkStyle,
-  dxSkinDevExpressStyle, dxSkinFoggy, dxSkinGlassOceans, dxSkinHighContrast,
-  dxSkiniMaginary, dxSkinLilian, dxSkinLiquidSky, dxSkinLondonLiquidSky,
-  dxSkinMcSkin, dxSkinMoneyTwins, dxSkinOffice2007Black, dxSkinOffice2007Blue,
-  dxSkinOffice2007Green, dxSkinOffice2007Pink, dxSkinOffice2007Silver,
-  dxSkinOffice2010Black, dxSkinOffice2010Blue, dxSkinOffice2010Silver,
-  dxSkinPumpkin, dxSkinSeven, dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus,
-  dxSkinSilver, dxSkinSpringTime, dxSkinStardust, dxSkinSummer2008,
-  dxSkinTheAsphaltWorld, dxSkinsDefaultPainters, dxSkinValentine, dxSkinVS2010,
-  dxSkinWhiteprint, dxSkinXmas2008Blue, dxSkinscxPCPainter, dxSkinsdxBarPainter;
+  cxGrid, cxPC;
 
 type
-  TCountryForm = class(TParentForm)
-    cxGridLevel: TcxGridLevel;
-    cxGrid: TcxGrid;
-    DataSource: TDataSource;
-    ClientDataSet: TClientDataSet;
-    cxPropertiesStore: TcxPropertiesStore;
-    dxBarManager: TdxBarManager;
-    dxBarManagerBar1: TdxBar;
-    ActionList: TActionList;
-    bbRefresh: TdxBarButton;
-    actRefresh: TdsdDataSetRefresh;
-    bbInsert: TdxBarButton;
-    dsdStoredProc: TdsdStoredProc;
-    bbEdit: TdxBarButton;
-    bbSetErased: TdxBarButton;
-    bbSetUnErased: TdxBarButton;
-    dsdGridToExcel: TdsdGridToExcel;
-    bbToExcel: TdxBarButton;
-    dxBarStatic: TdxBarStatic;
-    spErasedUnErased: TdsdStoredProc;
-    bbChoice: TdxBarButton;
-    cxGridDBTableView: TcxGridDBTableView;
-    clCode: TcxGridDBColumn;
-    clName: TcxGridDBColumn;
-    clErased: TcxGridDBColumn;
-    dsdUserSettingsStorageAddOn: TdsdUserSettingsStorageAddOn;
-    dsdChoiceGuides: TdsdChoiceGuides;
-    dsdDBViewAddOn: TdsdDBViewAddOn;
-    spInsertUpdateObject: TdsdStoredProc;
-    InsertRecord: TInsertRecord;
-    CountryChoiceForm: TOpenChoiceForm;
-    dsdSetErased: TdsdUpdateErased;
-    dsdUpdateErased1: TdsdUpdateErased;
-    spGet: TdsdStoredProc;
+  TCountryForm = class(TAncestorGuidesForm)
   private
     { Private declarations }
   public
     { Public declarations }
   end;
 
+var
+  CountryForm: TCountryForm;
+
 implementation
 
 {$R *.dfm}
-
-initialization
-  RegisterClass(TCountryForm);
 
 end.
