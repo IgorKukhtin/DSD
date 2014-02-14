5cd2a8b7f754ff895badf07a2a164c05e4a2e11d
innaf <innaf@ua.fm>
2014-02-14 18:39:14 +0200

    2

---
+++ Forms/Report/Report_CheckTax.pas
@@ -0,0 +1,51 @@
+unit Report_CheckTax;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, AncestorReport, cxGraphics, cxControls,
+  cxLookAndFeels, cxLookAndFeelPainters, dxSkinsCore, dxSkinsDefaultPainters,
+  dxSkinscxPCPainter, cxPCdxBarPopupMenu, cxStyles, cxCustomData, cxFilter,
+  cxData, cxDataStorage, cxEdit, Data.DB, cxDBData, cxContainer, Vcl.ComCtrls,
+  dxCore, cxDateUtils, dxSkinsdxBarPainter, dsdAddOn, ChoicePeriod,
+  dxBarExtItems, dxBar, cxClasses, dsdDB, Datasnap.DBClient, dsdAction,
+  Vcl.ActnList, cxPropertiesStore, cxLabel, cxTextEdit, cxMaskEdit,
+  cxDropDownEdit, cxCalendar, Vcl.ExtCtrls, cxGridLevel, cxGridCustomView,
+  cxGridCustomTableView, cxGridTableView, cxGridDBTableView, cxGrid, cxPC,
+  dsdGuides, cxButtonEdit, cxCurrencyEdit, Vcl.Menus;
+
+type
+  TReport_CheckTaxForm = class(TAncestorReportForm)
+    clInvNumber_Sale: TcxGridDBColumn;
+    clInvNumber_Tax: TcxGridDBColumn;
+    clGoodsCode: TcxGridDBColumn;
+    clGoodsName: TcxGridDBColumn;
+    clAmount_Sale: TcxGridDBColumn;
+    clAmountSumm_Sale: TcxGridDBColumn;
+    clAmount_Tax: TcxGridDBColumn;
+    clAmountSumm_Tax: TcxGridDBColumn;
+    clFromCode: TcxGridDBColumn;
+    clGoodsKindName: TcxGridDBColumn;
+    clOperDate_Sale: TcxGridDBColumn;
+    clOperDate_Tax: TcxGridDBColumn;
+    clFromName: TcxGridDBColumn;
+    clToCode: TcxGridDBColumn;
+    clToName: TcxGridDBColumn;
+    clPaidKindName: TcxGridDBColumn;
+    clPrice_Sale: TcxGridDBColumn;
+    clPrice_Tax: TcxGridDBColumn;
+    clDifference: TcxGridDBColumn;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+initialization
+  RegisterClass(TReport_CheckTaxForm);
+
+end.
