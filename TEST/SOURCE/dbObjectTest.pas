039f6a3aeee54d1a3df3b3f45642b75cf6f84e90
innaf <innaf@ua.fm>
2014-02-14 12:57:28 +0200

    2

--- TEST/SOURCE/dbObjectTest.pas
+++ TEST/SOURCE/dbObjectTest.pas
@@ -2135,30 +2135,30 @@ begin
   try
     // теперь делаем ссылку на себя и проверяем ошибку
     try
-      ObjectTest.InsertUpdateAssetGroup(Id, 1, 'Группа 1', Id);
+      ObjectTest.InsertUpdateAssetGroup(Id, -1, 'Тест Группа 1', Id);
       Check(false, 'Нет сообщение об ошибке');
     except
 
     end;
     // добавляем еще группу 2
     // делаем у группы 2 ссылку на группу 1
-    Id2 := ObjectTest.InsertUpdateAssetGroup(0, 2, 'Группа 2', Id);
+    Id2 := ObjectTest.InsertUpdateAssetGroup(0, -2, 'Тест Группа 2', Id);
     try
       // теперь ставим ссылку у группы 1 на группу 2 и проверяем ошибку
       try
-        ObjectTest.InsertUpdateAssetGroup(Id, 1, 'Группа 1', Id2);
+        ObjectTest.InsertUpdateAssetGroup(Id, -1, 'Тест Группа 1', Id2);
         Check(false, 'Нет сообщение об ошибке');
       except
 
       end;
       // добавляем еще группу 3
       // делаем у группы 3 ссылку на группу 2
-      Id3 := ObjectTest.InsertUpdateAssetGroup(0, 3, 'Группа 3', Id2);
+      Id3 := ObjectTest.InsertUpdateAssetGroup(0, -3, 'Тест Группа 3', Id2);
       try
         // группа 2 уже ссылка на группу 1
         // делаем у группы 1 ссылку на группу 3 и проверяем ошибку
         try
-          ObjectTest.InsertUpdateAssetGroup(Id, 1, 'Группа 1', Id3);
+          ObjectTest.InsertUpdateAssetGroup(Id, -1, 'Тест Группа 1', Id3);
           Check(false, 'Нет сообщение об ошибке');
         except
 
@@ -2191,6 +2191,8 @@ var
   MakerId: Integer;
 begin
   AssetGroupId := TAssetGroupTest.Create.GetDefault;
+  JuridicalId:=0;
+  MakerId:=0;
   result := InsertUpdateAsset(0, -1, 'Основные средства', date, 'InvNumber', 'FullName', 'SerialNumber'
                             , 'PassportNumber', 'Comment', AssetGroupId,JuridicalId,MakerId);
 end;
