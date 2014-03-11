9e72473b4e133db7855e5d1a0355820f706b57d6
DmytroManko <dnepro.ua@gmail.com>
2014-03-11 21:24:08 +0200

    +++

---
+++ DPR/Scale.dpr
@@ -0,0 +1,49 @@
+program Scale;
+
+uses
+  Vcl.Forms,
+  Controls,
+  Classes,
+  SysUtils,
+  Main in '..\Scale\Main.pas' {MainForm},
+  LoginForm in '..\SOURCE\LoginForm.pas' {LoginForm},
+  Storage in '..\SOURCE\Storage.pas',
+  UtilConst in '..\SOURCE\UtilConst.pas',
+  UtilConvert in '..\SOURCE\UtilConvert.pas',
+  MessagesUnit in '..\SOURCE\MessagesUnit.pas' {MessagesForm},
+  SimpleGauge in '..\SOURCE\SimpleGauge.pas' {SimpleGaugeForm},
+  Log in '..\SOURCE\Log.pas',
+  Authentication in '..\SOURCE\Authentication.pas',
+  CommonData in '..\SOURCE\CommonData.pas',
+  dsdDB in '..\SOURCE\COMPONENT\dsdDB.pas',
+  dsdGuides in '..\SOURCE\COMPONENT\dsdGuides.pas',
+  ParentForm in '..\SOURCE\ParentForm.pas' {ParentForm},
+  dsdAddOn in '..\SOURCE\COMPONENT\dsdAddOn.pas',
+  dsdAction in '..\SOURCE\COMPONENT\dsdAction.pas',
+  FormStorage in '..\SOURCE\FormStorage.pas',
+  ChoicePeriod in '..\SOURCE\COMPONENT\ChoicePeriod.pas' {PeriodChoiceForm},
+  ClientBankLoad in '..\SOURCE\COMPONENT\ClientBankLoad.pas',
+  ExternalLoad in '..\SOURCE\COMPONENT\ExternalLoad.pas',
+  ExternalData in '..\SOURCE\COMPONENT\ExternalData.pas',
+  MemDBFTable in '..\SOURCE\MemDBFTable.pas',
+  Document in '..\SOURCE\COMPONENT\Document.pas',
+  UnilWin in '..\SOURCE\UnilWin.pas',
+  Defaults in '..\SOURCE\COMPONENT\Defaults.pas',
+  ExternalSave in '..\SOURCE\COMPONENT\ExternalSave.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+//  Application.MainFormOnTaskbar := True;
+  // Процесс аутентификации
+  with TLoginForm.Create(Application) do
+    //Если все хорошо создаем главную форму Application.CreateForm();
+    if ShowModal = mrOk then begin
+//       TUpdater.AutomaticUpdateProgram;
+    Application.CreateForm(TMainForm, MainForm);
+  end;
+
+//  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
---
+++ Scale/Main.pas
@@ -0,0 +1,235 @@
+unit Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Menus, Data.DB,
+  Bde.DBTables, Vcl.Mask, Vcl.StdCtrls, Vcl.Buttons, Vcl.Grids,
+  Vcl.DBGrids, cxGraphics, cxControls, cxLookAndFeels,
+  cxLookAndFeelPainters, cxContainer, cxEdit, Vcl.ComCtrls, dxCore, cxDateUtils,
+  dxSkinsCore, dxSkinBlack, dxSkinBlue, dxSkinBlueprint, dxSkinCaramel,
+  dxSkinCoffee, dxSkinDarkRoom, dxSkinDarkSide, dxSkinDevExpressDarkStyle,
+  dxSkinDevExpressStyle, dxSkinFoggy, dxSkinGlassOceans, dxSkinHighContrast,
+  dxSkiniMaginary, dxSkinLilian, dxSkinLiquidSky, dxSkinLondonLiquidSky,
+  dxSkinMcSkin, dxSkinMoneyTwins, dxSkinOffice2007Black, dxSkinOffice2007Blue,
+  dxSkinOffice2007Green, dxSkinOffice2007Pink, dxSkinOffice2007Silver,
+  dxSkinOffice2010Black, dxSkinOffice2010Blue, dxSkinOffice2010Silver,
+  dxSkinPumpkin, dxSkinSeven, dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus,
+  dxSkinSilver, dxSkinSpringTime, dxSkinStardust, dxSkinSummer2008,
+  dxSkinTheAsphaltWorld, dxSkinsDefaultPainters, dxSkinValentine, dxSkinVS2010,
+  dxSkinWhiteprint, dxSkinXmas2008Blue, cxTextEdit, cxMaskEdit, cxDropDownEdit,
+  cxCalendar, dsdDB;
+
+type
+  TMainForm = class(TForm)
+    GridPanel: TPanel;
+    DBGrid: TDBGrid;
+    ButtonPanel: TPanel;
+    ButtonSaveAllItem: TSpeedButton;
+    ButtonDeleteItem: TSpeedButton;
+    ButtonExit: TSpeedButton;
+    ButtonCancelItem: TSpeedButton;
+    ButtonRefresh: TSpeedButton;
+    ButtonRefreshZakaz: TSpeedButton;
+    ButtonNewGetParams: TSpeedButton;
+    ButtonPrintBill_detail_byInvNumber: TSpeedButton;
+    ButtonChangePartionDate: TSpeedButton;
+    ButtonChangeNumberTare: TSpeedButton;
+    ButtonChangeNumberLevel: TSpeedButton;
+    ButtonExportToMail: TSpeedButton;
+    ButtonChangeMember: TSpeedButton;
+    ButtonExportToEDI: TSpeedButton;
+    ButtonChangePartionStr: TSpeedButton;
+    infoPanelTotalSumm: TPanel;
+    GBTotalSummGoods_Weight: TGroupBox;
+    PanelTotalSummGoods_Weight: TPanel;
+    TotalSummTare_Weight: TGroupBox;
+    PanelTotalSummTare_Weight: TPanel;
+    GBTotalSummGoods_Weight_Discount: TGroupBox;
+    PanelTotalSummGoods_Weight_Discount: TPanel;
+    gbTotalSumm: TGroupBox;
+    PanelTotalSumm: TPanel;
+    PanelZakaz: TPanel;
+    GroupBox1: TGroupBox;
+    DiffZakazSalePanel: TPanel;
+    GroupBox2: TGroupBox;
+    ZakazCountPanel: TPanel;
+    GroupBox3: TGroupBox;
+    ZakazChangePanel: TPanel;
+    GroupBox4: TGroupBox;
+    calcZakazCountPanel: TPanel;
+    GroupBox5: TGroupBox;
+    SaleCountPanel: TPanel;
+    GroupBox6: TGroupBox;
+    TotalDiffZakazSalePanel: TPanel;
+    GroupBox7: TGroupBox;
+    TotalZakazCountPanel: TPanel;
+    PanelSaveItem: TPanel;
+    ButtonSaveItem: TSpeedButton;
+    CodeInfoPanel: TPanel;
+    EnterGoodsCodeScanerPanel: TPanel;
+    EnterGoodsCodeScanerLabel: TLabel;
+    EnterGoodsCodeScanerEdit: TEdit;
+    EnterWeightPanel: TPanel;
+    EnterWeightLabel: TLabel;
+    EnterWeightEdit: TEdit;
+    gbBillDate: TGroupBox;
+    EnterGoodsCode_byZakazPanel: TPanel;
+    EnterGoodsCode_byZakazLabel: TLabel;
+    EnterGoodsCode_byZakazEdit: TEdit;
+    PanelCountTare: TPanel;
+    LabelCountTare: TLabel;
+    CountTareEdit: TEdit;
+    infoPanel_Scale: TPanel;
+    ScaleLabel: TLabel;
+    Panel_Scale: TPanel;
+    EnterKindPackageCode_byZakazPanel: TPanel;
+    EnterKindPackageCode_byZakazLabel: TLabel;
+    EnterKindPackageCode_byZakazEdit: TEdit;
+    EnterKindPackageName_byZakazPanel: TPanel;
+    gbPartionDate: TGroupBox;
+    PanelCountPoddon: TPanel;
+    LabelCountPoddon: TLabel;
+    CountPoddonEdit: TEdit;
+    PanelCountVanna: TPanel;
+    LabelCountVanna: TLabel;
+    CountVannaEdit: TEdit;
+    PanelCountUpakovka: TPanel;
+    LabelCountUpakovka: TLabel;
+    CountUpakovkaEdit: TEdit;
+    PanelPartionStr_MB: TPanel;
+    PartionStr_MBLabel: TLabel;
+    Panel1: TPanel;
+    infoPanelPartionStr_MB: TPanel;
+    PartionStr_MBEdit: TEdit;
+    PanelOperCount_sh: TPanel;
+    LabelOperCount_sh: TLabel;
+    OperCount_shEdit: TEdit;
+    PanelInfoItem: TPanel;
+    PanelProduction_Goods: TPanel;
+    LabelProduction_Goods: TLabel;
+    GBProduction_GoodsCode: TGroupBox;
+    PanelProduction_GoodsCode: TPanel;
+    EditProduction_GoodsCode: TEdit;
+    GBProduction_Goods_Weight: TGroupBox;
+    PanelProduction_Goods_Weight: TPanel;
+    GBProduction_GoodsName: TGroupBox;
+    PanelProduction_GoodsName: TPanel;
+    PanelTare_Goods: TPanel;
+    LabelTare_Goods: TLabel;
+    GBTare_GoodsCode: TGroupBox;
+    PanelTare_GoodsCode: TPanel;
+    GBTare_Goods_Weight: TGroupBox;
+    PanelTare_Goods_Weight: TPanel;
+    GBTare_GoodsName: TGroupBox;
+    PanelTare_GoodsName: TPanel;
+    gbTare_Goods_Count: TGroupBox;
+    PanelTare_Goods_Count: TPanel;
+    PanelSpace1: TPanel;
+    PanelSpace2: TPanel;
+    infoPanelTotalWeight: TPanel;
+    GBTotalWeight: TGroupBox;
+    PanelTotalWeight: TPanel;
+    GBDiscountWeight: TGroupBox;
+    PanelDiscountWeight: TPanel;
+    PanelInfo: TPanel;
+    PanelMessage: TPanel;
+    GBPassword: TGroupBox;
+    PanelPassword: TPanel;
+    PanelBillKind: TPanel;
+    infoPanel: TPanel;
+    PanelPartner: TPanel;
+    LabelPartner: TLabel;
+    PanelPartnerCode: TPanel;
+    EditPartnerCode: TEdit;
+    PanelPartnerName: TPanel;
+    PanelPriceList: TPanel;
+    PriceListNameLabel: TLabel;
+    PanelPriceListName: TPanel;
+    PanelRouteUnit: TPanel;
+    LabelRouteUnit: TLabel;
+    PanelRouteUnitCode: TPanel;
+    PanelRouteUnitName: TPanel;
+    PanelIsRecalc: TPanel;
+    DataSource: TDataSource;
+    Query: TQuery;
+    QueryProduction_Code: TIntegerField;
+    QueryProduction_Name: TStringField;
+    QueryKindPackageName: TStringField;
+    QueryPartionStr_MB: TStringField;
+    QueryPartionDate: TDateField;
+    QueryPriceListName: TStringField;
+    QueryDiscountWeight: TFloatField;
+    QueryProduction_Weight_Discount: TFloatField;
+    QueryProduction_Weight: TFloatField;
+    QueryTare_Weight: TFloatField;
+    QueryTare_Count: TFloatField;
+    QueryTare_Code: TIntegerField;
+    QueryTare_Name: TStringField;
+    QueryNumberTare: TIntegerField;
+    QueryNumberLevel: TIntegerField;
+    QueryInsertDate: TDateTimeField;
+    QueryUpdateDate: TDateTimeField;
+    QueryLastPrice: TFloatField;
+    QueryToatlSumm: TFloatField;
+    QueryId: TIntegerField;
+    QueryKindPackageId: TIntegerField;
+    QueryisErased: TSmallintField;
+    QueryOperCount_Upakovka: TFloatField;
+    QueryBillKindName: TStringField;
+    QueryOperCount_sh: TFloatField;
+    QueryZakaz: TQuery;
+    PopupMenu: TPopupMenu;
+    miPrintZakazMinus: TMenuItem;
+    miPrintZakazAll: TMenuItem;
+    miLine11: TMenuItem;
+    miPrintBill_byInvNumber: TMenuItem;
+    miPrintBill_andNaliog_byInvNumber: TMenuItem;
+    miPrintBillTotal_byClient: TMenuItem;
+    miPrintBillTotal_byFozzi: TMenuItem;
+    miLine12: TMenuItem;
+    miPrintSchet_byInvNumber: TMenuItem;
+    miPrintBillTransport_byInvNumber: TMenuItem;
+    miPrintBillTransportNew_byInvNumber: TMenuItem;
+    miPrintBillKachestvo_byInvNumber: TMenuItem;
+    miPrintBillNumberTare_byInvNumber: TMenuItem;
+    miPrintBillNotice_byInvNumber: TMenuItem;
+    miLine13: TMenuItem;
+    miPrintSaleAll: TMenuItem;
+    miPrint_Report_byTare: TMenuItem;
+    miPrint_Report_byMemberProduction: TMenuItem;
+    miLine14: TMenuItem;
+    miScaleIni_DB: TMenuItem;
+    miScaleIni_BI: TMenuItem;
+    miScaleIni_Zeus: TMenuItem;
+    miScaleIni_BI_R: TMenuItem;
+    miLine15: TMenuItem;
+    miScaleRun_DB: TMenuItem;
+    miScaleRun_BI: TMenuItem;
+    miScaleRun_Zeus: TMenuItem;
+    miScaleRun_BI_R: TMenuItem;
+    LockTimer: TTimer;
+    PartionDateEdit: TcxDateEdit;
+    cxDateEdit1: TcxDateEdit;
+    spTest: TdsdStoredProc;
+    procedure ButtonExportToEDIClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.dfm}
+
+procedure TMainForm.ButtonExportToEDIClick(Sender: TObject);
+begin
+ spTest.Execute;
+end;
+
+end.
