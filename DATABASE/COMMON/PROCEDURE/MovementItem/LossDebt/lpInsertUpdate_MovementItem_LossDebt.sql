b80107f82d40ff2a89a2c0fbd8627e60d7d4134e
K0sta <Ashtu777@gmail.com>
2014-03-12 20:52:24 +0200

    ok

--- DATABASE/COMMON/PROCEDURE/MovementItem/LossDebt/lpInsertUpdate_MovementItem_LossDebt.sql
+++ DATABASE/COMMON/PROCEDURE/MovementItem/LossDebt/lpInsertUpdate_MovementItem_LossDebt.sql
@@ -57,6 +57,106 @@ BEGIN
                   AND MovementItem.Id <> COALESCE (ioId, 0))
      THEN
          RAISE EXCEPTION 'Ошибка.В документе уже существует <%> <%> <%> <%> .Дублирование запрещено.', lfGet_Object_ValueData (inJuridicalId), lfGet_Object_ValueData (inPaidKindId), lfGet_Object_ValueData (inInfoMoneyId), lfGet_Object_ValueData (inContractId);
+-- Function: lpInsertUpdate_MovementItem_LossDebt ()
+
+DROP FUNCTION IF EXISTS lpInsertUpdate_MovementItem_LossDebt (Integer, Integer, Integer, TFloat, TFloat, Boolean, Integer, Integer, Integer, Integer, Integer);
+
+CREATE OR REPLACE FUNCTION lpInsertUpdate_MovementItem_LossDebt(
+ INOUT ioId                  Integer   , -- Ключ объекта <Элемент документа>
+    IN inMovementId          Integer   , -- ключ Документа
+    IN inJuridicalId         Integer   , -- Юр.лицо
+    IN inAmount              TFloat    , -- Сумма
+    IN inSumm                TFloat    , -- Сумма остатка (долг)
+    IN inIsCalculated        Boolean   , -- Сумма рассчитывается по остатку (да/нет)
+    IN inContractId          Integer   , -- Договор
+    IN inPaidKindId          Integer   , -- Вид форм оплаты
+    IN inInfoMoneyId         Integer   , -- Статьи назначения
+    IN inUnitId              Integer   , -- Подразделение
+    IN inUserId              Integer     -- Пользователь
+)                              
+RETURNS Integer AS
+$BODY$
+BEGIN
+     -- проверка
+     IF COALESCE (inJuridicalId, 0) = 0
+     THEN
+         RAISE EXCEPTION 'Ошибка.Не установлено <Юридическое лицо>.';
+     END IF;
+     IF COALESCE (inContractId, 0) = 0
+     THEN
+         RAISE EXCEPTION 'Ошибка.Не установлен <№ дог.>.';
+     END IF;
+     IF COALESCE (inPaidKindId, 0) = 0
+     THEN
+         RAISE EXCEPTION 'Ошибка.Не установлена <Форма оплаты>.';
+     END IF;
+     IF COALESCE (inInfoMoneyId, 0) = 0
+     THEN
+         RAISE EXCEPTION 'Ошибка.Не установлена <УП статья назначения>.';
+     END IF;
+
+     -- проверка
+     IF EXISTS (SELECT MovementItem.Id
+                FROM MovementItem
+                     JOIN MovementItemLinkObject AS MILinkObject_InfoMoney
+                                                 ON MILinkObject_InfoMoney.MovementItemId = MovementItem.Id
+                                                AND MILinkObject_InfoMoney.DescId = zc_MILinkObject_InfoMoney()
+                                                AND MILinkObject_InfoMoney.ObjectId = inInfoMoneyId
+                     JOIN MovementItemLinkObject AS MILinkObject_Contract
+                                                      ON MILinkObject_Contract.MovementItemId = MovementItem.Id
+                                                     AND MILinkObject_Contract.DescId = zc_MILinkObject_Contract()
+                                                     AND MILinkObject_Contract.ObjectId = inContractId
+                     JOIN MovementItemLinkObject AS MILinkObject_PaidKind
+                                                 ON MILinkObject_PaidKind.MovementItemId = MovementItem.Id
+                                                AND MILinkObject_PaidKind.DescId = zc_MILinkObject_PaidKind()
+                                                AND MILinkObject_PaidKind.ObjectId = inPaidKindId
+                WHERE MovementItem.MovementId = inMovementId
+                  AND MovementItem.ObjectId = inJuridicalId
+                  AND MovementItem.DescId = zc_MI_Master()
+                  AND MovementItem.Id <> COALESCE (ioId, 0))
+     THEN
+         RAISE EXCEPTION 'Ошибка.В документе уже существует <%> <%> <%> <%> .Дублирование запрещено.', lfGet_Object_ValueData (inJuridicalId), lfGet_Object_ValueData (inPaidKindId), lfGet_Object_ValueData (inInfoMoneyId), lfGet_Object_ValueData (inContractId);
+     END IF;
+
+
+     -- сохранили <Элемент документа>
+     ioId := lpInsertUpdate_MovementItem (ioId, zc_MI_Master(), inJuridicalId, inMovementId, inAmount, NULL);
+
+     -- сохранили свойство <Сумма рассчитывается по остатку (да/нет)>
+     PERFORM lpInsertUpdate_MovementItemBoolean (zc_MIBoolean_Calculated(), ioId, inIsCalculated);
+
+     -- сохранили свойство <Сумма остатка (долг)>
+     PERFORM lpInsertUpdate_MovementItemFloat (zc_MIFloat_Summ(), ioId, inSumm);
+
+     -- сохранили связь с <Договор>
+     PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_Contract(), ioId, inContractId);
+
+     -- сохранили связь с <Вид форм оплаты>
+     PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_PaidKind(), ioId, inPaidKindId);
+
+     -- сохранили связь с <Статьи назначения>
+     PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_InfoMoney(), ioId, inInfoMoneyId);
+
+     -- сохранили связь с <Подразделение>
+     PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_Unit(), ioId, inUnitId);
+     -- пересчитали Итоговые суммы по документу
+     PERFORM lpInsertUpdate_MovementFloat_TotalSumm (inMovementId);
+
+     -- сохранили протокол
+     -- PERFORM lpInsert_MovementItemProtocol (ioId, vbUserId);
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.
+ 10.03.14                                        *
+*/
+
+-- тест
+-- SELECT * FROM lpInsertUpdate_MovementItem_LossDebt (ioId:= 0, inMovementId:= 10, inGoodsId:= 1, inAmount:= 0, inAmountPartner:= 0, inPrice:= 1, inCountForPrice:= 1, inLiveWeight:= 0, inHeadCount:= 0, inPartionGoods:= '', inGoodsKindId:= 0, inSession:= '2')
      END IF;
 
 
