5e4e3017110894bf80edee9d2524e57d21a8005f
innaf <innaf@ua.fm>
2014-03-13 13:37:15 +0200

    плюс процки zc_Movement_WeighingProduction

---
+++ DATABASE/COMMON/PROCEDURE/Movement/WeighingProduction/gpSelect_Movement_WeighingProduction.sql
@@ -0,0 +1,104 @@
+-- Function: gpSelect_Movement_WeighingProduction()
+
+DROP FUNCTION IF EXISTS gpSelect_Movement_WeighingProduction (TDateTime, TDateTime, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpSelect_Movement_WeighingProduction(
+    IN inStartDate   TDateTime , --
+    IN inEndDate     TDateTime , --
+    IN inSession     TVarChar    -- сессия пользователя
+)
+RETURNS TABLE (Id Integer, InvNumber TVarChar, OperDate TDateTime, StatusCode Integer, StatusName TVarChar
+             , Parent TVarChar--, ParentId Integer
+             , StartWeighing TDateTime, EndWeighing TDateTime 
+             , MovementDesc TFloat
+             , FromId Integer, FromName TVarChar, ToId Integer, ToName TVarChar
+             , RouteSortingId Integer, RouteSortingName TVarChar
+             , UserId Integer, UserName TVarChar
+              )
+AS
+$BODY$
+   DECLARE vbUserId Integer;
+BEGIN
+     -- проверка прав пользователя на вызов процедуры
+     -- vbUserId := lpCheckRight (inSession, zc_Enum_Process_Select_Movement_WeighingProduction());
+     vbUserId:= lpGetUserBySession (inSession);
+
+     -- Результат
+     RETURN QUERY 
+     WITH tmpUserAdmin AS (SELECT ObjectLink_UserRole_View.UserId FROM ObjectLink_UserRole_View WHERE RoleId = zc_Enum_Role_Admin() AND ObjectLink_UserRole_View.UserId = vbUserId)
+        , tmpRoleAccessKey AS (SELECT AccessKeyId FROM Object_RoleAccessKey_View WHERE Object_RoleAccessKey_View.UserId = vbUserId AND NOT EXISTS (SELECT tmpUserAdmin.UserId FROM tmpUserAdmin) GROUP BY AccessKeyId
+                         UNION SELECT AccessKeyId FROM Object_RoleAccessKey_View WHERE EXISTS (SELECT tmpUserAdmin.UserId FROM tmpUserAdmin) GROUP BY AccessKeyId
+                              )
+       SELECT  Movement.Id
+             , Movement.InvNumber
+             , Movement.OperDate
+             , Object_Status.ObjectCode          AS StatusCode
+             , Object_Status.ValueData           AS StatusName
+
+             , Movement_Parent.InvNumber         AS Parent
+              
+             , MovementDate_StartWeighing.ValueData  AS StartWeighing  
+             , MovementDate_EndWeighing.ValueData    AS EndWeighing
+
+             , MovementFloat_MovementDesc.ValueData  AS MovementDesc
+
+             , Object_From.Id                  AS FromId
+             , Object_From.ValueData           AS FromName
+             , Object_To.Id                    AS ToId
+             , Object_To.ValueData             AS ToName
+             
+             , Object_User.Id                  AS UserId
+             , Object_User.ValueData           AS UserName
+
+       FROM Movement
+            LEFT JOIN Object AS Object_Status ON Object_Status.Id = Movement.StatusId
+            LEFT JOIN Movement AS Movement_Parent ON Movement_Parent.Id = Movement.ParentId
+
+            LEFT JOIN MovementDate AS MovementDate_StartWeighing
+                                   ON MovementDate_StartWeighing.MovementId =  Movement.Id
+                                  AND MovementDate_StartWeighing.DescId = zc_MovementDate_StartWeighing()
+            LEFT JOIN MovementDate AS MovementDate_EndWeighing
+                                   ON MovementDate_EndWeighing.MovementId =  Movement.Id
+                                  AND MovementDate_EndWeighing.DescId = zc_MovementDate_EndWeighing()
+                                  
+            LEFT JOIN MovementFloat AS MovementFloat_MovementDesc
+                                    ON MovementFloat_MovementDesc.MovementId =  Movement.Id
+                                   AND MovementFloat_MovementDesc.DescId = zc_MovementFloat_MovementDesc()
+            
+            LEFT JOIN MovementLinkObject AS MovementLinkObject_From
+                                         ON MovementLinkObject_From.MovementId = Movement.Id
+                                        AND MovementLinkObject_From.DescId = zc_MovementLinkObject_From()
+            LEFT JOIN Object AS Object_From ON Object_From.Id = MovementLinkObject_From.ObjectId
+            
+            LEFT JOIN MovementLinkObject AS MovementLinkObject_To
+                                         ON MovementLinkObject_To.MovementId = Movement.Id
+                                        AND MovementLinkObject_To.DescId = zc_MovementLinkObject_To()
+            LEFT JOIN Object AS Object_To ON Object_To.Id = MovementLinkObject_To.ObjectId
+            
+            LEFT JOIN MovementLinkObject AS MovementLinkObject_User
+                                         ON MovementLinkObject_User.MovementId = Movement.Id
+                                        AND MovementLinkObject_User.DescId = zc_MovementLinkObject_User()
+            LEFT JOIN Object AS Object_User ON Object_User.Id = MovementLinkObject_User.ObjectId
+            
+       WHERE Movement.DescId = zc_Movement_WeighingProduction()
+         AND Movement.OperDate BETWEEN inStartDate AND inEndDate;
+  
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+ALTER FUNCTION gpSelect_Movement_WeighingProduction (TDateTime, TDateTime, TVarChar) OWNER TO postgres;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.
+ 13.03.14         *
+*/
+
+-- тест
+-- SELECT * FROM gpSelect_Movement_WeighingProduction (inStartDate:= '01.01.2014', inEndDate:= '01.02.2014', inSession:= zfCalc_UserAdmin())
+
+
+
+
+
+
