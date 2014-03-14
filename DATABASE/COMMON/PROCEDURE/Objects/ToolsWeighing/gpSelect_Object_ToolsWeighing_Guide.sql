444c9de208ab10dfd9b92bdcf0731045502c0fd5
DmytroManko <dnepro.ua@gmail.com>
2014-03-14 16:42:55 +0200

    +

---
+++ DATABASE/COMMON/PROCEDURE/Objects/ToolsWeighing/gpSelect_Object_ToolsWeighing_Guide.sql
@@ -0,0 +1,96 @@
+-- Function: gpSelect_Object_ToolsWeighing_Guide()
+
+DROP FUNCTION IF EXISTS gpSelect_Object_ToolsWeighing_Guide (Integer, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpSelect_Object_ToolsWeighing_Guide(
+    IN inRootId      Integer   ,
+    IN inSession     TVarChar       -- сессия пользователя
+)
+RETURNS TABLE (
+--               DescId     Integer, DescName     TVarChar,
+--               FromId     Integer, FromName     TVarChar
+
+               ToId       Integer, ToName       TVarChar
+--               PaidKindId Integer, PaidKindName TVarChar,
+
+--               ColorGridName TVarChar
+               ) AS
+$BODY$
+   DECLARE vbUserId         Integer;
+   DECLARE vbAccessKeyAll   Boolean;
+   DECLARE vbDescCount      Integer;
+   DECLARE vbCurrentDesc    Integer;
+   DECLARE vbRootName       TVarChar;
+
+BEGIN
+   -- проверка прав пользователя на вызов процедуры
+   -- vbUserId:= lpCheckRight(inSession, zc_Enum_Process_Select_Object_ToolsWeighing());
+--   vbUserId:= lpGetUserBySession (inSession);
+   -- определяется - может ли пользовать видеть весь справочник
+--   vbAccessKeyAll:= zfCalc_AccessKey_GuideAll (vbUserId);
+    CREATE TEMP TABLE tmpDesc (Id integer, DescName TVarChar);
+
+    vbRootName    := (SELECT Object_ToolsWeighing_View.Name FROM Object_ToolsWeighing_View WHERE Object_ToolsWeighing_View.Id = inRootId);
+    vbDescCount   := (SELECT gpGetToolsPropertyValue FROM gpGetToolsPropertyValue (vbRootName, 'Movement', 'DescCount', '', '10', inSession));
+    vbCurrentDesc := 1;
+--     INSERT INTO tmpDesc (Id, DescName)
+--     VALUES (vbCurrentDesc, CAST('MovementDesc_1'  AS TVarChar));
+
+
+    LOOP
+     INSERT INTO tmpDesc (Id, DescName)
+     VALUES (vbCurrentDesc, CAST('MovementDesc_'||CAST(vbCurrentDesc AS TVarChar)  AS TVarChar));
+     vbCurrentDesc := vbCurrentDesc + 1;
+     EXIT WHEN vbCurrentDesc > vbDescCount;
+    END LOOP;
+
+   -- Результат
+    RETURN QUERY
+       SELECT
+
+--             MovementDesc.Id                        AS DescId
+--           , MovementDesc.ItemName                  AS DescName
+
+--             Object_From.Id                         AS FromId
+--           , Object_From.ValueData                  AS FromName
+
+             Object_To.Id                           AS ToId
+           , Object_To.ValueData                    AS ToName
+--           , Object_PaidKind.Id                     AS PaidKindId
+--           , Object_PaidKind.ValueData              AS PaidKindName
+
+--           , ColorGridValue.*                       AS ColorGridName
+
+       FROM tmpDesc
+--       LEFT JOIN gpGetToolsPropertyValue (vbRootName, 'Movement', tmpDesc.DescName, 'DescId', '0',  inSession) AS DescIdValue ON 1 = 1
+--       LEFT JOIN gpGetToolsPropertyValue (vbRootName, 'Movement', tmpDesc.DescName, 'FromId', '0',  inSession) AS FromIdValue ON 1 = 1
+--       LEFT JOIN gpGetToolsPropertyValue (vbRootName, 'Movement', tmpDesc.DescName, 'ToId', '0',  inSession) AS ToIdValue ON 1 = 1
+--       LEFT JOIN gpGetToolsPropertyValue (vbRootName, 'Movement', tmpDesc.DescName, 'PaidKindId', '0',  inSession) AS PaidKindIdValue ON 1 = 1
+
+
+--       LEFT JOIN MovementDesc ON MovementDesc.Id = CAST(gpGetToolsPropertyValue (vbRootName, 'Movement', tmpDesc.DescName, 'DescId', '0',  inSession) AS Integer)
+
+--       LEFT JOIN Object AS Object_From ON Object_From.Id = COALESCE (CAST(gpGetToolsPropertyValue (vbRootName, 'Movement', tmpDesc.DescName, 'FromId', '0',  inSession) AS Integer),0)
+
+       LEFT JOIN Object AS Object_To ON Object_To.Id = CAST(gpGetToolsPropertyValue (vbRootName, 'Movement', tmpDesc.DescName, 'ToId', '0',  inSession) AS Integer) --ToIdValue.gpGetToolsPropertyValue
+--       LEFT JOIN Object AS Object_PaidKind ON Object_PaidKind.Id = CAST(gpGetToolsPropertyValue (vbRootName, 'Movement', tmpDesc.DescName, 'PaidKindId', '0',  inSession) AS Integer) --PaidKindIdValue.gpGetToolsPropertyValue
+
+--       LEFT JOIN gpGetToolsPropertyValue (vbRootName, 'Movement', tmpDesc.DescName, 'ColorGrid', '0',  inSession) AS ColorGridValue ON 1 = 1
+      ;
+
+    DROP TABLE tmpDesc;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+ALTER FUNCTION gpSelect_Object_ToolsWeighing_Guide (Integer, TVarChar) OWNER TO postgres;
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.
+ 14.03.14                                                         *
+*/
+
+-- тест
+ SELECT * FROM gpSelect_Object_ToolsWeighing_Guide (88952, zfCalc_UserAdmin())
