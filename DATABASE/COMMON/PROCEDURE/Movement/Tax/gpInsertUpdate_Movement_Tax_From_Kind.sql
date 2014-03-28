23ff667e2676137892d2527eefc485f39353c231
innaf <innaf@ua.fm>
2014-03-29 00:33:55 +0200

    gpInsertUpdate_Movement_Tax_From_Kind

--- DATABASE/COMMON/PROCEDURE/Movement/Tax/gpInsertUpdate_Movement_Tax_From_Kind.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/Tax/gpInsertUpdate_Movement_Tax_From_Kind.sql
@@ -1,6 +1,8 @@
 -- Function: gpInsertUpdate_Movement_Tax_From_Kind()
 
-DROP FUNCTION IF EXISTS gpInsertUpdate_Movement_Tax_From_Kind (Integer, Integer, TVarChar);
+--DROP FUNCTION IF EXISTS gpInsertUpdate_Movement_Tax_From_Kind (Integer, Integer, TVarChar);
+--DROP FUNCTION IF EXISTS gpInsertUpdate_Movement_Tax_From_Kind (Integer, Integer, TVarChar, TVarChar, TVarChar);
+
 
 
 CREATE OR REPLACE FUNCTION gpInsertUpdate_Movement_Tax_From_Kind (
@@ -13,60 +15,92 @@ CREATE OR REPLACE FUNCTION gpInsertUpdate_Movement_Tax_From_Kind (
 RETURNS RECORD
 AS
 $BODY$
-   DECLARE vbTaxId        Integer;
-   DECLARE vbUserId       Integer;
-   DECLARE vbInvNumberPartner Integer;
-   DECLARE vbInvNumber Integer;
+   DECLARE vbTaxId            Integer;
+   DECLARE vbUserId           Integer;
+   DECLARE vbOperDate         TDateTime;
+   DECLARE vbInvNumber        TVarChar;
+   DECLARE vbInvNumberPartner TVarChar;
+   DECLARE vbPriceWithVAT     Boolean ;
+   DECLARE vbVATPercent       TFloat;
+   DECLARE vbFromId           Integer;
+   DECLARE vbToId             Integer;
+   DECLARE vbPartnerId        Integer;
+   DECLARE vbContractId       Integer;
 BEGIN
      -- проверка прав пользователя на вызов процедуры
      vbUserId := lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_Movement_Tax());
-/*
+   
+            IF COALESCE (inDocumentTaxKindId,0) =  zc_Enum_DocumentTaxKind_Tax()
+               THEN
+               /* выбираем реквизиты для обновления/создания шапки НН */
+               SELECT MovementLinkMovement.MovementChildId 
+                    , MovementSale.InvNumber
+                    , MovementSale.InvNumberPartner_Master                     
+                    , MovementSale.OperDate
+                    , MovementSale.PriceWithVAT
+                    , MovementSale.VATPercent
+                    , ObjectLink_Contract_JuridicalBasis.ChildObjectId  -- от кого
+                    , ObjectLink_Partner_Juridical.ChildObjectId          -- кому
+                    , MovementSale.ToId           	             -- контрагент 
+                    , MovementSale.ContractId 
+                    
+               INTO  vbTaxId, vbInvNumber, vbInvNumberPartner, vbOperDate, vbPriceWithVAT, vbVATPercent, vbFromId, vbToId, vbPartnerId, vbContractId
 
-       CASE inDocumentTaxKindId
-         WHEN zc_Enum_DocumentTaxKind_Tax() THEN BEGIN
-
-             SELECT MovementLinkMovement.ChildMovementId INTO vbTaxId 
-             FROM MovementLinkMovement 
-                  WHERE MovementLinkMovement.DescId = zc_MovementLinkMovement_Master()
-                    AND MovementLinkMovement.MovementId = inMovementId;
-
-             IF COALESCE(vbTaxId, 0) = 0 THEN
-
-                SELECT NEXTVAL ('movement_tax_seq')::TVarChar INTO vbInvNumber;
-                vbInvNumberPartner := vbInvNumber;
-
-
-                SELECT lpInsertUpdate_Movement_Tax(
-                       ioId := 0
-                     , inInvNumber := vbInvNumber
-                     , inInvNumberPartner := vbInvNumberPartner
-                     , inOperDate := MovementSale.OperDate
-                     , inChecked := false
-                     , inDocument := false
-                     , inPriceWithVAT := MovementSale.PriceWithVAT
-                     , inVATPercent := MovementSale.VATPercent
-                     , inFromId := MovementSale.FromId
-                     , inToId := ObjectLink_Partner_Juridical.ChildObjectId
-                     , inPartnerId := MovementSale.ToId          Integer   , -- Контрагент
-                     , inContractId := MovementSale.ContractId
-                     , inDocumentTaxKindId := MovementSale.DocumentTaxKindId,
-                     , inUserId := vbUserId) INTO vbTaxId
-               FROM gpGet_Movement_Sale(inMovementId, Today(*), inSession) AS MovementSale
+               FROM gpGet_Movement_Sale(inMovementId, CURRENT_DATE , inSession) AS MovementSale
                LEFT JOIN ObjectLink AS ObjectLink_Partner_Juridical
                                     ON ObjectLink_Partner_Juridical.ObjectId = MovementSale.ToId
-                                   AND ObjectLink_Partner_Juridical.DescId = zc_ObjectLink_Partner_Juridical();
-          END IF;
-       END;
+                                   AND ObjectLink_Partner_Juridical.DescId = zc_ObjectLink_Partner_Juridical()
+               
+               LEFT JOIN ObjectLink AS ObjectLink_Contract_JuridicalBasis
+                                    ON ObjectLink_Contract_JuridicalBasis.ObjectId = MovementSale.ContractId
+                                   AND ObjectLink_Contract_JuridicalBasis.DescId = zc_ObjectLink_Contract_JuridicalBasis()
+               
+               LEFT JOIN MovementLinkMovement ON MovementLinkMovement.MovementId = MovementSale.Id
+                                             AND MovementLinkMovement.DescId = zc_MovementLinkMovement_Master();
 
-       ESLE
-*/
-          SELECT '12345/789'
-              , Object_DocumentTaxKind.ValueData
-                INTO outInvNumberPartner_Master, outDocumentTaxKindName
-         FROM Object AS Object_DocumentTaxKind
-         WHERE Object_DocumentTaxKind.Id = inDocumentTaxKindId;
---      END;
 
+
+               SELECT tmp.ioInvNumberPartner
+                    , Object_DocumentTaxKind.ValueData
+                    , tmp.ioId                   
+               INTO outInvNumberPartner_Master, outDocumentTaxKindName, vbTaxId
+     
+               FROM lpInsertUpdate_Movement_Tax(
+                       ioId := COALESCE (vbTaxId,0)
+                     , inInvNumber := vbInvNumber
+                     , ioInvNumberPartner := vbInvNumberPartner                    
+                     , inOperDate := vbOperDate
+                     , inChecked := false
+                     , inDocument := false
+                     , inPriceWithVAT := vbPriceWithVAT
+                     , inVATPercent := vbVATPercent
+                     , inFromId := vbFromId                                  -- от кого
+                     , inToId := vbToId                                      -- кому
+                     , inPartnerId := vbPartnerId           	             -- контрагент 
+                     , inContractId := vbContractId
+                     , inDocumentTaxKindId := inDocumentTaxKindId
+                     , inUserId := vbUserId ) as tmp
+
+               LEFT JOIN Object AS Object_DocumentTaxKind ON Object_DocumentTaxKind.Id = inDocumentTaxKindID;
+
+               -- сохранили Продажи с Нологовой
+               PERFORM lpInsertUpdate_MovementLinkMovement (zc_MovementLinkMovement_Master(), inMovementId, vbTaxId);
+            
+               /*выбрать товары из продажы для обновления/создания НН*/
+
+               
+            ELSE 
+                           
+                  SELECT '12345/789'
+                    , Object_DocumentTaxKind.ValueData
+                  INTO outInvNumberPartner_Master, outDocumentTaxKindName
+
+                  FROM Object AS Object_DocumentTaxKind
+                  WHERE Object_DocumentTaxKind.Id = inDocumentTaxKindId;
+
+      
+            END IF;   
+  
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE;
@@ -76,6 +110,7 @@ ALTER FUNCTION gpInsertUpdate_Movement_Tax_From_Kind (Integer, Integer, TVarChar
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.
+ 29.03.14         *
  23.03.14                                        * all
  13.02.14                                                        *
 */
@@ -83,3 +118,16 @@ ALTER FUNCTION gpInsertUpdate_Movement_Tax_From_Kind (Integer, Integer, TVarChar
 -- тест
 -- SELECT * FROM gpInsertUpdate_Movement_Tax_From_Kind(inMovementId := 21838, inDocumentTaxKindId:=80770, inSession := '5'); -- все
 -- SELECT gpInsertUpdate_Movement_Tax_From_Kind FROM gpInsertUpdate_Movement_Tax_From_Kind(inMovementId := 21838, inDocumentTaxKindId:=80770, inSession := '5'); -- все
+
+
+      
+--select * from gpInsertUpdate_Movement_Tax_From_Kind(inMovementId := 16759 , inDocumentTaxKindId := zc_Enum_DocumentTaxKind_Tax() ,  inSession := '5');
+         
+/*select * from Movement
+join  where id = 176021
+*/
+/*select * from Movement
+LEFT JOIN MovementLinkMovement ON MovementLinkMovement.MovementId = Movement.Id
+                              AND MovementLinkMovement.DescId = zc_MovementLinkMovement_Master()
+ where  InvNumber = '140574'
+and Movement.descid = zc_Movement_Sale()*/
