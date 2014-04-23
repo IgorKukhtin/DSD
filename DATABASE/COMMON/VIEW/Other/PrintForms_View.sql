4c0a6b504ba28d8655a01eb5297b8794e6b2d008
DmytroManko <dnepro.ua@gmail.com>
2014-04-23 19:29:16 +0300

    +++

--- DATABASE/COMMON/VIEW/Other/PrintForms_View.sql
+++ DATABASE/COMMON/VIEW/Other/PrintForms_View.sql
@@ -180,6 +180,22 @@ AS
            , CAST (0 AS INTEGER)
            , CAST (0 AS INTEGER)
            , CAST ('PrintMovement_ReturnIn' AS TVarChar)
+      UNION
+-- Metro
+      SELECT
+             zc_movement_returnin()
+           , CAST ('ReturnIn' AS TVarChar)
+           , CAST ('01.01.2000' AS TDateTime)
+           , CAST ('01.01.2200' AS TDateTime)
+           , CAST (Object_Juridical.Id AS INTEGER)
+           , zc_Enum_PaidKind_FirstForm()
+           , CAST ('PrintMovement_ReturnIn32049199' AS TVarChar)
+      FROM Object AS Object_Juridical
+      JOIN ObjectHistory_JuridicalDetails_View AS OH_JuridicalDetails ON OH_JuridicalDetails.JuridicalId = Object_Juridical.Id
+       AND OH_JuridicalDetails.OKPO IN ('32049199')
+      WHERE Object_Juridical.DescId = zc_Object_Juridical()
+
+
 -- возвраты поставщ стандарт
       UNION
       SELECT
@@ -215,6 +231,7 @@ ALTER TABLE PrintForms_View OWNER TO postgres;
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.
+ 23.04.14                                                        * + PrintMovement_ReturnIn32049199
  07.04.14                                                        * + Bill
  03.04.14                                                        * + Bill
  02.04.14                                                        * + Adventis + Billa + Kray
--- TEST/SOURCE/LoadReportTest.pas
+++ TEST/SOURCE/LoadReportTest.pas
@@ -71,6 +71,7 @@ begin
   LoadReportFromFile('PrintMovement_Sale1', ReportPath + '\Товарный Учет\PrintMovement_Sale1.fr3');
   LoadReportFromFile('PrintMovement_Sale2', ReportPath + '\Товарный Учет\PrintMovement_Sale2.fr3');
   LoadReportFromFile('PrintMovement_ReturnIn', ReportPath + '\Товарный Учет\PrintMovement_ReturnIn.fr3');
+  LoadReportFromFile('PrintMovement_ReturnIn32049199', ReportPath + '\Товарный Учет\PrintMovement_ReturnIn32049199.fr3');
   LoadReportFromFile('PrintMovement_Tax', ReportPath + '\Товарный Учет\PrintMovement_Tax.fr3');
   LoadReportFromFile('PrintMovement_TaxCorrective', ReportPath + '\Товарный Учет\PrintMovement_TaxCorrective.fr3');
   LoadReportFromFile('PrintMovement_Bill', ReportPath + '\Товарный Учет\PrintMovement_Bill.fr3');
