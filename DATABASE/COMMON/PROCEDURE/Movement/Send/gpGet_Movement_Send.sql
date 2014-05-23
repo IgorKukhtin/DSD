97fd84f61cf18047d136aeae589c8a9ec91a70ff
DmytroManko <dnepro.ua@gmail.com>
2014-05-23 21:06:44 +0300

    +

--- DATABASE/COMMON/PROCEDURE/Movement/Send/gpGet_Movement_Send.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/Send/gpGet_Movement_Send.sql
@@ -1,9 +1,13 @@
 -- Function: gpGet_Movement_Send()
 
 -- DROP FUNCTION gpGet_Movement_Send (Integer, TVarChar);
+DROP FUNCTION IF EXISTS gpGet_Movement_SendOnPrice (Integer, TVarChar);
+DROP FUNCTION IF EXISTS gpGet_Movement_SendOnPrice (Integer, TDateTime, TVarChar);
+
 
 CREATE OR REPLACE FUNCTION gpGet_Movement_Send(
     IN inMovementId        Integer  , -- ключ Документа
+    IN inOperDate          TDateTime, -- дата Документа
     IN inSession           TVarChar   -- сессия пользователя
 )
 RETURNS TABLE (Id Integer, InvNumber TVarChar, OperDate TDateTime, StatusCode Integer, StatusName TVarChar
@@ -21,39 +25,35 @@ BEGIN
 
      IF COALESCE (inMovementId, 0) = 0
      THEN
-     RETURN QUERY 
-       WITH tmpUserTransport AS (SELECT UserId FROM ObjectLink_UserRole_View WHERE RoleId = zc_Enum_Role_Transport())
-       SELECT
-             0 AS Id
-           , NEXTVAL ('Movement_Send_seq') :: TVarChar AS InvNumber
-           , CURRENT_DATE :: TDateTime     AS OperDate
-           , lfObject_Status.Code          AS StatusCode
-           , lfObject_Status.Name          AS StatusName
-           , 0 :: TFloat                   AS TotalCount
-           , Object_Personal.Id            AS FromId
-           , Object_Personal.ValueData     AS FromName
-           , 0                             AS ToId
-           , '' :: TVarChar                AS ToName
-          FROM lfGet_Object_Status (zc_Enum_Status_UnComplete()) AS lfObject_Status
-               LEFT JOIN Object AS Object_Personal ON Object_Personal.Id IN (SELECT MIN (Object_Personal_View.PersonalId) FROM Object_Personal_View WHERE PersonalCode = 2)
-                                                  AND vbUserId IN (SELECT UserId FROM tmpUserTransport)
-       ;
+     RETURN QUERY
+         SELECT
+               0 AS Id
+             , CAST (NEXTVAL ('Movement_Send_seq') AS TVarChar) AS InvNumber
+             , inOperDate                                       AS OperDate
+             , Object_Status.Code                               AS StatusCode
+             , Object_Status.Name                               AS StatusName
+             , CAST (0 AS TFloat)                               AS TotalCount
+             , 0                     				            AS FromId
+             , CAST ('' AS TVarChar) 				            AS FromName
+             , 0                     				            AS ToId
+             , CAST ('' AS TVarChar) 				            AS ToName
+
+          FROM lfGet_Object_Status(zc_Enum_Status_UnComplete()) AS Object_Status;
+
      ELSE
 
-     RETURN QUERY 
+     RETURN QUERY
        SELECT
-             Movement.Id
-           , Movement.InvNumber
-           , Movement.OperDate
-           , Object_Status.ObjectCode          AS StatusCode
-           , Object_Status.ValueData           AS StatusName
-
-           , MovementFloat_TotalCount.ValueData          AS TotalCount
-          
-           , Object_From.Id                    AS FromId
-           , Object_From.ValueData             AS FromName
-           , Object_To.Id                      AS ToId
-           , Object_To.ValueData               AS ToName
+             Movement.Id                                        AS Id
+           , Movement.InvNumber                                 AS InvNumber
+           , Movement.OperDate                                  AS OperDate
+           , Object_Status.ObjectCode                           AS StatusCode
+           , Object_Status.ValueData                            AS StatusName
+           , MovementFloat_TotalCount.ValueData                 AS TotalCount
+           , Object_From.Id                                     AS FromId
+           , Object_From.ValueData                              AS FromName
+           , Object_To.Id                                       AS ToId
+           , Object_To.ValueData                                AS ToName
 
        FROM Movement
             LEFT JOIN Object AS Object_Status ON Object_Status.Id = Movement.StatusId
@@ -61,33 +61,37 @@ BEGIN
             LEFT JOIN MovementFloat AS MovementFloat_TotalCount
                                     ON MovementFloat_TotalCount.MovementId =  Movement.Id
                                    AND MovementFloat_TotalCount.DescId = zc_MovementFloat_TotalCount()
-                                   
+
             LEFT JOIN MovementLinkObject AS MovementLinkObject_From
                                          ON MovementLinkObject_From.MovementId = Movement.Id
                                         AND MovementLinkObject_From.DescId = zc_MovementLinkObject_From()
             LEFT JOIN Object AS Object_From ON Object_From.Id = MovementLinkObject_From.ObjectId
-           
+
             LEFT JOIN MovementLinkObject AS MovementLinkObject_To
                                          ON MovementLinkObject_To.MovementId = Movement.Id
                                         AND MovementLinkObject_To.DescId = zc_MovementLinkObject_To()
             LEFT JOIN Object AS Object_To ON Object_To.Id = MovementLinkObject_To.ObjectId
 
-       WHERE Movement.Id =  inMovementId;
+
+       WHERE Movement.Id =  inMovementId
+         AND Movement.DescId = zc_Movement_Send();
+
        END IF;
-  
+
 END;
 $BODY$
   LANGUAGE PLPGSQL VOLATILE;
-ALTER FUNCTION gpGet_Movement_Send (Integer, TVarChar) OWNER TO postgres;
+ALTER FUNCTION gpGet_Movement_Send (Integer, TDateTime, TVarChar) OWNER TO postgres;
 
 
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
-               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.
+ 22.05.14                                                        *
  07.12.13                                        * rename UserRole_View -> ObjectLink_UserRole_View
  09.11.13                                        * add tmpUserTransport
- 28.10.13                          * Дефолты для новых записей               
- 15.07.13         * удалили колонки               
+ 28.10.13                          * Дефолты для новых записей
+ 15.07.13         * удалили колонки
  09.07.13                                        * Красота
  08.07.13                                        * zc_MovementFloat_ChangePercent
  30.06.13                                        *
