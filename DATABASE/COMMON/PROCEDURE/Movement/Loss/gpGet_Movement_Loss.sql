5c1eca9994e615e6e8ab9238350f33507c15f71f
DmytroManko <dnepro.ua@gmail.com>
2014-05-27 19:01:53 +0300

    Loss, Send

--- DATABASE/COMMON/PROCEDURE/Movement/Loss/gpGet_Movement_Loss.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/Loss/gpGet_Movement_Loss.sql
@@ -1,38 +1,60 @@
 -- Function: gpGet_Movement_Loss()
 
 -- DROP FUNCTION gpGet_Movement_Loss (Integer, TVarChar);
+DROP FUNCTION IF EXISTS gpGet_Movement_LossOnPrice (Integer, TVarChar);
+DROP FUNCTION IF EXISTS gpGet_Movement_LossOnPrice (Integer, TDateTime, TVarChar);
+
 
 CREATE OR REPLACE FUNCTION gpGet_Movement_Loss(
     IN inMovementId        Integer  , -- ключ Документа
+    IN inOperDate          TDateTime, -- дата Документа
     IN inSession           TVarChar   -- сессия пользователя
 )
-RETURNS TABLE (Id Integer, InvNumber TVarChar, OperDate TDateTime
-             , StatusCode Integer, StatusName TVarChar
+RETURNS TABLE (Id Integer, InvNumber TVarChar, OperDate TDateTime, StatusCode Integer, StatusName TVarChar
              , TotalCount TFloat
              , FromId Integer, FromName TVarChar, ToId Integer, ToName TVarChar
-             )
+              )
 AS
 $BODY$
+  DECLARE vbUserId Integer;
 BEGIN
 
      -- проверка прав пользователя на вызов процедуры
-     -- PERFORM lpCheckRight (inSession, zc_Enum_Process_Get_Movement_Loss());
+     -- vbUserId := PERFORM lpCheckRight (inSession, zc_Enum_Process_Get_Movement_Loss());
+     vbUserId := inSession;
 
-     RETURN QUERY 
+     IF COALESCE (inMovementId, 0) = 0
+     THEN
+     RETURN QUERY
+         SELECT
+               0 AS Id
+             , CAST (NEXTVAL ('Movement_Loss_seq') AS TVarChar) AS InvNumber
+             , inOperDate                                       AS OperDate
+             , Object_Status.Code                               AS StatusCode
+             , Object_Status.Name                               AS StatusName
+             , CAST (0 AS TFloat)                               AS TotalCount
+             , 0                     				            AS FromId
+             , CAST ('' AS TVarChar) 				            AS FromName
+             , 0                     				            AS ToId
+             , CAST ('' AS TVarChar) 				            AS ToName
+
+          FROM lfGet_Object_Status(zc_Enum_Status_UnComplete()) AS Object_Status;
+
+     ELSE
+
+     RETURN QUERY
        SELECT
-             Movement.Id
-           , Movement.InvNumber
-           , Movement.OperDate
-           , Object_Status.ObjectCode           AS StatusCode
-           , Object_Status.ValueData            AS StatusName
+             Movement.Id                                        AS Id
+           , Movement.InvNumber                                 AS InvNumber
+           , Movement.OperDate                                  AS OperDate
+           , Object_Status.ObjectCode                           AS StatusCode
+           , Object_Status.ValueData                            AS StatusName
+           , MovementFloat_TotalCount.ValueData                 AS TotalCount
+           , Object_From.Id                                     AS FromId
+           , Object_From.ValueData                              AS FromName
+           , Object_To.Id                                       AS ToId
+           , Object_To.ValueData                                AS ToName
 
-           , MovementFloat_TotalCount.ValueData AS TotalCount
-                                                                    
-           , Object_From.Id                     AS FromId
-           , Object_From.ValueData              AS FromName
-           , Object_To.Id                       AS ToId
-           , Object_To.ValueData                AS ToName
-        
        FROM Movement
             LEFT JOIN Object AS Object_Status ON Object_Status.Id = Movement.StatusId
 
@@ -50,22 +72,23 @@ BEGIN
                                         AND MovementLinkObject_To.DescId = zc_MovementLinkObject_To()
             LEFT JOIN Object AS Object_To ON Object_To.Id = MovementLinkObject_To.ObjectId
 
-         WHERE Movement.Id =  inMovementId
+
+       WHERE Movement.Id =  inMovementId
          AND Movement.DescId = zc_Movement_Loss();
-  
+
+       END IF;
+
 END;
 $BODY$
-LANGUAGE PLPGSQL VOLATILE;
-ALTER FUNCTION gpGet_Movement_Loss (Integer, TVarChar) OWNER TO postgres;
+  LANGUAGE PLPGSQL VOLATILE;
+ALTER FUNCTION gpGet_Movement_Loss (Integer, TDateTime, TVarChar) OWNER TO postgres;
 
 
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
-               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
-               
- 18.07.13         *
-
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.
+ 26.05.14                                                        *
 */
 
 -- тест
--- SELECT * FROM gpGet_Movement_Loss (inMovementId:= 1, inSession:= '2')
+-- SELECT * FROM gpGet_Movement_Loss (inMovementId:= 1, inSession:= '9818')
--- DATABASE/COMMON/PROCEDURE/Movement/Loss/gpSelect_Movement_Loss.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/Loss/gpSelect_Movement_Loss.sql
@@ -2,19 +2,23 @@
 
 -- DROP FUNCTION gpSelect_Movement_Loss (TDateTime, TDateTime, TVarChar);
 
+DROP FUNCTION IF EXISTS gpSelect_Movement_Loss (TDateTime, TDateTime, TVarChar);
+DROP FUNCTION IF EXISTS gpSelect_Movement_Loss (TDateTime, TDateTime, Boolean, TVarChar);
+
 CREATE OR REPLACE FUNCTION gpSelect_Movement_Loss(
-    IN inStartDate   TDateTime , --
-    IN inEndDate     TDateTime , --
-    IN inSession     TVarChar    -- сессия пользователя
+    IN inStartDate     TDateTime , --
+    IN inEndDate       TDateTime , --
+    IN inIsErased      Boolean ,
+    IN inSession       TVarChar    -- сессия пользователя
 )
-RETURNS TABLE (Id Integer, InvNumber TVarChar, OperDate TDateTime
-             , StatusCode Integer, StatusName TVarChar
+RETURNS TABLE (Id Integer, InvNumber TVarChar, OperDate TDateTime, StatusCode Integer, StatusName TVarChar
              , TotalCount TFloat
-             , FromId Integer, FromName TVarChar
-             , ToId Integer, ToName TVarChar
+             , FromId Integer, FromName TVarChar, ToId Integer, ToName TVarChar
               )
+
 AS
 $BODY$
+   DECLARE vbUserId Integer;
 BEGIN
 
 -- inStartDate:= '01.01.2013';
@@ -22,23 +26,39 @@ BEGIN
 
      -- проверка прав пользователя на вызов процедуры
      -- PERFORM lpCheckRight (inSession, zc_Enum_Process_Select_Movement_Loss());
+--     vbUserId:= lpGetUserBySession (inSession);
 
-     RETURN QUERY 
+     RETURN QUERY
+     WITH tmpStatus AS (SELECT zc_Enum_Status_Complete()   AS StatusId
+                  UNION SELECT zc_Enum_Status_UnComplete() AS StatusId
+                  UNION SELECT zc_Enum_Status_Erased()     AS StatusId WHERE inIsErased = TRUE
+                       )
+        , tmpUserAdmin AS (SELECT UserId FROM ObjectLink_UserRole_View WHERE RoleId = zc_Enum_Role_Admin() AND UserId = vbUserId)
+        , tmpRoleAccessKey AS (SELECT AccessKeyId FROM Object_RoleAccessKey_View WHERE UserId = vbUserId AND NOT EXISTS (SELECT UserId FROM tmpUserAdmin) GROUP BY AccessKeyId
+                         UNION SELECT AccessKeyId FROM Object_RoleAccessKey_View WHERE EXISTS (SELECT UserId FROM tmpUserAdmin) GROUP BY AccessKeyId
+                              )
+
        SELECT
-             Movement.Id
-           , Movement.InvNumber
-           , Movement.OperDate
-           , Object_Status.ObjectCode          AS StatusCode
-           , Object_Status.ValueData           AS StatusName
-
+             Movement.Id                        AS Id
+           , Movement.InvNumber                 AS InvNumber
+           , Movement.OperDate                  AS OperDate
+           , Object_Status.ObjectCode           AS StatusCode
+           , Object_Status.ValueData            AS StatusName
            , MovementFloat_TotalCount.ValueData AS TotalCount
-                                                                   
-           , Object_From.Id                    AS FromId
-           , Object_From.ValueData             AS FromName
-           , Object_To.Id                      AS ToId
-           , Object_To.ValueData               AS ToName
-        
-       FROM Movement
+           , Object_From.Id                     AS FromId
+           , Object_From.ValueData              AS FromName
+           , Object_To.Id                       AS ToId
+           , Object_To.ValueData                AS ToName
+
+
+       FROM (SELECT Movement.id
+             FROM tmpStatus
+                  JOIN Movement ON Movement.OperDate BETWEEN inStartDate AND inEndDate  AND Movement.DescId = zc_Movement_Loss() AND Movement.StatusId = tmpStatus.StatusId
+--                  JOIN tmpRoleAccessKey ON tmpRoleAccessKey.AccessKeyId = Movement.AccessKeyId
+            ) AS tmpMovement
+
+            LEFT JOIN Movement ON Movement.id = tmpMovement.id
+
             LEFT JOIN Object AS Object_Status ON Object_Status.Id = Movement.StatusId
 
             LEFT JOIN MovementFloat AS MovementFloat_TotalCount
@@ -54,22 +74,21 @@ BEGIN
                                          ON MovementLinkObject_To.MovementId = Movement.Id
                                         AND MovementLinkObject_To.DescId = zc_MovementLinkObject_To()
             LEFT JOIN Object AS Object_To ON Object_To.Id = MovementLinkObject_To.ObjectId
+            ;
 
-       WHERE Movement.DescId = zc_Movement_Loss()
-         AND Movement.OperDate BETWEEN inStartDate AND inEndDate;
-  
 END;
 $BODY$
-LANGUAGE PLPGSQL VOLATILE;
-ALTER FUNCTION gpSelect_Movement_Loss (TDateTime, TDateTime, TVarChar) OWNER TO postgres;
+  LANGUAGE PLPGSQL VOLATILE;
+ALTER FUNCTION gpSelect_Movement_Loss (TDateTime, TDateTime, Boolean, TVarChar) OWNER TO postgres;
 
 
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
-               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
-               
- 18.07.13         *
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.
+ 26.05.14                                                        *
+
+
 */
 
 -- тест
--- SELECT * FROM gpSelect_Movement_Loss (inStartDate:= '30.01.2013', inEndDate:= '01.02.2013', inSession:= '2')
+-- SELECT * FROM gpSelect_Movement_Loss (inStartDate:= '30.01.2014', inEndDate:= '01.02.2014', inIsErased := FALSE, inSession:= '2')
