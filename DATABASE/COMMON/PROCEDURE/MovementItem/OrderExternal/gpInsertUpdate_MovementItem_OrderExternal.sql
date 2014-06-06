be38013b046a86d0e4a153edf838177b10f2fbfa
DmytroManko <dnepro.ua@gmail.com>
2014-06-06 18:46:47 +0300

    +++

--- DATABASE/COMMON/PROCEDURE/MovementItem/OrderExternal/gpInsertUpdate_MovementItem_OrderExternal.sql
+++ DATABASE/COMMON/PROCEDURE/MovementItem/OrderExternal/gpInsertUpdate_MovementItem_OrderExternal.sql
@@ -1,40 +1,43 @@
--- Function: gpInsertUpdate_MovementItem_ZakazExternal()
+-- Function: gpInsertUpdate_MovementItem_OrderExternal()
 
--- DROP FUNCTION gpInsertUpdate_MovementItem_ZakazExternal();
+-- DROP FUNCTION gpInsertUpdate_MovementItem_OrderExternal();
 
-CREATE OR REPLACE FUNCTION gpInsertUpdate_MovementItem_ZakazExternal(
+CREATE OR REPLACE FUNCTION gpInsertUpdate_MovementItem_OrderExternal(
  INOUT ioId                  Integer   , -- Ключ объекта <Элемент документа>
     IN inMovementId          Integer   , -- Ключ объекта <Документ>
     IN inGoodsId             Integer   , -- Товары
     IN inAmount              TFloat    , -- Количество
     IN inAmountSecond        TFloat    , -- Количество дозаказ
     IN inGoodsKindId         Integer   , -- Виды товаров
-
     IN inSession             TVarChar    -- сессия пользователя
-)                              
+)
 RETURNS Integer AS
 $BODY$
    DECLARE vbUserId Integer;
 BEGIN
 
      -- проверка прав пользователя на вызов процедуры
-     -- PERFORM lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_MovementItem_ZakazExternal());
+     -- PERFORM lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_MovementItem_OrderExternal());
      vbUserId := inSession;
 
      -- сохранили <Элемент документа>
      ioId := lpInsertUpdate_MovementItem (ioId, zc_MI_Master(), inGoodsId, inMovementId, inAmount, NULL);
-   
+
      -- сохранили свойство <Количество дозаказ>
      PERFORM lpInsertUpdate_MovementItemFloat (zc_MIFloat_AmountSecond(), ioId, inAmountSecond);
 
      -- сохранили связь с <Виды товаров>
      PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_GoodsKind(), ioId, inGoodsKindId);
 
-     -- создали объект <Связи Товары и Виды товаров>
-     PERFORM lpInsert_Object_GoodsByGoodsKind (inGoodsId, inGoodsKindId, vbUserId);
+     IF inGoodsId <> 0
+     THEN
+         -- создали объект <Связи Товары и Виды товаров>
+         PERFORM lpInsert_Object_GoodsByGoodsKind (inGoodsId, inGoodsKindId, vbUserId);
+     END IF;
 
      -- пересчитали Итоговые суммы по накладной
      PERFORM lpInsertUpdate_MovementFloat_TotalSumm (inMovementId);
+
 
      -- сохранили протокол
      -- PERFORM lpInsert_MovementItemProtocol (ioId, vbUserId);
@@ -46,10 +49,9 @@ LANGUAGE PLPGSQL VOLATILE;
 
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
-               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
- 01.08.13         * 
-
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.
+ 06.06.14                                                       *
 */
 
 -- тест
--- SELECT * FROM gpInsertUpdate_MovementItem_ZakazExternal (ioId:= 0, inMovementId:= 10, inGoodsId:= 1, inAmount:= 0, inAmountSecond:= 0, inGoodsKindId:= 0, inSession:= '2')
+-- SELECT * FROM gpInsertUpdate_MovementItem_OrderExternal (ioId:= 0, inMovementId:= 10, inGoodsId:= 1, inAmount:= 0, inHeadCount:= 0, inPartionGoods:= '', inGoodsKindId:= 0, inSession:= '2')
