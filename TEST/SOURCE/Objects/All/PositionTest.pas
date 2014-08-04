1691721bcab0c252f3f6b19a71095691976f9ce7
Arkad20 <Arkad202@gmail.com>
2014-08-04 13:55:26 +0300

    Тесты

---
+++ TEST/SOURCE/Objects/All/PositionTest.pas
@@ -0,0 +1,76 @@
+unit PositionTest;
+
+interface
+  uses dbTest, dbObjectTest, TestFramework, ObjectTest;
+
+type
+  TPositionTest = class(TdbObjectTestNew)
+  published
+    procedure ProcedureLoad; override;
+    procedure Test; override;
+  end;
+
+    TPosition = class(TObjectTest)
+  function InsertDefault: integer; override;
+  public
+    function InsertUpdatePosition(const Id, Code : integer; Name: string): integer;
+    constructor Create; override;
+  end;
+
+implementation
+
+uses ZDbcIntfs, SysUtils, Storage, DBClient, XMLDoc, CommonData, Forms,
+     UtilConvert, UtilConst, ZLibEx, zLibUtil, JuridicalTest, DB, CarModelTest,
+     InfoMoneyGroupTest, InfoMoneyDestinationTest;
+
+     { TPositionTest }
+constructor TPosition.Create;
+begin
+  inherited;
+  spInsertUpdate := 'gpInsertUpdate_Object_Position';
+  spSelect := 'gpSelect_Object_Position';
+  spGet := 'gpGet_Object_Position';
+end;
+
+function TPosition.InsertDefault: integer;
+begin
+  result := InsertUpdatePosition(0, -1, 'Должности');
+end;
+
+function TPosition.InsertUpdatePosition(const Id, Code: Integer;
+  Name: string): integer;
+begin
+  FParams.Clear;
+  FParams.AddParam('ioId', ftInteger, ptInputOutput, Id);
+  FParams.AddParam('inCode', ftInteger, ptInput, Code);
+  FParams.AddParam('inName', ftString, ptInput, Name);
+  result := InsertUpdate(FParams);
+end;
+
+  procedure TPositionTest.ProcedureLoad;
+begin
+  ScriptDirectory := ProcedurePath + 'OBJECTS\Position\';
+  inherited;
+end;
+
+procedure TPositionTest.Test;
+var Id: integer;
+    RecordCount: Integer;
+    ObjectTest: TPosition;
+begin
+  ObjectTest := TPosition.Create;
+  // Получим список
+RecordCount := ObjectTest.GetDataSet.RecordCount;
+  // Вставка группы
+  Id := ObjectTest.InsertDefault;
+  try
+    // Получение данных
+    with ObjectTest.GetRecord(Id) do
+      Check((FieldByName('Name').AsString = 'Должности'), 'Не сходятся данные Id = ' + FieldByName('id').AsString);
+  finally
+    ObjectTest.Delete(Id);
+  end;
+end;
+     initialization
+  TestFramework.RegisterTest('Объекты', TPositionTest.Suite);
+end.
---
+++ TEST/SOURCE/Objects/All/ReceiptTest.pas
@@ -0,0 +1,102 @@
+unit ReceiptTest;
+
+interface
+uses dbTest, dbObjectTest, TestFramework, ObjectTest;
+type
+  TReceiptTest = class(TdbObjectTestNew)
+  published
+    procedure ProcedureLoad; override;
+    procedure Test; override;
+  end;
+
+   TReceipt = class(TObjectTest)
+  private
+    function InsertDefault: integer; override;
+  public
+   function InsertUpdateReceipt(const Id: integer; Name, Code, Comment: string;
+                                Value, ValueCost, TaxExit, PartionValue, PartionCount, WeightPackage: Double;
+                                StartDate, EndDate: TDateTime;
+                                Main: boolean;
+                                GoodsId, GoodsKindId, GoodsKindCompleteId, ReceiptCostId, ReceiptKindId: integer): integer;
+    constructor Create; override;
+  end;
+
+implementation
+
+  uses ZDbcIntfs, SysUtils, Storage, DBClient, XMLDoc, CommonData, Forms,
+     UtilConvert, UtilConst, ZLibEx, zLibUtil, JuridicalTest, DB, CarModelTest,
+     InfoMoneyGroupTest, InfoMoneyDestinationTest, AssetGroupTest, GoodsTest,
+     GoodsKindTest, ReceiptCostTest;
+
+ {TReceiptTest}
+ constructor TReceipt.Create;
+begin
+  inherited;
+  spInsertUpdate := 'gpInsertUpdate_Object_Receipt';
+  spSelect := 'gpSelect_Object_Receipt';
+  spGet := 'gpGet_Object_Receipt';
+end;
+
+function TReceipt.InsertDefault: integer;
+var
+  GoodsId, GoodsKindId, ReceiptCostId: Integer;
+begin
+  ReceiptCostId := TReceiptCost.Create.GetDefault;
+  GoodsId:= TGoods.Create.GetDefault;
+  GoodsKindId:= TGoodsKind.Create.GetDefault;
+
+  result := InsertUpdateReceipt(0, 'Рецептура 1', '123', 'Рецептуры', 1, 2, 80, 2, 1,1 , date, date, true, GoodsId, GoodsKindId, 1, ReceiptCostId, 1);
+end;
+
+function TReceipt.InsertUpdateReceipt;
+begin
+  FParams.Clear;
+  FParams.AddParam('ioId', ftInteger, ptInputOutput, Id);
+  FParams.AddParam('Name', ftString, ptInput, Name);
+  FParams.AddParam('Code', ftString, ptInput, Code);
+  FParams.AddParam('Comment', ftString, ptInput, Comment);
+  FParams.AddParam('Value', ftFloat, ptInput, Value);
+  FParams.AddParam('ValueCost', ftFloat, ptInput, ValueCost);
+  FParams.AddParam('TaxExit', ftFloat, ptInput, TaxExit);
+  FParams.AddParam('PartionValue', ftFloat, ptInput, PartionValue);
+  FParams.AddParam('PartionCount', ftFloat, ptInput, PartionCount);
+  FParams.AddParam('WeightPackage', ftFloat, ptInput, WeightPackage);
+  FParams.AddParam('StartDate', ftDateTime, ptInput, StartDate);
+  FParams.AddParam('EndDate', ftDateTime, ptInput, EndDate);
+  FParams.AddParam('Main', ftBoolean, ptInput, Main);
+  FParams.AddParam('inGoodsId', ftInteger, ptInput, GoodsId);
+  FParams.AddParam('inGoodsKindId', ftInteger, ptInput, GoodsKindId);
+  FParams.AddParam('inGoodsKindCompleteId', ftInteger, ptInput, GoodsKindCompleteId);
+  FParams.AddParam('inReceiptCostId', ftInteger, ptInput, ReceiptCostId);
+  FParams.AddParam('inReceiptKindId', ftInteger, ptInput, ReceiptKindId);
+  result := InsertUpdate(FParams);
+end;
+
+ procedure TReceiptTest.ProcedureLoad;
+begin
+  ScriptDirectory := ProcedurePath + 'OBJECTS\Receipt\';
+  inherited;
+end;
+
+procedure TReceiptTest.Test;
+var Id: integer;
+    RecordCount: Integer;
+    ObjectTest: TReceipt;
+begin
+  ObjectTest := TReceipt.Create;
+  // Получим список
+ RecordCount := ObjectTest.GetDataSet.RecordCount;
+  // Вставка
+  Id := ObjectTest.InsertDefault;
+  try
+    // Получение данных о Рецептуре
+    with ObjectTest.GetRecord(Id) do
+      Check((FieldByName('Name').AsString = 'Рецептура 1'), 'Не сходятся данные Id = ' + FieldByName('id').AsString);
+  finally
+    ObjectTest.Delete(Id);
+  end;
+end;
+    initialization
+  TestFramework.RegisterTest('Объекты', TReceiptTest.Suite);
+
+end.
