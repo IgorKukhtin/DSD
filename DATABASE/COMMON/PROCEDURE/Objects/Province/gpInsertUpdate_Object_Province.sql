7f7492dba10e00cdf8ee981f4d93a58ea8f3c62c
innaf <innaf@ua.fm>
2014-10-20 14:32:13 +0300

    ++

--- DATABASE/COMMON/PROCEDURE/Objects/Province/gpInsertUpdate_Object_Province.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/Province/gpInsertUpdate_Object_Province.sql
@@ -25,7 +25,25 @@ BEGIN
    vbCode_calc:=lfGet_ObjectCode (inCode, zc_Object_Province()); 
    
    -- проверка прав уникальности для свойства <Наименование >
-   PERFORM lpCheckUnique_Object_ValueData(ioId, zc_Object_Province(), inName);
+   --PERFORM lpCheckUnique_Object_ValueData(ioId, zc_Object_Province(), inName);
+   -- проверка уникальность <Наименование> для !!!одноq!! области
+   IF TRIM (inName) <> '' AND COALESCE (inRegionId, 0) <> 0 
+   THEN
+       IF EXISTS (SELECT Object.Id
+                  FROM Object
+                       JOIN ObjectLink AS ObjectLink_Province_Region
+                                       ON ObjectLink_Province_Region.ObjectId = Object.Id
+                                      AND ObjectLink_Province_Region.DescId = zc_ObjectLink_Province_Region()
+                                      AND ObjectLink_Province_Region.ChildObjectId = inRegionId
+                                   
+                  WHERE TRIM (Object.ValueData) = TRIM (inName)
+                   AND Object.Id <> COALESCE (ioId, 0))
+       THEN
+           RAISE EXCEPTION 'Ошибка. Район <%> уже установлен у <%>.', TRIM (inName), lfGet_Object_ValueData (inRegionId);
+       END IF;
+   END IF;
+
+
    -- проверка прав уникальности для свойства <Код >
    PERFORM lpCheckUnique_Object_ObjectCode (ioId, zc_Object_Province(), vbCode_calc);
 
