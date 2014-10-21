2f0772e0ac8b4899b03d590eed772ec7f3bc6e77
K0sta <Ashtu777@gmail.com>
2014-10-21 16:00:24 +0300

    ok

--- Forms/Document/OrderExternalJournalChoice.pas
+++ Forms/Document/OrderExternalJournalChoice.pas
@@ -1,20 +1,11 @@
-unit OrderExternalJournal;
+unit OrderExternalJournalChoice;
 
 interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, AncestorJournal, cxGraphics, cxControls,
-  cxLookAndFeels, cxLookAndFeelPainters, cxPCdxBarPopupMenu, cxStyles,
-  cxCustomData, cxFilter, cxData, cxDataStorage, cxEdit, Data.DB, cxDBData,
-  cxImageComboBox, cxContainer, Vcl.ComCtrls, dxCore, cxDateUtils,
-  dxSkinsdxBarPainter, dsdAddOn, ChoicePeriod, Vcl.Menus, dxBarExtItems, dxBar,
-  cxClasses, dsdDB, Datasnap.DBClient, dsdAction, Vcl.ActnList,
-  cxPropertiesStore, cxLabel, cxTextEdit, cxMaskEdit, cxDropDownEdit,
-  cxCalendar, Vcl.ExtCtrls, cxGridLevel, cxGridCustomTableView, cxGridTableView,
-  cxGridDBTableView, cxGridCustomView, cxGrid, cxPC, cxCheckBox, cxCurrencyEdit,
-  cxButtonEdit, dsdGuides, frxClass, frxDBSet, dxSkinsCore,
-  dxSkinsDefaultPainters, dxSkinscxPCPainter, dxSkinBlack, dxSkinBlue,
+  cxLookAndFeels, cxLookAndFeelPainters, dxSkinsCore, dxSkinBlack, dxSkinBlue,
   dxSkinBlueprint, dxSkinCaramel, dxSkinCoffee, dxSkinDarkRoom, dxSkinDarkSide,
   dxSkinDevExpressDarkStyle, dxSkinDevExpressStyle, dxSkinFoggy,
   dxSkinGlassOceans, dxSkinHighContrast, dxSkiniMaginary, dxSkinLilian,
@@ -24,16 +15,19 @@ uses
   dxSkinOffice2010Blue, dxSkinOffice2010Silver, dxSkinPumpkin, dxSkinSeven,
   dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus, dxSkinSilver,
   dxSkinSpringTime, dxSkinStardust, dxSkinSummer2008, dxSkinTheAsphaltWorld,
-  dxSkinValentine, dxSkinVS2010, dxSkinWhiteprint, dxSkinXmas2008Blue;
+  dxSkinsDefaultPainters, dxSkinValentine, dxSkinVS2010, dxSkinWhiteprint,
+  dxSkinXmas2008Blue, dxSkinscxPCPainter, cxPCdxBarPopupMenu, cxStyles,
+  cxCustomData, cxFilter, cxData, cxDataStorage, cxEdit, Data.DB, cxDBData,
+  cxImageComboBox, cxContainer, Vcl.ComCtrls, dxCore, cxDateUtils,
+  dxSkinsdxBarPainter, dsdAddOn, ChoicePeriod, Vcl.Menus, dxBarExtItems, dxBar,
+  cxClasses, dsdDB, Datasnap.DBClient, dsdAction, Vcl.ActnList,
+  cxPropertiesStore, cxLabel, cxTextEdit, cxMaskEdit, cxDropDownEdit,
+  cxCalendar, Vcl.ExtCtrls, cxGridLevel, cxGridCustomTableView, cxGridTableView,
+  cxGridDBTableView, cxGridCustomView, cxGrid, cxPC, cxCheckBox, cxCurrencyEdit,
+  dsdGuides, cxButtonEdit;
 
 type
-  TOrderExternalJournalForm = class(TAncestorJournalForm)
-    PrintHeaderCDS: TClientDataSet;
-    PrintItemsCDS: TClientDataSet;
-    spSelectPrint: TdsdStoredProc;
-    actPrint: TdsdPrintAction;
-    bbPrint: TdxBarButton;
-    PrintItemsSverkaCDS: TClientDataSet;
+  TOrderExternalJournalChoiceForm = class(TAncestorJournalForm)
     colFromName: TcxGridDBColumn;
     colTotalCount: TcxGridDBColumn;
     colOperDatePartner: TcxGridDBColumn;
@@ -57,15 +51,23 @@ type
     InfoMoneyGroupName: TcxGridDBColumn;
     InfoMoneyDestinationName: TcxGridDBColumn;
     InfoMoneyName: TcxGridDBColumn;
+    dsdChoiceGuides: TdsdChoiceGuides;
+    bbSelect: TdxBarButton;
+    cxLabel6: TcxLabel;
+    edPartner: TcxButtonEdit;
+    PartnerGuides: TdsdGuides;
   private
     { Private declarations }
   public
     { Public declarations }
   end;
 
+var
+  OrderExternalJournalChoiceForm: TOrderExternalJournalChoiceForm;
+
 implementation
 
 {$R *.dfm}
 initialization
-  RegisterClass(TOrderExternalJournalForm);
+  RegisterClass(TOrderExternalJournalChoiceForm);
 end.
