6239b17f536d2b172d2923706334ebef0069b77b
innaf <innaf@ua.fm>
2014-11-17 18:23:00 +0200

    +

--- DATABASE/COMMON/PROCEDURE/Objects/ProvinceCity/gpSelect_Object_ProvinceCity.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/ProvinceCity/gpSelect_Object_ProvinceCity.sql
@@ -7,6 +7,7 @@ CREATE OR REPLACE FUNCTION gpSelect_Object_ProvinceCity(
 )
 RETURNS TABLE (Id Integer, Code Integer, Name TVarChar
              , CityId Integer, CityCode Integer, CityName TVarChar
+             , RegionName TVarChar, ProvinceName TVarChar, CityKindName TVarChar
              , isErased boolean) AS
 $BODY$BEGIN
 
@@ -22,6 +23,10 @@ $BODY$BEGIN
         , Object_City.Id            AS CityId
         , Object_City.ObjectCode    AS CityCode
         , Object_City.ValueData     AS CityName        
+        
+        , Object_Region.ValueData     AS RegionName 
+        , Object_Province.ValueData   AS ProvinceName  
+        , Object_CityKind.ValueData   AS CityKindName
 
         , Object_ProvinceCity.isErased             AS isErased
    FROM Object AS Object_ProvinceCity
@@ -29,6 +34,23 @@ $BODY$BEGIN
                              ON ObjectLink_ProvinceCity_City.ObjectId = Object_ProvinceCity.Id
                             AND ObjectLink_ProvinceCity_City.DescId = zc_ObjectLink_ProvinceCity_City()
         LEFT JOIN Object AS Object_City ON Object_City.Id = ObjectLink_ProvinceCity_City.ChildObjectId
+
+           LEFT JOIN ObjectLink AS ObjectLink_City_Region 
+                                ON ObjectLink_City_Region.ObjectId = Object_City.Id 
+                               AND ObjectLink_City_Region.DescId = zc_ObjectLink_City_Region()
+            LEFT JOIN Object AS Object_Region ON Object_Region.Id = ObjectLink_City_Region.ChildObjectId
+
+            LEFT JOIN ObjectLink AS ObjectLink_City_Province
+                                 ON ObjectLink_City_Province.ObjectId = Object_City.Id 
+                                AND ObjectLink_City_Province.DescId = zc_ObjectLink_City_Province()
+            LEFT JOIN Object AS Object_Province ON Object_Province.Id = ObjectLink_City_Province.ChildObjectId
+
+            LEFT JOIN ObjectLink AS ObjectLink_City_CityKind
+                                 ON ObjectLink_City_CityKind.ObjectId = Object_City.Id 
+                                AND ObjectLink_City_CityKind.DescId = zc_ObjectLink_City_CityKind()
+            LEFT JOIN Object AS Object_CityKind ON Object_CityKind.Id = ObjectLink_City_CityKind.ChildObjectId
+       
+
 
    WHERE Object_ProvinceCity.DescId = zc_Object_ProvinceCity();
 
@@ -43,6 +65,7 @@ ALTER FUNCTION gpSelect_Object_ProvinceCity(TVarChar) OWNER TO postgres;
 /*
  »—“Œ–»ﬂ –¿«–¿¡Œ“ »: ƒ¿“¿, ¿¬“Œ–
                ‘ÂÎÓÌ˛Í ».¬.    ÛıÚËÌ ».¬.    ÎËÏÂÌÚ¸Â‚  .».    Ã‡Ì¸ÍÓ ƒ.¿.
+ 17.11.14         *
  31.05.14         * 
 
 */
