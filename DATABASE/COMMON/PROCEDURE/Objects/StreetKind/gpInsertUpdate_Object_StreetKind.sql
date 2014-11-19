a40bd21dabe531439d9602b5954e0b85d1bcc62b
innaf <innaf@ua.fm>
2014-11-19 17:08:32 +0200

    +

--- DATABASE/COMMON/PROCEDURE/Objects/StreetKind/gpInsertUpdate_Object_StreetKind.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/StreetKind/gpInsertUpdate_Object_StreetKind.sql
@@ -17,8 +17,8 @@ $BODY$
    DECLARE vbCode_calc Integer;  
 BEGIN
    -- проверка прав пользователя на вызов процедуры
-   --vbUserId := lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_Object_StreetKind());
-   vbUserId := inSession;
+   vbUserId := lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_Object_StreetKind());
+   --vbUserId := inSession;
 
    -- пытаемся найти код
    IF ioId <> 0 AND COALESCE (inCode, 0) = 0 THEN inCode := (SELECT ObjectCode FROM Object WHERE Id = ioId); END IF;
