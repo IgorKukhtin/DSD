e72c3fa2dc8efa2931ab262d7e188b86d1f56746
K0sta <Ashtu777@gmail.com>
2014-11-24 16:47:13 +0200

    ok

--- DATABASE/COMMON/REPORTS/Account/External/gpReport_AccountExternal.sql
+++ DATABASE/COMMON/REPORTS/Account/External/gpReport_AccountExternal.sql
@@ -11,6 +11,7 @@ CREATE OR REPLACE FUNCTION gpReport_AccountExternal (
 RETURNS TABLE  (InvNumber Integer, OperDate TDateTime
               , CLIENTCODE Integer, CLIENTINN TVarChar, CLIENTOKPO TVarChar, CLIENTNAME TVarChar
               , SUMA TFloat, PDV TFloat, SUMAPDV TFloat
+              , OperDatePartner TDateTime, InvNumberPartner TVarChar, Comment TVarChar
               )  
 AS
 $BODY$
@@ -27,13 +28,31 @@ BEGIN
          , ObjectHistory_JuridicalDetails_ViewByDate.OKPO, Report_Account.ObjectName_Direction
          , (Report_Account.SummOut/1.2)::TFloat, (Report_Account.SummOut - Report_Account.SummOut/1.2)::TFloat, Report_Account.SummOut
 
+         , MovementDate_OperDatePartner.ValueData    AS OperDatePartner
+         , MovementString_InvNumberPartner.ValueData AS InvNumberPartner
+         , MIString_Comment.ValueData                AS Comment
+
     FROM 
       lpReport_Account(inStartDate := inStartDate, inEndDate := inEndDate
                      , inAccountGroupId := 0, inAccountDirectionId := 0, inInfoMoneyId := 0
                      , inAccountId := 9179, inBusinessId := 0, inProfitLossGroupId := 0, inProfitLossDirectionId := 0
-                     , inProfitLossId := 0, inBranchId := 0,  inUserId := vbUserId, inIsMovement := true) AS Report_Account
+                     , inProfitLossId := 0, inBranchId := 0,  inUserId := vbUserId, inIsMovement := TRUE) AS Report_Account
              JOIN ObjectHistory_JuridicalDetails_ViewByDate ON ObjectHistory_JuridicalDetails_ViewByDate.JuridicalId = Report_Account.ObjectId_Direction
               AND ObjectHistory_JuridicalDetails_ViewByDate.EndDate = zc_DateEnd()
+
+            LEFT JOIN MovementItem ON MovementItem.MovementId = Report_Account.MovementId
+
+            LEFT JOIN MovementItemString AS MIString_Comment 
+                                         ON MIString_Comment.MovementItemId = MovementItem.Id
+                                        AND MIString_Comment.DescId = zc_MIString_Comment()
+
+            LEFT JOIN MovementDate AS MovementDate_OperDatePartner
+                                   ON MovementDate_OperDatePartner.MovementId = Report_Account.MovementId
+                                  AND MovementDate_OperDatePartner.DescId = zc_MovementDate_OperDatePartner()
+
+            LEFT JOIN MovementString AS MovementString_InvNumberPartner
+                                     ON MovementString_InvNumberPartner.MovementId =  Report_Account.MovementId
+                                    AND MovementString_InvNumberPartner.DescId = zc_MovementString_InvNumberPartner()
 
     WHERE COALESCE(Report_Account.SummOut, 0) <> 0;
 
@@ -42,7 +61,6 @@ $BODY$
   LANGUAGE plpgsql VOLATILE;
 ALTER FUNCTION gpReport_AccountExternal (TDateTime, TDateTime, Integer, TVarChar) OWNER TO postgres;
 
-
 /*-------------------------------------------------------------------------------
  »—“Œ–»ﬂ –¿«–¿¡Œ“ »: ƒ¿“¿, ¿¬“Œ–
                ‘ÂÎÓÌ˛Í ».¬.    ÛıÚËÌ ».¬.    ÎËÏÂÌÚ¸Â‚  .».   Ã‡Ì¸ÍÓ ƒ.
@@ -50,4 +68,4 @@ ALTER FUNCTION gpReport_AccountExternal (TDateTime, TDateTime, Integer, TVarChar
 */
 
 -- ÚÂÒÚ
--- SELECT * FROM gpReport_Account (inStartDate:= '01.12.2013', inEndDate:= '31.12.2013', inAccountGroupId:= 0, inAccountDirectionId:= 0, inInfoMoneyId:= 0, inAccountId:= 0, inBusinessId:= 0, inProfitLossGroupId:= 0,  inProfitLossDirectionId:= 0,  inProfitLossId:= 0,  inBranchId:= 0, inSession:= zfCalc_UserAdmin());
+-- SELECT * FROM gpReport_AccountExternal (inStartDate:= '01.09.2014', inEndDate:= '30.09.2014', inAccountId:= 0, inSession:= zfCalc_UserAdmin());
