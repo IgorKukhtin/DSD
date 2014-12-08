2288b17867d27973acd174f503021106c0e1cd70
innaf <innaf@ua.fm>
2014-12-08 23:55:13 +0200

    +

---
+++ DATABASE/COMMON/PROCEDURE/Objects/GoodsQuality/gpInsertUpdate_Object_GoodsQuality.sql
@@ -0,0 +1,63 @@
+-- Function: gpInsertUpdate_Object_Cash()
+
+DROP FUNCTION IF EXISTS gpInsertUpdate_Object_Cash (Integer, Integer, TVarChar, Integer, Integer, Integer, Integer, TVarChar);
+DROP FUNCTION IF EXISTS gpInsertUpdate_Object_Cash (Integer, Integer, TVarChar, Integer, Integer, Integer, Integer, Integer, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpInsertUpdate_Object_Cash(
+ INOUT ioId	          Integer   ,    -- ключ объекта <Касса> 
+    IN inCode             Integer   ,    -- код объекта <Касса> 
+    IN inCashName         TVarChar  ,    -- Название объекта <Касса> 
+    IN inCurrencyId       Integer   ,    -- Валюта данной кассы 
+    IN inBranchId         Integer   ,    -- Какому филиалу принадлежит касса 
+    IN inJuridicalBasisId Integer   ,    -- Главное юр Лицо
+    IN inBusinessId       Integer   ,    -- Бизнес
+    IN inPaidKindId       Integer   ,    -- Форма оплаты 
+    IN inSession          TVarChar       -- сессия пользователя
+)
+  RETURNS integer AS
+$BODY$
+   DECLARE vbUserId Integer;
+ BEGIN
+   -- проверка прав пользователя на вызов процедуры
+   -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_Select_Object_Cash());
+   vbUserId:= lpGetUserBySession (inSession);
+
+   -- Если код не установлен, определяем его каи последний+1
+   inCode := lfGet_ObjectCode (inCode, zc_Object_Cash());
+    
+   -- проверка прав уникальности для свойства <Наименование Касса>  
+   PERFORM lpCheckUnique_Object_ValueData(ioId, zc_Object_Cash(), inCashName);
+   -- проверка прав уникальности для свойства <Код Кассы>
+   PERFORM lpCheckUnique_Object_ObjectCode (ioId, zc_Object_Cash(), inCode);
+
+   -- сохранили <Объект>
+   ioId := lpInsertUpdate_Object (ioId, zc_Object_Cash(), inCode, inCashName
+                                , inAccessKeyId:= CASE WHEN COALESCE (inBranchId, 0) = 0 THEN zc_Enum_Process_AccessKey_CashDnepr() ELSE (SELECT Object_Branch.AccessKeyId FROM Object AS Object_Branch WHERE Object_Branch.Id = inBranchId) END);
+
+   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Cash_Currency(), ioId, inCurrencyId);
+   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Cash_Branch(), ioId, inBranchId);
+   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Cash_JuridicalBasis(), ioId, inJuridicalBasisId);
+   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Cash_Business(), ioId, inBusinessId);
+   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Cash_PaidKind(), ioId, inPaidKindId);
+
+   -- сохранили протокол
+   PERFORM lpInsert_ObjectProtocol (ioId, vbUserId);
+   
+END;$BODY$
+  LANGUAGE plpgsql VOLATILE;
+ALTER FUNCTION gpInsertUpdate_Object_Cash (Integer, Integer, TVarChar, Integer, Integer, Integer, Integer, Integer, TVarChar) OWNER TO postgres;
+  
+ /*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 25.11.14         * add PaidKind                 
+ 28.12.13                                        * rename to zc_ObjectLink_Cash_JuridicalBasis
+ 26.12.13                                        * add inAccessKeyId
+ 24.11.13                                        * err lfGet_ObjectCode (zc_Object_Cash(), inCode)
+ 24.11.13                                        * Cyr1251
+ 10.06.13          *
+*/
+
+-- тест
+-- SELECT * FROM gpInsertUpdate_Object_Cash()
---
+++ DATABASE/COMMON/PROCEDURE/Objects/GoodsQuality/gpSelect_Object_GoodsQuality.sql
@@ -0,0 +1,83 @@
+-- Function: gpSelect_Object_Cash()
+
+DROP FUNCTION IF EXISTS gpSelect_Object_Cash(TVarChar);
+
+CREATE OR REPLACE FUNCTION gpSelect_Object_Cash(
+    IN inSession     TVarChar        -- сессия пользователя
+)
+RETURNS TABLE (Id Integer, Code Integer, Name TVarChar, isErased boolean, 
+               CurrencyName TVarChar, BranchName TVarChar,
+               JuridicalName TVarChar, 
+               BusinessName TVarChar, 
+               PaidKindName TVarChar)
+AS
+$BODY$
+   DECLARE vbUserId Integer;
+   DECLARE vbAccessKeyAll Boolean;
+BEGIN
+   -- проверка прав пользователя на вызов процедуры
+   -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_Select_Object_Cash());
+   vbUserId:= lpGetUserBySession (inSession);
+   -- определяется - может ли пользовать видеть весь справочник
+   vbAccessKeyAll:= zfCalc_AccessKey_GuideAll (vbUserId);
+
+   RETURN QUERY 
+   SELECT 
+     Object.Id          AS Id 
+   , Object.ObjectCode  AS Code
+   , Object.ValueData   AS Name
+   , Object.isErased    AS isErased
+   , Currency.ValueData AS CurrencyName
+   , Branch.ValueData   AS BranchName
+   , JuridicalBasis.ValueData  AS JuridicalName
+   , Business.ValueData AS BusinessName
+   , PaidKind.ValueData AS PaidKindName   
+
+   FROM Object
+        LEFT JOIN (SELECT AccessKeyId FROM Object_RoleAccessKey_View WHERE UserId = vbUserId GROUP BY AccessKeyId) AS tmpRoleAccessKey ON NOT vbAccessKeyAll AND tmpRoleAccessKey.AccessKeyId = Object.AccessKeyId
+        LEFT JOIN ObjectLink 
+               ON ObjectLink.ObjectId = Object.Id
+              AND ObjectLink.DescId = zc_ObjectLink_Cash_Currency()
+        LEFT JOIN Object AS Currency
+                         ON Currency.Id = ObjectLink.ChildObjectId
+        LEFT JOIN ObjectLink AS Cash_Branch
+                             ON Cash_Branch.ObjectId = Object.Id
+                            AND Cash_Branch.DescId = zc_ObjectLink_Cash_Branch()
+        LEFT JOIN Object AS Branch ON Branch.Id = Cash_Branch.ChildObjectId
+          
+        LEFT JOIN ObjectLink AS Cash_JuridicalBasis
+                             ON Cash_JuridicalBasis.ObjectId = Object.Id
+                            AND Cash_JuridicalBasis.DescId = zc_ObjectLink_Cash_JuridicalBasis()
+        LEFT JOIN Object AS JuridicalBasis ON JuridicalBasis.Id = Cash_JuridicalBasis.ChildObjectId
+           
+        LEFT JOIN ObjectLink AS Cash_Business
+                             ON Cash_Business.ObjectId = Object.Id
+                            AND Cash_Business.DescId = zc_ObjectLink_Cash_Business()
+        LEFT JOIN Object AS Business ON Business.Id = Cash_Business.ChildObjectId
+           
+        LEFT JOIN ObjectLink AS Cash_PaidKind
+                             ON Cash_PaidKind.ObjectId = Object.Id
+                            AND Cash_PaidKind.DescId = zc_ObjectLink_Cash_PaidKind()
+        LEFT JOIN Object AS PaidKind ON PaidKind.Id = Cash_PaidKind.ChildObjectId
+                      
+   WHERE Object.DescId = zc_Object_Cash()
+     AND (tmpRoleAccessKey.AccessKeyId IS NOT NULL OR vbAccessKeyAll)
+  ;
+  
+END;$BODY$
+  LANGUAGE plpgsql VOLATILE;
+ALTER FUNCTION gpSelect_Object_Cash (TVarChar) OWNER TO postgres;
+
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 25.11.14         * add PaidKind               
+ 28.12.13                                        * rename to zc_ObjectLink_Cash_JuridicalBasis
+ 24.12.13                                        * Cyr1251
+ 10.05.13          *
+*/
+
+-- тест
+-- SELECT * FROM gpSelect_Object_Cash (zfCalc_UserAdmin())
