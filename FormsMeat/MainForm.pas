270752fd5913ab8ca29cf3273d454e21e3a98534
DmytroManko <dnepro.ua@gmail.com>
2014-12-09 21:47:15 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	FormsMeat/MainForm.dfm
    	FormsMeat/MainForm.pas
    	TEST/DPR/ProjectTest.dpr
    	TEST/DPR/ProjectTest.dproj

--- FormsMeat/MainForm.pas
+++ FormsMeat/MainForm.pas
@@ -444,8 +444,10 @@ type
     bbReport_GoodsMI_ProductionUnion: TdxBarButton;
     actReport_GoodsMI_ProductionUnionMD: TdsdOpenForm;
     bbReport_GoodsMI_ProductionUnionMD: TdxBarButton;
+    actGoodsQuality: TdsdOpenForm;
+    bbGoodsQuality: TdxBarButton;
     actProductionUnionTech: TdsdOpenForm;
-    bbProductionUnionTech: TdxBarButton;
+    bbProductionUnionTech: TdxBarButton;    
     procedure actReport_OLAPSoldExecute(Sender: TObject);
   public
     { Public declarations }
