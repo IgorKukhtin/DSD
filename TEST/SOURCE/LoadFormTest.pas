c855e37fa0f9d6f10705334701a82f4ba78c4395
innaf <innaf@ua.fm>
2015-01-12 20:37:24 +0200

    +

--- TEST/SOURCE/LoadFormTest.pas
+++ TEST/SOURCE/LoadFormTest.pas
@@ -291,6 +291,11 @@ begin
   TdsdFormStorageFactory.GetStorage.Load('TContractTagForm');
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TContractTagEditForm'));
   TdsdFormStorageFactory.GetStorage.Load('TContractTagEditForm');
+  // Группы признаков договоров
+  TdsdFormStorageFactory.GetStorage.Save(GetForm('TContractTagGroupForm'));
+  TdsdFormStorageFactory.GetStorage.Load('TContractTagGroupForm');
+  TdsdFormStorageFactory.GetStorage.Save(GetForm('TContractTagGroupEditForm'));
+  TdsdFormStorageFactory.GetStorage.Load('TContractTagGroupEditForm');
 end;
 
 procedure TLoadFormTest.LoadContractKindFormTest;
