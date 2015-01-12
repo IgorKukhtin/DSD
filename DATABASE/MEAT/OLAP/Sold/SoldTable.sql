81666d53658a44af2e8e2f7818ff659d8ef8e8cc
K0sta <Ashtu777@gmail.com>
2015-01-12 22:18:41 +0200

    ok

--- DATABASE/MEAT/OLAP/Sold/SoldTable.sql
+++ DATABASE/MEAT/OLAP/Sold/SoldTable.sql
@@ -5,27 +5,38 @@
     - ËÌ‰ÂÍÒÓ‚
 */
 
-
 -- DROP TABLE SoldTable;
 
 /*-------------------------------------------------------------------------------*/
 CREATE TABLE SoldTable
 (
+   OperDate            TDateTime
+ , InvNumber           TVarChar
 
-   JuridicalId Integer
- , PartnerId Integer
- , ContractId Integer 
- , InfoMoneyId Integer
- , PaidKindId Integer
- , BranchId Integer
+ , JuridicalId        Integer
+ , PartnerId          Integer
+ , InfoMoneyId        Integer
+ , PaidKindId         Integer
+ , BranchId           Integer
+ , RetailId           Integer
+ , AreaId             Integer
+ , PartnerTagId       Integer
+ , ContractId         Integer 
+ , ContractTagId      Integer 
+ , ContractTagGroupId Integer 
 
- , GoodsId Integer
- , GoodsKindId Integer
- , OperDate TDateTime
- , InvNumber TVarChar
+ , PersonalId             Integer
+ , PersonalTradeId        Integer
+ , BranchId_Personal      Integer
 
- , AreaId                Integer
- , PartnerTagId          Integer
+ , TradeMarkId         Integer
+ , GoodsGroupAnalystId Integer
+ , GoodsTagId          Integer
+ , GoodsGroupId        Integer
+ , GoodsId             Integer
+ , GoodsKindId         Integer
+ , MeasureId           Integer
+ 
  , RegionId              Integer
  , ProvinceId            Integer
  , CityKindId            Integer
@@ -34,36 +45,48 @@ CREATE TABLE SoldTable
  , StreetKindId          Integer
  , StreetId              Integer
                            
- , Sale_Summ             TFloat  
- , Sale_SummCost         TFloat
- , Sale_Profit           TFloat
- , Sale_Amount_Weight    TFloat  
- , Sale_Amount_Sh        TFloat  
+ , Sale_Summ                 TFloat
+ , Sale_Summ_10300           TFloat
+ , Sale_SummCost             TFloat
+ , Sale_SummCost_10500       TFloat
+ , Sale_SummCost_40200       TFloat
+ , Sale_Amount_Weight        TFloat
+ , Sale_Amount_Sh            TFloat
+ , Sale_AmountPartner_Weight TFloat  
+ , Sale_AmountPartner_Sh     TFloat  
+ , Sale_Amount_10500_Weight  TFloat
+ , Sale_Amount_40200_Weight  TFloat
 
- , Return_Summ           TFloat  
- , Return_SummCost       TFloat
- , Return_Amount_Weight  TFloat  
- , Return_Amount_Sh      TFloat  
+ , Actions_Weight            TFloat
+ , Actions_Summ              TFloat
 
- , SaleReturn_Summ       TFloat
- , SaleReturn_SummCost   TFloat
- , SaleReturn_Profit     TFloat
- , SaleReturn_Amount_Weight TFloat
- , SaleReturn_Amount_Sh  TFloat
+ , Return_Summ                 TFloat
+ , Return_Summ_10300           TFloat
+ , Return_SummCost             TFloat
+ , Return_SummCost_40200       TFloat
+ , Return_Amount_Weight        TFloat
+ , Return_Amount_Sh            TFloat
+ , Return_AmountPartner_Weight TFloat  
+ , Return_AmountPartner_Sh     TFloat  
+ , Return_Amount_40200_Weight  TFloat
+
+ , SaleReturn_Summ           TFloat
+ , SaleReturn_SummCost       TFloat
+ , SaleReturn_Amount_Weight  TFloat
+ , SaleReturn_Amount_Sh      TFloat
 
  , Bonus                 TFloat
- , SaleBonus             TFloat
- , SaleBonusProfit       TFloat
  , Plan_Weight           TFloat
  , Plan_Summ             TFloat
  
- , Actions_Weight        TFloat
- , Actions_Summ          TFloat
+ , Money_Summ             TFloat
+ , SendDebt_Summ          TFloat
+ , Money_SendDebt_Summ    TFloat
 
- , Sale_AmountPartner_Weight     TFloat  
- , Sale_AmountPartner_Sh         TFloat  
- , Return_AmountPartner_Weight   TFloat  
- , Return_AmountPartner_Sh       TFloat  
+ , SaleBonus             TFloat
+ , Sale_Profit           TFloat
+ , SaleReturn_Profit     TFloat
+ , SaleBonusProfit       TFloat
 )
 WITH (
   OIDS=FALSE
@@ -75,32 +98,42 @@ GRANT ALL ON ALL TABLES IN SCHEMA public TO project;
 
 /*                                  »Ì‰ÂÍÒ˚                                      */
 
-CREATE INDEX idx_SoldTable_OperDate ON SoldTable(OperDate);
-CREATE INDEX idx_SoldTable_JuridicalId ON SoldTable(JuridicalId);
-CREATE INDEX idx_SoldTable_PartnerId ON SoldTable(PartnerId);
-CREATE INDEX idx_SoldTable_ContractId ON SoldTable(ContractId);
-CREATE INDEX idx_SoldTable_InfoMoneyId ON SoldTable(InfoMoneyId);
-CREATE INDEX idx_SoldTable_BranchId ON SoldTable(BranchId);
-CREATE INDEX idx_SoldTable_GoodsId ON SoldTable(GoodsId);
-CREATE INDEX idx_SoldTable_GoodsKindId ON SoldTable(GoodsKindId);
-CREATE INDEX idx_SoldTable_AreaId ON SoldTable(AreaId);
-CREATE INDEX idx_SoldTable_PartnerTagId ON SoldTable(PartnerTagId);
-CREATE INDEX idx_SoldTable_RegionId ON SoldTable(RegionId);
-CREATE INDEX idx_SoldTable_ProvinceId ON SoldTable(ProvinceId);
-CREATE INDEX idx_SoldTable_CityKindId ON SoldTable(CityKindId);
-CREATE INDEX idx_SoldTable_CityId ON SoldTable(CityId);
-CREATE INDEX idx_SoldTable_ProvinceCityId ON SoldTable(ProvinceCityId);
-CREATE INDEX idx_SoldTable_StreetKindId ON SoldTable(StreetKindId);
-CREATE INDEX idx_SoldTable_StreetId ON SoldTable(StreetId);
+CREATE INDEX idx_SoldTable_OperDate ON SoldTable (OperDate);
+CREATE INDEX idx_SoldTable_JuridicalId ON SoldTable (JuridicalId);
+CREATE INDEX idx_SoldTable_PartnerId ON SoldTable (PartnerId);
+CREATE INDEX idx_SoldTable_InfoMoneyId ON SoldTable (InfoMoneyId);
+CREATE INDEX idx_SoldTable_PaidKindId ON SoldTable (PaidKindId);
+CREATE INDEX idx_SoldTable_BranchId ON SoldTable (BranchId);
+CREATE INDEX idx_SoldTable_RetailId ON SoldTable (RetailId);
+CREATE INDEX idx_SoldTable_AreaId ON SoldTable (AreaId);
+CREATE INDEX idx_SoldTable_PartnerTagId ON SoldTable (PartnerTagId);
+CREATE INDEX idx_SoldTable_ContractId ON SoldTable (ContractId);
+CREATE INDEX idx_SoldTable_ContractTagId ON SoldTable (ContractTagId);
+CREATE INDEX idx_SoldTable_ContractTagGroupId ON SoldTable (ContractTagGroupId);
 
-/*-------------------------------------------------------------------------------*/
+CREATE INDEX idx_SoldTable_PersonalId ON SoldTable (PersonalId);
+CREATE INDEX idx_SoldTable_PersonalTradeId ON SoldTable (PersonalTradeId);
+CREATE INDEX idx_SoldTable_BranchId_Personal ON SoldTable (BranchId_Personal);
+
+CREATE INDEX idx_SoldTable_TradeMarkId ON SoldTable (TradeMarkId);
+CREATE INDEX idx_SoldTable_GoodsGroupAnalystId ON SoldTable (GoodsGroupAnalystId);
+CREATE INDEX idx_SoldTable_GoodsTagId ON SoldTable (GoodsTagId);
+CREATE INDEX idx_SoldTable_GoodsGroupId ON SoldTable (GoodsGroupId);
+CREATE INDEX idx_SoldTable_GoodsId ON SoldTable (GoodsId);
+CREATE INDEX idx_SoldTable_GoodsKindId ON SoldTable (GoodsKindId);
+CREATE INDEX idx_SoldTable_MeasureId ON SoldTable (MeasureId);
+
+CREATE INDEX idx_SoldTable_RegionId ON SoldTable (RegionId);
+CREATE INDEX idx_SoldTable_ProvinceId ON SoldTable (ProvinceId);
+CREATE INDEX idx_SoldTable_CityKindId ON SoldTable (CityKindId);
+CREATE INDEX idx_SoldTable_CityId ON SoldTable (CityId);
+CREATE INDEX idx_SoldTable_ProvinceCityId ON SoldTable (ProvinceCityId);
+CREATE INDEX idx_SoldTable_StreetKindId ON SoldTable (StreetKindId);
+CREATE INDEX idx_SoldTable_StreetId ON SoldTable (StreetId);
 
 
-
-/*
- œ–»Ã≈◊¿Õ»ﬂ:
- »—“Œ–»ﬂ –¿«–¿¡Œ“ »:
- ƒ¿“¿         ¿¬“Œ–
- ----------------
-                  ÎËÏÂÌÚ¸Â‚  .».    ÛıÚËÌ ».¬.   
+/*-------------------------------------------------------------------------------
+ »—“Œ–»ﬂ –¿«–¿¡Œ“ »: ƒ¿“¿, ¿¬“Œ–
+               ‘ÂÎÓÌ˛Í ».¬.    ÛıÚËÌ ».¬.    ÎËÏÂÌÚ¸Â‚  .».   Ã‡Ì¸ÍÓ ƒ.¿.
+ 12.01.15                                        * all
 */
