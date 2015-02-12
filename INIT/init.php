d28627dd107dc2744f7bbbfd2fd91da9fc231a91
K0sta <Ashtu777@gmail.com>
2015-02-12 18:09:59 +0200

    ok

--- INIT/init.php
+++ INIT/init.php
@@ -1,5 +1,5 @@
 <?php
   $connectstring = "port=5432 dbname=project host=localhost user=postgres password=postgres";
-  $host = "http://localhost/alan/";
+  $host = "http://localhost/dsd/";
   $isArchive = true;
 ?>
