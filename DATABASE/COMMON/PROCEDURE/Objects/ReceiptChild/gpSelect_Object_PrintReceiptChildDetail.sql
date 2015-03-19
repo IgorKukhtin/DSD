c634eae1615b894d0c23b2dbbce5f8325f22a851
Kukhtin Igor <kukhtinigor@hotmail.com>
2015-03-19 14:54:02 +0200

    ok

--- DATABASE/COMMON/PROCEDURE/Objects/ReceiptChild/gpSelect_Object_PrintReceiptChildDetail.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/ReceiptChild/gpSelect_Object_PrintReceiptChildDetail.sql
@@ -4,20 +4,69 @@ DROP FUNCTION IF EXISTS gpSelect_Object_PrintReceiptChildDetail (Integer, TVarCh
 
 CREATE OR REPLACE FUNCTION gpSelect_Object_PrintReceiptChildDetail(IN ReceiptId integer, IN inSession TVarChar)
 
-RETURNS TABLE (MainReceiptId Integer, GoodsCode Integer, GoodsName TVarChar, Value TFloat, ReceiptChildId Integer) AS
+RETURNS TABLE (MainReceiptId Integer, GoodsCode Integer, GoodsName TVarChar, Value TFloat, 
+               isWeightMain Boolean, isTaxExit Boolean,
+               MeasureName TVarChar, InfoMoneyName TVarChar,
+               GroupNumber Integer) AS
 
 $BODY$
 BEGIN
 
      -- проверка прав пользователя на вызов процедуры
      -- PERFORM lpCheckRight(inSession, zc_Enum_Process_Select_Object_ReceiptChild());
-     CREATE TEMP TABLE tmpReceiptTable(id Integer);
+     CREATE TEMP TABLE tmpReceiptTable(id Integer) ON COMMIT DROP;
      INSERT INTO tmpReceiptTable (Id) VALUES(ReceiptId);
 
      RETURN QUERY 
-      SELECT D.MainReceiptId, Object_Goods_View.GoodsCode, Object_Goods_View.GoodsName, D.Value, D.ReceiptChildId
+      SELECT DD.MainReceiptId
+           , DD.GoodsCode
+           , DD.GoodsName
+           , SUM(DD.Value)::TFloat AS Value
+           , DD.isWeightMain
+           , DD.isTaxExit                      
+           , DD.MeasureName
+           , DD.InfoMoneyName
+           , DD.GroupNumber 
+      FROM       
+      (SELECT           D.MainReceiptId
+                      , Object_Goods.GoodsCode
+                      , Object_Goods.GoodsName
+                      , D.Value
+                      , ObjectBoolean_WeightMain.ValueData AS isWeightMain
+                      , ObjectBoolean_TaxExit.ValueData    AS isTaxExit                      
+                      , Object_Measure.ValueData     AS MeasureName
+                      , Object_InfoMoney_View.InfoMoneyName AS InfoMoneyName
+                      , zfCalc_ReceiptChild_GroupNumber (inGoodsId   := D.GoodsId
+                                          , inGoodsKindId            := D.GoodsKindId
+                                          , inInfoMoneyDestinationId := Object_InfoMoney_View.InfoMoneyDestinationId
+                                          , inInfoMoneyId            := Object_InfoMoney_View.InfoMoneyId
+                                          , inWeightMain             := ObjectBoolean_WeightMain.ValueData
+                                           ) AS GroupNumber
        FROM lpSelect_Object_ReceiptChildDetail(0) AS D 
-         LEFT JOIN Object_Goods_View ON Object_Goods_View.GoodsId = D.GoodsId;
+          LEFT JOIN Object_Goods_View AS Object_Goods ON Object_Goods.GoodsId = D.GoodsId
+          LEFT JOIN ObjectLink AS ObjectLink_Goods_InfoMoney
+                               ON ObjectLink_Goods_InfoMoney.ObjectId = Object_Goods.GoodsId
+                              AND ObjectLink_Goods_InfoMoney.DescId = zc_ObjectLink_Goods_InfoMoney()
+          LEFT JOIN Object_InfoMoney_View ON Object_InfoMoney_View.InfoMoneyId = ObjectLink_Goods_InfoMoney.ChildObjectId
+          LEFT JOIN ObjectBoolean AS ObjectBoolean_WeightMain
+                                  ON ObjectBoolean_WeightMain.ObjectId = D.ReceiptChildId 
+                                 AND ObjectBoolean_WeightMain.DescId = zc_ObjectBoolean_ReceiptChild_WeightMain()
+          LEFT JOIN ObjectBoolean AS ObjectBoolean_TaxExit
+                                  ON ObjectBoolean_TaxExit.ObjectId = D.ReceiptChildId 
+                                 AND ObjectBoolean_TaxExit.DescId = zc_ObjectBoolean_ReceiptChild_TaxExit()
+          LEFT JOIN ObjectLink AS ObjectLink_Goods_Measure
+                               ON ObjectLink_Goods_Measure.ObjectId = Object_Goods.GoodsId 
+                              AND ObjectLink_Goods_Measure.DescId = zc_ObjectLink_Goods_Measure()
+          LEFT JOIN Object AS Object_Measure ON Object_Measure.Id = ObjectLink_Goods_Measure.ChildObjectId) AS DD
+          
+      GROUP BY DD.MainReceiptId
+           , DD.GoodsCode
+           , DD.GoodsName
+           , DD.isWeightMain
+           , DD.isTaxExit                      
+           , DD.MeasureName
+           , DD.InfoMoneyName
+           , DD.GroupNumber;
 
 
   
