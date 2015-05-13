dd3a11479c00b214d6571994b349015e6f5299cd
innaf <innaf@ua.fm>
2015-05-13 18:03:48 +0300

    +

--- TEST/SOURCE/LoadFormTest.pas
+++ TEST/SOURCE/LoadFormTest.pas
@@ -1054,6 +1054,11 @@ begin
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TReport_MotionGoodsDialogForm'));
   TdsdFormStorageFactory.GetStorage.Load('TReport_MotionGoodsDialogForm');
 
+  TdsdFormStorageFactory.GetStorage.Save(GetForm('TReport_MotionGoodsUpakForm'));
+  TdsdFormStorageFactory.GetStorage.Load('TReport_MotionGoodsUpakForm');
+  TdsdFormStorageFactory.GetStorage.Save(GetForm('TReport_MotionGoodsUpakDialogForm'));
+  TdsdFormStorageFactory.GetStorage.Load('TReport_MotionGoodsUpakDialogForm');
+
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TReport_GoodsBalanceForm'));
   TdsdFormStorageFactory.GetStorage.Load('TReport_GoodsBalanceForm');
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TReport_GoodsBalanceDialogForm'));
