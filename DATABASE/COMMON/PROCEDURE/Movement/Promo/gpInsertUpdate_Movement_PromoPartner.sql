cb341c9c8e69a5ecc5ccea75433c8313b228610b
POLAK <voanatol@mail.ru>
2015-11-17 16:51:35 +0200

    Акции

--- DATABASE/COMMON/PROCEDURE/Movement/Promo/gpInsertUpdate_Movement_PromoPartner.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/Promo/gpInsertUpdate_Movement_PromoPartner.sql
@@ -7,17 +7,27 @@ DROP FUNCTION IF EXISTS gpInsertUpdate_Movement_PromoPartner (
     TVarChar     -- сессия пользователя
 
 );
+DROP FUNCTION IF EXISTS gpInsertUpdate_Movement_PromoPartner (
+    Integer    , -- Ключ объекта <партнер для документа акции>
+    Integer    , -- Ключ родительского объекта <Документ акции>
+    Integer    , -- партнер
+    Integer    , -- Контракт
+    TVarChar     -- сессия пользователя
+
+);
 
 CREATE OR REPLACE FUNCTION gpInsertUpdate_Movement_PromoPartner(
  INOUT ioId                    Integer    , -- Ключ объекта <партнер для документа акции>
     IN inParentId              Integer    , -- Ключ родительского объекта <Документ акции>
     IN inPartnerId             Integer    , -- Ключ объекта <Контрагент / Юр лицо / Торговая Сеть>
+    IN inContractId            Integer    , -- Ключ объекта <Контракт>
     IN inSession               TVarChar    -- сессия пользователя
 )
 RETURNS Integer AS
 $BODY$
    DECLARE vbUserId Integer;
    DECLARE vbIsInsert Boolean;
+   DECLARE vbPartnerDescId Integer;
 BEGIN
     -- проверка прав пользователя на вызов процедуры
     --vbUserId:= lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_Movement_Promo());
@@ -44,8 +54,46 @@ BEGIN
     WHERE
         Movement_Promo.Id = inParentId;
     
+    --проверить соответствие контракта клиенту
+    IF COALESCE(inContractId,0) <> 0
+    THEN
+        vbPartnerDescId = (Select DescId from Object Where Id = inPartnerId);
+        IF vbPartnerDescId = zc_Object_Juridical()
+        THEN
+            IF NOT EXISTS(Select 1 from Object_Contract_View 
+                          Where Object_Contract_View.ContractId = inContractId 
+                            AND Object_Contract_View.JuridicalId = inPartnerId)
+            THEN
+                RAISE EXCEPTION 'Ошибка. Несоответствие контракта и партнера.';
+            END IF;
+        END IF;
+        IF vbPartnerDescId = zc_Object_Partner()
+        THEN
+            IF NOT EXISTS(Select 1 from Object_Contract_View
+                              Inner Join ObjectLink ON ObjectLink.ChildObjectId = Object_Contract_View.JuridicalId
+                                                   AND ObjectLink.DescId = zc_ObjectLink_Partner_Juridical()
+                          Where Object_Contract_View.ContractId = inContractId 
+                            AND ObjectLink.ObjectId = inPartnerId)
+            THEN
+                RAISE EXCEPTION 'Ошибка. Несоответствие контракта и партнера.';
+            END IF;
+        END IF;
+        IF vbPartnerDescId = zc_Object_Retail()
+        THEN
+            IF NOT EXISTS(Select 1 from Object_Contract_View
+                              Inner Join ObjectLink ON ObjectLink.ObjectId = Object_Contract_View.JuridicalId
+                                                   AND ObjectLink.DescId = zc_ObjectLink_Juridical_Retail()
+                          Where Object_Contract_View.ContractId = inContractId 
+                            AND ObjectLink.ChildObjectId = inPartnerId)
+            THEN
+                RAISE EXCEPTION 'Ошибка. Несоответствие контракта и партнера.';
+            END IF;
+        END IF;
+    END IF;
     -- сохранили связь с <Контрагент / Юр лицо / Торговая Сеть>
     PERFORM lpInsertUpdate_MovementLinkObject (zc_MovementLinkObject_Partner(), ioId, inPartnerId);
+    -- сохранили связь с <Контракт>
+    PERFORM lpInsertUpdate_MovementLinkObject (zc_MovementLinkObject_Contract(), ioId, inContractId);
     
     -- сохранили протокол
     PERFORM lpInsert_MovementProtocol (ioId, vbUserId, vbIsInsert);
@@ -57,5 +105,6 @@ $BODY$
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.  Воробкало А.А.
+ 17.11.15                                                                    *inContractId
  31.10.15                                                                    *
 */
--- DATABASE/COMMON/PROCEDURE/Movement/Promo/gpSelect_Movement_Promo_Print.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/Promo/gpSelect_Movement_Promo_Print.sql
@@ -14,18 +14,46 @@ AS
 $BODY$
     DECLARE vbUserId Integer;
     DECLARE vbValue TVarChar; 
-    
+    DECLARE vbCountGoods Integer;
+    DECLARE vbVAT TFloat;
+    DECLARE vbPriceList Integer;
 BEGIN
     -- проверка прав пользователя на вызов процедуры
     -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_Select_Movement_Promo());
     vbUserId:= inSession;
 
+    SELECT
+        COUNT(*)
+    INTO
+        vbCountGoods
+    FROM
+        MovementItem_PromoGoods_View AS MI_PromoGoods
+    WHERE MI_PromoGoods.MovementId = inMovementId
+      AND MI_PromoGoods.IsErased = FALSE;
+    
+    --узнали прайслист
+    SELECT
+        COALESCE(Movement_Promo.PriceListId,zc_PriceList_Basis())
+    INTO
+        vbPriceList
+    FROM
+        Movement_Promo_View AS Movement_Promo
+    WHERE
+        Movement_Promo.Id = inMovementId;
+    --вытащили значение "с НДС" и "значение НДС"
+    SELECT
+       PriceList.VATPercent
+    INTO
+       vbVAT
+    FROM
+        gpGet_Object_PriceList(vbPriceList,inSession) as PriceList;
+    
     RETURN QUERY
         SELECT
             1 as LineNo,
             ''::TVarChar as GroupName,
             'Сеть (Наименование сети, регионы на которые распространяется акция)'::TVarChar as LineName,
-            (SELECT STRING_AGG(Movement_PromoPartner.PartnerName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG(Movement_PromoPartner.PartnerName, chr(13)) 
              FROM Movement_PromoPartner_View AS Movement_PromoPartner 
              WHERE Movement_PromoPartner.ParentId = inMovementId
                AND Movement_PromoPartner.IsErased = FALSE)::TEXT AS LineValue
@@ -61,7 +89,7 @@ BEGIN
             5 as LineNo,
             ''::TVarChar as GroupName,
             'Условия участия (в счет маркетингового бюджета или по выставленному счету)'::TVarChar as LineName,
-            (SELECT STRING_AGG(MovementItem_PromoCondition.ConditionPromoName||': '||replace(TO_CHAR(MovementItem_PromoCondition.Amount,'FM9990D09')||' ','.0 ',''), chr(13)||chr(10)) 
+            (SELECT STRING_AGG(MovementItem_PromoCondition.ConditionPromoName||': '||replace(TO_CHAR(MovementItem_PromoCondition.Amount,'FM9990D09')||' ','.0 ',''), chr(13)) 
              FROM MovementItem_PromoCondition_View AS MovementItem_PromoCondition 
              WHERE MovementItem_PromoCondition.MovementId = inMovementId
                AND MovementItem_PromoCondition.IsErased = FALSE)::TEXT AS LineValue
@@ -70,7 +98,7 @@ BEGIN
             6 as LineNo,
             ''::TVarChar as GroupName,
             'Позиции'::TVarChar as LineName,
-            (SELECT STRING_AGG(MI_PromoGoods.GoodsName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG(MI_PromoGoods.GoodsName, chr(13)) 
              FROM MovementItem_PromoGoods_View AS MI_PromoGoods
              WHERE MI_PromoGoods.MovementId = inMovementId
                AND MI_PromoGoods.IsErased = FALSE)::TEXT AS LineValue
@@ -79,7 +107,7 @@ BEGIN
             7 as LineNo,
             ''::TVarChar as GroupName,
             '% дополнительной скидки'::TVarChar as LineName,
-            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.Amount,'FM9990D09')||' ','.0 ','')||'% '||chr(9)||MI_PromoGoods.GoodsName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.Amount,'FM9990D09')||' ','.0 ','')||'%   '||CASE WHEN vbCountGoods > 1 THEN MI_PromoGoods.GoodsName ELSE '' END, chr(13)) 
              FROM MovementItem_PromoGoods_View AS MI_PromoGoods
              WHERE MI_PromoGoods.MovementId = inMovementId
                AND MI_PromoGoods.IsErased = FALSE)::TEXT AS LineValue
@@ -88,7 +116,7 @@ BEGIN
             8 as LineNo,
             ''::TVarChar as GroupName,
             'Цена отгрузки без учета НДС, с учетом скидки, грн'::TVarChar as LineName,
-            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.PriceWithOutVAT,'FM9990D09')||' ','.0 ','')||' '||chr(9)||MI_PromoGoods.GoodsName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.PriceWithOutVAT,'FM9990D09')||' ','.0 ','')||'   '||CASE WHEN vbCountGoods > 1 THEN MI_PromoGoods.GoodsName ELSE '' END, chr(13)) 
              FROM MovementItem_PromoGoods_View AS MI_PromoGoods
              WHERE MI_PromoGoods.MovementId = inMovementId
                AND MI_PromoGoods.IsErased = FALSE)::TEXT AS LineValue
@@ -97,7 +125,7 @@ BEGIN
             9 as LineNo,
             ''::TVarChar as GroupName,
             'Цена отгрузки с учетом НДС, с учетом скидки, грн'::TVarChar as LineName,
-            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.PriceWithVAT,'FM9990D09')||' ','.0 ','')||' '||chr(9)||MI_PromoGoods.GoodsName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.PriceWithVAT,'FM9990D09')||' ','.0 ','')||'   '||CASE WHEN vbCountGoods > 1 THEN MI_PromoGoods.GoodsName ELSE '' END, chr(13)) 
              FROM MovementItem_PromoGoods_View AS MI_PromoGoods
              WHERE MI_PromoGoods.MovementId = inMovementId
                AND MI_PromoGoods.IsErased = FALSE)::TEXT AS LineValue
@@ -106,7 +134,7 @@ BEGIN
             10 as LineNo,
             ''::TVarChar as GroupName,
             'Объем продаж в аналогичный период, кг'::TVarChar as LineName,
-            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.AmountReal,'FM9990D09')||' ','.0 ','')||' '||chr(9)||MI_PromoGoods.GoodsName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.AmountReal,'FM9990D09')||' ','.0 ','')||'   '||CASE WHEN vbCountGoods > 1 THEN MI_PromoGoods.GoodsName ELSE '' END, chr(13)) 
              FROM MovementItem_PromoGoods_View AS MI_PromoGoods
              WHERE MI_PromoGoods.MovementId = inMovementId
                AND MI_PromoGoods.IsErased = FALSE)::TEXT AS LineValue
@@ -115,7 +143,7 @@ BEGIN
             11 as LineNo,
             ''::TVarChar as GroupName,
             'Планируемый объем продаж на акционный период (в кг)'::TVarChar as LineName,
-            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.AmountPlanMin,'FM9990D09')||' ','.0 ','')||' - '||replace(TO_CHAR(MI_PromoGoods.AmountPlanMax,'FM9990D09')||' ','.0 ','')||' '||chr(9)||MI_PromoGoods.GoodsName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.AmountPlanMin,'FM9990D09')||' ','.0 ','')||' - '||replace(TO_CHAR(MI_PromoGoods.AmountPlanMax,'FM9990D09')||' ','.0 ','')||'   '||CASE WHEN vbCountGoods > 1 THEN MI_PromoGoods.GoodsName ELSE '' END, chr(13)) 
              FROM MovementItem_PromoGoods_View AS MI_PromoGoods
              WHERE MI_PromoGoods.MovementId = inMovementId
                AND MI_PromoGoods.IsErased = FALSE)::TEXT AS LineValue
@@ -124,7 +152,7 @@ BEGIN
             12 as LineNo,
             ''::TVarChar as GroupName,
             'Вид упаковки'::TVarChar as LineName,
-            (SELECT STRING_AGG(MI_PromoGoods.GoodsName||': '||MI_PromoGoods.GoodsKindName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG(CASE WHEN vbCountGoods > 1 THEN MI_PromoGoods.GoodsName||': ' ELSE '' END||MI_PromoGoods.GoodsKindName, chr(13)) 
              FROM MovementItem_PromoGoods_View AS MI_PromoGoods
              WHERE MI_PromoGoods.MovementId = inMovementId
                AND MI_PromoGoods.IsErased = FALSE)::TEXT AS LineValue
@@ -133,13 +161,13 @@ BEGIN
             13 as LineNo,
             ''::TVarChar as GroupName,
             'Количество товара поставляемого в период акции в данном виде упаковки, кг'::TVarChar as LineName,
-            (SELECT STRING_AGG('_______ '||Chr(9)||MI_PromoGoods.GoodsName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG('_______    '||CASE WHEN vbCountGoods > 1 THEN MI_PromoGoods.GoodsName ELSE '' END, chr(13)) 
              FROM MovementItem_PromoGoods_View AS MI_PromoGoods
              WHERE MI_PromoGoods.MovementId = inMovementId
                AND MI_PromoGoods.IsErased = FALSE)::TEXT AS LineValue
         UNION ALL
         SELECT
-            13 as LineNo,
+            14 as LineNo,
             ''::TVarChar as GroupName,
             'Рекламная поддержка (газета, сопровождение наружной рекламой (сити-лайтами, билбордами), ТВ, радио, дегустации и прочее)'::TVarChar as LineName,
             (SELECT Movement_Promo.AdvertisingName 
@@ -150,7 +178,7 @@ BEGIN
             NULL as LineNo,
             'Дополнительная информация'::TVarChar as GroupName,
             'Цена в прайс-листе (грн)'::TVarChar as LineName,
-            (SELECT STRING_AGG(replace(TO_CHAR(MI_PromoGoods.Price,'FM9990D09')||' ','.0 ','')||' '||chr(9)||MI_PromoGoods.GoodsName, chr(13)||chr(10)) 
+            (SELECT STRING_AGG(replace(TO_CHAR(ROUND(MI_PromoGoods.Price*((100+vbVAT)/100),2),'FM9990D09')||' ','.0 ','')||'   '||CASE WHEN vbCountGoods > 1 THEN MI_PromoGoods.GoodsName ELSE '' END, chr(13))
              FROM MovementItem_PromoGoods_View AS MI_PromoGoods
              WHERE MI_PromoGoods.MovementId = inMovementId
                AND MI_PromoGoods.IsErased = FALSE)::TEXT AS LineValue
