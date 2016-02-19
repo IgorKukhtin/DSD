1909e58e1515ca4d7cd0c1110f4d13f2fde60442
POLAK <voanatol@mail.ru>
2016-02-19 10:44:31 +0200

    LossDept Export tabletki

--- DATABASE/FARMACY/PROCEDURE/MovementItemContainer/LossDebt/lpComplete_Movement_LossDebt.sql
+++ DATABASE/FARMACY/PROCEDURE/MovementItemContainer/LossDebt/lpComplete_Movement_LossDebt.sql
@@ -18,10 +18,71 @@ BEGIN
      -- !!!обязательно!!! очистили таблицу - элементы документа, со всеми свойствами для формирования Аналитик в проводках
      DELETE FROM _tmpItem;
 
+    WITH tmpItem AS (
+                        SELECT
+                            MovementItem_LossDebt_View.Id
+                          , MovementItem_LossDebt_View.JuridicalId
+                          , Movement_LossDebt_View.JuridicalBasisId
+                          , Movement_LossDebt_View.OperDate
+                        FROM
+                            Movement_LossDebt_View
+                            LEFT OUTER JOIN MovementItem_LossDebt_View ON MovementItem_LossDebt_View.MovementId =  Movement_LossDebt_View.Id
+                        WHERE 
+                            Movement_LossDebt_View.Id =  inMovementId
+                            AND
+                            MovementItem_LossDebt_View.isErased = FALSE
+                            AND
+                            MovementItem_LossDebt_View.isCalculated = TRUE
+                    ),
+         tmpContainer AS (
+                            SELECT
+                                tmpItem.Id
+                              , tmpItem.OperDate
+                              , Container.Id AS ContainerId
+                              , Container.Amount
+                            FROM
+                                tmpItem
+                                INNER JOIN ContainerLinkObject AS CLO_Juridical
+                                                               ON CLO_Juridical.DescId = zc_ContainerLinkObject_Juridical()
+                                                              AND CLO_Juridical.ObjectId = tmpItem.JuridicalId
+                                INNER JOIN ContainerLinkObject AS CLO_JuridicalBasis
+                                                               ON CLO_JuridicalBasis.DescId = zc_ContainerLinkObject_JuridicalBasis()
+                                                              AND CLO_JuridicalBasis.ObjectId = tmpItem.JuridicalBasisId
+                                INNER JOIN Container ON Container.DescId = zc_Container_Summ()
+                                                    AND Container.Id = CLO_Juridical.ContainerId
+                                                    AND Container.Id = CLO_JuridicalBasis.ContainerId
+                         ),
+         tmpConatinerRemains AS (
+                                    SELECT
+                                        tmpContainer.Id
+                                      , tmpContainer.ContainerId
+                                      , tmpContainer.Amount - COALESCE(SUM(MovementItemContainer.Amount),0) AS Amount
+                                    FROM
+                                        tmpContainer
+                                        LEFT OUTER JOIN MovementItemContainer ON MovementItemContainer.ContainerId = tmpContainer.ContainerId
+                                                                             AND MovementItemContainer.OperDate > tmpContainer.OperDate
+                                    GROUP BY
+                                        tmpContainer.Id
+                                      , tmpContainer.ContainerId
+                                      , tmpContainer.Amount
+                               ),
+        tmpRemains AS (
+                        SELECT
+                            tmpConatinerRemains.Id
+                           ,SUM(tmpConatinerRemains.Amount) AS Amount 
+                        FROM
+                            tmpConatinerRemains
+                        GROUP BY
+                            tmpConatinerRemains.Id
+                      )
    INSERT INTO _tmpItem(MovementItemId, ObjectId, OperSumm, AccountId, JuridicalId_Basis, OperDate)   
    SELECT MovementItem_LossDebt_View.Id
         , MovementItem_LossDebt_View.JuridicalId
-        , MovementItem_LossDebt_View.SummDebet - MovementItem_LossDebt_View.SummKredit 
+        , CASE 
+              WHEN MovementItem_LossDebt_View.isCalculated = TRUE 
+                  THEN (MovementItem_LossDebt_View.SummDebet - MovementItem_LossDebt_View.SummKredit)-COALESCE(tmpRemains.Amount,0)
+          ELSE MovementItem_LossDebt_View.SummDebet - MovementItem_LossDebt_View.SummKredit
+          END
         , lpInsertFind_Object_Account (inAccountGroupId         := zc_Enum_AccountGroup_70000()
                                      , inAccountDirectionId     := zc_Enum_AccountDirection_70100()
                                      , inInfoMoneyDestinationId := zc_Enum_InfoMoneyDestination_10200()
@@ -29,8 +90,13 @@ BEGIN
                                      , inUserId                 := inUserId)
         , Movement_LossDebt_View.JuridicalBasisId
         , Movement_LossDebt_View.OperDate
-     FROM MovementItem_LossDebt_View, Movement_LossDebt_View 
-    WHERE MovementItem_LossDebt_View.MovementId =  Movement_LossDebt_View.Id AND Movement_LossDebt_View.Id =  inMovementId;
+     FROM 
+         Movement_LossDebt_View
+         LEFT OUTER JOIN MovementItem_LossDebt_View ON MovementItem_LossDebt_View.MovementId = Movement_LossDebt_View.Id
+                                                   AND MovementItem_LossDebt_View.isErased = FALSE
+         LEFT OUTER JOIN tmpRemains ON MovementItem_LossDebt_View.Id = tmpRemains.Id
+    WHERE
+        Movement_LossDebt_View.Id =  inMovementId;
     
 --     CREATE TEMP TABLE _tmpMIContainer_insert (Id Integer, DescId Integer, MovementDescId Integer, MovementId Integer, MovementItemId Integer, ContainerId Integer, ParentId Integer
   --                                           , AccountId Integer, AnalyzerId Integer, ObjectId_Analyzer Integer, WhereObjectId_Analyzer Integer, ContainerId_Analyzer Integer
