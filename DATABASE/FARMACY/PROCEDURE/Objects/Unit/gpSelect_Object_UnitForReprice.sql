44873096c879031cc770d6e599d533818e3466a2
innaf <innaf@ua.fm>
2016-02-24 23:47:29 +0200

    +

--- DATABASE/FARMACY/PROCEDURE/Objects/Unit/gpSelect_Object_UnitForReprice.sql
+++ DATABASE/FARMACY/PROCEDURE/Objects/Unit/gpSelect_Object_UnitForReprice.sql
@@ -14,11 +14,23 @@ BEGIN
 
     RETURN QUERY 
      
-        SELECT Object_Unit_View.Id
-             , Object_Unit_View.Name
-        FROM Object_Unit_View
-        WHERE Object_Unit_View.isLeaf = TRUE
-        ORDER BY Object_Unit_View.Name
+        SELECT Object_Unit.Id          AS Id  
+             , Object_Unit.ValueData   AS Name
+        FROM Object AS Object_Unit
+
+        LEFT JOIN ObjectBoolean AS ObjectBoolean_isLeaf 
+                                ON ObjectBoolean_isLeaf.ObjectId = Object_Unit.Id
+                               AND ObjectBoolean_isLeaf.DescId = zc_ObjectBoolean_isLeaf()
+
+        LEFT JOIN ObjectBoolean AS ObjectBoolean_RepriceAuto 
+                                ON ObjectBoolean_RepriceAuto.ObjectId = Object_Unit.Id
+                               AND ObjectBoolean_RepriceAuto.DescId = zc_ObjectBoolean_Unit_RepriceAuto()
+
+        WHERE Object_Unit.DescId = zc_Object_Unit()
+          AND COALESCE (ObjectBoolean_isLeaf.ValueData,False) = TRUE
+          AND COALESCE (ObjectBoolean_RepriceAuto.ValueData,False) = TRUE
+
+        ORDER BY Object_Unit.ValueData
        ;
 END;
 $BODY$
