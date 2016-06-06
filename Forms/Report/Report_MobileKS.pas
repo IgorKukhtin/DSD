00c4e1b0cc78f87d92e0cc16a63818e752acfe8d
bodyalev <bodyalev@ex.ua>
2016-06-06 11:16:12 +0300

    Отчет "Затраты на мобильную связь"

---
+++ Forms/Report/Report_MobileKS.pas
@@ -0,0 +1,97 @@
+unit Report_MobileKS;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, ParentForm, cxGraphics, cxControls,
+  cxLookAndFeels, cxLookAndFeelPainters, cxStyles, 
+  dxSkinscxPCPainter, cxCustomData, cxFilter, cxData,
+  cxDataStorage, cxEdit, Data.DB, cxDBData, cxCheckBox, dxSkinsdxBarPainter,
+  dsdAddOn, dsdDB, dsdAction, Vcl.ActnList, dxBarExtItems, dxBar, cxClasses,
+  cxPropertiesStore, Datasnap.DBClient, cxGridLevel, cxGridCustomTableView,
+  cxGridTableView, cxGridDBTableView, cxGridCustomView, cxGrid, cxSpinEdit,
+  Vcl.Menus, cxCalendar, cxSplitter, dxSkinsCore, dxSkinBlack, dxSkinBlue,
+  dxSkinBlueprint, dxSkinCaramel, dxSkinCoffee, dxSkinDarkRoom, dxSkinDarkSide,
+  dxSkinDevExpressDarkStyle, dxSkinDevExpressStyle, dxSkinFoggy,
+  dxSkinGlassOceans, dxSkinHighContrast, dxSkiniMaginary, dxSkinLilian,
+  dxSkinLiquidSky, dxSkinLondonLiquidSky, dxSkinMcSkin, dxSkinMoneyTwins,
+  dxSkinOffice2007Black, dxSkinOffice2007Blue, dxSkinOffice2007Green,
+  dxSkinOffice2007Pink, dxSkinOffice2007Silver, dxSkinOffice2010Black,
+  dxSkinOffice2010Blue, dxSkinOffice2010Silver, dxSkinPumpkin, dxSkinSeven,
+  dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus, dxSkinSilver,
+  dxSkinSpringTime, dxSkinStardust, dxSkinSummer2008, dxSkinTheAsphaltWorld,
+  dxSkinsDefaultPainters, dxSkinValentine, dxSkinVS2010, dxSkinWhiteprint,
+  dxSkinXmas2008Blue;
+
+type
+  TReport_MobileKSForm = class(TParentForm)
+    cxGrid: TcxGrid;
+    cxGridDBTableView: TcxGridDBTableView;
+    cxGridLevel: TcxGridLevel;
+    DataSource: TDataSource;
+    ClientDataSet: TClientDataSet;
+    cxPropertiesStore: TcxPropertiesStore;
+    dxBarManager: TdxBarManager;
+    dxBarManagerBar1: TdxBar;
+    bbRefresh: TdxBarButton;
+    bbInsert: TdxBarButton;
+    bbSetErased: TdxBarButton;
+    bbSetUnErased: TdxBarButton;
+    bbGridToExcel: TdxBarButton;
+    dxBarStatic1: TdxBarStatic;
+    ActionList: TActionList;
+    actRefresh: TdsdDataSetRefresh;
+    dsdSetErased: TdsdUpdateErased;
+    dsdSetUnErased: TdsdUpdateErased;
+    dsdGridToExcel1: TdsdGridToExcel;
+    dsdStoredProc: TdsdStoredProc;
+    dsdUserSettingsStorageAddOn: TdsdUserSettingsStorageAddOn;
+    spErasedUnErased: TdsdStoredProc;
+    dsdDBViewAddOn: TdsdDBViewAddOn;
+    actShowAll: TBooleanStoredProcAction;
+    bbShowAll: TdxBarButton;
+    ProtocolOpenForm: TdsdOpenForm;
+    bbProtocolOpenForm: TdxBarButton;
+    actInsert: TdsdInsertUpdateAction;
+    pmGrid: TPopupMenu;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    N5: TMenuItem;
+    cxGridDBTableViewDateList: TcxGridDBColumn;
+    cxGrid1: TcxGrid;
+    gridReport: TcxGridDBTableView;
+    gridReportMobilePhone: TcxGridDBColumn;
+    gridReportEmployeeName: TcxGridDBColumn;
+    gridReportRegion: TcxGridDBColumn;
+    gridReportTotalSum: TcxGridDBColumn;
+    gridReportTariffName: TcxGridDBColumn;
+    gridReportMonthly: TcxGridDBColumn;
+    gridReportNavigator: TcxGridDBColumn;
+    gridReportLim: TcxGridDBColumn;
+    gridReportLimOver: TcxGridDBColumn;
+    gridReportLimDuty: TcxGridDBColumn;
+    cxGridLevel1: TcxGridLevel;
+    DataSource1: TDataSource;
+    ClientDataSet1: TClientDataSet;
+    dsdStoredProc1: TdsdStoredProc;
+    dsdDBViewAddOn1: TdsdDBViewAddOn;
+    gridReportBillDate: TcxGridDBColumn;
+    cxSplitter1: TcxSplitter;
+    dsdLoadXMLKS1: TdsdLoadXMLKS;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+initialization
+  RegisterClass(TReport_MobileKSForm);
+end.
