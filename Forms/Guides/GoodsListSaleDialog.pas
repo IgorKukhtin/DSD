3990322f2dcc9688d17a99b85744fff77cbcbf43
innaf <innaf@ua.fm>
2016-10-18 22:04:51 +0300

    +

---
+++ Forms/Guides/GoodsListSaleDialog.pas
@@ -0,0 +1,37 @@
+unit GoodsListSaleDialog;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, AncestorDialog, cxGraphics,
+  cxLookAndFeels, cxLookAndFeelPainters, Vcl.Menus, dxSkinsCore,
+  dxSkinsDefaultPainters, cxControls, cxContainer, cxEdit, Vcl.ComCtrls, dxCore,
+  cxDateUtils, dsdGuides, cxButtonEdit, cxTextEdit, cxMaskEdit, cxDropDownEdit,
+  cxCalendar, cxLabel, dsdDB, Vcl.ActnList, dsdAction, cxPropertiesStore,
+  dsdAddOn, Vcl.StdCtrls, cxButtons, ChoicePeriod, cxCheckBox;
+
+type
+  TGoodsListSaleDialogForm = class(TAncestorDialogForm)
+    cxLabel3: TcxLabel;
+    edRetail: TcxButtonEdit;
+    cxLabel4: TcxLabel;
+    edJuridical: TcxButtonEdit;
+    cxLabel5: TcxLabel;
+    edContract: TcxButtonEdit;
+    RetailGuides: TdsdGuides;
+    JuridicalGuides: TdsdGuides;
+    ContractGuides: TdsdGuides;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+initialization
+  RegisterClass(TGoodsListSaleDialogForm);
+end.
