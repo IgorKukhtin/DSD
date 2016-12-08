bdcea5e4a9c24a677a7d06c12e3c3a005f88e231
innaf <innaf@ua.fm>
2016-12-08 12:44:28 +0200

    +

--- DATABASE/FARMACY/PROCEDURE/MovementItem/PriceList/gpUpdate_MI_Price.sql
+++ DATABASE/FARMACY/PROCEDURE/MovementItem/PriceList/gpUpdate_MI_Price.sql
@@ -5,23 +5,24 @@ DROP FUNCTION IF EXISTS gpUpdate_MI_Price(Integer, Integer, Integer, TFloat, TFl
 CREATE OR REPLACE FUNCTION gpUpdate_MI_Price(
     IN inId                  Integer   , -- Ключ объекта <Элемент документа>
     IN inMovementId          Integer   , -- Ключ объекта <Документ>
-    IN inGoodsMainId         Integer   , -- Товары
-    IN inAmount              TFloat    , -- Цена
+    IN inGoodsId             Integer   , -- Товары
+ INOUT ioAmount              TFloat    , -- Цена
     IN inPercent             TFloat    , -- % скидки
     IN inSession             TVarChar    -- сессия пользователя
 )
-RETURNS VOID AS
+RETURNS TFloat AS
 $BODY$
    DECLARE vbUserId Integer;
-   DECLARE vbAmount Integer;
+   --DECLARE vbAmount TFloat;
 BEGIN
      -- проверка прав пользователя на вызов процедуры
      -- PERFORM lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_MovementItem_PriceList());
      vbUserId := inSession;
 
+     ioAmount :=  CAST ((ioAmount + (ioAmount * inPercent / 100)) ::NUMERIC (16, 2) AS TFloat);
 
      -- обновили цену в <Элемент документа>
-     PERFORM lpInsertUpdate_MovementItem (inId, zc_MI_Master(), inGoodsMainId, inMovementId, inAmount, NULL);
+     PERFORM lpInsertUpdate_MovementItem (inId, zc_MI_Master(), inGoodsId, inMovementId, ioAmount, NULL);
 
      -- сохранили протокол
      PERFORM lpInsert_MovementItemProtocol (inId, vbUserId, FALSE);
@@ -34,8 +35,8 @@ LANGUAGE PLPGSQL VOLATILE;
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.
- 08.12.16         *
+ 19.09.14                         *
 */
 
 -- тест
--- SELECT * FROM gpUpdate_MI_Price (inId:= 0, inMovementId:= 10, inGoodsMainId:= 1, inAmount:= 0, inSession:= '2')
+-- SELECT * FROM gpUpdate_MI_Price (inId:= 0, inMovementId:= 10, inGoodsId:= 1, ioAmount:= 0, inPercent := inSession:= '2')
