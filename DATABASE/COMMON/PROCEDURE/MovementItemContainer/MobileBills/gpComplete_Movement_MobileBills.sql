5f778d54b43455a54f41dc35d303ccf8b39ad324
K0sta <Ashtu777@gmail.com>
2017-02-07 09:22:49 +0200

    ok

--- DATABASE/COMMON/PROCEDURE/MovementItemContainer/MobileBills/gpComplete_Movement_MobileBills.sql
+++ DATABASE/COMMON/PROCEDURE/MovementItemContainer/MobileBills/gpComplete_Movement_MobileBills.sql
@@ -14,11 +14,13 @@ BEGIN
      -- проверка прав пользователя на вызов процедуры
      vbUserId:= lpCheckRight (inSession, zc_Enum_Process_Complete_MobileBills());
 
+     -- создаются временные таблицы - для формирование данных для проводок
+     PERFORM lpComplete_Movement_Finance_CreateTemp();
+
      -- проводим Документ + сохранили протокол
-     PERFORM lpComplete_Movement (inMovementId := inMovementId
-                                , inDescId     := zc_Movement_MobileBills()
-                                , inUserId     := vbUserId
-                                 );
+     PERFORM lpComplete_Movement_MobileBills (inMovementId := inMovementId
+                                            , inUserId     := vbUserId
+                                             );
 
 END;
 $BODY$
@@ -31,4 +33,6 @@ $BODY$
  */
 
 -- тест
---
+-- SELECT * FROM lpUnComplete_Movement (inMovementId:= 3581, inUserId:= zfCalc_UserAdmin() :: Integer)
+-- SELECT * FROM gpComplete_Movement_PersonalService (inMovementId:= 3581, inSession:= zfCalc_UserAdmin())
+-- SELECT * FROM gpSelect_MovementItemContainer_Movement (inMovementId:= 3581, inSession:= zfCalc_UserAdmin())
