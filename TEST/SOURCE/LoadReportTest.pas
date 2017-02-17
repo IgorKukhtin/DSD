044131e10cd9889c287c37029e73dd16c9624c1a
K0sta <Ashtu777@gmail.com>
2017-02-17 10:06:42 +0200

    ok

--- TEST/SOURCE/LoadReportTest.pas
+++ TEST/SOURCE/LoadReportTest.pas
@@ -89,8 +89,7 @@ procedure TLoadReportTest.LoadAllReportFormTest;
 var
  i : integer;
 begin
- 
-   
+
   LoadReportFromFile('Отчет движение по ОС', ReportPath + '\Отчеты (товарные)\Отчет движение по ОС.fr3');
   LoadReportFromFile('Отчет движение по товару (МО)', ReportPath + '\Отчеты (товарные)\Отчет движение по товару (МО).fr3');
   LoadReportFromFile('Отчет движение по товару (Итого)', ReportPath + '\Отчеты (товарные)\Отчет движение по товару (Итого).fr3');
