54822dc290e39c885ab6e547ecab1331ebe16e55
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-02-17 17:37:59 +0200

    [+] первичная реализация gpSelectMobile_Object_Route

--- DATABASE/COMMON/PROCEDURE/ProjectMobile/gpSelectMobile_Object_Route.sql
+++ DATABASE/COMMON/PROCEDURE/ProjectMobile/gpSelectMobile_Object_Route.sql
@@ -14,14 +14,36 @@ RETURNS TABLE (Id         Integer
 )
 AS $BODY$
   DECLARE vbUserId Integer;
+  DECLARE vbPersonalId Integer;
 BEGIN
   -- проверка прав пользователя на вызов процедуры
   -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_...());
-  vbUserId:= lpGetUserBySession (inSession);
+  vbUserId := lpGetUserBySession (inSession);
+
+  vbPersonalId := (SELECT PersonalId FROM gpGetMobile_Object_Const (inSession));
 
   -- Результат
-  RETURN;
-  -- RETURN QUERY
+  IF vbPersonalId IS NOT NULL THEN
+    RETURN QUERY
+      WITH tmpRoute AS (
+        SELECT ObjectLink_Partner_Route.ChildObjectId AS RouteId
+        FROM ObjectLink AS ObjectLink_Partner_PersonalTrade
+          JOIN ObjectLink AS ObjectLink_Partner_Route
+                          ON ObjectLink_Partner_Route.ObjectId = ObjectLink_Partner_PersonalTrade.ObjectId
+                         AND ObjectLink_Partner_Route.DescId = zc_ObjectLink_Partner_Route()
+        WHERE ObjectLink_Partner_PersonalTrade.ChildObjectId = vbPersonalId
+          AND ObjectLink_Partner_PersonalTrade.DescId = zc_ObjectLink_Partner_PersonalTrade()
+          AND ObjectLink_Partner_Route.ChildObjectId IS NOT NULL
+      )
+      SELECT
+        Object_Route.Id
+        , Object_Route.ObjectCode
+        , Object_Route.ValueData
+        , Object_Route.isErased
+        , EXISTS(SELECT 1 FROM tmpRoute WHERE tmpRoute.RouteId = Object_Route.Id) AS isSync
+      FROM Object AS Object_Route
+      WHERE object_route.DescId = zc_Object_Route();
+  END IF;
 
 END; $BODY$
   LANGUAGE plpgsql VOLATILE;
