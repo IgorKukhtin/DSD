34a1f14e06580b9367c4e41883daa448eb391bff
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-02-21 15:00:52 +0200

    [+] синхронизация товаров (Goods)

--- DATABASE/COMMON/PROCEDURE/ProjectMobile/gpSelectMobile_Object_Goods.sql
+++ DATABASE/COMMON/PROCEDURE/ProjectMobile/gpSelectMobile_Object_Goods.sql
@@ -3,8 +3,8 @@
 DROP FUNCTION IF EXISTS gpSelectMobile_Object_Goods (TDateTime, TVarChar);
 
 CREATE OR REPLACE FUNCTION gpSelectMobile_Object_Goods (
-  IN inSyncDateIn TDateTime, -- Дата/время последней синхронизации - когда "успешно" загружалась входящая информация - актуальные справочники, цены, акции, долги, остатки и т.д
-  IN inSession    TVarChar   -- сессия пользователя
+    IN inSyncDateIn TDateTime, -- Дата/время последней синхронизации - когда "успешно" загружалась входящая информация - актуальные справочники, цены, акции, долги, остатки и т.д
+    IN inSession    TVarChar   -- сессия пользователя
 )
 RETURNS TABLE (Id           Integer
              , ObjectCode   Integer  -- Код
@@ -16,19 +16,41 @@ RETURNS TABLE (Id           Integer
              , MeasureId    Integer  -- Единица измерения
              , isErased     Boolean  -- Удаленный ли элемент
              , isSync       Boolean  -- Синхронизируется (да/нет)
-)
-AS $BODY$
-  DECLARE vbUserId Integer;
+              )
+AS 
+$BODY$
+   DECLARE vbUserId Integer;
 BEGIN
-  -- проверка прав пользователя на вызов процедуры
-  -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_...());
-  vbUserId:= lpGetUserBySession (inSession);
+      -- проверка прав пользователя на вызов процедуры
+      -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_...());
+      vbUserId:= lpGetUserBySession (inSession);
 
-  -- Результат
-  RETURN;
-  -- RETURN QUERY
+      -- Результат
+      RETURN QUERY
+        WITH tmpProtocol AS (SELECT ObjectProtocol.ObjectId AS GoodsId, MAX(ObjectProtocol.OperDate) AS MaxOperDate
+                             FROM ObjectProtocol
+                                  JOIN Object AS Object_Goods
+                                              ON Object_Goods.Id = ObjectProtocol.ObjectId
+                                             AND Object_Goods.DescId = zc_Object_Goods() 
+                             WHERE ObjectProtocol.OperDate > inSyncDateIn
+                             GROUP BY ObjectProtocol.ObjectId
+                            )
+        SELECT Object_Goods.Id
+             , Object_Goods.ObjectCode
+             , Object_Goods.ValueData
+             , CAST(0.0 AS TFloat) AS Weight
+             , CAST(0.0 AS TFloat) AS Remains
+             , CAST(0.0 AS TFloat) AS Forecast
+             , CAST(0 AS Integer)  AS GoodsGroupId
+             , CAST(0 AS Integer)  AS MeasureId
+             , Object_Goods.isErased
+             , (NOT Object_Goods.isErased) AS isSync
+        FROM Object AS Object_Goods
+             JOIN tmpProtocol ON tmpProtocol.GoodsId = Object_Goods.Id
+        WHERE Object_Goods.DescId = zc_Object_Goods();
 
-END; $BODY$
+END; 
+$BODY$
   LANGUAGE plpgsql VOLATILE;
 
 /*-------------------------------------------------------------------------------
