ac0a6485e3cb7221ff872fe9160560e464a5c67e
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-02-27 13:39:30 +0200

    [+] StoreReal

--- DATABASE/COMMON/PROCEDURE/Movement/StoreReal/Movement_StoreReal_seq.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/StoreReal/Movement_StoreReal_seq.sql
@@ -1,15 +1,16 @@
 DO $$
 BEGIN
-  IF NOT EXISTS(SELECT 1 FROM pg_statio_all_sequences WHERE relname = 'movement_storereal_seq') THEN
-    CREATE SEQUENCE movement_storereal_seq
-    INCREMENT 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-    CACHE 1;
-    ALTER SEQUENCE movement_storereal_seq
-      OWNER TO postgres;
-  END IF;
+      IF NOT EXISTS(SELECT 1 FROM pg_statio_all_sequences WHERE relname = 'movement_storereal_seq')
+      THEN
+           CREATE SEQUENCE movement_storereal_seq
+           INCREMENT 1
+           MINVALUE 1
+           MAXVALUE 9223372036854775807
+           START 1
+           CACHE 1;
+           ALTER SEQUENCE movement_storereal_seq
+             OWNER TO postgres;
+      END IF;
 END $$;
 
 
