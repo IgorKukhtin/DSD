63c045acdd666bfbc6ea279bcd00b7dace7f43b3
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-03-17 13:19:42 +0200

    ProjectMobile: Процедура выгрузки данных по акциям #7 (gpSelectMobile_Movement_Promo)

---
+++ DATABASE/COMMON/PROCEDURE/ProjectMobile/gpSelectMobile_Movement_Promo.sql
@@ -0,0 +1,82 @@
+-- Function: gpSelectMobile_Movement_Promo()
+
+DROP FUNCTION IF EXISTS gpSelectMobile_Movement_Promo (TDateTime, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpSelectMobile_Movement_Promo(
+    IN inSyncDateIn TDateTime, -- Дата/время последней синхронизации - когда "успешно" загружалась входящая информация - актуальные справочники, цены, акции, долги, остатки и т.д
+    IN inSession    TVarChar   -- сессия пользователя
+)
+RETURNS TABLE (Id              Integer   -- Уникальный идентификатор, формируется в Главной БД, и используется при синхронизации
+             , InvNumber       TVarChar  -- Номер документа
+             , OperDate        TDateTime -- Дата документа
+             , StatusId        Integer   -- Виды статусов
+             , StartSale       TDateTime -- Дата начала отгрузки по акционной цене
+             , EndSale         TDateTime -- Дата окончания отгрузки по акционной цене
+             , isChangePercent Boolean   -- учитывать % скидки по договору, *важно* - если FALSE, тогда в строчной части заявки ChangePercent всегда = 0 
+             , CommentMain     TVarChar  -- Примечание (общее)
+              )
+
+AS
+$BODY$
+   DECLARE vbUserId Integer;
+   DECLARE vbPersonalId Integer;
+BEGIN
+      -- проверка прав пользователя на вызов процедуры
+      -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_...());
+      vbUserId:= lpGetUserBySession (inSession);
+
+      vbPersonalId:= (SELECT PersonalId FROM gpGetMobile_Object_Const (inSession));
+
+      -- Результат
+      IF vbPersonalId IS NOT NULL 
+      THEN
+           RETURN QUERY
+             WITH tmpPromoPartner AS (SELECT DISTINCT Movement_PromoPartner.ParentId AS ParentId
+                                      FROM Movement AS Movement_PromoPartner
+                                           JOIN MovementLinkObject AS MovementLinkObject_Partner
+                                                                   ON MovementLinkObject_Partner.MovementId = Movement_PromoPartner.Id
+                                                                  AND MovementLinkObject_Partner.DescId = zc_MovementLinkObject_Partner() 
+                                           JOIN ObjectLink AS ObjectLink_Partner_PersonalTrade
+                                                           ON ObjectLink_Partner_PersonalTrade.ObjectId = MovementLinkObject_Partner.ObjectId 
+                                                          AND ObjectLink_Partner_PersonalTrade.DescId = zc_ObjectLink_Partner_PersonalTrade()
+                                                          AND ObjectLink_Partner_PersonalTrade.ChildObjectId = vbPersonalId
+                                      WHERE Movement_PromoPartner.DescId = zc_Movement_PromoPartner()
+                                        AND Movement_PromoPartner.ParentId IS NOT NULL 
+                                     )
+             SELECT Movement_Promo.Id
+                  , Movement_Promo.InvNumber
+                  , Movement_Promo.Operdate
+                  , Movement_Promo.StatusId
+                  , MovementDate_StartSale.ValueData     AS StartSale
+                  , MovementDate_EndSale.ValueData       AS EndSale
+                  , (MI_Child.ObjectId IS NULL)          AS isChangePercent
+                  , MovementString_CommentMain.ValueData AS CommentMain
+             FROM Movement AS Movement_Promo
+                  JOIN tmpPromoPartner ON tmpPromoPartner.ParentId = Movement_Promo.Id
+                  LEFT JOIN MovementDate AS MovementDate_StartSale
+                                         ON MovementDate_StartSale.MovementId = Movement_Promo.Id
+                                        AND MovementDate_StartSale.DescId = zc_MovementDate_StartSale()
+                  LEFT JOIN MovementDate AS MovementDate_EndSale
+                                         ON MovementDate_EndSale.MovementId = Movement_Promo.Id
+                                        AND MovementDate_EndSale.DescId = zc_MovementDate_EndSale()
+                  LEFT JOIN MovementItem AS MI_Child
+                                         ON MI_Child.MovementId = Movement_Promo.Id
+                                        AND MI_Child.DescId = zc_MI_Child() 
+                                        AND MI_Child.ObjectId = zc_Enum_ConditionPromo_ContractChangePercentOff() -- без учета % скидки по договору
+                                        AND NOT MI_Child.isErased
+                  LEFT JOIN MovementString AS MovementString_CommentMain
+                                           ON MovementString_CommentMain.MovementId = Movement_Promo.Id
+                                          AND MovementString_CommentMain.DescId = zc_MovementString_CommentMain() 
+             WHERE Movement_Promo.DescId = zc_Movement_Promo();
+      END IF;
+END;
+$BODY$
+  LANGUAGE PLPGSQL VOLATILE;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Воробкало А.А.   Ярошенко Р.Ф.
+ 16.03.17                                                                          *
+*/
+
+-- SELECT * FROM gpSelectMobile_Movement_Promo (inSyncDateIn:= zc_DateStart(), inSession:= zfCalc_UserAdmin())
