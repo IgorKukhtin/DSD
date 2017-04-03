f4858ce8897d24e87f79aa4ea4df505ced250cb8
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-04-03 18:38:11 +0300

    Сохранение данных с Мобилки в Главную БД по документу "Визит на торговую точку" Movement_Visit

---
+++ DATABASE/COMMON/PROCEDURE/ProjectMobile/gpInsertUpdateMobile_Movement_Visit.sql
@@ -0,0 +1,60 @@
+-- Function: gpInsertUpdateMobile_Movement_Visit()
+
+DROP FUNCTION IF EXISTS gpInsertUpdateMobile_Movement_Visit (TVarChar, TVarChar, TDateTime, Integer, TVarChar, TDateTime, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpInsertUpdateMobile_Movement_Visit (
+    IN inGUID       TVarChar  , -- Глобальный уникальный идентификатор для синхронизации с мобильными устройствами
+    IN inInvNumber  TVarChar  , -- Номер документа
+    IN inOperDate   TDateTime , -- Дата документа
+    IN inPartnerId  Integer   , -- Контрагент
+    IN inComment    TVarChar  , -- Примечание
+    IN inInsertDate TDateTime , -- Дата/время создания
+    IN inSession    TVarChar    -- сессия пользователя
+)
+RETURNS Integer 
+AS
+$BODY$
+   DECLARE vbId Integer;
+   DECLARE vbUserId Integer;
+BEGIN
+      -- проверка прав пользователя на вызов процедуры
+      -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_...());
+      vbUserId:= lpGetUserBySession (inSession);
+
+      -- получаем Id документа по GUID
+      SELECT MovementString_GUID.MovementId 
+      INTO vbId 
+      FROM MovementString AS MovementString_GUID
+           JOIN Movement AS Movement_Visit
+                         ON Movement_Visit.Id = MovementString_GUID.MovementId
+                        AND Movement_Visit.DescId = zc_Movement_Visit()
+      WHERE MovementString_GUID.DescId = zc_MovementString_GUID() 
+        AND MovementString_GUID.ValueData = inGUID;
+
+      vbId:= lpInsertUpdate_Movement_Visit (ioId:= vbId
+                                          , inInvNumber:= inInvNumber
+                                          , inOperDate:= inOperDate
+                                          , inPartnerId:= inPartnerId
+                                          , inComment:= inComment 
+                                          , inUserId:= vbUserId
+                                           );
+
+      -- сохранили свойство <Дата/время создания на мобильном устройстве>
+      PERFORM lpInsertUpdate_MovementDate(zc_MovementDate_InsertMobile(), vbId, inInsertDate);
+
+      -- сохранили свойство <Глобальный уникальный идентификатор>
+      PERFORM lpInsertUpdate_MovementString (zc_MovementString_GUID(), vbId, inGUID);
+
+      RETURN vbId;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Ярошенко Р.Ф.
+ 16.02.17                                                        *                                          
+*/
+
+-- тест
+-- SELECT * FROM gpInsertUpdateMobile_Movement_Visit (inGUID:= '{2F3BD890-0022-45F7-A1E2-9324BC312C76}', inInvNumber:= '-7', inOperDate:= CURRENT_DATE, inPartnerId:= 17819, inComment:= 'Це з мобілки прийшло :)', inInsertDate:= CURRENT_TIMESTAMP, inSession:= zfCalc_UserAdmin());
