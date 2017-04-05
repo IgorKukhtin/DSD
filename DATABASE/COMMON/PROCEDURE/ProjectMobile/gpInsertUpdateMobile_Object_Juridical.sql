747a69bf06df1160f18a464df1291c9edcb52b3e
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-04-05 18:00:25 +0300

    Сохранение данных с Мобилки в Главную БД по новому контрагенту и юр. лицу

---
+++ DATABASE/COMMON/PROCEDURE/ProjectMobile/gpInsertUpdateMobile_Object_Juridical.sql
@@ -0,0 +1,76 @@
+-- Function: gpInsertUpdateMobile_Object_Juridical
+
+DROP FUNCTION IF EXISTS gpInsertUpdateMobile_Object_Juridical (Integer, TVarChar, TVarChar, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpInsertUpdateMobile_Object_Juridical (
+ INOUT ioId      Integer  , -- ключ объекта <Юридическое лицо>
+    IN inGUID    TVarChar , -- Глобальный уникальный идентификатор
+    IN inName    TVarChar , -- Название объекта <Юридическое лицо>
+    IN inSession TVarChar   -- сессия пользователя
+)
+RETURNS Integer
+AS
+$BODY$
+   DECLARE vbUserId Integer;
+   DECLARE vbId Integer;
+   DECLARE vbPriceListId_def Integer;
+BEGIN
+      
+      -- проверка прав пользователя на вызов процедуры
+      -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_...());
+      vbUserId:= lpGetUserBySession (inSession);
+
+      IF COALESCE (inGUID, '') = ''
+      THEN
+           RAISE EXCEPTION 'Ошибка. Не задан глобальный уникальный идентификатор';
+      END IF;
+   
+      vbPriceListId_def:= (SELECT PriceListId_def FROM gpGetMobile_Object_Const (inSession));
+
+      -- ищем юр. лицо по значению глобального уникального идентификатора
+      SELECT ObjectString_Juridical_GUID.ObjectId
+      INTO vbId
+      FROM ObjectString AS ObjectString_Juridical_GUID
+           JOIN Object AS Object_Juridical
+                       ON Object_Juridical.Id = ObjectString_Juridical_GUID.ObjectId
+                      AND Object_Juridical.DescId = zc_Object_Juridical()
+      WHERE ObjectString_Juridical_GUID.DescId = zc_ObjectString_Juridical_GUID()
+        AND ObjectString_Juridical_GUID.ValueData = inGUID;
+
+      -- сохраняем юр. лицо
+      ioId:= gpInsertUpdate_Object_Juridical (ioId               := COALESCE (vbId, 0) -- ключ объекта <Юридическое лицо>
+                                            , inCode             := 0                  -- свойство <Код Юридического лица>
+                                            , inName             := inName             -- Название объекта <Юридическое лицо>
+                                            , inGLNCode          := NULL               -- Код GLN
+                                            , inisCorporate      := false              -- Признак наша ли собственность это юридическое лицо
+                                            , inisTaxSummary     := false              -- Признак сводная налоговая
+                                            , inisDiscountPrice  := false              -- Печать в накладной цену со скидкой
+                                            , inisPriceWithVAT   := false              -- Печать в накладной цену с НДС (да/нет)
+                                            , inDayTaxSummary    := 0                  -- Кол-во дней для сводной налоговой
+                                            , inJuridicalGroupId := 8362               -- Группы юридических лиц (03-ПОКУПАТЕЛИ)
+                                            , inGoodsPropertyId  := 0                  -- Классификаторы свойств товаров
+                                            , inRetailId         := 0                  -- Торговая сеть
+                                            , inRetailReportId   := 0                  -- Торговая сеть(отчет)
+                                            , inInfoMoneyId      := 30502              -- Статьи назначения (Прочие товары)
+                                            , inPriceListId      := vbPriceListId_def  -- Прайс-лист
+                                            , inPriceListPromoId := 0                  -- Прайс-лист(Акционный)
+                                            , inStartPromo       := NULL               -- Дата начала акции
+                                            , inEndPromo         := NULL               -- Дата окончания акции
+                                            , inSession          := inSession          -- текущий пользователь
+                                             );
+
+      -- сохранили свойство <Глобальный уникальный идентификатор>
+      PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Juridical_GUID(), ioId, inGUID);
+
+END;
+$BODY$
+  LANGUAGE PLPGSQL VOLATILE;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Ярошенко Р.Ф.
+ 05.04.17                                                         *
+*/
+
+-- тест
+-- SELECT * FROM gpInsertUpdateMobile_Object_Juridical (ioId:= 0, inGUID:= '{CCCCEF83-D391-4CDB-A471-AF9DD07AC7D9}', inName:= 'Юр. лицо с моб. устройства', inSession:= zfCalc_UserAdmin())
