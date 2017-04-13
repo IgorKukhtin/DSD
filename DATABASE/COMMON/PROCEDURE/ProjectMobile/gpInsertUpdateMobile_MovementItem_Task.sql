106c0eb3aea80addaaf4da4befba04da1210d393
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-04-13 18:36:05 +0300

    При сохранении задания с мобилки в главную БД необходимо сохранить только признак выполнения, примечание, дата+время выполнения задания

--- DATABASE/COMMON/PROCEDURE/ProjectMobile/gpInsertUpdateMobile_MovementItem_Task.sql
+++ DATABASE/COMMON/PROCEDURE/ProjectMobile/gpInsertUpdateMobile_MovementItem_Task.sql
@@ -14,27 +14,19 @@ RETURNS Integer AS
 $BODY$
    DECLARE vbUserId Integer;
    DECLARE vbId Integer;
-   DECLARE vbPartnerId Integer;
-   DECLARE vbDescription TVarChar;
 BEGIN
       -- проверка прав пользователя на вызов процедуры
       -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_...());
       vbUserId:= lpGetUserBySession (inSession);
 
+      -- проверяем существование задания
       SELECT MI_Task.Id
-           , MI_Task.ObjectId AS PartnerId
-           , MIString_Description.ValueData AS Description
       INTO vbId
-         , vbPartnerId
-         , vbDescription
       FROM Movement AS Movement_Task
            JOIN MovementItem AS MI_Task 
                              ON MI_Task.MovementId = Movement_Task.Id
                             AND MI_Task.DescId = zc_MI_Master() 
                             AND MI_Task.Id = inId
-           LEFT JOIN MovementItemString AS MIString_Description
-                                        ON MIString_Description.MovementItemId = MI_Task.Id
-                                       AND MIString_Description.DescId = zc_MIString_Description() 
       WHERE Movement_Task.DescId = zc_Movement_Task()
         AND Movement_Task.Id = inMovementId;
 
@@ -42,13 +34,6 @@ BEGIN
       THEN
            RAISE EXCEPTION 'Ошибка. Задание не заведено.';
       END IF; 
-
-      vbId := lpInsertUpdate_MovementItem_Task (ioId:= inId
-                                              , inMovementId:= inMovementId
-                                              , inPartnerId:= vbPartnerId
-                                              , inDescription:= vbDescription
-                                              , inUserId:= vbUserId
-                                               );
 
       -- сохранили свойство <Выполнено (да/нет)>
       PERFORM lpInsertUpdate_MovementItemBoolean (zc_MIBoolean_Close(), vbId, inClosed);
