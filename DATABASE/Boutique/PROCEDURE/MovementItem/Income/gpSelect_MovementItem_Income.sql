d9393890ca21a50bdda111f13f72aab26465bb01
K0sta <Ashtu777@gmail.com>
2017-04-24 21:31:58 +0300

    ok

--- DATABASE/Boutique/PROCEDURE/MovementItem/Income/gpSelect_MovementItem_Income.sql
+++ DATABASE/Boutique/PROCEDURE/MovementItem/Income/gpSelect_MovementItem_Income.sql
@@ -5,7 +5,7 @@ DROP FUNCTION IF EXISTS gpSelect_MovementItem_Income (Integer, Boolean, TVarChar
 
 CREATE OR REPLACE FUNCTION gpSelect_MovementItem_Income(
     IN inMovementId       Integer      , -- ключ Документа
-    IN inIsErased         Boolean      , -- 
+    IN inIsErased         Boolean      , --
     IN inSession          TVarChar       -- сессия пользователя
 )
 RETURNS TABLE (Id Integer, PartionId Integer, GoodsId Integer, GoodsCode Integer, GoodsName TVarChar
@@ -25,22 +25,19 @@ RETURNS TABLE (Id Integer, PartionId Integer, GoodsId Integer, GoodsCode Integer
 AS
 $BODY$
   DECLARE vbUserId Integer;
-
-  DECLARE vbUnitId Integer;
-  DECLARE vbPriceListId Integer;
 BEGIN
      -- проверка прав пользователя на вызов процедуры
-     -- vbUserId := PERFORM lpCheckRight (inSession, zc_Enum_Process_Select_MI_Income());
      vbUserId:= lpGetUserBySession (inSession);
 
-     -- Результат такой
-     RETURN QUERY 
+
+     -- Результат
+     RETURN QUERY
        WITH tmpMI AS (SELECT MovementItem.Id
                            , MovementItem.ObjectId AS GoodsId
                            , MovementItem.PartionId
-                           , MovementItem.Amount 
+                           , MovementItem.Amount
                            , COALESCE (MIFloat_OperPrice.ValueData, 0)       AS OperPrice
-                           , COALESCE (MIFloat_CountForPrice.ValueData, 1)   AS CountForPrice 
+                           , COALESCE (MIFloat_CountForPrice.ValueData, 1)   AS CountForPrice
                            , COALESCE (MIFloat_OperPriceList.ValueData, 0)   AS OperPriceList
                            , MovementItem.isErased
                        FROM (SELECT FALSE AS isErased UNION ALL SELECT inIsErased AS isErased WHERE inIsErased = TRUE) AS tmpIsErased
@@ -52,16 +49,13 @@ BEGIN
                                                        AND MIFloat_CountForPrice.DescId = zc_MIFloat_CountForPrice()
                             LEFT JOIN MovementItemFloat AS MIFloat_OperPrice
                                                         ON MIFloat_OperPrice.MovementItemId = MovementItem.Id
-                                                       AND MIFloat_OperPrice.DescId = zc_MIFloat_OperPrice()    
+                                                       AND MIFloat_OperPrice.DescId = zc_MIFloat_OperPrice()
                             LEFT JOIN MovementItemFloat AS MIFloat_OperPriceList
                                                         ON MIFloat_OperPriceList.MovementItemId = MovementItem.Id
                                                        AND MIFloat_OperPriceList.DescId = zc_MIFloat_OperPriceList()
-                       )
-
-       -- результат
+                     )
+       -- Результат
        SELECT
-
-
              tmpMI.Id
            , tmpMI.PartionId
            , Object_Goods.Id          AS GoodsId
@@ -70,12 +64,12 @@ BEGIN
            , ObjectString_Goods_GoodsGroupFull.ValueData AS GoodsGroupNameFull
            , Object_Measure.ValueData AS MeasureName
            , Object_Juridical.ValueData as JuridicalName
-           , Object_CompositionGroup.ValueData   AS CompositionGroupName  
+           , Object_CompositionGroup.ValueData   AS CompositionGroupName
            , Object_Composition.ValueData   AS CompositionName
            , Object_GoodsInfo.ValueData     AS GoodsInfoName
            , Object_LineFabrica.ValueData   AS LineFabricaName
            , Object_Label.ValueData         AS LabelName
-           , Object_GoodsSize.ValueData     AS GoodsSizeName 
+           , Object_GoodsSize.ValueData     AS GoodsSizeName
 
            , tmpMI.Amount
 
@@ -96,29 +90,25 @@ BEGIN
            , tmpMI.isErased
 
        FROM tmpMI
-            LEFT JOIN Object AS Object_Goods ON Object_Goods.Id = tmpMI.GoodsId
-            LEFT JOIN Object_PartionGoods ON Object_PartionGoods.MovementItemId = tmpMI.PartionId                                 
 
-            LEFT JOIN Object AS Object_GoodsGroup ON Object_GoodsGroup.Id = Object_PartionGoods.GoodsGroupId
-            LEFT JOIN Object AS Object_Measure ON Object_Measure.Id = Object_PartionGoods.MeasureId
-            LEFT JOIN Object AS Object_Composition ON Object_Composition.Id = Object_PartionGoods.CompositionId
+            LEFT JOIN Object_PartionGoods               ON Object_PartionGoods.MovementItemId = tmpMI.PartionId
 
-            LEFT JOIN ObjectLink AS ObjectLink_Composition_CompositionGroup
-                                 ON ObjectLink_Composition_CompositionGroup.ObjectId = Object_Composition.Id 
-                                AND ObjectLink_Composition_CompositionGroup.DescId = zc_ObjectLink_Composition_CompositionGroup()
-            LEFT JOIN Object AS Object_CompositionGroup ON Object_CompositionGroup.Id = ObjectLink_Composition_CompositionGroup.ChildObjectId
+            LEFT JOIN Object AS Object_Goods            ON Object_Goods.Id       = tmpMI.GoodsId
+            LEFT JOIN Object AS Object_Measure          ON Object_Measure.Id     = Object_PartionGoods.MeasureId
+            LEFT JOIN Object AS Object_Composition      ON Object_Composition.Id = Object_PartionGoods.CompositionId
+            LEFT JOIN Object AS Object_CompositionGroup ON Object_CompositionGroup.Id = Object_PartionGoods.CompositionGroupId
 
-            LEFT JOIN Object AS Object_GoodsInfo ON Object_GoodsInfo.Id = Object_PartionGoods.GoodsInfoId
-            LEFT JOIN Object AS Object_LineFabrica ON Object_LineFabrica.Id = Object_PartionGoods.LineFabricaId 
-            LEFT JOIN Object AS Object_Label ON Object_Label.Id = Object_PartionGoods.LabelId
-            LEFT JOIN Object AS Object_GoodsSize ON Object_GoodsSize.Id = Object_PartionGoods.GoodsSizeId
-           
+            LEFT JOIN Object AS Object_GoodsInfo        ON Object_GoodsInfo.Id   = Object_PartionGoods.GoodsInfoId
+            LEFT JOIN Object AS Object_LineFabrica      ON Object_LineFabrica.Id = Object_PartionGoods.LineFabricaId
+            LEFT JOIN Object AS Object_Label            ON Object_Label.Id       = Object_PartionGoods.LabelId
+            LEFT JOIN Object AS Object_GoodsSize        ON Object_GoodsSize.Id   = Object_PartionGoods.GoodsSizeId
+            LEFT JOIN Object AS Object_Juridical        ON Object_Juridical.Id   = Object_PartionGoods.JuridicalId
+
             LEFT JOIN ObjectString AS ObjectString_Goods_GoodsGroupFull
                                    ON ObjectString_Goods_GoodsGroupFull.ObjectId = tmpMI.GoodsId
-                                  AND ObjectString_Goods_GoodsGroupFull.DescId = zc_ObjectString_Goods_GroupNameFull()
-           LEFT JOIN Object AS Object_Juridical ON Object_Juridical.Id = Object_PartionGoods.JuridicalId
-                                  
-       ;
+                                  AND ObjectString_Goods_GoodsGroupFull.DescId   =  zc_ObjectString_Goods_GroupNameFull()
+
+           ;
 
 END;
 $BODY$
