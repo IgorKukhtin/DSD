495fcbe7ab5692141809be22098f71de9af158e6
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-04-26 16:54:07 +0300

    Проверить, почему грузятся пустые договора на моб. устройство Ошибка с удалением договоров в локальной базе, т.е в селекте вернулось isSync = фалсе по 4 договорам, а  в моб устр-ве их видно При создании заявки предлагаются товары с нулевыми ценами.

--- DATABASE/COMMON/PROCEDURE/ProjectMobile/gpGetMobile_Object_Const.sql
+++ DATABASE/COMMON/PROCEDURE/ProjectMobile/gpGetMobile_Object_Const.sql
@@ -149,7 +149,7 @@ BEGIN
             -- AS LastDateIn
             -- AS LastDateOut
 
-            , '1.7.0'::TVarChar             AS MobileVersion
+            , '1.8.0'::TVarChar             AS MobileVersion
             , 'ProjectMobile.apk'::TVarChar AS MobileAPKFileName
 
             , Object_PriceList_def.Id        AS PriceListId_def
