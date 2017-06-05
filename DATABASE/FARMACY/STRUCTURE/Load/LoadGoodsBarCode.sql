1d8d929e5d761a8cf348a44f0feafa2577193ef7
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-06-05 18:28:06 +0300

    Организовать базу Ш/К препаратов: 1) инициализация 2) обновление 3) сканирование : использование при инвентаризации и в FarmacyCash

---
+++ DATABASE/FARMACY/STRUCTURE/Load/LoadGoodsBarCode.sql
@@ -0,0 +1,47 @@
+/*
+  Создание 
+    - таблицы LoadGoodsBarCode (промежуточная таблица загрузки)
+    - связей
+    - индексов
+*/
+
+-- Table: LoadGoodsBarCode
+
+-- DROP TABLE LoadGoodsBarCode;
+
+/*-------------------------------------------------------------------------------*/
+CREATE TABLE LoadGoodsBarCode
+(
+  Id               serial NOT NULL,
+  GoodsId          Integer,
+  GoodsMainId      Integer,
+  GoodsMorionId    Integer,
+  GoodsBarCodeId   Integer,
+  GoodsJuridicalId Integer,
+  JuridicalId      Integer,
+  Code             Integer,   -- Наш Код товара
+  Name             TVarChar,  -- Название товара
+  ProducerName     TVarChar,  -- Производитель
+  GoodsCode        TVarChar,  -- Код товара поставщика
+  CommonCode       Integer,   -- Код Мориона
+  BarCode          TVarChar,  -- Штрих-код
+  JuridicalName    TVarChar,  -- Поставщик
+  ErrorText        TVarChar,
+  CONSTRAINT pk_LoadGoodsBarCode PRIMARY KEY (Id)
+);
+
+CREATE UNIQUE INDEX idx_LoadGoodsBarCode_Code ON LoadGoodsBarCode (Code);
+CREATE UNIQUE INDEX idx_LoadGoodsBarCode_BarCode ON LoadGoodsBarCode (BarCode);
+
+/*-------------------------------------------------------------------------------*/
+
+
+
+/*
+ ПРИМЕЧАНИЯ:
+ ИСТОРИЯ РАЗРАБОТКИ:
+ ДАТА         АВТОР
+ ----------------
+                 Климентьев К.И.   Кухтин И.В.  Ярошенко Р.Ф.
+ 05.06.17                                          * 
+*/
