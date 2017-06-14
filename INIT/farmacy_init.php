717dbb1ae7d016a8d720bdd10521faf777c14308
innaf <innaf@ua.fm>
2017-06-14 17:32:02 +0300

    ++

--- INIT/farmacy_init.php
+++ INIT/farmacy_init.php
@@ -1,5 +1,5 @@
 <?php
-  $connectstring = "port=5432 dbname=farmacy host=91.210.37.210 user=postgres password=postgres";
-  $host = "http://91.210.37.210/farmacy/index.php";
+  $connectstring = "port=5432 dbname=farmacy_test host=91.210.37.210 user=postgres password=postgres";
+  $host = "http://91.210.37.210/farmacytest/index.php";
   $isArchive = true;
 ?>
