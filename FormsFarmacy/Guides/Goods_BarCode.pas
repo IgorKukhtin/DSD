bbf6aec7217185e34ad224aa4e7c07f14ea939b4
innaf <innaf@ua.fm>
2017-07-11 15:43:16 +0300

    ++

---
+++ FormsFarmacy/Guides/Goods_BarCode.pas
@@ -0,0 +1,74 @@
+unit Goods_BarCode;
+
+interface
+
+uses
+  DataModul, Winapi.Windows, ParentForm, cxGraphics, cxControls, cxLookAndFeels,
+  cxLookAndFeelPainters, cxStyles, cxCustomData, cxFilter, cxData,
+  cxDataStorage, cxEdit, Data.DB, cxDBData, cxCurrencyEdit, cxCheckBox,
+  dsdAddOn, dsdDB, dsdAction, System.Classes, Vcl.ActnList, dxBarExtItems,
+  dxBar, cxClasses, cxPropertiesStore, Datasnap.DBClient, cxGridLevel,
+  cxGridCustomTableView, cxGridTableView, cxGridDBTableView, cxGridCustomView,
+  Vcl.Controls, cxGrid, AncestorGuides, cxPCdxBarPopupMenu, Vcl.Menus, cxPC,
+  Vcl.DBActns, dxSkinsCore, dxSkinsDefaultPainters, dxSkinscxPCPainter,
+  dxSkinsdxBarPainter, dxSkinBlack, dxSkinBlue, dxSkinBlueprint, dxSkinCaramel,
+  dxSkinCoffee, dxSkinDarkRoom, dxSkinDarkSide, dxSkinDevExpressDarkStyle,
+  dxSkinDevExpressStyle, dxSkinFoggy, dxSkinGlassOceans, dxSkinHighContrast,
+  dxSkiniMaginary, dxSkinLilian, dxSkinLiquidSky, dxSkinLondonLiquidSky,
+  dxSkinMcSkin, dxSkinMoneyTwins, dxSkinOffice2007Black, dxSkinOffice2007Blue,
+  dxSkinOffice2007Green, dxSkinOffice2007Pink, dxSkinOffice2007Silver,
+  dxSkinOffice2010Black, dxSkinOffice2010Blue, dxSkinOffice2010Silver,
+  dxSkinPumpkin, dxSkinSeven, dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus,
+  dxSkinSilver, dxSkinSpringTime, dxSkinStardust, dxSkinSummer2008,
+  dxSkinTheAsphaltWorld, dxSkinValentine, dxSkinVS2010, dxSkinWhiteprint,
+  dxSkinXmas2008Blue;
+
+type
+  TGoods_BarCodeForm = class(TAncestorGuidesForm)
+    Code: TcxGridDBColumn;
+    Name: TcxGridDBColumn;
+    NDSKindName: TcxGridDBColumn;
+    MeasureName: TcxGridDBColumn;
+    isErased: TcxGridDBColumn;
+    GoodsGroupName: TcxGridDBColumn;
+    spRefreshOneRecord: TdsdDataSetRefresh;
+    spGet: TdsdStoredProc;
+    mactAfterInsert: TMultiAction;
+    DataSetInsert1: TDataSetInsert;
+    DataSetPost1: TDataSetPost;
+    FormParams: TdsdFormParams;
+    spGetOnInsert: TdsdStoredProc;
+    spRefreshOnInsert: TdsdExecStoredProc;
+    InsertRecord1: TInsertRecord;
+    MinimumLot: TcxGridDBColumn;
+    UpdateDataSet: TdsdUpdateDataSet;
+    spUpdate_Goods_BarCode: TdsdStoredProc;
+    IsClose: TcxGridDBColumn;
+    IsTop: TcxGridDBColumn;
+    PercentMarkup: TcxGridDBColumn;
+    Price: TcxGridDBColumn;
+    isFirst: TcxGridDBColumn;
+    Color_calc: TcxGridDBColumn;
+    RetailCode: TcxGridDBColumn;
+    RetailName: TcxGridDBColumn;
+    bbPublished: TdxBarButton;
+    isMarketToday: TcxGridDBColumn;
+    isSp: TcxGridDBColumn;
+    LastPriceDate: TcxGridDBColumn;
+    CountPrice: TcxGridDBColumn;
+    MorionCode: TcxGridDBColumn;
+    BarCode: TcxGridDBColumn;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+initialization
+  RegisterClass(TGoods_BarCodeForm);
+
+end.
--- TEST/SOURCE/LoadFarmacyFormTest.pas
+++ TEST/SOURCE/LoadFarmacyFormTest.pas
@@ -326,7 +326,10 @@ end;
 
 procedure TLoadFormTest.LoadGoodsFormTest;
 begin
-
+  TdsdFormStorageFactory.GetStorage.Save(GetForm('TGoods_BarCodeForm'));
+  TdsdFormStorageFactory.GetStorage.Load('TGoods_BarCodeForm');
+  exit;
+  //
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TGoodsAllForm'));
   TdsdFormStorageFactory.GetStorage.Load('TGoodsAllForm');
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TGoodsAllRetailForm'));
