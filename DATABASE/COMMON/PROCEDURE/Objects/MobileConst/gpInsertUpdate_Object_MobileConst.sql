6b0e8d55601438687ee6cde397c5dd97e3e3d449
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-07-25 18:17:02 +0300

    Завести новый ObjectDesc - ObjectConstMobile для централизованного ведения констант для мобильного приложения. Описать новую структуру в redmine. Добавить константу версии мобильного приложения т.к. изменение № версии - должно происходить через программу

--- DATABASE/COMMON/PROCEDURE/Objects/MobileConst/gpInsertUpdate_Object_MobileConst.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/MobileConst/gpInsertUpdate_Object_MobileConst.sql
@@ -1,47 +1,40 @@
 -- Function: gpInsertUpdate_ObjObjectect_MobileConst
 
 DROP FUNCTION IF EXISTS gpInsertUpdate_Object_MobileConst (Integer, TVarChar, TVarChar, TVarChar, Integer, Integer, Integer, TFloat, Integer, TVarChar);
+DROP FUNCTION IF EXISTS gpInsertUpdate_Object_MobileConst (Integer, Integer, TVarChar, TVarChar, Integer, Integer, Integer, TFloat, TVarChar);
 
 CREATE OR REPLACE FUNCTION gpInsertUpdate_Object_MobileConst (
  INOUT ioId                Integer  ,
-    IN inType              TVarChar , -- Тип констант. "Public" или "Private"
+    IN inCode              Integer  , -- Значение 0 - для общих настроек; Значение ИД пользователя - для частных настроек
     IN inMobileVersion     TVarChar , -- Версия мобильного приложения. Пример: "1.0.3.625"
     IN inMobileAPKFileName TVarChar , -- Название ".apk" файла мобильного приложения
     IN inOperDateDiff      Integer  , -- На сколько дней позже создавать док Возврат и Приход денег
     IN inReturnDayCount    Integer  , -- Сколько дней принимаются возвраты по старым ценам
     IN inCriticalOverDays  Integer  , -- Количество дней просрочки, после которого формирование заявки невозможно
     IN inCriticalDebtSum   TFloat   , -- Сумма долга, после которого формирование заявки невозможно
-    IN inUserId            Integer  , -- Связь констант для мобильного приложения с пользователем
     IN inSession           TVarChar   -- сессия пользователя
 )
 RETURNS Integer
 AS $BODY$
   DECLARE vbUserId Integer;
+  DECLARE vbDescId Integer;
   DECLARE vbCode Integer;
 BEGIN
       -- проверка прав пользователя на вызов процедуры
       -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_...());
       vbUserId:= lpGetUserBySession (inSession);
+      vbDescId:= 0;
+      vbCode:= COALESCE (inCode, 0);
 
-      IF inType NOT IN (zc_Enum_MobileConst_Public(), zc_Enum_MobileConst_Private())
+      IF vbCode <> 0
       THEN
-           RAISE EXCEPTION 'Неверный тип констант для мобильного приложения: %', COALESCE (inType, 'NULL');
-      END IF;
+           SELECT Object.DescId INTO vbDescId FROM Object WHERE Object.Id = vbCode;
 
-      IF COALESCE (inUserId, 0) <> 0 AND NOT EXISTS (SELECT 1 FROM Object AS Object_User WHERE Object_User.Id = inUserId AND Object_User.DescId = zc_Object_User())
-      THEN
-           RAISE EXCEPTION 'Несуществующий ИД пользователя: %', inUserId;
+           IF vbDescId NOT IN (zc_Object_User())
+           THEN
+                RAISE EXCEPTION 'Для частных настроек необходимо задать пользователя';
+           END IF;
       END IF;
-
-      IF COALESCE (inUserId, 0) = 0 AND inType = zc_Enum_MobileConst_Private() 
-      THEN 
-           RAISE EXCEPTION 'Для частных настроек необходимо задать пользователя';
-      END IF;
-
-      vbCode:= CASE inType 
-                    WHEN zc_Enum_MobileConst_Public() THEN 0::Integer 
-                    WHEN zc_Enum_MobileConst_Private() THEN inUserId
-               END; 
 
       ioId:= NULL; 
 
@@ -49,16 +42,15 @@ BEGIN
       INTO ioId
       FROM Object AS Object_MobileConst
       WHERE Object_MobileConst.DescId = zc_Object_MobileConst()
-        AND Object_MobileConst.ObjectCode = vbCode
-        AND Object_MobileConst.ValueData = inType;
+        AND Object_MobileConst.ObjectCode = vbCode;
 
       IF ioId IS NULL
       THEN 
            -- сохранили <Объект>
-           ioId:= lpInsertUpdate_Object (ioId, zc_Object_MobileConst(), vbCode, inType);
+           ioId:= lpInsertUpdate_Object (ioId, zc_Object_MobileConst(), vbCode, '');
       END IF;
 
-      IF inType = zc_Enum_MobileConst_Public()
+      IF vbCode = 0
       THEN
            IF inMobileVersion IS NOT NULL 
            THEN 
@@ -82,12 +74,6 @@ BEGIN
       -- сохранили св-во <Сумма долга, после которого формирование заявки невозможно>
       PERFORM lpInsertUpdate_ObjectFloat(zc_ObjectFloat_MobileConst_CriticalDebtSum(), ioId, inCriticalDebtSum);
 
-      IF inType = zc_Enum_MobileConst_Private()
-      THEN
-           -- сохранили связь с <Связь констант для мобильного приложения с пользователем>
-           PERFORM lpInsertUpdate_ObjectLink(zc_ObjectLink_MobileConst_User(), ioId, inUserId);
-      END IF;
-
       -- сохранили протокол
       PERFORM lpInsert_ObjectProtocol (ioId, vbUserId);
 END; $BODY$
@@ -103,26 +89,24 @@ END; $BODY$
 -- тест
 /*
 SELECT * FROM gpInsertUpdate_Object_MobileConst (ioId:= 0::Integer
-                                               , inType:= zc_Enum_MobileConst_Public()
-                                               , inMobileVersion:= '1.26.0'::TVarChar
+                                               , inCode:= 0
+                                               , inMobileVersion:= '1.27.0'::TVarChar
                                                , inMobileAPKFileName:= 'ProjectMobile.apk'::TVarChar
                                                , inOperDateDiff:= 0::Integer
                                                , inReturnDayCount:= 14::Integer
                                                , inCriticalOverDays:= 21::Integer
                                                , inCriticalDebtSum:= 1::TFloat
-                                               , inUserId:= 0::Integer
                                                , inSession:= zfCalc_UserAdmin()
                                                 ); 
 
 SELECT * FROM gpInsertUpdate_Object_MobileConst (ioId:= 0::Integer
-                                               , inType:= zc_Enum_MobileConst_Private()
-                                               , inMobileVersion:= '1.26.0'::TVarChar
+                                               , inCode:= lpGetUserBySession (zfCalc_UserAdmin())
+                                               , inMobileVersion:= '1.27.0'::TVarChar
                                                , inMobileAPKFileName:= 'ProjectMobile.apk'::TVarChar
                                                , inOperDateDiff:= 1::Integer
                                                , inReturnDayCount:= 14::Integer
                                                , inCriticalOverDays:= 21::Integer
                                                , inCriticalDebtSum:= 25::TFloat
-                                               , inUserId:= lpGetUserBySession (zfCalc_UserAdmin())
                                                , inSession:= zfCalc_UserAdmin()
                                                 ); 
 */
--- DATABASE/COMMON/PROCEDURE/Objects/MobileConst/lpGet_Object_MobileConst.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/MobileConst/lpGet_Object_MobileConst.sql
@@ -6,32 +6,29 @@ CREATE OR REPLACE FUNCTION lpGet_Object_MobileConst (
     IN inId Integer
 )
 RETURNS TABLE (Id                Integer  
-             , ObjectCode        Integer
-             , ValueData         TVarChar 
+             , Code              Integer
+             , Name              TVarChar 
              , MobileVersion     TVarChar  -- Версия мобильного приложения. Пример: "1.0.3.625"
              , MobileAPKFileName TVarChar  -- Название ".apk" файла мобильного приложения
              , OperDateDiff      Integer   -- На сколько дней позже создавать док Возврат и Приход денег
              , ReturnDayCount    Integer   -- Сколько дней принимаются возвраты по старым ценам
              , CriticalOverDays  Integer   -- Количество дней просрочки, после которого формирование заявки невозможно
              , CriticalDebtSum   TFloat    -- Сумма долга, после которого формирование заявки невозможно
-             , UserId            Integer   -- Связь констант для мобильного приложения с пользователем
-             , UserName          TVarChar
               )
 AS $BODY$
 BEGIN
       RETURN QUERY  
         SELECT Object_MobileConst.Id
-             , Object_MobileConst.ObjectCode
-             , Object_MobileConst.ValueData
+             , Object_MobileConst.ObjectCode                   AS Code
+             , Object_Code.ValueData                           AS Name
              , ObjectString_MobileVersion.ValueData            AS MobileVersion
              , ObjectString_MobileAPKFileName.ValueData        AS MobileAPKFileName
              , ObjectFloat_OperDateDiff.ValueData::Integer     AS OperDateDiff
              , ObjectFloat_ReturnDayCount.ValueData::Integer   AS ReturnDayCount
              , ObjectFloat_CriticalOverDays.ValueData::Integer AS CriticalOverDays
              , ObjectFloat_CriticalDebtSum.ValueData           AS CriticalDebtSum
-             , ObjectLink_User.ChildObjectId                   AS UserId
-             , Object_User.ValueData                           AS UserName
         FROM Object AS Object_MobileConst
+             LEFT JOIN Object AS Object_Code ON Object_Code.Id = Object_MobileConst.ObjectCode
              LEFT JOIN ObjectString AS ObjectString_MobileVersion 
                                     ON ObjectString_MobileVersion.ObjectId = Object_MobileConst.Id
                                    AND ObjectString_MobileVersion.DescId = zc_ObjectString_MobileConst_MobileVersion ()
@@ -50,10 +47,6 @@ BEGIN
              LEFT JOIN ObjectFloat AS ObjectFloat_CriticalDebtSum
                                    ON ObjectFloat_CriticalDebtSum.ObjectId = Object_MobileConst.Id
                                   AND ObjectFloat_CriticalDebtSum.DescId = zc_ObjectFloat_MobileConst_CriticalDebtSum()
-             LEFT JOIN ObjectLink AS ObjectLink_User                     
-                                  ON ObjectLink_User.ObjectId = Object_MobileConst.Id
-                                 AND ObjectLink_User.DescId = zc_ObjectLink_MobileConst_User()
-             LEFT JOIN Object AS Object_User ON Object_User.Id = ObjectLink_User.ChildObjectId                    
         WHERE Object_MobileConst.Id = inId 
           AND Object_MobileConst.DescId = zc_Object_MobileConst()  
         ;
