969ecdb304efb8f26854e7112f4d4d5e98e7b040
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-08-31 16:21:43 +0300

    В папке DOC\ТЗ-Аптеки\Автоматизация создания прайс-листов new - есть описание ТЗ: Medbrowse.docx, XML new.docx - описание нескольких выгрузок для сайта http://neboley.dp.ua/

--- ExportForSiteNeBoley/MainUnit.dfm
+++ ExportForSiteNeBoley/MainUnit.dfm
@@ -13,6 +13,7 @@ object MainForm: TMainForm
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
   object ButtonPanel: TPanel
@@ -27,7 +28,7 @@ object MainForm: TMainForm
       Top = 8
       Width = 97
       Height = 25
-      Caption = #1045#1082#1089#1087#1086#1088#1090
+      Action = SaveAction
       TabOrder = 1
     end
     object RunButton: TButton
@@ -35,7 +36,7 @@ object MainForm: TMainForm
       Top = 8
       Width = 97
       Height = 25
-      Caption = #1057#1092#1086#1088#1084#1080#1088#1086#1074#1072#1090#1100
+      Action = RunAction
       TabOrder = 0
     end
   end
@@ -45,33 +46,106 @@ object MainForm: TMainForm
     Width = 624
     Height = 380
     Align = alClient
+    BorderStyle = cxcbsNone
     TabOrder = 1
     object ExportGridDBTableView1: TcxGridDBTableView
       Navigator.Buttons.CustomButtons = <>
-      DataController.DataSource = DataSource1
+      DataController.DataSource = DataSource
       DataController.Summary.DefaultGroupSummaryItems = <>
       DataController.Summary.FooterSummaryItems = <>
       DataController.Summary.SummaryGroups = <>
+      OptionsData.Deleting = False
+      OptionsData.Editing = False
+      OptionsData.Inserting = False
+      OptionsView.GroupByBox = False
+      OptionsView.Indicator = True
+      object colGoodsCode: TcxGridDBColumn
+        DataBinding.FieldName = 'goodscode'
+      end
+      object colGoodsNameForSite: TcxGridDBColumn
+        DataBinding.FieldName = 'goodsnameforsite'
+        Width = 228
+      end
+      object colPrice: TcxGridDBColumn
+        DataBinding.FieldName = 'price'
+      end
+      object colGoodsURL: TcxGridDBColumn
+        DataBinding.FieldName = 'goodsurl'
+        Width = 247
+      end
     end
     object ExportGridLevel1: TcxGridLevel
       GridView = ExportGridDBTableView1
     end
   end
-  object ZConnection1: TZConnection
+  object ZConnection: TZConnection
     ControlsCodePage = cCP_UTF16
-    Port = 0
+    HostName = '91.210.37.210'
+    Port = 5432
+    Database = 'farmacy_test'
+    User = 'postgres'
+    Password = 'postgres'
+    Protocol = 'postgresql-9'
+    LibraryLocation = 'C:\DSD\DSD.GIT\BIN\libpq.dll'
     Left = 56
     Top = 108
   end
-  object ZReadOnlyQuery1: TZReadOnlyQuery
-    Connection = ZConnection1
+  object ZQuery: TZReadOnlyQuery
+    Connection = ZConnection
+    SQL.Strings = (
+      
+        'SELECT DISTINCT * FROM gpSelect_GoodsOnUnit_ForSite_NeBoley (zfC' +
+        'alc_UserSite())')
     Params = <>
-    Left = 152
+    Left = 148
+    Top = 108
+    object ZQuerygoodscode: TIntegerField
+      DisplayLabel = #1050#1086#1076
+      FieldName = 'goodscode'
+      ReadOnly = True
+    end
+    object ZQuerygoodsnameforsite: TWideMemoField
+      DisplayLabel = #1053#1072#1080#1084#1077#1085#1086#1074#1072#1085#1080#1077
+      FieldName = 'goodsnameforsite'
+      ReadOnly = True
+      BlobType = ftWideMemo
+    end
+    object ZQueryprice: TFloatField
+      DisplayLabel = #1062#1077#1085#1072
+      FieldName = 'price'
+      ReadOnly = True
+    end
+    object ZQuerygoodsurl: TWideMemoField
+      DisplayLabel = 'url'
+      FieldName = 'goodsurl'
+      ReadOnly = True
+      BlobType = ftWideMemo
+    end
+  end
+  object DataSource: TDataSource
+    DataSet = ZQuery
+    Left = 236
     Top = 108
   end
-  object DataSource1: TDataSource
-    DataSet = ZReadOnlyQuery1
-    Left = 256
-    Top = 108
+  object ActionList: TActionList
+    Left = 56
+    Top = 168
+    object RunAction: TAction
+      Caption = #1057#1092#1086#1088#1084#1080#1088#1086#1074#1072#1090#1100
+      OnExecute = RunActionExecute
+      OnUpdate = RunActionUpdate
+    end
+    object SaveAction: TAction
+      Caption = #1045#1082#1089#1087#1086#1088#1090
+      OnExecute = SaveActionExecute
+      OnUpdate = SaveActionUpdate
+    end
+  end
+  object Timer: TTimer
+    Enabled = False
+    Interval = 3000
+    OnTimer = TimerTimer
+    Left = 148
+    Top = 172
   end
 end
--- ExportForSiteNeBoley/MainUnit.pas
+++ ExportForSiteNeBoley/MainUnit.pas
@@ -9,7 +9,7 @@ uses
   cxCustomData, cxFilter, cxData, cxDataStorage, cxEdit, Data.DB, cxDBData, Vcl.StdCtrls,
   cxGridLevel, cxClasses, cxGridCustomView, cxGridCustomTableView, cxGridTableView,
   cxGridDBTableView, cxGrid, Vcl.ExtCtrls, ZAbstractConnection, ZConnection, ZAbstractRODataset,
-  ZDataset;
+  ZDataset, Vcl.ActnList, cxGridExportLink;
 
 type
   TMainForm = class(TForm)
@@ -19,11 +19,34 @@ type
     ExportGrid: TcxGrid;
     ExportButton: TButton;
     RunButton: TButton;
-    ZConnection1: TZConnection;
-    ZReadOnlyQuery1: TZReadOnlyQuery;
-    DataSource1: TDataSource;
+    ZConnection: TZConnection;
+    ZQuery: TZReadOnlyQuery;
+    DataSource: TDataSource;
+    ZQuerygoodscode: TIntegerField;
+    ZQuerygoodsnameforsite: TWideMemoField;
+    ZQueryprice: TFloatField;
+    ZQuerygoodsurl: TWideMemoField;
+    colGoodsCode: TcxGridDBColumn;
+    colGoodsNameForSite: TcxGridDBColumn;
+    colPrice: TcxGridDBColumn;
+    colGoodsURL: TcxGridDBColumn;
+    ActionList: TActionList;
+    RunAction: TAction;
+    SaveAction: TAction;
+    Timer: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure RunActionExecute(Sender: TObject);
+    procedure RunActionUpdate(Sender: TObject);
+    procedure SaveActionExecute(Sender: TObject);
+    procedure SaveActionUpdate(Sender: TObject);
+    procedure TimerTimer(Sender: TObject);
   private
     { Private declarations }
+    FilePath: string;
+    Running, Saving: Boolean;
+    procedure AddToLog(S: string);
+    procedure ExportRun;
+    procedure ExportSave;
   public
     { Public declarations }
   end;
@@ -34,5 +57,159 @@ var
 implementation
 
 {$R *.dfm}
+
+uses
+  System.IniFiles;
+
+procedure TMainForm.AddToLog(S: string);
+var
+  F: TextFile;
+  LogFileName: string;
+begin
+  LogFileName := ChangeFileExt(Application.ExeName, '.log');
+  AssignFile(F, LogFileName);
+
+  if FileExists(LogFileName) then
+    Append(F)
+  else
+    Rewrite(F);
+
+  Writeln(F, FormatDateTime('yyyy.mm.dd hh:mm:ss.zzz', Now) + ' ' + S);
+  CloseFile(F);
+end;
+
+procedure TMainForm.ExportRun;
+var
+  OldCursor: TCursor;
+begin
+  AddToLog('Формирование отчета ...');
+
+  Running := True;
+  Application.ProcessMessages;
+  ZQuery.DisableControls;
+  OldCursor := Screen.Cursor;
+  Screen.Cursor := crHourGlass;
+
+  try
+    ZQuery.Close;
+
+    try
+      ZQuery.Open;
+    except
+      on E: Exception do
+        AddToLog(E.Message);
+    end;
+  finally
+    Screen.Cursor := OldCursor;
+    ZQuery.EnableControls;
+    Running := False;
+  end;
+end;
+
+procedure TMainForm.ExportSave;
+var
+  OldDecimalSeparator: Char;
+begin
+  Saving := True;
+  AddToLog('Выгрузка отчета ...');
+
+  if not ForceDirectories(ExtractFilePath(FilePath)) then
+  begin
+    AddToLog(Format('Не могу создать директорию выгрузки "%s"', [ExtractFilePath(FilePath)]));
+    Saving := False;
+    Exit;
+  end;
+
+  try
+    OldDecimalSeparator := FormatSettings.DecimalSeparator;
+    FormatSettings.DecimalSeparator := '.';
+    ExportGridToText(FilePath, ExportGrid, True, True, ';', '', '', 'CSV');
+    FormatSettings.DecimalSeparator := OldDecimalSeparator;
+    Saving := False;
+  except
+    on E: Exception do
+    begin
+      AddToLog(E.Message);
+      Saving := False;
+    end;
+  end;
+end;
+
+procedure TMainForm.FormCreate(Sender: TObject);
+var
+  IniFile: TIniFile;
+begin
+  Running := False;
+  Saving := False;
+  IniFile := TIniFile.Create(ChangeFileExt(Application.ExeName, '.ini'));
+
+  try
+    FilePath := IniFile.ReadString('Options', 'FilePath', ExtractFilePath(Application.ExeName) + 'neboley.csv');
+    IniFile.WriteString('Options', 'FilePath', FilePath);
+
+    ZConnection.HostName := IniFile.ReadString('Connect', 'HostName', '91.210.37.210');
+    IniFile.WriteString('Connect', 'HostName', ZConnection.HostName);
+
+    ZConnection.Database := IniFile.ReadString('Connect', 'Database', 'farmacy');
+    IniFile.WriteString('Connect', 'Database', ZConnection.Database);
+
+    ZConnection.User := IniFile.ReadString('Connect', 'User', 'postgres');
+    IniFile.WriteString('Connect', 'User', ZConnection.User);
+
+    ZConnection.Password := IniFile.ReadString('Connect', 'Password', 'postgres');
+    IniFile.WriteString('Connect', 'Password', ZConnection.Password);
+  finally
+    IniFile.Free;
+  end;
+
+  ZConnection.LibraryLocation := ExtractFilePath(Application.ExeName) + 'libpq.dll';
+
+  try
+    ZConnection.Connect;
+  except
+    on E: Exception do
+    begin
+      AddToLog(E.Message);
+      Close;
+    end;
+  end;
+
+  if FindCmdLineSwitch('auto') then
+  begin
+    ButtonPanel.Visible := False;
+    Timer.Enabled := True;
+  end;
+end;
+
+procedure TMainForm.RunActionExecute(Sender: TObject);
+begin
+  ExportRun;
+end;
+
+procedure TMainForm.RunActionUpdate(Sender: TObject);
+begin
+  RunAction.Enabled := not Running;
+end;
+
+procedure TMainForm.SaveActionExecute(Sender: TObject);
+begin
+  ExportSave;
+end;
+
+procedure TMainForm.SaveActionUpdate(Sender: TObject);
+begin
+  SaveAction.Enabled := not Saving;
+end;
+
+procedure TMainForm.TimerTimer(Sender: TObject);
+begin
+  try
+    ExportRun;
+    ExportSave;
+  finally
+    Timer.Enabled := False;
+    Close;
+  end;
+end;
 
 end.
