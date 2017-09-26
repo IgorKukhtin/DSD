83e691b1cfd3744ed8e530e9ee72ed961de676f6
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2017-09-26 18:17:35 +0300

    Важно: В папке DOC\ТЗ-Аптеки\Автоматизация создания прайс-листов new - есть описание ТЗ: Medbrowse.docx, XML new.docx - описание нескольких выгрузок для сайта http://neboley.dp.ua/

---
+++ DATABASE/FARMACY/REPORTS/Remains/gpSelect_GoodsOnUnit_ForSite_NeBoley_New.sql
@@ -0,0 +1,111 @@
+-- Function: gpSelect_GoodsOnUnit_ForSite_NeBoley
+
+DROP FUNCTION IF EXISTS gpSelect_GoodsOnUnit_ForSite_NeBoley (TVarChar);
+
+CREATE OR REPLACE FUNCTION gpSelect_GoodsOnUnit_ForSite_NeBoley (
+    IN inSession TVarChar -- сессия пользователя
+)
+RETURNS TABLE (GoodsId          Integer   -- идентификатор товара нашей сети
+             , GoodsCode        Integer   -- код товара нашей сети
+             , GoodsNameForSite TBlob     -- наименование товара для сайта
+             , Price            TFloat    -- минимальная цена среди поставщиков и аптек
+             , GoodsURL         TBlob     -- URL товара
+              )
+AS $BODY$
+  DECLARE vbUserId Integer;
+BEGIN
+     -- проверка прав пользователя на вызов процедуры
+     -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_...());
+     vbUserId:= lpGetUserBySession (inSession);
+
+WITH tmpUnit AS (SELECT Object_Unit.Id AS UnitId
+                 FROM Object AS Object_Unit
+                      JOIN ObjectLink AS ObjectLink_Unit_Juridical
+                                      ON ObjectLink_Unit_Juridical.ObjectId = Object_Unit.Id
+                                     AND ObjectLink_Unit_Juridical.DescId = zc_ObjectLink_Unit_Juridical()
+                      JOIN ObjectLink AS ObjectLink_Juridical_Retail
+                                      ON ObjectLink_Juridical_Retail.ObjectId = ObjectLink_Unit_Juridical.ChildObjectId
+                                     AND ObjectLink_Juridical_Retail.DescId = zc_ObjectLink_Juridical_Retail()
+                                     AND ObjectLink_Juridical_Retail.ChildObjectId = 4 -- торговая сеть "Не болей" 
+                      JOIN ObjectBoolean AS ObjectBoolean_isLeaf
+                                         ON ObjectBoolean_isLeaf.ObjectId = Object_Unit.Id
+                                        AND ObjectBoolean_isLeaf.DescId = zc_ObjectBoolean_isLeaf() 
+                                        AND ObjectBoolean_isLeaf.ValueData = TRUE
+                 WHERE Object_Unit.DescId = zc_Object_Unit()
+                )
+   , tmpUnitPrice AS (SELECT tmpUnit.UnitId
+                           , ObjectLink_Price_Goods.ChildObjectId AS GoodsId
+                           , ObjectFloat_Price_Value.ValueData    AS Price
+                      FROM tmpUnit
+                           JOIN ObjectLink AS ObjectLink_Price_Unit
+                                           ON ObjectLink_Price_Unit.DescId = zc_ObjectLink_Price_Unit()
+                                          AND ObjectLink_Price_Unit.ChildObjectId = tmpUnit.UnitId
+                           JOIN ObjectLink AS ObjectLink_Price_Goods
+                                           ON ObjectLink_Price_Goods.DescId = zc_ObjectLink_Price_Goods()
+                                          AND ObjectLink_Price_Goods.ObjectId = ObjectLink_Price_Unit.ObjectId
+                           JOIN ObjectLink AS ObjectLink_Goods_Object
+                                           ON ObjectLink_Goods_Object.ObjectId = ObjectLink_Price_Goods.ChildObjectId
+                                          AND ObjectLink_Goods_Object.DescId = zc_ObjectLink_Goods_Object()
+                                          AND ObjectLink_Goods_Object.ChildObjectId = 4 -- !!!NeBoley!!!
+                           JOIN ObjectFloat AS ObjectFloat_Price_Value
+                                            ON ObjectFloat_Price_Value.DescId = zc_ObjectFloat_Price_Value()
+                                           AND ObjectFloat_Price_Value.ObjectId = ObjectLink_Price_Unit.ObjectId
+                     )
+   , tmpRemains AS (SELECT Container.ObjectId               AS GoodsId
+                         , MIN (tmpUnitPrice.Price)::TFloat AS Price
+                    FROM Container
+                         JOIN tmpUnitPrice ON tmpUnitPrice.UnitId = Container.WhereObjectId
+                                          AND tmpUnitPrice.GoodsId = Container.ObjectId 
+                    WHERE Container.DescId = zc_Container_Count()
+                      AND Container.Amount <> 0
+                    GROUP BY Container.ObjectId
+                   )
+   , tmpMovementPriceListLast AS (SELECT Movement_PriceList.Id AS MovementId
+                                       , ROW_NUMBER() OVER (PARTITION BY MovementLinkObject_Juridical.ObjectId ORDER BY Movement_PriceList.OperDate DESC) AS RowNum
+                                  FROM Movement AS Movement_PriceList
+                                       JOIN MovementLinkObject AS MovementLinkObject_Juridical 
+                                                               ON MovementLinkObject_Juridical.DescId = zc_MovementLinkObject_Juridical()
+                                                              AND MovementLinkObject_Juridical.MovementId = Movement_PriceList.Id 
+                                       JOIN ObjectLink AS ObjectLink_JuridicalSettings_Juridical
+                                                       ON ObjectLink_JuridicalSettings_Juridical.DescId = zc_ObjectLink_JuridicalSettings_Juridical()
+                                                      AND ObjectLink_JuridicalSettings_Juridical.ChildObjectId = MovementLinkObject_Juridical.ObjectId
+                                       JOIN ObjectLink AS ObjectLink_JuridicalSettings_Retail
+                                                       ON ObjectLink_JuridicalSettings_Retail.DescId = zc_ObjectLink_JuridicalSettings_Retail()
+                                                      AND ObjectLink_JuridicalSettings_Retail.ObjectId = ObjectLink_JuridicalSettings_Juridical.ObjectId
+                                                      AND ObjectLink_JuridicalSettings_Retail.ChildObjectId = 4 
+                                       JOIN ObjectBoolean AS ObjectBoolean_JuridicalSettings_Site
+                                                          ON ObjectBoolean_JuridicalSettings_Site.DescId = zc_ObjectBoolean_JuridicalSettings_Site()
+                                                         AND ObjectBoolean_JuridicalSettings_Site.ObjectId = ObjectLink_JuridicalSettings_Juridical.ObjectId
+                                                         AND ObjectBoolean_JuridicalSettings_Site.ValueData = TRUE
+                                  WHERE Movement_PriceList.DescId = zc_Movement_PriceList()
+                                    AND Movement_PriceList.StatusId <> zc_Enum_Status_Erased()
+                                 )
+   , tmpMovementPriceList AS (SELECT MovementId FROM tmpMovementPriceListLast WHERE RowNum = 1)
+   , tmpPriceList AS (SELECT MI_PriceList.ObjectId             AS GoodsId
+                           , MIN (MI_PriceList.Amount)::TFloat AS Price
+                      FROM tmpMovementPriceList
+                           JOIN MovementItem AS MI_PriceList
+                                             ON MI_PriceList.DescId = zc_MI_Master()
+                                            AND MI_PriceList.MovementId = tmpMovementPriceList.MovementId  
+                      GROUP BY MI_PriceList.ObjectId
+                     )
+   , tmpAll AS (SELECT tmpRemains.GoodsId
+                     , tmpRemains.Price   AS RemainsPrice
+                     , tmpPriceList.Price AS PriceListPrice
+                FROM tmpRemains
+                     JOIN tmpPriceList ON tmpRemains.GoodsId = tmpPriceList.GoodsId
+               )
+SELECT tmpAll.* FROM tmpAll
+
+
+END; $BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Ярошенко Р.Ф.
+ 30.08.17                                                        *
+*/
+
+-- тест
+-- SELECT DISTINCT * FROM gpSelect_GoodsOnUnit_ForSite_NeBoley (inSession:= zfCalc_UserSite());
