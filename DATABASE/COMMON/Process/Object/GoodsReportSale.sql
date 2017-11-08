dfd1af83bb8dca856876f38711e247b3a5d515c5
innaf <innaf@ua.fm>
2017-11-08 11:00:30 +0200

    +

---
+++ DATABASE/COMMON/Process/Object/GoodsReportSale.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION zc_Enum_Process_InsertUpdate_Object_GoodsReportSale() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_InsertUpdate_Object_GoodsReportSale' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
+
+DO $$
+BEGIN
+
+PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_InsertUpdate_Object_GoodsReportSale()
+                                  , inDescId:= zc_Object_Process()
+                                  , inCode:= 1
+                                  , inName:= 'Справочник <'||(SELECT ItemName FROM ObjectDesc WHERE Id = zc_Object_GoodsReportSale())||'> - сохранение данных.'
+                                  , inEnumName:= 'zc_Enum_Process_InsertUpdate_Object_GoodsReportSale');
+
+
+END $$;
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.
+ 08.10.17         *
+*/
