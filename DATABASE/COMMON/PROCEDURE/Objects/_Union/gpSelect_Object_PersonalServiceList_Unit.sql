98b650153d548171e2a95f32984b6436e5e6d8be
innaf <innaf@ua.fm>
2017-12-04 11:15:02 +0200

    ++

--- DATABASE/COMMON/PROCEDURE/Objects/_Union/gpSelect_Object_PersonalServiceList_Unit.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/_Union/gpSelect_Object_PersonalServiceList_Unit.sql
@@ -1,16 +1,15 @@
 -- Function: gpSelect_Object_PersonalServiceList_Unit()
 
 DROP FUNCTION IF EXISTS gpSelect_Object_PersonalServiceList_Unit (TVarChar);
+DROP FUNCTION IF EXISTS gpSelect_Object_PersonalServiceList_Unit (Boolean, TVarChar);
 
 CREATE OR REPLACE FUNCTION gpSelect_Object_PersonalServiceList_Unit(
+    IN inIsShowDel         Boolean ,
     IN inSession           TVarChar     -- сессия пользователя
 )
-RETURNS TABLE (Id Integer, Code Integer, Name TVarChar, ItemName TVarChar, isErased Boolean
-             , InfoMoneyId Integer, InfoMoneyCode Integer, InfoMoneyGroupName TVarChar, InfoMoneyDestinationName TVarChar, InfoMoneyName TVarChar, InfoMoneyName_all TVarChar
-             , PaidKindId Integer, PaidKindName TVarChar
-             , ContractId Integer, ContractCode Integer, ContractNumber TVarChar, ContractStateKindCode Integer, StartDate TDateTime, EndDate TDateTime
-             , ContractTagName TVarChar, ContractKindName TVarChar
-             , OKPO TVarChar
+RETURNS TABLE (Id Integer, Code Integer, Name TVarChar
+             , DescId Integer, DescName TVarChar, isErased Boolean
+             , BranchName TVarChar, PaidKindName TVarChar
               )
 AS
 $BODY$
@@ -22,36 +21,6 @@ BEGIN
      vbUserId := inSession;
 
      RETURN QUERY
-     SELECT Object_Cash.Id
-          , Object_Cash.ObjectCode     
-          , Object_Cash.Valuedata AS Name
-          , ObjectDesc.ItemName
-          , Object_Cash.isErased
-          , View_InfoMoney.InfoMoneyId
-          , View_InfoMoney.InfoMoneyCode
-          , View_InfoMoney.InfoMoneyGroupName
-          , View_InfoMoney.InfoMoneyDestinationName
-          , View_InfoMoney.InfoMoneyName
-          , View_InfoMoney.InfoMoneyName_all
-          , NULL::Integer AS PaidKindId
-          , ''::TVarChar  AS PaidKindName
-          , NULL::Integer AS ContractId
-          , NULL::Integer AS ContractCode
-          , ''::TVarChar  AS ContractNumber
-          , NULL::Integer AS ContractStateKindCode
-          , NULL::TDateTime AS StartDate
-          , NULL::TDateTime AS EndDate
-          , ''::TVarChar AS ContractTagName
-          , ''::TVarChar AS ContractKindName
-          , ''::TVarChar AS OKPO
-     FROM Object AS Object_Cash
-          LEFT JOIN ObjectDesc ON ObjectDesc.Id = Object_Cash.DescId
-          LEFT JOIN View_InfoMoney_40801 AS View_InfoMoney ON 1 = 1
-     WHERE Object_Cash.DescId = zc_Object_Cash()
-       AND Object_Cash.isErased = FALSE
-       
-       
-
         SELECT 
             Object_Unit.Id          AS Id
           , Object_Unit.ObjectCode  AS Code
@@ -61,12 +30,6 @@ BEGIN
           , Object_Unit.isErased
           , Object_Branch.ValueData AS BranchName
           , NULL::TVarChar          AS PaidKindName
-          
-          , NULL::TVarChar          AS UnitName
-          , NULL::Integer           AS PositionCode
-          , NULL::TVarChar          AS PositionName
-          , False::Boolean          AS isDateOut
-
         FROM Object AS Object_Unit
             LEFT JOIN ObjectDesc ON ObjectDesc.Id = Object_Unit.DescId
             LEFT OUTER JOIN ObjectLink AS ObjectLink_Unit_Branch
@@ -114,4 +77,4 @@ $BODY$
 */
 
 -- тест
--- 
+-- select * from gpSelect_Object_PersonalServiceList_Unit (False, '5')
