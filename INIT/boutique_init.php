48e4f1c8f4e404a7b33e37a9d8637b3d78465322
K0sta <Ashtu777@gmail.com>
2017-12-12 09:10:06 +0200

    ok

--- INIT/boutique_init.php
+++ INIT/boutique_init.php
@@ -1,5 +1,5 @@
 <?php
-  $connectstring = "port=5432 dbname=boutique host=192.168.10.3 user=postgres password=postgres";
-  $host = "http://192.168.10.3:8080/boutique/index.php";
+  $connectstring = "port=5432 dbname=boutique host=localhost user=postgres password=postgres";
+  $host = "http://localhost/boutique/index.php";
   $isArchive = true;
 ?>
