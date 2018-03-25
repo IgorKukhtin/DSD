40482e992777fa74df84b4169915e87590229fb2
innaf <innaf@ua.fm>
2018-03-25 22:04:30 +0300

    +

--- DATABASE/Boutique/PROCEDURE/MovementItem/Income/gpGet_MI_Income_OperPriceList.sql
+++ DATABASE/Boutique/PROCEDURE/MovementItem/Income/gpGet_MI_Income_OperPriceList.sql
@@ -1,8 +1,11 @@
 -- Function: gpGet_MI_Income_OperPriceList()
 
 DROP FUNCTION IF EXISTS gpGet_MI_Income_OperPriceList (TFloat, TFloat, TFloat, TVarChar);
+DROP FUNCTION IF EXISTS gpGet_MI_Income_OperPriceList (Integer, Integer, TFloat, TFloat, TFloat, TVarChar);
 
 CREATE OR REPLACE FUNCTION gpGet_MI_Income_OperPriceList(
+    IN inMovementId        Integer  , --
+    IN inGoodsId           Integer  , --
     IN inOperPrice         TFloat   , --
     IN inCountForPrice     TFloat   , --
  INOUT ioOperPriceList     TFloat   , -- 
@@ -12,10 +15,24 @@ RETURNS TFloat
 AS
 $BODY$
    DECLARE vbUserId Integer;
+   DECLARE vbOperPrice TFloat;
 BEGIN
      -- проверка прав пользователя на вызов процедуры
      -- vbUserId:= lpGetUserBySession (inSession);
 
+     inOperPrice := (SELECT MAX(tmp.OperPrice)
+                     FROM (
+                           SELECT inOperPrice AS OperPrice
+                          UNION ALL
+                           SELECT COALESCE (MIFloat_OperPrice.ValueData, 0) AS OperPrice
+                           FROM MovementItem 
+                                LEFT JOIN MovementItemFloat AS MIFloat_OperPrice
+                                                            ON MIFloat_OperPrice.MovementItemId = MovementItem.Id
+                                                           AND MIFloat_OperPrice.DescId         = zc_MIFloat_OperPrice()
+                           WHERE MovementItem.MovementId = inMovementId AND MovementItem.ObjectId = inGoodsId
+                           ) AS tmp
+                    );
+     
      -- округление без копеек и до +/-50 гривен, т.е. последние цифры или 50 или сотни
      ioOperPriceList:= (CAST ( (inOperPrice * zc_Enum_GlobalConst_IncomeKoeff() / inCountForPrice) / 50 AS NUMERIC (16,0)) * 50) :: TFloat;
                 
@@ -26,6 +43,7 @@ $BODY$
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Манько Д.А.  Воробкало А.А.
+ 24.03.18         *
  06.06.17         *
 */
 
