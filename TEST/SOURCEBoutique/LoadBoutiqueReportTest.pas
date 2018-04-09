daf5297d03dc375cb5abd12dd7dd89f9d54d5dc7
innaf <innaf@ua.fm>
2018-04-09 11:54:37 +0300

    +

--- TEST/SOURCEBoutique/LoadBoutiqueReportTest.pas
+++ TEST/SOURCEBoutique/LoadBoutiqueReportTest.pas
@@ -63,18 +63,18 @@ begin
   LoadReportFromFile('PrintReport_ReturnIn', ReportPath + '\PrintReport_ReturnIn.fr3');
   LoadReportFromFile('PrintReport_Uniform', ReportPath + '\PrintReport_Uniform.fr3');
   LoadReportFromFile('PrintReport_Goods_RemainsCurrent', ReportPath + '\PrintReport_Goods_RemainsCurrent.fr3');
-  LoadReportFromFile('Print_Check_GoodsAccount', ReportPath + '\Печать чека оплаты.fr3');
-  LoadReportFromFile('Print_Check', ReportPath + '\Печать чека.fr3');
+  LoadReportFromFile('Print_Check_GoodsAccount', ReportPath + '\Print_Check_GoodsAccount.fr3');
+  LoadReportFromFile('Print_Check', ReportPath + '\Print_Check.fr3');
   LoadReportFromFile('Движение по покупателю (Акт сверки)', ReportPath + '\Движение по покупателю (Акт сверки).fr3');
   LoadReportFromFile('Движение по покупателю', ReportPath + '\Движение по покупателю.fr3');
   LoadReportFromFile('Отчет по расчетам', ReportPath + '\Отчет по расчетам.fr3');
-  LoadReportFromFile('PrintMovement_Income', ReportPath + '\Приход от поставщика.fr3');
-  LoadReportFromFile('PrintMovement_IncomeIn', ReportPath + '\Приход от поставщика вх цена.fr3');
-  LoadReportFromFile('PrintMovement_ReturnOut', ReportPath + '\Возврат поставщику.fr3');
-  LoadReportFromFile('PrintMovement_Send', ReportPath + '\Перемещение.fr3');
-  LoadReportFromFile('PrintMovement_SendIn', ReportPath + '\Перемещение вх цена.fr3');
-  LoadReportFromFile('PrintMovement_Loss', ReportPath + '\Списание.fr3');
-  LoadReportFromFile('PrintMovement_IncomeSticker', ReportPath + '\Печать стикера самоклейки.fr3');
+  LoadReportFromFile('PrintMovement_Income', ReportPath + '\PrintMovement_Income.fr3');
+//  LoadReportFromFile('PrintMovement_IncomeIn', ReportPath + '\Приход от поставщика вх цена.fr3');
+  LoadReportFromFile('PrintMovement_ReturnOut', ReportPath + '\PrintMovement_ReturnOut.fr3');
+  LoadReportFromFile('PrintMovement_Send', ReportPath + '\PrintMovement_Send.fr3');
+  LoadReportFromFile('PrintMovement_SendIn', ReportPath + '\PrintMovement_SendIn.fr3');
+  LoadReportFromFile('PrintMovement_Loss', ReportPath + '\PrintMovement_Loss.fr3');
+  LoadReportFromFile('PrintMovement_IncomeSticker', ReportPath + '\PrintMovement_IncomeSticker.fr3');
 end;
 
 procedure TLoadReportTest.SetUp;
