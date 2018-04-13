5464e690392335c63b9b46981748069be0219a95
K0sta <Ashtu777@gmail.com>
2018-04-13 19:11:10 +0300

    ok

--- INIT/realfarmacy_init.php
+++ INIT/realfarmacy_init.php
@@ -1,5 +1,5 @@
 <?php
   $connectstring = "port=5432 dbname=farmacy host=91.210.37.210 user=postgres password=postgres";
-  $host = "http://91.210.37.210/farmacy/index.php";
+  $host = "http://31.193.90.210/farmacy/index.php";
   $isArchive = true;
 ?>
