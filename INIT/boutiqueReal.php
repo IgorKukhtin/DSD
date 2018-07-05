67e293a1d473ba05ac7b44bf9205fd0bcecb2305
K0sta <Ashtu777@gmail.com>
2018-07-05 19:20:24 +0300

    ok

--- INIT/boutiqueReal.php
+++ INIT/boutiqueReal.php
@@ -1,5 +1,6 @@
 <?php
    $connectstring = "port=5432 dbname=boutique host=212.115.238.8 user=postgres password=ET862tira";
-   $host = "http://212.115.238.8/boutique/index.php";
+   $host = "http://94.27.55.146/boutique/index.php";
    $isArchive = true;
 ?>
+;212.115.238.8
