e3304d526b2097287d43dcb4116daf929641c582
K0sta <Ashtu777@gmail.com>
2018-08-29 22:00:17 +0300

    ok

--- DATABASE/COMMON/PROCEDURE/Objects/ToolsWeighing/Scale/gpSelect_Scale_Partner.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/ToolsWeighing/Scale/gpSelect_Scale_Partner.sql
@@ -193,8 +193,8 @@ BEGIN
                             SELECT lfSelect.LocationId FROM lfSelect_Object_Unit_List (8382) AS lfSelect -- Админ
                            UNION 
                             SELECT lfSelect.LocationId FROM lfSelect_Object_Unit_List (8427) AS lfSelect WHERE lfSelect.LocationId <> 8429 -- Общефирменные + Отдел логистики
-                           UNION 
-                            SELECT lfSelect.LocationId FROM lfSelect_Object_Unit_List (8432) AS lfSelect -- Общепроизводственные
+                           -- UNION 
+                           --  SELECT lfSelect.LocationId FROM lfSelect_Object_Unit_List (8432) AS lfSelect -- Общепроизводственные
                             
                            )
           , tmpMember AS (SELECT lfSelect.MemberId        AS MemberId
