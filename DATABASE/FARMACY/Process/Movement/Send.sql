9b574253aa0cc67fc355029ea9a5ed10c631de51
K0sta <Ashtu777@gmail.com>
2018-09-18 15:24:55 +0300

    ok

--- DATABASE/FARMACY/Process/Movement/Send.sql
+++ DATABASE/FARMACY/Process/Movement/Send.sql
@@ -1,5 +1,6 @@
 -- Status
 CREATE OR REPLACE FUNCTION zc_Enum_Process_CompleteDate_Send() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_CompleteDate_Send' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql IMMUTABLE;
+CREATE OR REPLACE FUNCTION zc_Enum_Process_Insert_MI_Send_Remains() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_Insert_MI_Send_Remains' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql IMMUTABLE;
 
 DO $$
 
@@ -12,6 +13,11 @@ PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_CompleteDate_Send()
                                   , inName:= 'Документ <'||(SELECT ItemName FROM MovementDesc WHERE Id = zc_Movement_Send())||'> - Проведение задним числом.'
                                   , inEnumName:= 'zc_Enum_Process_CompleteDate_Send');
 
+PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_Insert_MI_Send_Remains()
+                                  , inDescId:= zc_Object_Process()
+                                  , inCode:= 1
+                                  , inName:= 'Элемент документа <'||(SELECT ItemName FROM MovementDesc WHERE Id = zc_Movement_OrderInternal())||'> - Списание всего остаток с точки.'
+                                  , inEnumName:= 'zc_Enum_Process_Insert_MI_Send_Remains');
 END $$;
 
 
