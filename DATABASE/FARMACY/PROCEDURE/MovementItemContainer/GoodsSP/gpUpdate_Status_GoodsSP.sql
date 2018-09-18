f446b85cd9b240bb08294f8a3838be62d28a6130
innaf <innaf@ua.fm>
2018-09-18 14:22:37 +0300

    ++

--- DATABASE/FARMACY/PROCEDURE/MovementItemContainer/GoodsSP/gpUpdate_Status_GoodsSP.sql
+++ DATABASE/FARMACY/PROCEDURE/MovementItemContainer/GoodsSP/gpUpdate_Status_GoodsSP.sql
@@ -14,7 +14,7 @@ BEGIN
          WHEN zc_Enum_StatusCode_UnComplete() THEN
             PERFORM gpUnComplete_Movement_GoodsSP (inMovementId, inSession);
          WHEN zc_Enum_StatusCode_Complete() THEN
-            PERFORM gpComplete_Movement_GoodsSP (inMovementId, TRUE, inSession);
+            PERFORM gpComplete_Movement_GoodsSP (inMovementId, inSession);
          WHEN zc_Enum_StatusCode_Erased() THEN
             PERFORM gpSetErased_Movement_GoodsSP (inMovementId, inSession);
          ELSE
