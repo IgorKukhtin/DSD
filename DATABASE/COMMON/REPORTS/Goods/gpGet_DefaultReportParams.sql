bfd27b069e295dbe70fb9bfadee292dbbfab2487
innaf <innaf@ua.fm>
2019-01-10 00:31:32 +0200

    +

--- DATABASE/COMMON/REPORTS/Goods/gpGet_DefaultReportParams.sql
+++ DATABASE/COMMON/REPORTS/Goods/gpGet_DefaultReportParams.sql
@@ -60,13 +60,11 @@ BEGIN
                 , Object_GoodsGroup.Id           AS GoodsGroupId
                 , Object_GoodsGroup.ValueData    AS GoodsGroupname
 
-           FROM Object AS Object_Unit
-                LEFT JOIN Object AS Object_UnitGroup ON Object_UnitGroup.Id = 8460        -- группа складов Возвраты общие
-                LEFT JOIN Object AS Object_GoodsGroupGP ON Object_GoodsGroupGP.Id = 1832  -- группа товаров ГП
+           FROM Object AS Object_GoodsGroupGP 
                 LEFT JOIN Object AS Object_GoodsGroup ON Object_GoodsGroup.Id = 1979      -- группа товаров Тушенка
-           WHERE Object_Unit.Id = 8459                                                    -- Склад Реализации
+           WHERE Object_GoodsGroupGP.Id = 1832                                            -- группа товаров ГП
             AND inReportName = 'Report_Goods_byMovementReal'
-    
+    ;
  
 END;
 $BODY$
@@ -75,8 +73,10 @@ $BODY$
 /*-------------------------------------------------------------------------------
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 09.01.19         *
  04.12.16         *  
 */
 
 -- тест
 -- SELECT * FROM gpGet_DefaultReportParams (inReportName:= '', inSession:= '5');
+-- select * from gpGet_DefaultReportParams(inReportName := 'Report_Goods_byMovementReal' ,  inSession := '5');
