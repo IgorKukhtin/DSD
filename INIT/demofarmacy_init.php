557063cd7072c1ef2f390fb341c1e828dd24e53a
K0sta <Ashtu777@gmail.com>
2019-02-20 20:53:25 +0200

    ok

--- INIT/demofarmacy_init.php
+++ INIT/demofarmacy_init.php
@@ -1,5 +1,5 @@
 <?php
-  $connectstring = "port=5432 dbname=dev2 host=172.17.2.36 user=postgres password=eej9oponahT4gah3";
+  $connectstring = "port=5432 dbname=farmacy_demo host=172.17.2.36 user=postgres password=eej9oponahT4gah3";
   $host = "http://localhost/farmacyDemo/index.php";
   $isArchive = true;
 ?>
