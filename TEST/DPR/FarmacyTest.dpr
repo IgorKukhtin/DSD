1a6d05481ee9eb0bc8c0dddde04c563774ca663b
innaf <innaf@ua.fm>
2019-04-17 08:28:11 +0300

    +

--- TEST/DPR/FarmacyTest.dpr
+++ TEST/DPR/FarmacyTest.dpr
@@ -701,7 +701,7 @@ begin
 
   if FindCmdLineSwitch('realfarmacy', true)
   then gc_AdminPassword := 'Админ1234'
-  else gc_AdminPassword := 'Админ1234';
+  else gc_AdminPassword := 'Админ1111';
 
   gc_ProgramName := 'Farmacy.exe';
 
