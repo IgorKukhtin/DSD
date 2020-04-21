5ff22c56868f606545338d4c5f6e9bcdc9fd2d2b
Oleg <olegsh1264@gmail.com>
2020-04-21 10:49:07 +0300

    Роль

---
+++ DATABASE/FARMACY/PROCEDURE/MovementItem/Check/gpUpdate_MovementItem_Check_NDSKindId.sql
@@ -0,0 +1,74 @@
+ -- Function: gpUpdate_MovementItem_Check_NDSKindId()
+
+DROP FUNCTION IF EXISTS gpUpdate_MovementItem_Check_NDSKindId (Integer, Integer, Integer, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpUpdate_MovementItem_Check_NDSKindId(
+    IN inId                  Integer   , -- Ключ объекта <строка документа>
+    IN inMovementId          Integer   , -- Ключ объекта <Документ>
+    IN inNDSKindId           Integer   , --  вид НДС
+    IN inSession             TVarChar    -- сессия пользователя
+)
+RETURNS VOID AS
+$BODY$
+   DECLARE vbUserId Integer;
+   DECLARE vbStatusId Integer;
+BEGIN
+
+    -- проверка прав пользователя на вызов процедуры
+    -- PERFORM lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_MovementItem_Income());
+    vbUserId := lpGetUserBySession (inSession);
+
+    IF 3 <> inSession::Integer AND 375661 <> inSession::Integer AND 4183126 <> inSession::Integer AND 
+      8001630 <> inSession::Integer AND 9560329 <> inSession::Integer
+    THEN
+      RAISE EXCEPTION 'Изменение <Вид НДС> вам запрещено.';
+    END IF;
+
+    SELECT 
+      StatusId
+    INTO
+      vbStatusId
+    FROM Movement 
+    WHERE Id = inMovementId;
+            
+    IF COALESCE(inMovementId,0) = 0
+    THEN
+        RAISE EXCEPTION 'Документ не записан.';
+    END IF;
+
+    IF vbStatusId <> zc_Enum_Status_UnComplete() 
+    THEN
+        RAISE EXCEPTION 'Ошибка.Изменение подразделения в статусе <%> не возможно.', lfGet_Object_ValueData (vbStatusId);
+    END IF;
+
+    -- Провкряем элемент по документу
+    IF COALESCE (inId, 0) = 0
+       OR NOT EXISTS (SELECT 1 FROM MovementItem WHERE Id = inId)
+    THEN
+        RAISE EXCEPTION 'Не найден элемент по документe';
+    END IF;
+
+    -- Находим элемент по документу и товару
+    IF COALESCE (inMovementId, 0) = 0
+       OR NOT EXISTS (SELECT 1 FROM MovementItem WHERE Id = inId)
+    THEN
+        RAISE EXCEPTION 'Не задан документ или неправельная связь';
+    END IF;
+
+    -- сохранили <Элемент документа>
+    PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_NDSKind(), inId, inNDSKindId);
+
+    -- сохранили протокол
+    PERFORM lpInsert_MovementItemProtocol (inId, vbUserId, False);
+  
+END;
+$BODY$
+  LANGUAGE PLPGSQL VOLATILE;
+ALTER FUNCTION gpUpdate_MovementItem_Check_NDSKindId (Integer, Integer, Integer, TVarChar) OWNER TO postgres;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Шаблий О.В.
+ 19.04.20                                                       *
+*/
+
