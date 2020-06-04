325610f2b507f205440583c15b661da1bf9d52db
K0sta <Ashtu777@gmail.com>
2020-06-04 14:36:20 +0300

    wms_to_host_message

--- DATABASE/COMMON/PROCEDURE/Wms/Import/Create Table wms_to_host_message.sql
+++ DATABASE/COMMON/PROCEDURE/Wms/Import/Create Table wms_to_host_message.sql
@@ -1,17 +1,35 @@
+/*
+  Создание 
+    - таблицы wms_to_host_message
+    - связей
+    - индексов
+*/
+
+-- DROP TABLE wms_to_host_message;
+
+
+/*-------------------------------------------------------------------------------*/
 CREATE TABLE wms_to_host_message
     (
-    id               SERIAL NOT NULL PRIMARY KEY,
-    type             TVarChar NOT NULL,   -- значение to_host_header_message.Type, например 'receiving_result'
-    header_id        integer  NOT NULL,
-    detail_id        integer  NOT NULL,
-    movement_id      integer,  -- order_id -> Movement.id или <receiving_result.inc_id> -> wms_MI_Incoming.id 
-    sku_id           integer, -- уникальный код товара
-    name             TVarChar, -- имя груза 
-    qty              numeric,
-    weight           numeric,
-    weight_biz       numeric,
-    production_date  TDateTime,
-	operdate         TDateTime,
-    insertDate       TDateTime DEFAULT clock_timestamp(),
-	done             boolean DEFAULT false
+    Id               SERIAL   NOT NULL PRIMARY KEY,
+    Type             TVarChar NOT NULL,   -- значение to_host_header_message.Type
+    Header_Id        Integer  NOT NULL,
+    Detail_Id        Integer  NOT NULL,
+    MovementId       Integer,            -- order_id -> Movement.id или <receiving_result.inc_id> -> wms_MI_Incoming.id 
+    Sku_Id           Integer,            -- уникальный код товара
+    Name             TVarChar,           -- имя груза 
+    Qty              TFloat,
+    Weight           TFloat,
+    Weight_biz       TFloat,
+    Production_Date  TDateTime,
+    OperDate         TDateTime,          -- Дата, время когда документ проведен в WMS, т.е. в oracle это from_host_header_message.CREATED
+    InsertDate       TDateTime DEFAULT CLOCK_TIMESTAMP(),
+    Done             Boolean   DEFAULT FALSE
 );
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.    Скородумов С.
+ 04.06.20                                        *
+ 03.06.20                                                          *
+*/
