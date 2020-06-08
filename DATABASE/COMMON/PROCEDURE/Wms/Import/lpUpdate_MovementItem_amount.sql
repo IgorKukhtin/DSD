847bd526fe3bb673cfcbfc5576b736d2935025a4
K0sta <Ashtu777@gmail.com>
2020-06-09 00:23:24 +0300

    lpUpdate_MovementItem_amount

--- DATABASE/COMMON/PROCEDURE/Wms/Import/lpUpdate_MovementItem_amount.sql
+++ DATABASE/COMMON/PROCEDURE/Wms/Import/lpUpdate_MovementItem_amount.sql
@@ -6,109 +6,108 @@ DROP FUNCTION IF EXISTS lpUpdate_MovementItem_amount (Integer, TVarChar, TVarCha
 
 CREATE OR REPLACE FUNCTION lpUpdate_MovementItem_amount (
     IN inIncomingId    Integer,   -- номер задания на упаковку
-    IN inName          TVarChar,  -- имя груза   
+    IN inName          TVarChar,  -- имя груза
     IN inSession       TVarChar   -- сессия пользователя
 )
 RETURNS VOID
 AS
-$BODY$ 
+$BODY$
   DECLARE vbMovementId Integer;
   DECLARE vbOperDate   TDateTime;
   DECLARE vbFromId     Integer;
   DECLARE vbToId       Integer;
   DECLARE vbPaidKindId Integer;
-  DECLARE vbContractId Integer;    
+  DECLARE vbContractId Integer;
 BEGIN
 
-    WITH CheckAmount AS (
-        -- выбираем записи из таб. MovementItem, которые соответствуют inIncomingId и у которых amount отличается от qty из сообщения 'receiving_result'  
-        SELECT MI.Id, InMsg.Qty 
-        FROM wms_MI_Incoming AS Incoming
-            INNER JOIN wms_to_host_message AS InMsg ON Incoming.Id = InMsg.MovementId 
-            INNER JOIN wms_Movement_WeighingProduction AS Movement
-                                                       ON Movement.OperDate    = Incoming.OperDate
-                                                      AND Movement.GoodsId     = Incoming.GoodsId
-                                                      AND Movement.GoodsKindId = Incoming.GoodsKindId 
-            INNER JOIN wms_MI_WeighingProduction AS MI_WP 
-                                                 ON MI_WP.MovementId      = Movement.Id  
-                                                AND MI_WP.GoodsTypeKindId = Incoming.GoodsTypeKindId
-            INNER JOIN Object AS Object_BarCodeBox ON Object_BarCodeBox.Id = MI_WP.BarCodeBoxId
-            INNER JOIN MovementItem AS MI ON MI.MovementId = MI_WP.parentid   
-            
-        WHERE Incoming.Id                 = inIncomingId  
-          AND Object_BarCodeBox.ValueData = inName 
-          AND MI.amount <> InMsg.Qty    
-    )
+    WITH tmpMI AS (-- данные из receiving_result
+                   SELECT MI.Id
+                        , CASE WHEN ObjectLink_Goods_Measure.ChildObjectId = zc_Measure_Sh() THEN wms_message.Qty ELSE wms_message.Weight END AS Amount
+                   FROM wms_MI_Incoming AS MI_Incoming
+                        INNER JOIN wms_to_host_message AS wms_message ON wms_message.MovementId = MI_Incoming.Id
+                        INNER JOIN wms_Movement_WeighingProduction AS Movement_WP
+                                                                   ON Movement_WP.OperDate    = MI_Incoming.OperDate
+                                                                  AND Movement_WP.GoodsId     = MI_Incoming.GoodsId
+                                                                  AND Movement_WP.GoodsKindId = MI_Incoming.GoodsKindId
+                        INNER JOIN wms_MI_WeighingProduction AS MI_WP
+                                                             ON MI_WP.MovementId      = Movement_WP.Id
+                                                            AND MI_WP.GoodsTypeKindId = MI_Incoming.GoodsTypeKindId
+                        INNER JOIN Object AS Object_BarCodeBox ON Object_BarCodeBox.Id        = MI_WP.BarCodeBoxId
+                                                              AND Object_BarCodeBox.ValueData = inName
+                        LEFT JOIN ObjectLink AS ObjectLink_Goods_Measure
+                                             ON ObjectLink_Goods_Measure.ObjectId = MI_WP.GoodsId
+                                            AND ObjectLink_Goods_Measure.DescId   = zc_ObjectLink_Goods_Measure()
+                        INNER JOIN Movement ON Movement.Id = MI_WP.ParentId
+                        INNER JOIN MovementItem AS MI
+                                                ON MI.MovementId = Movement.Id
+                                               AND MI.DescId     = zc_MI_Master()
+                                               -- только если изменилось кол-во
+                                               AND MI.Amount <> CASE WHEN ObjectLink_Goods_Measure.ChildObjectId = zc_Measure_Sh() THEN wms_message.Qty ELSE wms_message.Weight END
+                   WHERE MI_Incoming.Id = inIncomingId
+                  )
+    -- обновляем MovementItem.amount значениями из таб. CheckAmount
+    UPDATE MovementItem SET Amount = tmpMI.Amount
+    FROM tmpMI
+    WHERE MovementItem.Id = tmpMI.Id;
 
-  -- обновляем MovementItem.amount значениями из таб. CheckAmount
-  UPDATE MovementItem
-  SET    amount = CheckAmount.Qty 
-  FROM   CheckAmount
-  WHERE  MovementItem.Id = CheckAmount.Id;
-  
-  
- 
-  -- нужно обновить дату операции в таб. Movement на значение, которое вернул WMS в to_host_header_message.START_DATE для сообщения 'receiving_result'
-  
-  -- получим ключ имеющегося документа и дату операции, значение которой сохранено в ALAN wms_to_host_message.operdate
-  SELECT Movement.Id, InMsg.OperDate 
-  INTO   vbMovementId, vbOperDate
-  FROM   wms_MI_Incoming AS Incoming
-  
-    INNER JOIN wms_to_host_message AS InMsg ON Incoming.Id = InMsg.MovementId 
-    
-    INNER JOIN wms_Movement_WeighingProduction AS Movement_WP
-                                               ON Movement_WP.OperDate    = Incoming.OperDate
-                                              AND Movement_WP.GoodsId     = Incoming.GoodsId
-                                              AND Movement_WP.GoodsKindId = Incoming.GoodsKindId 
-                                              
-    INNER JOIN wms_MI_WeighingProduction AS MI_WP 
-                                         ON MI_WP.MovementId      = Movement_WP.Id  
-                                        AND MI_WP.GoodsTypeKindId = Incoming.GoodsTypeKindId
-                                        
-    INNER JOIN Object AS Object_BarCodeBox ON Object_BarCodeBox.Id = MI_WP.BarCodeBoxId
-    INNER JOIN MovementItem AS MI ON MI.MovementId = MI_WP.parentid  
-    INNER JOIN Movement ON Movement.Id = MI.MovementId    
 
-  WHERE Incoming.Id                 = inIncomingId  
-    AND Object_BarCodeBox.ValueData = inName 
-    AND MI.amount <> InMsg.Qty;   
-    
- 
-  -- эти поля взяли из Заявки
-  SELECT gpGet.FromId
-       , gpGet.ToId
-       , gpGet.PaidKindId
-       , gpGet.ContractId 
-         INTO vbFromid
-            , vbToid
-            , vbPaidKindId
-            , vbContractId  
-  FROM gpGet_Movement_OrderExternal (inMovementId := vbMovementId
-                                   , inOperDate   := CURRENT_TIMESTAMP
-                                   , inSession    := inSession
-                                    ) AS gpGet; 
- 
-  -- выполняем обновление даты операции в существующем документе 
-  PERFORM gpInsertUpdate_Scale_Movement (inId                := COALESCE(vbMovementId, -1) -- если vbMovementId=NULL нужно предотвратить создание нового документа (поэтому передаем -1, не 0) 
-                                      , inOperDate           := COALESCE(vbOperDate, CURRENT_TIMESTAMP)
-                                      , inMovementDescId     := 0
-                                      , inMovementDescNumber := 0
-                                      , inFromId             := vbFromId
-                                      , inToId               := vbToId
-                                      , inContractId         := vbContractId
-                                      , inPaidKindId         := vbPaidkindId
-                                      , inPriceListId        := 0
-                                      , inMovementId_order   := vbMovementId
-                                      , inChangePercent      := 0
-                                      , inBranchCode         := 0
-                                      , inSession            := inSession
-                                       );
+
+     -- нужно обновить дату операции в таб. Movement на значение, которое вернул WMS в to_host_header_message.START_DATE для сообщения 'receiving_result'
+
+
+    -- выполняем обновление даты операции в существующем документе
+    PERFORM gpInsertUpdate_Scale_Movement (inId                 := tmpMI.MovementId
+                                         , inOperDate           := tmpMI.OperDate
+                                         , inMovementDescId     := 0
+                                         , inMovementDescNumber := 0
+                                         , inFromId             := MovementLinkObject_From.ObjectId
+                                         , inToId               := MovementLinkObject_To.ObjectId
+                                         , inContractId         := MovementLinkObject_Contract.ObjectId
+                                         , inPaidKindId         := MovementLinkObject_PaidKind.ObjectId
+                                         , inPriceListId        := 0
+                                         , inMovementId_order   := NULL
+                                         , inChangePercent      := 0
+                                         , inBranchCode         := 0
+                                         , inSession            := inSession
+                                          )
+    FROM (-- данные из receiving_result
+          SELECT DISTINCT
+                 MI_WP.ParentId       AS MovementId
+               , wms_message.StarDate AS OperDate
+          FROM wms_MI_Incoming AS MI_Incoming
+               INNER JOIN wms_to_host_message AS wms_message ON wms_message.MovementId = MI_Incoming.Id
+               INNER JOIN wms_Movement_WeighingProduction AS Movement_WP
+                                                          ON Movement_WP.OperDate    = MI_Incoming.OperDate
+                                                         AND Movement_WP.GoodsId     = MI_Incoming.GoodsId
+                                                         AND Movement_WP.GoodsKindId = MI_Incoming.GoodsKindId
+               INNER JOIN wms_MI_WeighingProduction AS MI_WP
+                                                    ON MI_WP.MovementId      = Movement_WP.Id
+                                                   AND MI_WP.GoodsTypeKindId = MI_Incoming.GoodsTypeKindId
+               INNER JOIN Object AS Object_BarCodeBox ON Object_BarCodeBox.Id        = MI_WP.BarCodeBoxId
+                                                     AND Object_BarCodeBox.ValueData = inName
+               LEFT JOIN ObjectLink AS ObjectLink_Goods_Measure
+                                    ON ObjectLink_Goods_Measure.ObjectId = MI_WP.GoodsId
+                                   AND ObjectLink_Goods_Measure.DescId   = zc_ObjectLink_Goods_Measure()
+          WHERE MI_Incoming.Id = inIncomingId
+         ) AS tmpMI
+         LEFT JOIN MovementLinkObject AS MovementLinkObject_From
+                                      ON MovementLinkObject_From.MovementId = tmpMI.MovementId
+                                     AND MovementLinkObject_From.DescId     = zc_MovementLinkObject_From()
+         LEFT JOIN MovementLinkObject AS MovementLinkObject_To
+                                      ON MovementLinkObject_To.MovementId = tmpMI.MovementId
+                                     AND MovementLinkObject_To.DescId     = zc_MovementLinkObject_To()
+         LEFT JOIN MovementLinkObject AS MovementLinkObject_Contract
+                                      ON MovementLinkObject_Contract.MovementId = tmpMI.MovementId
+                                     AND MovementLinkObject_Contract.DescId     = zc_MovementLinkObject_Contract()
+         LEFT JOIN MovementLinkObject AS MovementLinkObject_PaidKind
+                                      ON MovementLinkObject_PaidKind.MovementId = tmpMI.MovementId
+                                     AND MovementLinkObject_PaidKind.DescId     = zc_MovementLinkObject_PaidKind()
+    ;
 
 END;
 $BODY$
  LANGUAGE PLPGSQL VOLATILE;
- 
+
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.    Скородумов С.Г.
