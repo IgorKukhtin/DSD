1fdf4336f0beee7e4011fc0fad7e1facc16f168c
sergey <tartuff22@gmail.com>
2020-06-10 22:07:22 +0300

    -реализована проверка входных данных импорта для пакета receiving_result -в приложении реализована обработка сообщений об ошибках во входных данных

--- DATABASE/COMMON/PROCEDURE/Wms/Import/receiving_result/gpUpdate_wms_receiving_result.sql
+++ DATABASE/COMMON/PROCEDURE/Wms/Import/receiving_result/gpUpdate_wms_receiving_result.sql
@@ -11,8 +11,56 @@ CREATE OR REPLACE FUNCTION gpUpdate_wms_receiving_result (
 )
 RETURNS VOID    
 AS
-$BODY$         
+$BODY$      
+  DECLARE vbIncomingId Integer;   
 BEGIN
+     -- ищем наш Id задания на упаковку, который вернул WMS в пакете <receiving_result> в атрибуте 'inc_id'    
+     vbIncomingId:= (SELECT DISTINCT MI_Incoming.Id
+                     FROM wms_to_host_message AS wms_message
+                          -- наш Id задания на упаковку 
+                          INNER JOIN wms_MI_Incoming AS MI_Incoming ON MI_Incoming.Id = wms_message.MovementId
+
+                     WHERE wms_message.Id = inId
+                    );     
+                    
+     -- если наш Id задания на упаковку не найден, прекращаем дальнейшую обрабоку пакета
+     IF COALESCE (vbIncomingId, 0) = 0
+     THEN
+         RAISE EXCEPTION 'Site="W" Procname="gpUpdate_wms_receiving_result()" Descr="В пакете <receiving_result> WMS вернул inc_id = <%>, который не найден в таблице wms_MI_Incoming"'
+                        , (SELECT wms_message.MovementId FROM wms_to_host_message AS wms_message WHERE wms_message.Id = inId)
+                         ;
+     END IF;     
+     
+     
+     -- ищем наш Id задания на упаковку который вернул WMS в пакете <receiving_result> в атрибуте 'inc_id' и который согласуется с именем груза - Ш/К ящика в атрибуте 'name' 
+     vbIncomingId:= (SELECT DISTINCT MI_Incoming.Id
+                     FROM wms_to_host_message AS wms_message
+                          -- наш Id задания на упаковку
+                          INNER JOIN wms_MI_Incoming AS MI_Incoming ON MI_Incoming.Id = wms_message.MovementId
+
+                          INNER JOIN wms_Movement_WeighingProduction AS Movement_WP
+                                                                     ON Movement_WP.OperDate    = MI_Incoming.OperDate
+                                                                    AND Movement_WP.GoodsId     = MI_Incoming.GoodsId
+                                                                    AND Movement_WP.GoodsKindId = MI_Incoming.GoodsKindId
+                          INNER JOIN wms_MI_WeighingProduction AS MI_WP
+                                                               ON MI_WP.MovementId      = Movement_WP.Id
+                                                              AND MI_WP.GoodsTypeKindId = MI_Incoming.GoodsTypeKindId
+
+                          INNER JOIN Object AS Object_BarCodeBox ON Object_BarCodeBox.Id        = MI_WP.BarCodeBoxId
+                                                                -- имя груза - Ш/К ящика
+                                                                AND Object_BarCodeBox.ValueData = wms_message.Name
+                     WHERE wms_message.Id = inId
+                    );   
+
+     -- если наш Id задания на упаковку не найден, прекращаем дальнейшую обрабоку пакета
+     IF COALESCE (vbIncomingId, 0) = 0
+     THEN
+         RAISE EXCEPTION 'Site="W" Procname="gpUpdate_wms_receiving_result()" Descr="В пакете <receiving_result> WMS вернул Id задания на упаковку = <%>, имя груза = <%>, которые не согласуются с данными в нашей базе данных"'
+                        , (SELECT wms_message.MovementId FROM wms_to_host_message AS wms_message WHERE wms_message.Id = inId)
+                        , (SELECT wms_message.Name       FROM wms_to_host_message AS wms_message WHERE wms_message.Id = inId)
+                         ;
+     END IF;                    
+     
 
      -- обновили StatusId_Wms - отметили что наш Груз был принят
      PERFORM lpUpdate_wms_receiving_result_MI (inId      := inId
@@ -34,6 +82,7 @@ $BODY$
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.    Скородумов С.Г.
+ 10.06.20                                                          *              
  09.06.20                                        *               
  08.06.20                                                          *
  05.06.20                                                          *
