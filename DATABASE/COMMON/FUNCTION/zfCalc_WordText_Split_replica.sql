e97c9a4443b26ee530aeb41ed2836a999d8d9b59
innaf <innaf@ua.fm>
2020-07-03 22:19:51 +0300

    +replica

---
+++ DATABASE/COMMON/FUNCTION/zfCalc_WordText_Split_replica.sql
@@ -0,0 +1,50 @@
+-- Function: zfCalc_WordText_Split_replica
+
+DROP FUNCTION IF EXISTS zfCalc_WordText_Split_replica (Text, Integer);
+
+CREATE OR REPLACE FUNCTION zfCalc_WordText_Split_replica(
+    IN inValue Text,
+    IN inIndex Integer
+)
+RETURNS Text
+AS
+$BODY$
+   DECLARE vbResult TVarChar;
+   DECLARE vbIndex Integer;
+   DECLARE vbIndex_find Integer;
+BEGIN
+    vbIndex:= 1;
+    vbIndex_find:= 0;
+ 
+    inValue := REPLACE (REPLACE (inValue,'{',''), '}','');
+        -- старт
+        WHILE vbIndex <= LENGTH (inValue) AND vbIndex_find <> inIndex
+        LOOP
+           IF TRIM (SPLIT_PART (inValue, ',', vbIndex)) <> ''
+           THEN vbIndex_find:= vbIndex_find + 1; -- нашли не пустое слово
+           END IF;
+
+           IF vbIndex_find = inIndex
+           THEN vbResult:= TRIM (SPLIT_PART (inValue, ',', vbIndex)); -- это то слово что надо
+           END IF;
+
+            -- теперь следуюющий
+            vbIndex := vbIndex + 1;
+        END LOOP;
+ 
+    RETURN COALESCE (vbResult, '');
+
+END;
+$BODY$
+  LANGUAGE PLPGSQL IMMUTABLE;
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 03.07.20         *
+*/
+
+-- тест
+-- SELECT * FROM zfCalc_WordText_Split_replica (inValue:= '{1 ,2} ', inIndex:= 1)
+
