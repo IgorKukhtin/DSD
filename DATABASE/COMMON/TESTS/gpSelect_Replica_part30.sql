e910e6048f02477ee7500aba4da484bc6ae0325e
innaf <innaf@ua.fm>
2020-07-07 12:21:04 +0300

    +

--- DATABASE/COMMON/TESTS/gpSelect_Replica_part30.sql
+++ DATABASE/COMMON/TESTS/gpSelect_Replica_part30.sql
@@ -13,9 +13,9 @@ BEGIN
 
    RETURN QUERY 
    SELECT 30 AS Part
-        , ('LEFT JOIN ' || tmpData.table_name || ' ON ' || tmpData.table_name ||'.' || zfCalc_WordText_Split_replica (tmpData.pk_keys, 1) || ' = zfCalc_WordText_Split_replica (table_update_data.pk_values, 1)'
-          || CASE WHEN zfCalc_WordText_Split_replica (tmpData.pk_keys, 2) <> '' THEN ' AND ' || tmpData.table_name ||'.' || zfCalc_WordText_Split_replica (tmpData.pk_keys, 2) || ' = zfCalc_WordText_Split (table_update_data.pk_values, 2)' ELSE '' END
-          || CASE WHEN zfCalc_WordText_Split_replica (tmpData.pk_keys, 3) <> '' THEN ' AND ' || tmpData.table_name ||'.' || zfCalc_WordText_Split_replica (tmpData.pk_keys, 3) || ' = zfCalc_WordText_Split (table_update_data.pk_values, 3)' ELSE '' END) :: Text AS Value
+        , ('LEFT JOIN ' || tmpData.table_name || ' ON ' || tmpData.table_name ||'.' || zfCalc_WordText_Split_replica (tmpData.pk_keys, 1) || ' = zfCalc_WordText_Split_replica (table_update_data.pk_values, 1) ::Integer'
+          || CASE WHEN zfCalc_WordText_Split_replica (tmpData.pk_keys, 2) <> '' THEN ' AND ' || tmpData.table_name ||'.' || zfCalc_WordText_Split_replica (tmpData.pk_keys, 2) || ' = zfCalc_WordText_Split (table_update_data.pk_values, 2) ::Integer' ELSE '' END
+          || CASE WHEN zfCalc_WordText_Split_replica (tmpData.pk_keys, 3) <> '' THEN ' AND ' || tmpData.table_name ||'.' || zfCalc_WordText_Split_replica (tmpData.pk_keys, 3) || ' = zfCalc_WordText_Split (table_update_data.pk_values, 3) ::Integer' ELSE '' END) :: Text AS Value
    FROM (SELECT DISTINCT table_name, pk_keys
          FROM _replica.table_update_data AS tmp
          WHERE tmp.Id BETWEEN inId_start AND inId_end
