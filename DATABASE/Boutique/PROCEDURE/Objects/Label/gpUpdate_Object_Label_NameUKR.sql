fea21d68a3e3d5bf005f2b75e8a2bace96fc1427
innaf <innaf@ua.fm>
2020-08-25 12:58:04 +0300

    +

---
+++ DATABASE/Boutique/PROCEDURE/Objects/Label/gpUpdate_Object_Label_NameUKR.sql
@@ -0,0 +1,39 @@
+-- Название для ценника
+
+DROP FUNCTION IF EXISTS gpUpdate_Object_Label_NameUKR (Integer, TVarChar, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpUpdate_Object_Label_NameUKR(
+    IN inId           Integer,       -- Ключ объекта <Название для ценника>    
+    IN inName_UKR     TVarChar,      -- Название объекта <Название для ценника> укр
+    IN inSession      TVarChar       -- сессия пользователя
+)
+RETURNS VOID
+AS
+$BODY$
+  DECLARE vbUserId Integer;
+BEGIN
+
+   -- проверка прав пользователя на вызов процедуры
+   -- PERFORM lpCheckRight(inSession, zc_Enum_Process_Label());
+   vbUserId:= lpGetUserBySession (inSession);
+
+   -- сохранили свойство
+   PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Label_UKR(), inId, inName_UKR);
+
+   -- сохранили протокол
+   PERFORM lpInsert_ObjectProtocol (inId, vbUserId);
+
+END;
+$BODY$
+
+LANGUAGE plpgsql VOLATILE;
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 25.08.20          *
+*/
+
+-- тест
+--
