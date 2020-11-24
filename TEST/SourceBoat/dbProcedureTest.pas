f63acac92fad89dee0af58141781f4f958da0236
K0sta <Ashtu777@gmail.com>
2020-11-24 21:14:04 +0200

    ok

--- TEST/SourceBoat/dbProcedureTest.pas
+++ TEST/SourceBoat/dbProcedureTest.pas
@@ -16,9 +16,7 @@ implementation
 uses zLibUtil, utilConst;
 
 const
-  FunctionPath = '..\DATABASE\COMMON\FUNCTION\';
-  ReportsPath = '..\DATABASE\COMMON\REPORTS\';
-  CommonProcedurePath = '..\DATABASE\COMMON\PROCEDURE\';
+  CommonProcedurePath = '..\DATABASE\Boat\PROCEDURE\';
 
 { TdbProcedureTest }
 
@@ -27,15 +25,15 @@ begin
   ScriptDirectory := CommonProcedurePath + 'OBJECTS\_Common\';
   ProcedureLoad;
 
-  ExecFile(CommonProcedurePath + 'OBJECTS\User\gpInsertUpdate_Object_User.sql', ZQuery);
-  ExecFile(CommonProcedurePath + 'OBJECTS\User\gpSelect_Object_User.sql', ZQuery);
-  ExecFile(CommonProcedurePath + 'OBJECTS\User\gpGet_Object_User.sql', ZQuery);
+  ExecFile(CommonProcedurePath + 'ObjectsTools\User\gpInsertUpdate_Object_User.sql', ZQuery);
+  ExecFile(CommonProcedurePath + 'ObjectsTools\User\gpSelect_Object_User.sql', ZQuery);
+  ExecFile(CommonProcedurePath + 'ObjectsTools\User\gpGet_Object_User.sql', ZQuery);
 
-  ExecFile(CommonProcedurePath + 'OBJECTS\Form\gpInsertUpdate_Object_Form.sql', ZQuery);
-  ExecFile(CommonProcedurePath + 'OBJECTS\Form\gpGet_Object_Form.sql', ZQuery);
+  ExecFile(CommonProcedurePath + 'ObjectsTools\Form\gpInsertUpdate_Object_Form.sql', ZQuery);
+  ExecFile(CommonProcedurePath + 'ObjectsTools\Form\gpGet_Object_Form.sql', ZQuery);
 
-  ExecFile(CommonProcedurePath + 'OBJECTS\UserFormSettings\gpInsertUpdate_Object_UserFormSettings.sql', ZQuery);
-  ExecFile(CommonProcedurePath + 'OBJECTS\UserFormSettings\gpGet_Object_UserFormSettings.sql', ZQuery);
+  ExecFile(CommonProcedurePath + 'ObjectsTools\UserFormSettings\gpInsertUpdate_Object_UserFormSettings.sql', ZQuery);
+  ExecFile(CommonProcedurePath + 'ObjectsTools\UserFormSettings\gpGet_Object_UserFormSettings.sql', ZQuery);
 end;
 
 procedure TdbProcedureTest.CreateCommonProtocolProcedure;
