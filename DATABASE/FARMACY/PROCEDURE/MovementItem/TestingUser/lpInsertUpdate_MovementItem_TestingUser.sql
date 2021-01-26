f2e2985684fe15300af946be4c38c78c49b7ee9c
Oleg <olegsh1264@gmail.com>
2021-01-26 21:03:18 +0200

    Отчеты

--- DATABASE/FARMACY/PROCEDURE/MovementItem/TestingUser/lpInsertUpdate_MovementItem_TestingUser.sql
+++ DATABASE/FARMACY/PROCEDURE/MovementItem/TestingUser/lpInsertUpdate_MovementItem_TestingUser.sql
@@ -5,7 +5,7 @@ DROP FUNCTION IF EXISTS lpInsertUpdate_MovementItem_TestingUser (Integer, Intege
 CREATE OR REPLACE FUNCTION lpInsertUpdate_MovementItem_TestingUser(
  INOUT ioId                  Integer   , -- Ключ объекта <Элемент документа>
     IN inUserId              Integer   , -- Сотрудник
-    IN inResult              TFloat   , -- Результат теста
+    IN inResult              TFloat    , -- Результат теста
     IN inDateTest            TDateTime , -- Дата теста
     IN inSession             TVarChar    -- сессия пользователя
 )
@@ -15,6 +15,7 @@ $BODY$
   DECLARE vbResult TFloat = 0;
   DECLARE vbMovement Integer;
   DECLARE vbOperDate TDateTime;
+  DECLARE vbDateTest TDateTime;
 BEGIN
 
   -- приводим дату к первому числу месяца
@@ -44,36 +45,48 @@ BEGIN
    -- Проверяем если уже есть по такому сотруднику
   IF COALESCE (ioId, 0) = 0 AND EXISTS(SELECT Id FROM MovementItem WHERE MovementId = vbMovement AND ObjectId = inUserId)
   THEN
-    SELECT Id, Amount, COALESCE(MovementItemFloat.ValueData, 0)
-    INTO ioId, vbResult, vbAttempts
+    SELECT Id, Amount, COALESCE(MovementItemFloat.ValueData, 0), COALESCE(MovementItemDate.ValueData, vbOperDate)
+    INTO ioId, vbResult, vbAttempts, vbDateTest
     FROM MovementItem
          LEFT OUTER JOIN MovementItemFloat ON MovementItemFloat.DescId = zc_MIFloat_TestingUser_Attempts()
                                           AND MovementItemFloat.MovementItemId = MovementItem.Id
+         LEFT JOIN MovementItemDate ON MovementItemDate.MovementItemId = MovementItem.Id
+                                    AND MovementItemDate.DescId = zc_MIDate_TestingUser()
     WHERE MovementItem.MovementId = vbMovement
       AND MovementItem.ObjectId = inUserId;
   ELSE
     IF COALESCE (ioId, 0) <> 0
     THEN
-      SELECT Amount, COALESCE(MovementItemFloat.ValueData, 0)
-      INTO vbResult, vbAttempts
+      SELECT Amount, COALESCE(MovementItemFloat.ValueData, 0), COALESCE(MovementItemDate.ValueData, vbOperDate)
+      INTO vbResult, vbAttempts, vbDateTest
       FROM MovementItem
            LEFT OUTER JOIN MovementItemFloat ON MovementItemFloat.DescId = zc_MIFloat_TestingUser_Attempts()
                                             AND MovementItemFloat.MovementItemId = MovementItem.Id
+           LEFT JOIN MovementItemDate ON MovementItemDate.MovementItemId = MovementItem.Id
+                                     AND MovementItemDate.DescId = zc_MIDate_TestingUser()
       WHERE MovementItem.Id = ioId;
+    ELSE
+      vbResult := 0;
+      vbAttempts := 0;
+      vbDateTest := vbOperDate;
     END IF;
   END IF;
 
-  if COALESCE (ioId, 0) = 0 OR inResult > vbResult
+  if COALESCE (ioId, 0) = 0 OR (inResult >= 85 OR vbResult < 85) AND vbDateTest < inDateTest
   THEN
 
      -- сохранили <Элемент документа>
-    ioId := lpInsertUpdate_MovementItem (ioId, zc_MI_Master(), inUserId, vbMovement, inResult, NULL);
+    ioId := lpInsertUpdate_MovementItem (ioId, zc_MI_Master(), inUserId, vbMovement, CASE WHEN inResult > vbResult THEN inResult ELSE vbResult END, NULL);
 
      -- сохранили свойство <Дата теста>
     PERFORM lpInsertUpdate_MovementItemDate (zc_MIDate_TestingUser(), ioId, inDateTest);
 
      -- сохранили свойство <Количество попыток>
-    PERFORM lpInsertUpdate_MovementItemFloat (zc_MIFloat_TestingUser_Attempts(), ioId, vbAttempts + 1);
+    IF vbAttempts = 0 OR vbResult < 85
+    THEN 
+      PERFORM lpInsertUpdate_MovementItemFloat (zc_MIFloat_TestingUser_Attempts(), ioId, vbAttempts + 1);
+    END IF;
+      
   END IF;
 
    -- сохранили протокол
@@ -87,6 +100,7 @@ LANGUAGE PLPGSQL VOLATILE;
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Шаблий О.В.
+ 26.01.21        *
  25.06.19        *
  15.10.18        *
  11.09.18        *
