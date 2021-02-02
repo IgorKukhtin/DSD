56f817c2867e84f0bdd5be5a2ffecaf9065a44b1
innaf <innaf@ua.fm>
2021-02-02 21:18:25 +0200

    ++

--- DATABASE/Boat/PROCEDURE/Objects/Client/gpInsertUpdate_Object_Client.sql
+++ DATABASE/Boat/PROCEDURE/Objects/Client/gpInsertUpdate_Object_Client.sql
@@ -2,13 +2,32 @@
 
 DROP FUNCTION IF EXISTS gpInsertUpdate_Object_Client (Integer, Integer, TVarChar, TVarChar, TVarChar);
 DROP FUNCTION IF EXISTS gpInsertUpdate_Object_Client (Integer, Integer, TVarChar, TFloat, TVarChar, TVarChar);
+DROP FUNCTION IF EXISTS gpInsertUpdate_Object_Client (Integer, Integer
+                                                    , TVarChar, TVarChar, TVarChar, TVarChar, TVarChar
+                                                    , TVarChar, TVarChar, TVarChar, TVarChar, TVarChar, TVarChar
+                                                    , TFloat, TFloat, TFloat, Integer, Integer, Integer, Integer,TVarChar);
 
 CREATE OR REPLACE FUNCTION gpInsertUpdate_Object_Client(
  INOUT ioId              Integer,       -- ключ объекта <Бренд>
  INOUT ioCode            Integer,       -- свойство <Код Бренда>
     IN inName            TVarChar,      -- главное Название Бренда
-    IN inDiscountTax     TFloat  ,      -- % скидки
     IN inComment         TVarChar,      --
+    IN inFax             TVarChar,
+    IN inPhone           TVarChar,
+    IN inMobile          TVarChar,
+    IN inIBAN            TVarChar,
+    IN inStreet          TVarChar,
+    IN inMember          TVarChar,
+    IN inWWW             TVarChar,
+    IN inEmail           TVarChar,
+    IN inCodeDB          TVarChar,
+    IN inDiscountTax     TFloat  ,      -- % скидки
+    IN inDayCalendar     TFloat ,
+    IN inDayBank         TFloat ,
+    IN inBankId          Integer , 
+    IN inPLZId           Integer ,
+    IN inInfoMoneyId     Integer ,
+    IN inTaxKindId       Integer ,
     IN inSession         TVarChar       -- сессия пользователя
 )
 RETURNS RECORD
@@ -35,9 +54,43 @@ BEGIN
 
    -- сохранили свойство <>
    PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_Comment(), ioId, inComment);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_Fax(), ioId, inFax);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_Phone(), ioId, inPhone);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_Mobile(), ioId, inMobile);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_IBAN(), ioId, inIBAN);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_Street(), ioId, inStreet);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_Member(), ioId, inMember);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_WWW(), ioId, inWWW);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_Email(), ioId, inEmail);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectString(zc_ObjectString_Client_CodeDB(), ioId, inCodeDB);
+
 
    -- сохранили свойство <>
    PERFORM lpInsertUpdate_ObjectFloat(zc_ObjectFloat_Client_DiscountTax(), ioId, inDiscountTax);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectFloat(zc_ObjectFloat_Client_DayCalendar(), ioId, inDayCalendar);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectFloat(zc_ObjectFloat_Client_Bank(), ioId, inDayBank);
+
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Client_PLZ(), ioId, inPLZId);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Client_Bank(), ioId, inBankId);
+
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Client_InfoMoney(), ioId, inInfoMoneyId);
+   -- сохранили свойство <>
+   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Client_TaxKind(), ioId, inTaxKindId);
+
 
    IF vbIsInsert = TRUE THEN
       -- сохранили свойство <Дата создания>
@@ -56,6 +109,7 @@ $BODY$
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 02.02.21         *
  04.01.21         *
  08.10.20         *
 */
--- DATABASE/Boat/PROCEDURE/Objects/Client/gpSelect_Object_Client.sql
+++ DATABASE/Boat/PROCEDURE/Objects/Client/gpSelect_Object_Client.sql
@@ -8,8 +8,19 @@ CREATE OR REPLACE FUNCTION gpSelect_Object_Client(
    
 )
 RETURNS TABLE (Id Integer, Code Integer, Name TVarChar
-             , DiscountTax TFloat
+             , Fax TVarChar, Phone TVarChar, Mobile TVarChar
+             , IBAN TVarChar, Street TVarChar, Member TVarChar
+             , WWW TVarChar, Email TVarChar, CodeDB TVarChar
              , Comment TVarChar
+             , BankId Integer, BankName TVarChar
+             , PLZId Integer, PLZName TVarChar
+             , TaxKindId Integer, TaxKindName TVarChar
+             , InfoMoneyId Integer, InfoMoneyCode Integer, InfoMoneyName TVarChar, InfoMoneyName_all TVarChar
+             , InfoMoneyGroupId Integer, InfoMoneyGroupCode Integer, InfoMoneyGroupName TVarChar
+             , InfoMoneyDestinationId Integer, InfoMoneyDestinationCode Integer, InfoMoneyDestinationName TVarChar
+             , DiscountTax TFloat
+             , DayCalendar TFloat
+             , DayBank     TFloat
              , InsertName TVarChar
              , InsertDate TDateTime
              , isErased Boolean)
@@ -29,9 +40,42 @@ BEGIN
              Object_Client.Id                AS Id
            , Object_Client.ObjectCode        AS Code
            , Object_Client.ValueData         AS Name
-           
+
+           , COALESCE (ObjectString_Fax.ValueData,'')::TVarChar      AS Fax
+           , COALESCE (ObjectString_Phone.ValueData,'')::TVarChar    AS Phone
+           , COALESCE (ObjectString_Mobile.ValueData,'')::TVarChar   AS Mobile
+           , COALESCE (ObjectString_IBAN.ValueData,'')::TVarChar     AS IBAN
+           , COALESCE (ObjectString_Street.ValueData,'')::TVarChar   AS Street
+           , COALESCE (ObjectString_Member.ValueData,'')::TVarChar   AS Member
+           , COALESCE (ObjectString_WWW.ValueData,'')::TVarChar      AS WWW
+           , COALESCE (ObjectString_Email.ValueData,'')::TVarChar    AS Email
+           , COALESCE (ObjectString_CodeDB.ValueData,'')::TVarChar   AS CodeDB
+           , COALESCE (ObjectString_Comment.ValueData,'')::TVarChar  AS Comment
+
+           , Object_Bank.Id                  AS BankId
+           , Object_Bank.ValueData           AS BankName
+           , Object_PLZ.Id                   AS PLZId
+           , Object_PLZ.ValueData            AS PLZName
+
+           , Object_TaxKind.Id               AS TaxKindId
+           , Object_TaxKind.ValueData        AS TaxKindName
+
+           , Object_InfoMoney_View.InfoMoneyId
+           , Object_InfoMoney_View.InfoMoneyCode
+           , Object_InfoMoney_View.InfoMoneyName
+           , Object_InfoMoney_View.InfoMoneyName_all
+
+           , Object_InfoMoney_View.InfoMoneyGroupId
+           , Object_InfoMoney_View.InfoMoneyGroupCode
+           , Object_InfoMoney_View.InfoMoneyGroupName
+
+           , Object_InfoMoney_View.InfoMoneyDestinationId
+           , Object_InfoMoney_View.InfoMoneyDestinationCode
+           , Object_InfoMoney_View.InfoMoneyDestinationName
+
            , COALESCE (ObjectFloat_DiscountTax.ValueData,0) ::TFloat AS DiscountTax
-           , ObjectString_Comment.ValueData  AS Comment
+           , COALESCE (ObjectFloat_DayCalendar.ValueData,0) ::TFloat AS DayCalendar
+           , COALESCE (ObjectFloat_Bank.ValueData,0)        ::TFloat AS DayBank
 
            , Object_Insert.ValueData         AS InsertName
            , ObjectDate_Insert.ValueData     AS InsertDate
@@ -41,9 +85,63 @@ BEGIN
                                  ON ObjectString_Comment.ObjectId = Object_Client.Id
                                 AND ObjectString_Comment.DescId = zc_ObjectString_Client_Comment()  
 
+          LEFT JOIN ObjectString AS ObjectString_Fax
+                                 ON ObjectString_Fax.ObjectId = Object_Client.Id
+                                AND ObjectString_Fax.DescId = zc_ObjectString_Client_Fax()  
+          LEFT JOIN ObjectString AS ObjectString_Phone
+                                 ON ObjectString_Phone.ObjectId = Object_Client.Id
+                                AND ObjectString_Phone.DescId = zc_ObjectString_Client_Phone()
+          LEFT JOIN ObjectString AS ObjectString_Mobile
+                                 ON ObjectString_Mobile.ObjectId = Object_Client.Id
+                                AND ObjectString_Mobile.DescId = zc_ObjectString_Client_Mobile()
+          LEFT JOIN ObjectString AS ObjectString_IBAN
+                                 ON ObjectString_IBAN.ObjectId = Object_Client.Id
+                                AND ObjectString_IBAN.DescId = zc_ObjectString_Client_IBAN()
+          LEFT JOIN ObjectString AS ObjectString_Street
+                                 ON ObjectString_Street.ObjectId = Object_Client.Id
+                                AND ObjectString_Street.DescId = zc_ObjectString_Client_Street()
+          LEFT JOIN ObjectString AS ObjectString_Member
+                                 ON ObjectString_Member.ObjectId = Object_Client.Id
+                                AND ObjectString_Member.DescId = zc_ObjectString_Client_Member()
+          LEFT JOIN ObjectString AS ObjectString_WWW
+                                 ON ObjectString_WWW.ObjectId = Object_Client.Id
+                                AND ObjectString_WWW.DescId = zc_ObjectString_Client_WWW()
+          LEFT JOIN ObjectString AS ObjectString_Email
+                                 ON ObjectString_Email.ObjectId = Object_Client.Id
+                                AND ObjectString_Email.DescId = zc_ObjectString_Client_Email()
+          LEFT JOIN ObjectString AS ObjectString_CodeDB
+                                 ON ObjectString_CodeDB.ObjectId = Object_Client.Id
+                                AND ObjectString_CodeDB.DescId = zc_ObjectString_Client_CodeDB()
+
           LEFT JOIN ObjectFloat AS ObjectFloat_DiscountTax
                                 ON ObjectFloat_DiscountTax.ObjectId = Object_Client.Id
                                AND ObjectFloat_DiscountTax.DescId = zc_ObjectFloat_Client_DiscountTax()  
+          LEFT JOIN ObjectFloat AS ObjectFloat_DayCalendar
+                                ON ObjectFloat_DayCalendar.ObjectId = Object_Client.Id
+                               AND ObjectFloat_DayCalendar.DescId = zc_ObjectFloat_Client_DayCalendar() 
+          LEFT JOIN ObjectFloat AS ObjectFloat_Bank
+                                ON ObjectFloat_Bank.ObjectId = Object_Client.Id
+                               AND ObjectFloat_Bank.DescId = zc_ObjectFloat_Client_Bank() 
+
+          LEFT JOIN ObjectLink AS ObjectLink_PLZ
+                               ON ObjectLink_PLZ.ObjectId = Object_Client.Id
+                              AND ObjectLink_PLZ.DescId = zc_ObjectLink_Client_PLZ()
+          LEFT JOIN Object AS Object_PLZ ON Object_PLZ.Id = ObjectLink_PLZ.ChildObjectId
+
+          LEFT JOIN ObjectLink AS ObjectLink_Bank
+                               ON ObjectLink_Bank.ObjectId = Object_Client.Id
+                              AND ObjectLink_Bank.DescId = zc_ObjectLink_Client_Bank()
+          LEFT JOIN Object AS Object_Bank ON Object_Bank.Id = ObjectLink_Bank.ChildObjectId
+
+          LEFT JOIN ObjectLink AS ObjectLink_InfoMoney
+                               ON ObjectLink_InfoMoney.ObjectId = Object_Client.Id
+                              AND ObjectLink_InfoMoney.DescId = zc_ObjectLink_Client_InfoMoney()
+          LEFT JOIN Object_InfoMoney_View ON Object_InfoMoney_View.InfoMoneyId = ObjectLink_InfoMoney.ChildObjectId
+
+          LEFT JOIN ObjectLink AS ObjectLink_TaxKind
+                               ON ObjectLink_TaxKind.ObjectId = Object_Client.Id
+                              AND ObjectLink_TaxKind.DescId = zc_ObjectLink_Client_TaxKind()
+          LEFT JOIN Object AS Object_TaxKind ON Object_TaxKind.Id = ObjectLink_TaxKind.ChildObjectId 
 
           LEFT JOIN ObjectLink AS ObjectLink_Insert
                                ON ObjectLink_Insert.ObjectId = Object_Client.Id
