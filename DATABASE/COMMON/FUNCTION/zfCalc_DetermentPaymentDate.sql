b2e4f926e54fb983b77c34d6fc5a72f35c264af2
K0sta <Ashtu777@gmail.com>
2021-06-27 20:56:17 +0300

    ok

--- DATABASE/COMMON/FUNCTION/zfCalc_DetermentPaymentDate.sql
+++ DATABASE/COMMON/FUNCTION/zfCalc_DetermentPaymentDate.sql
@@ -1,45 +1,42 @@
--- Function: zfcalc_determentpaymentdate (integer, integer, tdatetime)
+-- Function: zfCalc_DetermentPaymentDate (Integer, Integer, TDateTime)
 
-DROP FUNCTION IF EXISTS zfcalc_determentpaymentdate (integer, integer, tdatetime);
+DROP FUNCTION IF EXISTS zfCalc_DetermentPaymentDate (Integer, Integer, TDateTime);
 
--- DROP FUNCTION zfcalc_determentpaymentdate(integer, integer, tdatetime);
-
-CREATE OR REPLACE FUNCTION zfcalc_determentpaymentdate (incontractconditionid integer, indaycount integer, indate tdatetime)
-  RETURNS tdatetime AS
+CREATE OR REPLACE FUNCTION zfCalc_DetermentPaymentDate (inContractConditionId Integer, inDayCount Integer, inDate TDateTime)
+RETURNS TDateTime
+AS
 $BODY$
 BEGIN
 
      CASE inContractConditionId
-         WHEN 0 THEN Return(inDate);
-         WHEN zc_Enum_ContractConditionKind_DelayDayCalendar() THEN RETURN(inDate::Date - inDayCount);
-         WHEN zc_Enum_ContractConditionKind_DelayDayBank() THEN 
-              BEGIN
-                RETURN(SELECT 
-                   MIN(CalendarDate)
-                FROM
-                  (SELECT ObjectDate_Value.valuedata AS CalendarDate
-                     FROM ObjectDate AS ObjectDate_Value
-                LEFT JOIN ObjectBoolean AS ObjectBoolean_Working 
-                       ON ObjectBoolean_Working.ObjectId = ObjectDate_Value.ObjectId 
-                      AND ObjectBoolean_Working.DescId = zc_ObjectBoolean_Calendar_Working()
-                    WHERE ObjectDate_Value.DescId = zc_ObjectDate_Calendar_Value()
-                      AND ObjectDate_Value.valuedata < inDate 
-                      AND ObjectBoolean_Working.ValueData = true
-                 ORDER BY CalendarDate DESC
-                 LIMIT inDayCount) AS Result);                                       	
-              END;
+         WHEN 0 THEN RETURN (inDate);
+         WHEN zc_Enum_ContractConditionKind_DelayDayCalendar() THEN RETURN (inDate::Date - inDayCount);
+         WHEN zc_Enum_ContractConditionKind_DelayDayBank()
+              THEN
+                RETURN (SELECT MIN (CalendarDate)
+                        FROM (SELECT ObjectDate_Value.valuedata AS CalendarDate
+                              FROM ObjectDate AS ObjectDate_Value
+                                   LEFT JOIN ObjectBoolean AS ObjectBoolean_Working
+                                          ON ObjectBoolean_Working.ObjectId = ObjectDate_Value.ObjectId
+                                         AND ObjectBoolean_Working.DescId   = zc_ObjectBoolean_Calendar_Working()
+                              WHERE ObjectDate_Value.DescId    = zc_ObjectDate_Calendar_Value()
+                                AND ObjectDate_Value.valuedata < inDate
+                                AND ObjectBoolean_Working.ValueData = TRUE
+                              ORDER BY CalendarDate DESC
+                              LIMIT inDayCount
+                             ) AS Result
+                       );
      END CASE;
 
 END;
 $BODY$
   LANGUAGE plpgsql IMMUTABLE;
-ALTER FUNCTION zfcalc_determentpaymentdate(integer, integer, tdatetime)  OWNER TO postgres;
 
 /*
  ÈÑÒÎÐÈß ÐÀÇÐÀÁÎÒÊÈ: ÄÀÒÀ, ÀÂÒÎÐ
                Ôåëîíþê È.Â.   Êóõòèí È.Â.   Êëèìåíòüåâ Ê.È.
- 31.03.15                         * 
+ 31.03.15                         *
 */
 
 -- òåñò
--- SELECT * FROM gpUpdate_IsMedoc (ioId:= 0, inSession:= '2')
+-- SELECT * FROM zfCalc_DetermentPaymentDate (inContractConditionId:= zc_Enum_ContractConditionKind_DelayDayBank(), inDayCount:= 10, inDate:= CURRENT_DATE)
