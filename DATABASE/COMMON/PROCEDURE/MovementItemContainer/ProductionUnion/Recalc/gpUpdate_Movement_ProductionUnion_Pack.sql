0522f6f594e342b571bc3ff6dd63fe0489ef15db
K0sta <Ashtu777@gmail.com>
2021-07-03 17:54:00 +0300

    ok

--- DATABASE/COMMON/PROCEDURE/MovementItemContainer/ProductionUnion/Recalc/gpUpdate_Movement_ProductionUnion_Pack.sql
+++ DATABASE/COMMON/PROCEDURE/MovementItemContainer/ProductionUnion/Recalc/gpUpdate_Movement_ProductionUnion_Pack.sql
@@ -21,6 +21,7 @@ BEGIN
         <> EXTRACT (DAY FROM CURRENT_DATE) / 2 - FLOOR (EXTRACT (DAY FROM CURRENT_DATE) / 2)
       --OR EXTRACT (DAY FROM inStartDate) < 14
           )
+    --AND 1=0
    THEN
        RETURN;
    END IF;
