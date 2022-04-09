963bda08bc6b31bb466b486eb7c4dd9312bf42bc
K0sta <ashtu777@gmail.com>
2022-04-09 13:36:26 +0300

    ok

--- DATABASE/COMMON/Process/Movement/_common.sql
+++ DATABASE/COMMON/Process/Movement/_common.sql
@@ -2,6 +2,9 @@
 CREATE OR REPLACE FUNCTION zc_Enum_Process_UpdateMovement_isCopy() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_UpdateMovement_isCopy' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql IMMUTABLE;
 
 -- Документ <...>
+CREATE OR REPLACE FUNCTION zc_Enum_Process_UpdateMovement_Branch() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_UpdateMovement_Branch' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql IMMUTABLE;
+
+-- Документ <...>
 CREATE OR REPLACE FUNCTION zc_Enum_Process_Update_MI_Price_Currency() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_Update_MI_Price_Currency' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql IMMUTABLE;
 
 --
@@ -17,6 +20,13 @@ PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_UpdateMovement_isCopy
                                   , inName:= 'Начисление бонусов Да/Нет.'
                                   , inEnumName:= 'zc_Enum_Process_UpdateMovement_isCopy');
 
+
+-- Документ <>
+PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_UpdateMovement_Branch()
+                                  , inDescId:= zc_Object_Process()
+                                  , inCode:= 1
+                                  , inName:= 'Проведение документов - нет проверки по филиалу.'
+                                  , inEnumName:= 'zc_Enum_Process_UpdateMovement_Branch');
 
 -- Документ <>
 PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_Update_MI_Price_Currency()
