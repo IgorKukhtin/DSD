4a97cd1f7fede80420a8000fde1da9701043f2f0
K0sta <ashtu777@gmail.com>
2022-05-09 22:54:39 +0300

    ok

--- DATABASE/Boat/PROCEDURE/MovementItem/Inventory/gpGet_Partion_byBarcode.sql
+++ DATABASE/Boat/PROCEDURE/MovementItem/Inventory/gpGet_Partion_byBarcode.sql
@@ -35,7 +35,7 @@ BEGIN
         -- Если это Штрихкод
         IF COALESCE (inBarCode, '') <> '' 
         THEN
-             -- Проверка
+             -- Проверка - 1
              IF 1 < (SELECT COUNT(*)
                      FROM Object
                           INNER JOIN ObjectString AS ObjectString_EAN
@@ -48,25 +48,39 @@ BEGIN
                  RAISE EXCEPTION 'Ошибка.Штрих-код <%> найден у разных Комплектующих.%<%>%и <%>'
                                , inBarCode
                                , CHR (13)
-                               , lfGet_Object_ValueData ((SELECT MIN (Object.Id)
-                                                          FROM Object
-                                                               INNER JOIN ObjectString AS ObjectString_EAN
-                                                                                       ON ObjectString_EAN.ObjectId  = Object.Id
-                                                                                      AND ObjectString_EAN.DescId    = zc_ObjectString_EAN()
-                                                                                      AND ObjectString_EAN.ValueData = TRIM (inBarCode)
-                                                          WHERE Object.DescId = zc_Object_Goods()))
+                               , (SELECT CASE WHEN ObjectString_Article.ValueData <> ''
+                                              THEN '(' || ObjectString_Article.ValueData || ') '
+                                              ELSE '{' || Object.ObjectCode :: TVarChar  || '} '
+                                         END || Object.ValueData
+                                  FROM Object
+                                       INNER JOIN ObjectString AS ObjectString_EAN
+                                                               ON ObjectString_EAN.ObjectId  = Object.Id
+                                                              AND ObjectString_EAN.DescId    = zc_ObjectString_EAN()
+                                                              AND ObjectString_EAN.ValueData = TRIM (inBarCode)
+                                       LEFT JOIN ObjectString AS ObjectString_Article
+                                                              ON ObjectString_Article.ObjectId  = Object.Id
+                                                             AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                  WHERE Object.DescId = zc_Object_Goods()
+                                  ORDER BY Object.Id ASC LIMIT 1)
                                , CHR (13)
-                               , lfGet_Object_ValueData ((SELECT MAX (Object.Id)
-                                                          FROM Object
-                                                               INNER JOIN ObjectString AS ObjectString_EAN
-                                                                                       ON ObjectString_EAN.ObjectId  = Object.Id
-                                                                                      AND ObjectString_EAN.DescId    = zc_ObjectString_EAN()
-                                                                                      AND ObjectString_EAN.ValueData = TRIM (inBarCode)
-                                                          WHERE Object.DescId = zc_Object_Goods()))
+                               , (SELECT CASE WHEN ObjectString_Article.ValueData <> ''
+                                              THEN '(' || ObjectString_Article.ValueData || ') '
+                                              ELSE '{' || Object.ObjectCode :: TVarChar  || '} '
+                                         END || Object.ValueData
+                                  FROM Object
+                                       INNER JOIN ObjectString AS ObjectString_EAN
+                                                               ON ObjectString_EAN.ObjectId  = Object.Id
+                                                              AND ObjectString_EAN.DescId    = zc_ObjectString_EAN()
+                                                              AND ObjectString_EAN.ValueData = TRIM (inBarCode)
+                                       LEFT JOIN ObjectString AS ObjectString_Article
+                                                              ON ObjectString_Article.ObjectId  = Object.Id
+                                                             AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                  WHERE Object.DescId = zc_Object_Goods()
+                                  ORDER BY Object.Id DESC LIMIT 1)
                                 ;
              END IF;
                          
-             -- Поиск
+             -- Поиск - 1
              vbGoodsId:= (SELECT Object.Id
                           FROM Object
                               INNER JOIN ObjectString AS ObjectString_EAN
@@ -76,13 +90,123 @@ BEGIN
                           WHERE Object.DescId = zc_Object_Goods()
                          );
              
+
              -- если НЕ нашли
              IF COALESCE (vbGoodsId, 0) = 0
              THEN
-                 RAISE EXCEPTION 'Ошибка.Элемент с Штрих-кодом = <%> не найден.', inBarCode;
+                 -- Проверка - 2
+                 IF 1 < (SELECT COUNT(*)
+                         FROM Object
+                              INNER JOIN ObjectString AS ObjectString_Article
+                                                      ON ObjectString_Article.ObjectId  = Object.Id
+                                                     AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                                     AND ObjectString_Article.ValueData = TRIM (inBarCode)
+                         WHERE Object.DescId = zc_Object_Goods()
+                        )
+                 THEN
+                     RAISE EXCEPTION 'Ошибка.Artikel Nr <%> найден у разных Комплектующих.%<%>%и <%>'
+                                   , inBarCode
+                                   , CHR (13)
+                                   , (SELECT CASE WHEN ObjectString_Article.ValueData <> ''
+                                                  THEN '(' || ObjectString_Article.ValueData || ') '
+                                                  ELSE '{' || Object.ObjectCode :: TVarChar  || '} '
+                                             END || Object.ValueData
+                                      FROM Object
+                                           INNER JOIN ObjectString AS ObjectString_Article
+                                                                   ON ObjectString_Article.ObjectId  = Object.Id
+                                                                  AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                                                  AND ObjectString_Article.ValueData = TRIM (inBarCode)
+                                      WHERE Object.DescId = zc_Object_Goods()
+                                      ORDER BY Object.Id ASC LIMIT 1)
+                                   , CHR (13)
+                                   , (SELECT CASE WHEN ObjectString_Article.ValueData <> ''
+                                                  THEN '(' || ObjectString_Article.ValueData || ') '
+                                                  ELSE '{' || Object.ObjectCode :: TVarChar  || '} '
+                                             END || Object.ValueData
+                                      FROM Object
+                                           INNER JOIN ObjectString AS ObjectString_Article
+                                                                   ON ObjectString_Article.ObjectId  = Object.Id
+                                                                  AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                                                  AND ObjectString_Article.ValueData = TRIM (inBarCode)
+                                      WHERE Object.DescId = zc_Object_Goods()
+                                      ORDER BY Object.Id DESC LIMIT 1)
+                                    ;
+                 END IF;
+
+                 -- Поиск - 2
+                 vbGoodsId:= (SELECT Object.Id
+                              FROM Object
+                                  INNER JOIN ObjectString AS ObjectString_Article
+                                                          ON ObjectString_Article.ObjectId  = Object.Id
+                                                         AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                                         AND ObjectString_Article.ValueData = TRIM (inBarCode)
+                              WHERE Object.DescId = zc_Object_Goods()
+                             );
              END IF;
    
-  
+             -- если НЕ нашли
+             IF COALESCE (vbGoodsId, 0) = 0
+             THEN
+                 -- Проверка - 3
+                 IF 1 < (SELECT COUNT(*)
+                         FROM Object
+                              INNER JOIN ObjectString AS ObjectString_Article
+                                                      ON ObjectString_Article.ObjectId  = Object.Id
+                                                     AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                                     AND REPLACE (REPLACE (REPLACE (REPLACE (REPLACE (ObjectString_Article.ValueData, '.', ''), '-', ''), ' ', ''), '=', ''), ',', '') = TRIM (inBarCode)
+                         WHERE Object.DescId = zc_Object_Goods()
+                        )
+                 THEN
+                     RAISE EXCEPTION 'Ошибка.Artikel Nr all <%> найден у разных Комплектующих.%<%>%и <%>'
+                                   , inBarCode
+                                   , CHR (13)
+                                   , (SELECT CASE WHEN ObjectString_Article.ValueData <> ''
+                                                  THEN '(' || ObjectString_Article.ValueData || ') '
+                                                  ELSE '{' || Object.ObjectCode :: TVarChar  || '} '
+                                             END || Object.ValueData
+                                      FROM Object
+                                           INNER JOIN ObjectString AS ObjectString_Article
+                                                                   ON ObjectString_Article.ObjectId  = Object.Id
+                                                                  AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                                                  AND REPLACE (REPLACE (REPLACE (REPLACE (REPLACE (ObjectString_Article.ValueData, '.', ''), '-', ''), ' ', ''), '=', ''), ',', '') = TRIM (inBarCode)
+                                      WHERE Object.DescId = zc_Object_Goods()
+                                      ORDER BY Object.Id ASC LIMIT 1)
+                                   , CHR (13)
+                                   , (SELECT CASE WHEN ObjectString_Article.ValueData <> ''
+                                                  THEN '(' || ObjectString_Article.ValueData || ') '
+                                                  ELSE '{' || Object.ObjectCode :: TVarChar  || '} '
+                                             END || Object.ValueData
+                                      FROM Object
+                                           INNER JOIN ObjectString AS ObjectString_Article
+                                                                   ON ObjectString_Article.ObjectId  = Object.Id
+                                                                  AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                                                  AND REPLACE (REPLACE (REPLACE (REPLACE (REPLACE (ObjectString_Article.ValueData, '.', ''), '-', ''), ' ', ''), '=', ''), ',', '') = TRIM (inBarCode)
+                                      WHERE Object.DescId = zc_Object_Goods()
+                                      ORDER BY Object.Id DESC LIMIT 1)
+                                    ;
+                 END IF;
+
+                 -- Поиск - 3
+                 vbGoodsId:= (SELECT Object.Id
+                              FROM Object
+                                  INNER JOIN ObjectString AS ObjectString_Article
+                                                          ON ObjectString_Article.ObjectId  = Object.Id
+                                                         AND ObjectString_Article.DescId    = zc_ObjectString_Article()
+                                                         AND REPLACE (REPLACE (REPLACE (REPLACE (REPLACE (ObjectString_Article.ValueData, '.', ''), '-', ''), ' ', ''), '=', ''), ',', '') = TRIM (inBarCode)
+                              WHERE Object.DescId = zc_Object_Goods()
+                             );
+             END IF;
+
+             -- если НЕ нашли
+             IF COALESCE (vbGoodsId, 0) = 0 AND LENGTH (inBarCode) = 13
+             THEN
+                 RAISE EXCEPTION 'Ошибка.Элемент с Штрих-кодом = <%> не найден.', inBarCode;
+             ELSEIF COALESCE (vbGoodsId, 0) = 0
+             THEN
+                 RAISE EXCEPTION 'Ошибка.Artikel Nr = <%> не найден.', inBarCode;
+             END IF;
+
+
         END IF;
    
         
