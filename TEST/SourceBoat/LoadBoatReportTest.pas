57c6013d258ad43ed018cf4da242a4f36c3817f4
innafelon <innafelon@gmail.com>
2022-05-21 21:53:23 +0300

    +

--- TEST/SourceBoat/LoadBoatReportTest.pas
+++ TEST/SourceBoat/LoadBoatReportTest.pas
@@ -77,8 +77,8 @@ begin
     LoadReportFromFile('PrintMovement_ProductionPersonal', ReportPath + '\PrintMovement_ProductionPersonal.fr3');
     exit;
     }
-    LoadReportFromFile('PrintReceiptGoods_StructureGoods', ReportPath + '\PrintReceiptGoods_StructureGoods.fr3');
-    LoadReportFromFile('PrintReceiptProdModel_StructureGoods', ReportPath + '\PrintReceiptProdModel_StructureGoods.fr3');
+    //LoadReportFromFile('PrintReceiptGoods_StructureGoods', ReportPath + '\PrintReceiptGoods_StructureGoods.fr3');
+    //LoadReportFromFile('PrintReceiptProdModel_StructureGoods', ReportPath + '\PrintReceiptProdModel_StructureGoods.fr3');
     LoadReportFromFile('PrintReceiptProdModel_Structure', ReportPath + '\PrintReceiptProdModel_Structure.fr3');
     exit;
     LoadReportFromFile('PrintProduct_StructureGoods', ReportPath + '\PrintProduct_StructureGoods.fr3');
