1e29b0a0282e631f6ba81bd8c5249416332d1743
K0sta <ashtu777@gmail.com>
2022-05-26 16:31:06 +0300

    ok

---
+++ DATABASE/COMMON/FUNCTION/zfCalc_User_isIrna.sql
@@ -0,0 +1,40 @@
+-- Function: zfCalc_User_isIrna
+
+DROP FUNCTION IF EXISTS zfCalc_UserIrna (Integer);
+DROP FUNCTION IF EXISTS zfCalc_User_isIrna (Integer);
+
+CREATE OR REPLACE FUNCTION zfCalc_User_isIrna (in inUserId Integer)
+RETURNS Boolean
+AS
+$BODY$
+BEGIN
+
+     IF EXISTS (SELECT 1 FROM ObjectLink_UserRole_View WHERE UserId = inUserId AND RoleId IN (8101714 -- Роль Ирна - все
+                                                                                            , 8101715 -- Роль Ирна - производство
+                                                                                             ))
+     THEN
+          RETURN TRUE;
+
+     ELSEIF EXISTS (SELECT 1 FROM ObjectLink_UserRole_View WHERE UserId = inUserId AND RoleId = zc_Enum_Role_Admin())
+         -- Роль доступ Алан + Ирна
+         OR EXISTS (SELECT 1 FROM ObjectLink_UserRole_View WHERE UserId = inUserId AND RoleId = 8101711)
+     THEN
+         RETURN NULL;
+
+     ELSE 
+          RETURN FALSE;
+     END IF;
+
+END;
+$BODY$
+  LANGUAGE PLPGSQL IMMUTABLE;
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 26.05.22                                        *
+*/
+
+-- тест
+-- SELECT zfCalc_User_isIrna (5) as forAdmin, zfCalc_User_isIrna (10) as forOth, zfCalc_User_isIrna (9457) as forIrna
--- DATABASE/COMMON/PROCEDURE/Objects/Juridical/gpGet_User_JuridicalBasis.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/Juridical/gpGet_User_JuridicalBasis.sql
@@ -1,5 +1,6 @@
 -- Function: gpSelect_Movement_Income()
 
+DROP FUNCTION IF EXISTS gpGet_Juridical_Basis (Integer);
 DROP FUNCTION IF EXISTS gpGet_User_JuridicalBasis (TVarChar);
 
 CREATE OR REPLACE FUNCTION gpGet_User_JuridicalBasis(
@@ -8,18 +9,29 @@ CREATE OR REPLACE FUNCTION gpGet_User_JuridicalBasis(
 RETURNS TABLE(JuridicalBasisId integer, JuridicalBasisName TVarChar)
 AS
 $BODY$
+ DECLARE vbUserId Integer;
 BEGIN
+     -- проверка прав пользователя на вызов процедуры
+     vbUserId:= lpGetUserBySession (inSession);
 
-     RETURN QUERY
-      SELECT Object.Id        AS JuridicalBasisId
-           , Object.ValueData AS JuridicalBasisName
-      FROM Object 
-      WHERE Object.Id = zc_Juridical_Basis();
+     IF zfCalc_User_isIrna (vbUserId) = TRUE
+     THEN
+         RETURN QUERY
+          SELECT Object.Id        AS JuridicalBasisId
+               , Object.ValueData AS JuridicalBasisName
+          FROM Object 
+          WHERE Object.Id = zc_Juridical_Irna();
+     ELSE
+         RETURN QUERY
+          SELECT Object.Id        AS JuridicalBasisId
+               , Object.ValueData AS JuridicalBasisName
+          FROM Object 
+          WHERE Object.Id = zc_Juridical_Basis();
+     END IF;
 
 END;
 $BODY$
   LANGUAGE PLPGSQL VOLATILE;
-ALTER FUNCTION gpGet_User_JuridicalBasis (TVarChar) OWNER TO postgres;
 
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
@@ -28,4 +40,4 @@ ALTER FUNCTION gpGet_User_JuridicalBasis (TVarChar) OWNER TO postgres;
 */
 
 -- тест
--- SELECT * FROM gpGet_User_JuridicalBasis (inSession := '2')
+-- SELECT * FROM gpGet_User_JuridicalBasis (inSession := '5')
