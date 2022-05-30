73f360be9127dd71872e2e57e0bb6459a14cebcf
K0sta <ashtu777@gmail.com>
2022-05-30 15:25:08 +0300

    ok

---
+++ INIT/Merlin_init_real.php
@@ -0,0 +1,5 @@
+<?php
+  $connectstring = "port=5432 dbname=project_merlin host=94.27.55.146 user=postgres password=ET862tira";
+  $host = "http://94.27.55.146/project_merlin/index.php";
+  $isArchive = true;
+?>
