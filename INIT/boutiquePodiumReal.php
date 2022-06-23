68d19c6e8657c7b630647413c699bea6cc44cfbf
K0sta <ashtu777@gmail.com>
2022-06-23 22:02:07 +0300

    ok

--- INIT/boutiquePodiumReal.php
+++ INIT/boutiquePodiumReal.php
@@ -1,5 +1,5 @@
 <?php
    $connectstring = "port=5432 dbname=podium host=212.115.239.119 user=postgres password=ET862TIRA";
-   $host = "http://212.115.239.119:80/boutique/index.php";
+   $host = "http://91.243.196.144:80/boutique/index.php";
    $isArchive = true;
 ?>
