29c57f3d8d0c8afa3f4c2a0a642311df8378cb2c
Oleg <olegsh1264@gmail.com>
2022-06-24 22:17:00 +0300

    1303

---
+++ DATABASE/FARMACY/PROCEDURE/Objects/IntenalSP_1303/gpGet_Object_IntenalSP_1303.sql
@@ -0,0 +1,47 @@
+-- Function: gpGet_Object_IntenalSP_1303 (Integer,TVarChar)
+
+DROP FUNCTION IF EXISTS gpGet_Object_IntenalSP_1303 (Integer, TVarChar);
+
+CREATE OR REPLACE FUNCTION gpGet_Object_IntenalSP_1303(
+    IN inId          Integer,        -- Должности
+    IN inSession     TVarChar        -- сессия пользователя
+)
+RETURNS TABLE (Id Integer, Code Integer, Name TVarChar
+             , isErased Boolean) AS
+$BODY$
+BEGIN
+
+    -- проверка прав пользователя на вызов процедуры
+    -- PERFORM lpCheckRight(inSession, zc_Enum_Process_Get_Object_IntenalSP_1303());
+
+   IF COALESCE (inId, 0) = 0
+   THEN
+       RETURN QUERY 
+       SELECT
+             CAST (0 as Integer)    AS Id
+           , lfGet_ObjectCode(0, zc_Object_IntenalSP_1303()) AS Code
+           , CAST ('' as TVarChar)  AS NAME
+           , CAST (NULL AS Boolean) AS isErased;
+   ELSE
+       RETURN QUERY 
+       SELECT Object_IntenalSP_1303.Id          AS Id
+            , Object_IntenalSP_1303.ObjectCode  AS Code
+            , Object_IntenalSP_1303.ValueData   AS Name
+            , Object_IntenalSP_1303.isErased    AS isErased
+       FROM Object AS Object_IntenalSP_1303
+       WHERE Object_IntenalSP_1303.Id = inId;
+   END IF;
+   
+END;
+$BODY$
+
+LANGUAGE plpgsql VOLATILE;
+
+/*-------------------------------------------------------------------------------
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Шаблий О.В.
+ 24.06.22                                                       * 
+*/
+
+-- тест
+-- SELECT * FROM gpGet_Object_IntenalSP_1303(0,'2')
---
+++ DATABASE/FARMACY/PROCEDURE/Objects/MakerCountrySP_1303/gpSelect_Object_MakerCountrySP_1303.sql
@@ -0,0 +1,39 @@
+-- Function: gpSelect_Object_MakerCountrySP_1303(TVarChar)
+
+DROP FUNCTION IF EXISTS gpSelect_Object_MakerCountrySP_1303(TVarChar);
+
+CREATE OR REPLACE FUNCTION gpSelect_Object_MakerCountrySP_1303(
+    IN inSession     TVarChar       -- сессия пользователя
+)
+RETURNS TABLE (Id Integer, Code Integer, Name TVarChar
+             , isErased boolean) AS
+$BODY$
+BEGIN
+
+     -- проверка прав пользователя на вызов процедуры
+     -- PERFORM lpCheckRight(inSession, zc_Enum_Process_Select_Object_MakerCountrySP_1303());
+
+   RETURN QUERY 
+     SELECT Object_MakerCountrySP_1303.Id                 AS Id
+          , Object_MakerCountrySP_1303.ObjectCode         AS Code
+          , Object_MakerCountrySP_1303.ValueData          AS Name
+          , Object_MakerCountrySP_1303.isErased           AS isErased
+     FROM OBJECT AS Object_MakerCountrySP_1303
+     WHERE Object_MakerCountrySP_1303.DescId = zc_Object_MakerCountrySP_1303();
+  
+END;
+$BODY$
+ 
+LANGUAGE plpgsql VOLATILE;
+ALTER FUNCTION gpSelect_Object_MakerCountrySP_1303(TVarChar) OWNER TO postgres;
+
+
+/*-------------------------------------------------------------------------------
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.   Шаблий О.В.
+ 24.06.22                                                       *              
+
+*/
+
+-- тест
+-- SELECT * FROM gpSelect_Object_MakerCountrySP_1303('2')
