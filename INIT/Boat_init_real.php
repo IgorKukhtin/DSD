2cf9f49b0dd2a84a933b7bd2d94fbf16f7c2fdcb
K0sta <ashtu777@gmail.com>
2022-07-31 10:47:03 +0300

    ok

--- INIT/Boat_init_real.php
+++ INIT/Boat_init_real.php
@@ -1,1 +1,5 @@
-http://217.92.58.239:11011/projectBoat_utf8/index.php
+<?php
+  $connectstring = "port=5432 dbname=projectboat host=217.92.58.239:11011 user=postgres password=ET862tira";
+  $host = "http://217.92.58.239:11011/projectBoat_utf8/index.php";
+  $isArchive = true;
+?>
