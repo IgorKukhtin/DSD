6e21a2fef1ae50414bced342d1a8dff8ecd20ab6
K0sta <ashtu777@gmail.com>
2022-09-29 13:38:39 +0300

    ok

---
+++ DATABASE/COMMON/VIEW/01Object/Object_Unit_check_isOrder_View.sql
@@ -0,0 +1,25 @@
+-- View: Object_Unit_check_isOrder_View
+
+-- DROP VIEW IF EXISTS Object_Unit_check_isOrder_View;
+
+CREATE OR REPLACE VIEW Object_Unit_check_isOrder_View AS
+
+   SELECT Object_Unit.Id         AS UnitId
+        , Object_Unit.ObjectCode AS UnitCode
+        , Object_Unit.ValueData  AS UnitName
+   FROM Object AS Object_Unit
+   WHERE Object_Unit.Id = 8459 -- Розподільчий комплекс
+    --AND 1=0
+   ;
+
+ALTER TABLE Object_Unit_check_isOrder_View OWNER TO postgres;
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 29.09.22                                        *
+*/
+
+-- тест
+-- SELECT * FROM Object_Unit_check_isOrder_View
