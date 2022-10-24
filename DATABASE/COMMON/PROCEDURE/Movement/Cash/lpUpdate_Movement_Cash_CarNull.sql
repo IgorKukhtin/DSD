0cb877a72d9184cf74b5800e1a865d7938a30cdc
innafelon <mailto:innafelon@gmail.com>
2022-10-24 20:45:09 +0300

    +

--- DATABASE/COMMON/PROCEDURE/Movement/Cash/lpUpdate_Movement_Cash_CarNull.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/Cash/lpUpdate_Movement_Cash_CarNull.sql
@@ -62,10 +62,10 @@ $BODY$
 --
 
 
-/*
+/*   --обнуление переметра Авто
 
-SELECT Movement.Id
-    -- , lpUpdate_Movement_Cash_CarNull (inMovementId:= Movement.Id, inId:= MovementItem.Id, inUserId = zfCalc_UserAdmin()::Integer)
+SELECT ---Movement.Id
+       lpUpdate_Movement_Cash_CarNull (inMovementId:= Movement.Id, inId:= MovementItem.Id, inUserId = zfCalc_UserAdmin()::Integer)
 FROM Movement
     INNER JOIN MovementItem ON MovementItem.MovementId = Movement.Id
                            AND MovementItem.DescId = zc_MI_Master()
@@ -77,6 +77,37 @@ FROM Movement
 WHERE Movement.DescId = zc_Movement_Cash()
  AND Movement.StatusId = zc_Enum_Status_Complete()
  AND Movement.OperDate BETWEEN  '01.08.2022' AND '01.11.2022'
+
+*/
+
+
+
+
+ /* --выборка статей их документов Касса , где выбран Авто     
+ 
+SELECT DISTINCT
+       Min (Movement.OperDate)  AS OperDate_min
+     , Max (Movement.OperDate)  AS OperDate_max
+     , MILinkObject_InfoMoney.ObjectId AS InfoMoneyId
+     , Object_InfoMoney.ValueDAta      AS InfoMoneyName
+FROM Movement
+     INNER JOIN MovementItem ON MovementItem.MovementId = Movement.Id
+                            AND MovementItem.DescId = zc_MI_Master()
+                            AND MovementItem.isErased = False
+     INNER JOIN MovementItemLinkObject AS MILinkObject_Car
+                                ON MILinkObject_Car.MovementItemId = MovementItem.Id
+                               AND MILinkObject_Car.DescId = zc_MILinkObject_Car()
+                               AND MILinkObject_Car.ObjectId > 0
+     INNER JOIN MovementItemLinkObject AS MILinkObject_InfoMoney
+                                       ON MILinkObject_InfoMoney.MovementItemId = MovementItem.Id
+                                      AND MILinkObject_InfoMoney.DescId = zc_MILinkObject_InfoMoney()
+     LEFT JOIN Object AS Object_InfoMoney ON Object_InfoMoney.Id = MILinkObject_InfoMoney.ObjectId
+
+WHERE Movement.DescId = zc_Movement_Cash()
+ AND Movement.StatusId = zc_Enum_Status_Complete()
+and Movement.OperDate BETWEEN  '01.10.2022' AND '01.11.2022'
+group by MILinkObject_InfoMoney.ObjectId 
+       , Object_InfoMoney.VAlueData    
 
 
 */
