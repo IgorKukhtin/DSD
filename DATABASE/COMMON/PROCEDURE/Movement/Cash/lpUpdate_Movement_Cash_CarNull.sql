d56d6867b97ecd77c635f58687f2cf3d33de973a
innafelon <mailto:innafelon@gmail.com>
2022-10-24 20:35:55 +0300

    +

---
+++ DATABASE/COMMON/PROCEDURE/Movement/Cash/lpUpdate_Movement_Cash_CarNull.sql
@@ -0,0 +1,82 @@
+-- Function: lpUpdate_Movement_Cash_CarNull()
+
+DROP FUNCTION IF EXISTS lpUpdate_Movement_Cash_CarNull (Integer, Integer, Integer);
+
+CREATE OR REPLACE FUNCTION lpUpdate_Movement_Cash_CarNull(
+    IN inId                    Integer   , -- Ключ объекта <строка>
+    IN ininMovementId          Integer   , -- Ключ объекта <документ>
+    IN inUserId                Integer     -- Пользователь
+)
+RETURNS VOID
+AS
+$BODY$
+   DECLARE vbStatusId Integer;
+BEGIN
+
+     -- проверка, !!!только если это не выпалата по ведомости!!!
+     IF ( COALESCE (inMovementId, 0) = 0 OR COALESCE (inId, 0) = 0)
+     THEN
+        RETURN;
+     END IF;
+
+
+     vbStatusId := (SELECT Movement.StatusId FROM Movement WHERE Movement.Id = inMovementId);
+
+     --если документ проведен распроводим
+     IF vbStatusId = zc_Enum_Status_Complete()
+     THEN
+         PERFORM lpUnComplete_Movement (inMovementId := ioId
+                                      , inUserId     := vbUserId);
+     END IF;
+     
+      -- обнуляем АВТО
+     PERFORM lpInsertUpdate_MovementItemLinkObject (zc_MILinkObject_Car(), inId, NULL);
+
+     --если документ был проведен - проводим
+     IF vbStatusId = zc_Enum_Status_Complete()
+     THEN
+         -- создаются временные таблицы - для формирование данных для проводок
+         PERFORM lpComplete_Movement_Finance_CreateTemp();
+
+         -- проводим Документ
+         PERFORM lpComplete_Movement_Cash (inMovementId := inMovementId
+                                         , inUserId     := vbUserId
+                                          ); 
+     END IF;
+     
+     
+     -- сохранили протокол
+     --PERFORM lpInsert_MovementItemProtocol (inId, inUserId, FALSE);
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 24.10.22         *
+*/
+
+-- тест
+--
+
+
+/*
+
+SELECT Movement.Id
+    -- , lpUpdate_Movement_Cash_CarNull (inMovementId:= Movement.Id, inId:= MovementItem.Id, inUserId = zfCalc_UserAdmin()::Integer)
+FROM Movement
+    INNER JOIN MovementItem ON MovementItem.MovementId = Movement.Id
+                           AND MovementItem.DescId = zc_MI_Master()
+                           AND MovementItem.isErased = False
+    INNER JOIN MovementItemLinkObject AS MILinkObject_Car
+                                      ON MILinkObject_Car.MovementItemId = MovementItem.Id
+                                     AND MILinkObject_Car.DescId = zc_MILinkObject_Car()
+                                     AND MILinkObject_Car.ObjectId > 0
+WHERE Movement.DescId = zc_Movement_Cash()
+ AND Movement.StatusId = zc_Enum_Status_Complete()
+ AND Movement.OperDate BETWEEN  '01.08.2022' AND '01.11.2022'
+
+
+*/
