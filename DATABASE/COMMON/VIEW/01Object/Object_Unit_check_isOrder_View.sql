982624fb2edb079eda7c1c81d89a631ef01c184e
K0sta <ashtu777@gmail.com>
2022-11-08 15:30:46 +0200

    ok

--- DATABASE/COMMON/VIEW/01Object/Object_Unit_check_isOrder_View.sql
+++ DATABASE/COMMON/VIEW/01Object/Object_Unit_check_isOrder_View.sql
@@ -10,6 +10,26 @@ CREATE OR REPLACE VIEW Object_Unit_check_isOrder_View_two AS
    FROM Object AS Object_Unit
    WHERE Object_Unit.Id = 8459 -- Розподільчий комплекс
    --AND 1=0
+
+  UNION
+   SELECT Object_Unit.Id         AS UnitId
+        , Object_Unit.ObjectCode AS UnitCode
+        , Object_Unit.ValueData  AS UnitName
+   FROM ObjectLink
+        JOIN Object AS Object_Unit ON Object_Unit.Id = ObjectLink.ObjectId
+   WHERE ObjectLink.ChildObjectId = 8460 -- Возвраты общие
+     AND ObjectLink.DescId = zc_ObjectLink_Unit_Parent()
+   --AND 1=0
+
+  UNION
+   SELECT Object_Unit.Id         AS UnitId
+        , Object_Unit.ObjectCode AS UnitCode
+        , Object_Unit.ValueData  AS UnitName
+   FROM ObjectLink
+        JOIN Object AS Object_Unit ON Object_Unit.Id = ObjectLink.ObjectId
+   WHERE ObjectLink.ChildObjectId > 0
+     AND ObjectLink.DescId = zc_ObjectLink_Unit_Branch()
+   --AND 1=0
    ;
 
 ALTER TABLE Object_Unit_check_isOrder_View_two OWNER TO postgres;
