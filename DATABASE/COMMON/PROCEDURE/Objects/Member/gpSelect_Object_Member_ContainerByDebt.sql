3d5230b134f460de28e200f22af50c8154f3ceae
K0sta <ashtu777@gmail.com>
2022-11-13 12:32:00 +0200

    ok

--- DATABASE/COMMON/PROCEDURE/Objects/Member/gpSelect_Object_Member_ContainerByDebt.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/Member/gpSelect_Object_Member_ContainerByDebt.sql
@@ -31,18 +31,28 @@ BEGIN
    -- Результат
    RETURN QUERY 
        WITH tmpContainer AS
-            (SELECT CLO_Member.ObjectId    AS MemberId
+            (SELECT CLO_Member.ObjectId AS MemberId
+                 -- COALESCE (ObjectLink_Member.ChildObjectId, CLO_Member.ObjectId) AS MemberId
                   , CLO_InfoMoney.ObjectId AS InfoMoneyId
                   , CLO_Branch.ObjectId    AS BranchId
                   , CLO_Car.ObjectId       AS CarId
                   , CLO_JuridicalBasis.ObjectId AS JuridicalBasisId
                   , Object_Account.Id      AS AccountId
                   , Container.Amount
+                  , Container.Id
+                  , ObjectLink_Member.ChildObjectId AS MemberId_jur
              FROM Container                                        
                   INNER JOIN ContainerLinkObject AS CLO_Member
                                                  ON CLO_Member.ContainerId = Container.Id
                                                 AND CLO_Member.DescId = zc_ContainerLinkObject_Member()
                                                 AND CLO_Member.ObjectId > 0
+                  LEFT JOIN ContainerLinkObject AS CLO_Goods
+                                                ON CLO_Goods.ContainerId = Container.Id
+                                               AND CLO_Goods.DescId = zc_ContainerLinkObject_Goods()
+                                               AND CLO_Goods.ObjectId > 0
+                  LEFT JOIN ObjectLink AS ObjectLink_Member
+                                       ON ObjectLink_Member.ObjectId = CLO_Member.ObjectId
+                                      AND ObjectLink_Member.DescId      = zc_ObjectLink_Personal_Member()
                   LEFT JOIN ContainerLinkObject AS CLO_InfoMoney
                                                 ON CLO_InfoMoney.ContainerId = Container.Id
                                                AND CLO_InfoMoney.DescId = zc_ContainerLinkObject_InfoMoney()
@@ -58,7 +68,8 @@ BEGIN
                   LEFT JOIN Object AS Object_Account ON Object_Account.Id = Container.ObjectId
              WHERE Container.DescId = zc_Container_Summ()
                AND Container.Amount <> 0
-               AND (CLO_Member.ObjectId = inMemberId OR COALESCE (inMemberId, 0) = 0)
+               AND (CLO_Member.ObjectId = inMemberId OR ObjectLink_Member.ObjectId = inMemberId OR COALESCE (inMemberId, 0) = 0)
+               AND CLO_Goods.ContainerId IS NULL
             )
 
        -- Результат
@@ -68,15 +79,20 @@ BEGIN
             , View_InfoMoney.InfoMoneyId
             , View_InfoMoney.InfoMoneyCode
             , View_InfoMoney.InfoMoneyName
-            , View_InfoMoney.InfoMoneyName_all
-            , Object_Branch.Id                 AS BranchId
-            , Object_Branch.ObjectCode         AS BranchCode
-            , Object_Branch.ValueData          AS BranchName
-            , Object_JuridicalBasis.Id         AS JuridicalBasisId
-            , Object_JuridicalBasis.ValueData  AS JuridicalBasisName              
-            , Object_Account.Id                AS AccountId
-            , Object_Account.ValueData         AS AccountName
-            , Object_Car.Id                    AS CarId
+            , (View_InfoMoney.InfoMoneyName_all
+              || ' ('
+              || CASE WHEN tmpContainer.MemberId_jur > 0 THEN '*' ELSE '' END
+              || tmpContainer.Id :: TVarChar
+              || ')'
+              ) :: TVarChar AS InfoMoneyName_all
+            , Object_Branch.Id                    AS BranchId
+            , Object_Branch.ObjectCode            AS BranchCode
+            , Object_Branch.ValueData             AS BranchName
+            , Object_JuridicalBasis.Id            AS JuridicalBasisId
+            , Object_JuridicalBasis.ValueData     AS JuridicalBasisName              
+            , Object_Account_View.AccountId       AS AccountId
+            , Object_Account_View.AccountName_All AS AccountName
+            , Object_Car.Id                       AS CarId
             , (COALESCE (Object_CarModel.ValueData, '') || ' ' || COALESCE (Object_Car.ValueData, '')) :: TVarChar AS CarName
             , CASE WHEN tmpContainer.Amount > 0 THEN tmpContainer.Amount ELSE 0 END                    ::TFloat AS AmountDebet
             , CASE WHEN tmpContainer.Amount < 0 THEN -1 * tmpContainer.Amount ELSE 0 END               ::TFloat AS AmountKredit
@@ -86,7 +102,7 @@ BEGIN
             LEFT JOIN Object AS Object_Member ON Object_Member.Id = tmpContainer.MemberId
             LEFT JOIN Object AS Object_Branch ON Object_Branch.Id = tmpContainer.BranchId
             LEFT JOIN Object AS Object_JuridicalBasis ON Object_JuridicalBasis.Id = tmpContainer.JuridicalBasisId
-            LEFT JOIN Object AS Object_Account ON Object_Account.Id = tmpContainer.AccountId
+            LEFT JOIN Object_Account_View             ON Object_Account_View.AccountId = tmpContainer.AccountId
             LEFT JOIN Object_InfoMoney_View AS View_InfoMoney ON View_InfoMoney.InfoMoneyId = tmpContainer.InfoMoneyId
             LEFT JOIN Object AS Object_Car ON Object_Car.Id = tmpContainer.CarId
             
