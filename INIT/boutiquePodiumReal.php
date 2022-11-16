d1cd877efba0caeb352cc221f3dc5a053fcc37da
K0sta <ashtu777@gmail.com>
2022-11-16 10:58:08 +0200

    ok

--- INIT/boutiquePodiumReal.php
+++ INIT/boutiquePodiumReal.php
@@ -1,5 +1,5 @@
 <?php
-   $connectstring = "port=5432 dbname=podium host=212.115.239.119 user=postgres password=ET862TIRA";
+   $connectstring = "port=5432 dbname=podium host=91.243.196.144:80 user=postgres password=ET862TIRA";
    $host = "http://91.243.196.144:80/boutique/index.php";
    $isArchive = true;
 ?>
--- INIT/boutiquePodiumTest.php
+++ INIT/boutiquePodiumTest.php
@@ -1,5 +1,5 @@
 <?php
-   $connectstring = "port=5432 dbname=boutique_test host=94.27.55.146 user=postgres password=ET862tira";
-   $host = "http://94.27.55.146/boutique_test/index.php";
+   $connectstring = "port=5432 dbname=podium_test host=172.20.0.2 user=postgres password=ET862tira";
+   $host = "http://in.mer-lin.org.ua/podium_test/index.php";
    $isArchive = true;
 ?>
