b291438346aca214793e5284792d62fce805c89f
K0sta <ashtu777@gmail.com>
2022-11-16 10:58:00 +0200

    ok

--- INIT/podium_init.php
+++ INIT/podium_init.php
@@ -1,5 +1,5 @@
 <?php
-   $connectstring = "port=5432 dbname=podium host=212.115.239.119 user=postgres password=ET862TIRA";
-   $host = "http://91.243.196.144:80/boutique/index.php";
+   $connectstring = "port=5432 dbname=podium_test host=172.20.0.2 user=postgres password=ET862tira";
+   $host = "http://in.mer-lin.org.ua/podium_test/index.php";
    $isArchive = true;
 ?>
