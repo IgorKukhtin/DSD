426524e52121590d97abb4c2c236605e3702a951
K0sta <ashtu777@gmail.com>
2023-02-16 10:26:10 +0200

    ok

--- INIT/init-SRV.php
+++ INIT/init-SRV.php
@@ -1,5 +1,6 @@
 <?php
   $connectstring = "port=5432 dbname=project_24012023 host=192.168.0.194 user=project password=vas6ok";
-  $host = "http://project-main.alan.dp.ua/project/index.php";
+  $host = "https://project-main.alan.dp.ua/project/index.php";
   $isArchive = true;
 ?>
+http://localhost/project/index.php
