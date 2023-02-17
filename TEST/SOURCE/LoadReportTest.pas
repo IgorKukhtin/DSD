49408d81d40a0719806979a26391b801fb47c69f
innafelon <mailto:innafelon@gmail.com>
2023-02-17 20:11:39 +0200

    +

--- TEST/SOURCE/LoadReportTest.pas
+++ TEST/SOURCE/LoadReportTest.pas
@@ -616,9 +616,9 @@ begin
 
   LoadReportFromFile('PrintMovement_TTN_43233918', ReportPath + '\Товарный Учет\PrintMovement_TTN_43233918.fr3');
   exit;
-  LoadReportFromFile('PrintMovement_TTN_071021', ReportPath + '\Товарный Учет\PrintMovement_TTN_071021.fr3');
-  exit;
   }
+  LoadReportFromFile('PrintMovement_TTN_071021', ReportPath + '\Товарный Учет\PrintMovement_TTN_071021.fr3');
+ 
   LoadReportFromFile('PrintMovement_TTN', ReportPath + '\Товарный Учет\PrintMovement_TTN.fr3');
   exit;
   {
