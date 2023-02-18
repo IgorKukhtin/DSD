3c24de2597ee9d3ca60575dc58d0da7ddd9018c4
K0sta <ashtu777@gmail.com>
2023-02-18 20:56:27 +0200

    ok

--- INIT/boatLocal.bat
+++ INIT/boatLocal.bat
@@ -1,1 +1,2 @@
 copy /Y Boat_init_local.php Boat_init.php
+copy /Y Boat_init_local.php "../Bin/_64/INIT/Boat_init.php"
--- INIT/boatTest.bat
+++ INIT/boatTest.bat
@@ -1,1 +1,2 @@
 copy /Y Boat_init_test.php Boat_init.php
+copy /Y Boat_init_test.php "../Bin/_64/INIT/Boat_init.php"
