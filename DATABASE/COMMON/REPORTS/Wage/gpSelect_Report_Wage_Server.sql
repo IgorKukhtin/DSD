057bd31095437cc55e18718adad4203139d36a5e
K0sta <ashtu777@gmail.com>
2023-03-16 10:31:10 +0200

    ok

--- DATABASE/COMMON/REPORTS/Wage/gpSelect_Report_Wage_Server.sql
+++ DATABASE/COMMON/REPORTS/Wage/gpSelect_Report_Wage_Server.sql
@@ -761,7 +761,7 @@ BEGIN
         WHERE tmpMemberPersonalServiceList.PersonalServiceListId > 0 OR vbObjectId_Constraint_Branch = 0
           AND (ObjectLink_Personal_Member_find.ChildObjectId = inMemberId OR COALESCE (inMemberId, 0) = 0)
 
-       UNION ALL
+        UNION ALL
         SELECT
             0  :: Integer  AS StaffListId
           , 0  :: Integer  AS StaffListCode
