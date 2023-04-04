7bd502270b2d896e116b268afee1806bdd7e90fa
innafelon <mailto:innafelon@gmail.com>
2023-04-04 19:54:02 +0300

    ++

--- DATABASE/COMMON/PROCEDURE/Movement/StoreReal/gpGet_Movement_StoreReal.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/StoreReal/gpGet_Movement_StoreReal.sql
@@ -18,18 +18,24 @@ RETURNS TABLE (Id Integer
              , InsertName TVarChar
              , PartnerId Integer
              , PartnerName TVarChar
-             , Comment TVarChar
+             , Comment TVarChar 
+             , InvNumberFull_last1 TVarChar
+             , InvNumberFull_last2 TVarChar
+             , InvNumberFull_last3 TVarChar
               )
 AS
 $BODY$
    DECLARE vbUserId   Integer;
    DECLARE vbUserName TVarChar;
+   DECLARE vbPartnerId Integer;
 BEGIN
       -- проверка прав пользователя на вызов процедуры
       -- vbUserId := PERFORM lpCheckRight (inSession, zc_Enum_Process_Get_Movement_StoreReal());
       vbUserId:= lpGetUserBySession (inSession);
       vbUserName:= (SELECT Object.ValueData FROM Object WHERE Object.Id = vbUserId);
 
+      vbPartnerId := (SELECT MLO.ObjectId FROM MovementLinkObject AS MLO WHERE MLO.MovementId = inMovementId AND MLO.DescId = zc_MovementLinkObject_Partner());
+      
       IF COALESCE(inMovementId, 0) = 0 
       THEN
            RETURN QUERY
@@ -43,11 +49,35 @@ BEGIN
                   , Null ::TDateTime                            AS InsertMobileDate
                   , vbUserName                                  AS InserName 
                   , 0::Integer                                  AS PartnerId
-                  , ''::TVarChar                                AS PartnerName
-                  , ''::TVarChar                                AS Comment   
+                  , '' ::TVarChar                               AS PartnerName
+                  , '' ::TVarChar                               AS Comment
+
+                  , '' ::TVarChar                               AS InvNumberFull_last1
+                  , '' ::TVarChar                               AS InvNumberFull_last2
+                  , '' ::TVarChar                               AS InvNumberFull_last3 
              FROM lfGet_Object_Status(zc_Enum_Status_UnComplete()) AS Object_Status;
       ELSE
            RETURN QUERY
+           WITH 
+            --предыдущие 3 документа
+            tmpMovement_last AS (SELECT MAX (CASE WHEN tmp.Ord = 1 THEN ('№ ' || tmp.InvNumber || ' от ' || zfConvert_DateToString (tmp.OperDate)  ) ELSE '' END) :: TVarChar AS InvNumberFull_last1
+                                     , MAX (CASE WHEN tmp.Ord = 2 THEN ('№ ' || tmp.InvNumber || ' от ' || zfConvert_DateToString (tmp.OperDate)  ) ELSE '' END) :: TVarChar AS InvNumberFull_last2
+                                     , MAX (CASE WHEN tmp.Ord = 3 THEN ('№ ' || tmp.InvNumber || ' от ' || zfConvert_DateToString (tmp.OperDate)  ) ELSE '' END) :: TVarChar AS InvNumberFull_last3
+                                FROM (SELECT Movement.*
+                                           , ROW_NUMBER() OVER (ORDER BY Movement.OperDate Desc) AS Ord
+                                      FROM Movement
+                                           INNER JOIN MovementLinkObject AS MovementLinkObject_Partner
+                                                                         ON MovementLinkObject_Partner.MovementId = Movement.Id
+                                                                        AND MovementLinkObject_Partner.DescId = zc_MovementLinkObject_Partner()
+                                                                        AND MovementLinkObject_Partner.ObjectId = vbPartnerId
+                                      WHERE Movement.DescId = zc_Movement_StoreReal()
+                                        AND Movement.StatusId <> zc_Enum_Status_Erased()
+                                        AND Movement.Id <> inMovementId
+                                      ) AS tmp
+                                WHERE tmp.Ord < 4
+                                )
+
+
              SELECT Movement.Id                           
                   , Movement.InvNumber                        
                   , Movement.OperDate                      
@@ -60,6 +90,9 @@ BEGIN
                   , Object_Partner.Id                AS PartnerId
                   , Object_Partner.ValueData         AS PartnerName
                   , MovementString_Comment.ValueData AS Comment
+                  , tmpMovement_last.InvNumberFull_last1 ::TVarChar
+                  , tmpMovement_last.InvNumberFull_last2 ::TVarChar
+                  , tmpMovement_last.InvNumberFull_last3 ::TVarChar
              FROM Movement
                   LEFT JOIN Object AS Object_Status ON Object_Status.Id = Movement.StatusId
 
@@ -88,6 +121,8 @@ BEGIN
                                                ON MovementLinkObject_Partner.MovementId = Movement.Id
                                               AND MovementLinkObject_Partner.DescId = zc_MovementLinkObject_Partner()
                   LEFT JOIN Object AS Object_Partner ON Object_Partner.Id = MovementLinkObject_Partner.ObjectId
+                  
+                  LEFT JOIN tmpMovement_last ON 1 = 1
              
              WHERE Movement.Id = inMovementId
                AND Movement.DescId = zc_Movement_StoreReal();
