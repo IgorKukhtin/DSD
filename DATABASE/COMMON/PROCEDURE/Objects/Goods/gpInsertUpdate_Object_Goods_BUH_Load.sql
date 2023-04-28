ffca006191b56d12bfce7670420311ae968a8060
K0sta <ashtu777@gmail.com>
2023-04-28 13:12:34 +0300

    ok

--- DATABASE/COMMON/PROCEDURE/Objects/Goods/gpInsertUpdate_Object_Goods_BUH_Load.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/Goods/gpInsertUpdate_Object_Goods_BUH_Load.sql
@@ -6,8 +6,8 @@ DROP FUNCTION IF EXISTS gpInsertUpdate_Object_Goods_BUH_Load (TDateTime, Integer
 CREATE OR REPLACE FUNCTION gpInsertUpdate_Object_Goods_BUH_Load(
     IN inDate_BUH            TDateTime ,
     IN inGoodsCode           Integer   , -- Код объекта <Товар>
-    IN inGoodsName           TVarChar  , -- 
-    IN inGoodsName_New       TVarChar  , -- 
+    IN inGoodsName           TVarChar  , --
+    IN inGoodsName_New       TVarChar  , --
     IN inSession             TVarChar    -- сессия пользователя
 )
 RETURNS VOID
@@ -20,6 +20,10 @@ $BODY$
 BEGIN
      -- проверка прав пользователя на вызов процедуры
      vbUserId := lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_Object_Goods());
+
+
+     IF vbUserId = 5 THEN vbUserId:= zc_Enum_Process_Auto_PrimeCost(); END IF;
+
 
      -- !!!Пустой код - Пропустили!!!
      IF COALESCE (inGoodsCode, 0) = 0 THEN
@@ -41,26 +45,37 @@ BEGIN
      END IF;
 
 
-     --сохраненное значение переносим в  zc_ObjectString_Goods_BUH на дату, если zc_ObjectString_Goods_BUH не пусто  = ошибка 
+     -- в этом случа выход
+     IF EXISTS (SELECT 1 FROM ObjectDate WHERE ObjectDate.ObjectId = vbGoodsId AND ObjectDate.DescId = zc_ObjectDate_Goods_BUH() AND ObjectDate.ValueData = inDate_BUH)
+     THEN
+         RETURN;
+     END IF;
+
+
+     -- сохраненное значение переносим в  zc_ObjectString_Goods_BUH на дату, если zc_ObjectString_Goods_BUH не пусто  = ошибка
      vbGoodsName_BUH := (SELECT OS FROM ObjectString AS OS WHERE OS.DescId = zc_ObjectString_Goods_BUH() and OS.ObjectId = vbGoodsId);
-     
+
      IF COALESCE (vbGoodsName_BUH,'') <> ''
      THEN
+         -- в этом случа выход
+         RETURN;
+         --
          RAISE EXCEPTION 'Ошибка.Для Товара - <(%) %> уже заполнено <Название (бухг.)>.', inGoodsCode, inGoodsName;
      END IF;
 
 
-     vbGoodsName := (SELECT Object.ValueData FROM Object WHERE Object.DescId = zc_Object_Goods() and Object.Id = vbGoodsId);  
-     
+     vbGoodsName := (SELECT Object.ValueData FROM Object WHERE Object.DescId = zc_Object_Goods() and Object.Id = vbGoodsId);
+
      -- сохранили новое название
      PERFORM lpInsertUpdate_Object (vbGoodsId, zc_Object_Goods(), inGoodsCode, inGoodsName_New);
-     
+
      -- сохранили свойство <Название БУХГ>
      PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Goods_BUH(), vbGoodsId, vbGoodsName);
      -- сохранили свойство <Дата до которой действует Название товара(бухг.)>
      PERFORM lpInsertUpdate_ObjectDate (zc_ObjectDate_Goods_BUH(), vbGoodsId, inDate_BUH);
 
-
+     -- сохранили протокол
+     PERFORM lpInsert_ObjectProtocol (vbGoodsId, vbUserId);
 
 END;
 $BODY$
