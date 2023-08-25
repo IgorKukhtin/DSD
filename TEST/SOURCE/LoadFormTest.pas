a029444a68f693de8ecd8924788748ee9cdaf552
innafelon <mailto:innafelon@gmail.com>
2023-08-25 19:47:43 +0300

    +

--- TEST/SOURCE/LoadFormTest.pas
+++ TEST/SOURCE/LoadFormTest.pas
@@ -882,26 +882,26 @@ end;
 
 procedure TLoadFormTest.LoadPersonalServiceFormTest;
 begin
-
+  {
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TPriceNalogDialogForm'));
   TdsdFormStorageFactory.GetStorage.Load('TPriceNalogDialogForm');
-  {
+
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TReport_PersonalService_RecalcForm'));
   TdsdFormStorageFactory.GetStorage.Load('TReport_PersonalService_RecalcForm');
   exit;
-   }
+
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TPersonalServiceForm'));
   TdsdFormStorageFactory.GetStorage.Load('TPersonalServiceForm');
 
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TPersonalServiceJournalForm'));
   TdsdFormStorageFactory.GetStorage.Load('TPersonalServiceJournalForm');
   exit;
-  {
+  }
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TPersonalServiceItemJournalForm'));
   TdsdFormStorageFactory.GetStorage.Load('TPersonalServiceItemJournalForm');
 
   exit;
-
+  {
   TdsdFormStorageFactory.GetStorage.Save(GetForm('TPersonalServiceJournalChoiceForm'));
   TdsdFormStorageFactory.GetStorage.Load('TPersonalServiceJournalChoiceForm');
   }
