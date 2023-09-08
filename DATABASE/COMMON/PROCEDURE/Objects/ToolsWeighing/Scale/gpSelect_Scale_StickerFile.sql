e7b1799635c57dc4cdcee7161eea371326d27ebe
K0sta <ashtu777@gmail.com>
2023-09-08 23:12:39 +0300

    ok

--- DATABASE/COMMON/PROCEDURE/Objects/ToolsWeighing/Scale/gpSelect_Scale_StickerFile.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/ToolsWeighing/Scale/gpSelect_Scale_StickerFile.sql
@@ -6,11 +6,17 @@ CREATE OR REPLACE FUNCTION gpSelect_Scale_StickerFile(
     IN inBranchCode            Integer,      --
     IN inSession               TVarChar      -- сессия пользователя
 )
-RETURNS TABLE (Id             Integer
-             , Code           Integer
-             , FileName       TVarChar
-             , FileValue      Text
-             , FileValue2     TBlob
+RETURNS TABLE (Id                   Integer
+             , Code                 Integer
+             , FormName             TVarChar
+             , FileName             TVarChar
+             , FileValue            Text
+             , FileValue2           TBlob
+
+             , FormName_70_70       TVarChar
+             , FileName_70_70       TVarChar
+             , FileValue_70_70      Text
+             , FileValue2_70_70     TBlob
               )
 AS
 $BODY$
@@ -25,19 +31,38 @@ BEGIN
        SELECT Object_StickerFile.Id
             , Object_StickerFile.ObjectCode AS Code
               -- название файла fr3 в БАЗЕ
+            , ObjectForm.ValueData                                     AS FormName
             , (Object_StickerFile.ValueData || '.Sticker') :: TVarChar AS FileName
-              -- название файла fr3 в БАЗЕ
+              -- fr3 в БАЗЕ
             , ObjectBlob.ValueData :: Text AS FileValue
             , ObjectBlob.ValueData         AS FileValue2
+
+              -- название файла fr3 в БАЗЕ
+            , ObjectForm_70_70.ValueData         AS FormName_70_70
+            , CASE WHEN ObjectForm_70_70.ValueData <> '' THEN Object_StickerFile.ValueData || '_70_70.Sticker' ELSE '' END :: TVarChar AS FileName_70_70
+              -- fr3 в БАЗЕ
+            , ObjectBlob_70_70.ValueData :: Text AS FileValue_70_70
+            , ObjectBlob_70_70.ValueData         AS FileValue2_70_70
+
        FROM Object AS Object_StickerFile
             LEFT JOIN Object AS ObjectForm
                              ON ObjectForm.ValueData = (Object_StickerFile.ValueData || '.Sticker') :: TVarChar
                             AND ObjectForm.DescId    = zc_Object_Form()
             LEFT JOIN ObjectBlob ON ObjectBlob.ObjectId = ObjectForm.Id
                                 AND ObjectBlob.DescId   = zc_objectBlob_Form_Data()
+            --
+            LEFT JOIN Object AS ObjectForm_70_70
+                             ON ObjectForm_70_70.ValueData = (Object_StickerFile.ValueData || '_70_70.Sticker') :: TVarChar
+                            AND ObjectForm_70_70.DescId    = zc_Object_Form()
+            LEFT JOIN ObjectBlob AS ObjectBlob_70_70
+                                ON ObjectBlob_70_70.ObjectId = ObjectForm_70_70.Id
+                                AND ObjectBlob_70_70.DescId   = zc_objectBlob_Form_Data()
        WHERE Object_StickerFile.isErased = FALSE
          AND Object_StickerFile.DescId   = zc_Object_StickerFile()
          AND Object_StickerFile.ValueData <> ''
+       --AND (Object_StickerFile.ValueData NOT ILIKE 'ШАБЛОН + ТМ Фітнес Формат - Український'
+       --  OR vbUserId = 5
+       --    )
       ;
 
 END;
