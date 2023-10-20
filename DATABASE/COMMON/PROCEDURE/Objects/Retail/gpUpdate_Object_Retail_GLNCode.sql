42d7dce50fa5d55f085ace8504741551e3476d26
K0sta <ashtu777@gmail.com>
2023-10-20 21:04:32 +0300

    ok

--- DATABASE/COMMON/PROCEDURE/Objects/Retail/gpUpdate_Object_Retail_GLNCode.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/Retail/gpUpdate_Object_Retail_GLNCode.sql
@@ -19,39 +19,52 @@ CREATE OR REPLACE FUNCTION gpUpdate_Object_Retail_GLNCode(
 $BODY$
    DECLARE vbUserId Integer;
 BEGIN
-   -- проверка прав пользователя на вызов процедуры
-   vbUserId := lpCheckRight(inSession, zc_Enum_Process_Update_Object_Retail_GLNCode());
 
-   -- сохранили св-во <Код GLN - Получатель>
-   PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Retail_GLNCode(), ioId, inGLNCode);
-   -- сохранили св-во <Код GLN - Поставщик>
-   PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Retail_GLNCodeCorporate(), ioId, inGLNCodeCorporate);
-
-   IF inOKPO = ''
+   -- проверка
+   IF  inGLNCode <> COALESCE ((SELECT OS.ValueData FROM ObjectString AS OS WHERE OS.DescId = zc_ObjectString_Retail_GLNCode() AND OS.ObjectId = ioId), '')
+    OR inGLNCodeCorporate <> COALESCE ((SELECT OS.ValueData FROM ObjectString AS OS WHERE OS.DescId = zc_ObjectString_Retail_GLNCodeCorporate() AND OS.ObjectId = ioId), '')
+    OR inOKPO <> COALESCE ((SELECT OS.ValueData FROM ObjectString AS OS WHERE OS.DescId = zc_ObjectString_Retail_OKPO() AND OS.ObjectId = ioId), '')
+    OR inGoodsPropertyId <> COALESCE ((SELECT OL.ChildObjectId FROM ObjectLink AS OL WHERE OL.DescId = zc_ObjectString_Retail_OKPO() AND OL.ObjectId = ioId), 0)
    THEN
-       -- сохранили св-во <>
-       PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Retail_OKPO(), ioId, NULL);
-   ELSE
-       -- сохранили св-во <>
-       PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Retail_OKPO(), ioId, inOKPO);
+       -- проверка прав пользователя на вызов процедуры
+       vbUserId := lpCheckRight(inSession, zc_Enum_Process_Update_Object_Retail_GLNCode());
+
+       -- сохранили св-во <Код GLN - Получатель>
+       PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Retail_GLNCode(), ioId, inGLNCode);
+       -- сохранили св-во <Код GLN - Поставщик>
+       PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Retail_GLNCodeCorporate(), ioId, inGLNCodeCorporate);
+
+       -- сохранили связь с <Классификаторы свойств товаров>
+       PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Retail_GoodsProperty(), ioId, inGoodsPropertyId);   
+
+       IF inOKPO = ''
+       THEN
+           -- сохранили св-во <>
+           PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Retail_OKPO(), ioId, NULL);
+       ELSE
+           -- сохранили св-во <>
+           PERFORM lpInsertUpdate_ObjectString (zc_ObjectString_Retail_OKPO(), ioId, inOKPO);
+       END IF;
+
+   ELSE 
+       vbUserId := lpGetUserBySession (inSession);
+
    END IF;
+
 
    -- сохранили связь с <Сотрудник (Ответственный представитель маркетингового отдела)>
    PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Retail_PersonalMarketing(), ioId, inPersonalMarketingId);  
    -- сохранили связь с <Сотрудник (Ответственный представитель коммерческого отдела)>
    PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Retail_PersonalTrade(), ioId, inPersonalTradeId);  
 
-
-   -- сохранили связь с <Классификаторы свойств товаров>
-   PERFORM lpInsertUpdate_ObjectLink (zc_ObjectLink_Retail_GoodsProperty(), ioId, inGoodsPropertyId);   
-
    -- сохранили протокол
    PERFORM lpInsert_ObjectProtocol (ioId, vbUserId);
    
+
+   --RAISE EXCEPTION 'Ошибка.OK';
+
 END;$BODY$
   LANGUAGE plpgsql VOLATILE;
---ALTER FUNCTION gpUpdate_Object_Retail_GLNCode (Integer, TVarChar, TVarChar, Integer, TVarChar) OWNER TO postgres;
-
 
 /*-------------------------------------------------------------------------------*/
 /*
