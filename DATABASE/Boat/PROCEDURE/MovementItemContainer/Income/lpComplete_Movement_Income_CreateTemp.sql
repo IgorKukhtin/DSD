8d5c6544fb3a174e1e8659e5392882718740e1b2
K0sta <ashtu777@gmail.com>
2023-10-20 21:06:46 +0300

    ok

--- DATABASE/Boat/PROCEDURE/MovementItemContainer/Income/lpComplete_Movement_Income_CreateTemp.sql
+++ DATABASE/Boat/PROCEDURE/MovementItemContainer/Income/lpComplete_Movement_Income_CreateTemp.sql
@@ -10,32 +10,45 @@ BEGIN
      -- таблица - Проводки
      PERFORM lpComplete_Movement_All_CreateTemp();
 
-     -- таблица - элементы по контрагенту, со всеми свойствами для формирования Аналитик в проводках
-     CREATE TEMP TABLE _tmpItem_SummPartner (MovementItemId Integer, ContainerId Integer, AccountId Integer, ContainerId_VAT Integer, AccountId_VAT Integer
-                                           , GoodsId Integer, PartionId Integer
-                                           , OperSumm TFloat, OperSumm_VAT TFloat
-                                            ) ON COMMIT DROP;
+     IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = LOWER ('_tmpItem'))
+     THEN
 
-     -- таблица - элементы документа, со всеми свойствами для формирования Аналитик в проводках
-     CREATE TEMP TABLE _tmpItem (MovementItemId Integer
-                               , ContainerId_Summ Integer, ContainerId_Goods Integer
-                               , GoodsId Integer, PartionId Integer
-                               , OperCount TFloat, OperPrice_orig TFloat, OperPrice TFloat, CountForPrice TFloat
-                               , OperSumm TFloat, OperSumm_cost TFloat, OperSumm_VAT TFloat
-                               , AccountId Integer, InfoMoneyGroupId Integer, InfoMoneyDestinationId Integer, InfoMoneyId Integer
-                                ) ON COMMIT DROP;
+         DELETE FROM _tmpItem_SummPartner;
+         -- таблица - элементы документа, со всеми свойствами для формирования Аналитик в проводках
+         DELETE FROM _tmpItem;
+         -- таблица - сколько осталось зарезервировать для Заказов клиента
+         DELETE FROM _tmpReserveDiff;
+         -- таблица - элементы Резерв для Заказов клиента
+         DELETE FROM _tmpReserveRes;
 
-     -- таблица - сколько осталось зарезервировать для Заказов клиента
-     CREATE TEMP TABLE _tmpReserveDiff (MovementId_order Integer, OperDate_order TDateTime
-                                      , GoodsId Integer
-                                      , AmountPartner TFloat
-                                       ) ON COMMIT DROP;
-     -- таблица - элементы Резерв для Заказов клиента
-     CREATE TEMP TABLE _tmpReserveRes (MovementItemId Integer, ParentId Integer
-                                     , GoodsId Integer
-                                     , Amount TFloat
-                                     , MovementId_order Integer
-                                      ) ON COMMIT DROP;
+     ELSE
+         -- таблица - элементы по контрагенту, со всеми свойствами для формирования Аналитик в проводках
+         CREATE TEMP TABLE _tmpItem_SummPartner (MovementItemId Integer, ContainerId Integer, AccountId Integer, ContainerId_VAT Integer, AccountId_VAT Integer
+                                               , GoodsId Integer, PartionId Integer
+                                               , OperSumm TFloat, OperSumm_VAT TFloat
+                                                ) ON COMMIT DROP;
+
+         -- таблица - элементы документа, со всеми свойствами для формирования Аналитик в проводках
+         CREATE TEMP TABLE _tmpItem (MovementItemId Integer
+                                   , ContainerId_Summ Integer, ContainerId_Goods Integer
+                                   , GoodsId Integer, PartionId Integer
+                                   , OperCount TFloat, OperPrice_orig TFloat, OperPrice TFloat, CountForPrice TFloat
+                                   , OperSumm TFloat, OperSumm_cost TFloat, OperSumm_VAT TFloat
+                                   , AccountId Integer, InfoMoneyGroupId Integer, InfoMoneyDestinationId Integer, InfoMoneyId Integer
+                                    ) ON COMMIT DROP;
+
+         -- таблица - сколько осталось зарезервировать для Заказов клиента
+         CREATE TEMP TABLE _tmpReserveDiff (MovementId_order Integer, OperDate_order TDateTime
+                                          , GoodsId Integer
+                                          , AmountPartner TFloat
+                                           ) ON COMMIT DROP;
+         -- таблица - элементы Резерв для Заказов клиента
+         CREATE TEMP TABLE _tmpReserveRes (MovementItemId Integer, ParentId Integer
+                                         , GoodsId Integer
+                                         , Amount TFloat
+                                         , MovementId_order Integer
+                                          ) ON COMMIT DROP;
+     END IF;
 
 END;$BODY$
   LANGUAGE plpgsql VOLATILE;
