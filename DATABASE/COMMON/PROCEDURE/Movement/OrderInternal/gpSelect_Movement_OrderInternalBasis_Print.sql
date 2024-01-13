7c89259636a2055982defa11bdd4e80d4c11b7d8
K0sta <ashtu777@gmail.com>
2024-01-13 14:48:54 +0200

    ok

--- DATABASE/COMMON/PROCEDURE/Movement/OrderInternal/gpSelect_Movement_OrderInternalBasis_Print.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/OrderInternal/gpSelect_Movement_OrderInternalBasis_Print.sql
@@ -325,6 +325,7 @@ BEGIN
                                                                            AND View_GoodsByGoodsKind.GoodsKindId = Object_GoodsKind.Id
        WHERE tmpMI.Amount <> 0 OR tmpMI.AmountSecond <> 0 OR tmpMI.AmountSend <> 0 OR tmpMI.AmountPartner <> 0 OR tmpMI.AmountPartnerPrior <> 0 OR tmpMI.AmountPartnerSecond <> 0 -- OR tmpMI.Amount_calc <> 0
           OR tmpMI.AmountRemains <> 0
+          OR tmpMI.AmountSendOut <> 0
        ;
     RETURN NEXT Cursor2;
 
