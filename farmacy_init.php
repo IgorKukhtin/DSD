24cfbd54681268517210cf66910a1a3ea32bd0c3
Oleg <olegdh1264@gmail.com>
2024-01-26 17:05:54 +0200

    *

--- farmacy_init.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $connectstring = "port=5432 dbname=farmacy host=172.17.2.5 user=postgres password=eej9oponahT4gah3";
-  $host = "http://farmacy.neboley.dp.ua/index.php";
-  $isArchive = true;
-?>
-
