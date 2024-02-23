624670f83b263599c7fd14d9e222eebb4566946a
Oleg <olegdh1264@gmail.com>
2024-02-24 01:14:50 +0200

    BoatMobile

--- DPR/DropBoxBoat.dpr
+++ DPR/DropBoxBoat.dpr
@@ -1,4 +1,4 @@
-program GetEmailBoat;
+program DropBoxBoat;
 
 uses
   MidasLib,
@@ -33,8 +33,7 @@ uses
   dsdExportToXLSAction in '..\SOURCE\COMPONENT\dsdExportToXLSAction.pas',
   dsdTranslator in '..\SOURCE\COMPONENT\dsdTranslator.pas',
   PUSHMessage in '..\SOURCE\COMPONENT\PUSHMessage.pas' {PUSHMessageForm},
-  DialogPswSms in '..\SOURCE\DialogPswSms.pas' {DialogPswSmsForm},
-  sevenzip in '..\FormsBoat\MainUnitService\GetEmailBoat\sevenzip.pas';
+  DialogPswSms in '..\SOURCE\DialogPswSms.pas' {DialogPswSmsForm};
 
 {$R *.res}
 
