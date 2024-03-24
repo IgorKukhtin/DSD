42e01297c5b52d1cb53445fa57e3825c39132d86
K0sta <ashtu777@gmail.com>
2024-03-24 11:12:10 +0200

    ok

--- DATABASE/COMMON/VIEW/01Object/Object_GoodsByGoodsKind_View.sql
+++ DATABASE/COMMON/VIEW/01Object/Object_GoodsByGoodsKind_View.sql
@@ -4,19 +4,27 @@ DROP VIEW IF EXISTS Object_GoodsByGoodsKind_View;
 
 CREATE OR REPLACE VIEW Object_GoodsByGoodsKind_View AS
        SELECT
-             ObjectLink_GoodsByGoodsKind_GoodsKind.ObjectId    AS Id 
-           , ObjectLink_GoodsByGoodsKind_Goods.ChildObjectId   AS GoodsId
-           , Object_Goods.ObjectCode                           AS GoodsCode
-           , Object_Goods.ValueData                            AS GoodsName
-           , ObjectLink_GoodsByGoodsKind_GoodsKind.ChildObjectId AS GoodsKindId
-           , Object_GoodsKind.ObjectCode                       AS GoodsKindCode
-           , Object_GoodsKind.ValueData                        AS GoodsKindName
+             ObjectLink_GoodsByGoodsKind_GoodsKind.ObjectId           AS Id 
+           , ObjectLink_GoodsByGoodsKind_Goods.ChildObjectId          AS GoodsId
+           , Object_Goods.ObjectCode                                  AS GoodsCode
+           , Object_Goods.ValueData                                   AS GoodsName
+           , ObjectLink_GoodsByGoodsKind_GoodsKind.ChildObjectId      AS GoodsKindId
+           , Object_GoodsKind.ObjectCode                              AS GoodsKindCode
+           , Object_GoodsKind.ValueData                               AS GoodsKindName
+           , COALESCE (ObjectFloat_NormInDays.ValueData, 0) :: TFloat AS NormInDays
+           , Object_GoodsByGoodsKind.isErased                         AS isErased
        FROM ObjectLink AS ObjectLink_GoodsByGoodsKind_Goods
             JOIN ObjectLink AS ObjectLink_GoodsByGoodsKind_GoodsKind
                             ON ObjectLink_GoodsByGoodsKind_GoodsKind.ObjectId = ObjectLink_GoodsByGoodsKind_Goods.ObjectId
                            AND ObjectLink_GoodsByGoodsKind_GoodsKind.DescId = zc_ObjectLink_GoodsByGoodsKind_GoodsKind()
+            LEFT JOIN Object AS Object_GoodsByGoodsKind ON Object_GoodsByGoodsKind.Id = ObjectLink_GoodsByGoodsKind_Goods.ObjectId
             LEFT JOIN Object AS Object_Goods ON Object_Goods.Id = ObjectLink_GoodsByGoodsKind_Goods.ChildObjectId
             LEFT JOIN Object AS Object_GoodsKind ON Object_GoodsKind.Id = ObjectLink_GoodsByGoodsKind_GoodsKind.ChildObjectId
+            
+            LEFT JOIN ObjectFloat AS ObjectFloat_NormInDays
+                                  ON ObjectFloat_NormInDays.ObjectId = ObjectLink_GoodsByGoodsKind_Goods.ObjectId
+                                 AND ObjectFloat_NormInDays.DescId   = zc_ObjectFloat_GoodsByGoodsKind_NormInDays()
+            
        WHERE ObjectLink_GoodsByGoodsKind_Goods.DescId = zc_ObjectLink_GoodsByGoodsKind_Goods();
 
 
@@ -31,4 +39,4 @@ ALTER TABLE Object_GoodsByGoodsKind_View  OWNER TO postgres;
 */
 
 -- тест
--- SELECT * FROM Object_Goods_View
+-- SELECT * FROM Object_GoodsByGoodsKind_View
