d17d159c77d3444b5854c0f14200a62381563348
K0sta <ashtu777@gmail.com>
2024-04-10 20:32:40 +0300

    ok

--- DATABASE/COMMON/PROCEDURE/MovementItemContainer/Sale/lpComplete_Movement_Sale.sql
+++ DATABASE/COMMON/PROCEDURE/MovementItemContainer/Sale/lpComplete_Movement_Sale.sql
@@ -232,6 +232,7 @@ END IF;*/
               WHERE COALESCE (tmpMISale.AmountPartner,0) - COALESCE (tmpMIOrder.Amount,0) > tmpMIOrder.Amount15
                -- !!! замовлення в яких більше 2,7 кілограма.
                AND (COALESCE (tmpMIOrder.Amount,0) = 0 OR tmpMIOrder.Amount > 2.7)
+               --AND (COALESCE (tmpMIOrder.Amount,0) = 0 OR tmpMIOrder.Amount > 5)
               LIMIT 1
              ) AS tmp;
 
