e0cc1034c280defd84e08c1f275552d4398b1778
innafelon <innafelon@gmail.com>
2024-06-13 16:06:23 +0300

    +

--- DATABASE/COMMON/Process/Object/StickerProperty.sql
+++ DATABASE/COMMON/Process/Object/StickerProperty.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION zc_Enum_Process_InsertUpdate_Object_StickerProperty() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_InsertUpdate_Object_StickerProperty' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION zc_Enum_Process_Update_Object_StickerProperty_CK() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_Update_Object_StickerProperty_CK' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
-
+CREATE OR REPLACE FUNCTION zc_Enum_Process_Update_Object_StickerProperty_NormInDays_not() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_Update_Object_StickerProperty_NormInDays_not' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
+  
 DO $$
 BEGIN
 
@@ -11,10 +12,15 @@ PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_InsertUpdate_Object_S
                                   , inEnumName:= 'zc_Enum_Process_InsertUpdate_Object_StickerProperty');
 PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_Update_Object_StickerProperty_CK()
                                   , inDescId:= zc_Object_Process()
-                                  , inCode:= 1
+                                  , inCode:= 2
                                   , inName:= 'Справочник <'||(SELECT ItemName FROM ObjectDesc WHERE Id = zc_Object_StickerProperty())||'> - сохранение данных.'
                                   , inEnumName:= 'zc_Enum_Process_Update_Object_StickerProperty_CK');
 
+PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_Update_Object_StickerProperty_NormInDays_not()
+                                  , inDescId:= zc_Object_Process()
+                                  , inCode:= 3
+                                  , inName:= 'Справочник <'||(SELECT ItemName FROM ObjectDesc WHERE Id = zc_Object_StickerProperty())||'> - изменение данных.'
+                                  , inEnumName:= 'zc_Enum_Process_Update_Object_StickerProperty_NormInDays_not');
 END $$;
 
 
