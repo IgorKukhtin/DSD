c8d768b1d4fed6889de45b213a8a538667560a14
K0sta <ashtu777@gmail.com>
2024-07-08 15:43:41 +0300

    ok

--- DATABASE/Boat/PROCEDURE/ProjectBoatMobile/gpGet_Movement_MobilebyOrderClient.sql
+++ DATABASE/Boat/PROCEDURE/ProjectBoatMobile/gpGet_Movement_MobilebyOrderClient.sql
@@ -21,95 +21,130 @@ $BODY$
 BEGIN
      -- проверка прав пользователя на вызов процедуры
      vbUserId:= lpGetUserBySession (inSession);
-     
+
      IF SUBSTRING(inBarCode, 1, 4) = zc_BarCodePref_Movement()
      THEN
+         -- Не удален
+         SELECT Movement.Id, Movement.StatusId, Movement.InvNumber, Movement.DescId
+                INTO vbId, vbStatusId, vbInvNumber, vbDescId
+         FROM Movement
+         WHERE Movement.DescId  IN (zc_Movement_OrderClient(), zc_Movement_OrderInternal())
+           AND Movement.Id = SUBSTRING (inBarCode, 5, 8) :: Integer
+           AND Movement.StatusId <> zc_Enum_Status_Erased()
+        ;
 
-       SELECT Movement.Id, Movement.StatusId, Movement.InvNumber, Movement.DescId
-       INTO vbId, vbStatusId, vbInvNumber, vbDescId
-       FROM Movement
-       WHERE Movement.DescId  IN (zc_Movement_OrderClient(), zc_Movement_OrderInternal())
-         AND Movement.Id = SUBSTRING(inBarCode, 5, 8)::Integer;     
+         -- если не нашли
+         IF COALESCE (vbId, 0) = 0
+         THEN
+             -- ВСЕ
+             SELECT Movement.Id, Movement.StatusId, Movement.InvNumber, Movement.DescId
+                    INTO vbId, vbStatusId, vbInvNumber, vbDescId
+             FROM Movement
+             WHERE Movement.DescId  IN (zc_Movement_OrderClient(), zc_Movement_OrderInternal())
+               AND Movement.Id = SUBSTRING (inBarCode, 5, 8) :: Integer
+            ;
+         END IF;
 
      ELSEIF COALESCE(inBarCode, '') = '' AND COALESCE(inInvNumber, '') <> ''
      THEN
+         -- Не удален
+         SELECT Movement.Id, Movement.StatusId, Movement.InvNumber, Movement.DescId
+                INTO vbId, vbStatusId, vbInvNumber, vbDescId
+         FROM Movement
+         WHERE Movement.DescId IN (zc_Movement_OrderClient(), zc_Movement_OrderInternal())
+           AND Movement.InvNumber = TRIM (inInvNumber)
+           AND Movement.StatusId <> zc_Enum_Status_Erased()
+         ORDER BY CASE WHEN Movement.DescId = zc_Movement_OrderClient() THEN 0 ELSE 1 END
+         LIMIT 1
+        ;
 
-       SELECT Movement.Id, Movement.StatusId, Movement.InvNumber, Movement.DescId
-       INTO vbId, vbStatusId, vbInvNumber, vbDescId
-       FROM Movement
-       WHERE Movement.DescId IN (zc_Movement_OrderClient(), zc_Movement_OrderInternal())
-         AND Movement.InvNumber = TRIM(inInvNumber)
-       ORDER BY CASE WHEN Movement.DescId = zc_Movement_OrderClient() THEN 0 ELSE 1 END
-       LIMIT 1;     
+         -- если не нашли
+         IF COALESCE (vbId, 0) = 0
+         THEN
+             -- ВСЕ
+             SELECT Movement.Id, Movement.StatusId, Movement.InvNumber, Movement.DescId
+                    INTO vbId, vbStatusId, vbInvNumber, vbDescId
+             FROM Movement
+             WHERE Movement.DescId IN (zc_Movement_OrderClient(), zc_Movement_OrderInternal())
+               AND Movement.InvNumber = TRIM (inInvNumber)
+               AND Movement.StatusId <> zc_Enum_Status_Erased()
+             ORDER BY CASE WHEN Movement.DescId = zc_Movement_OrderClient() THEN 0 ELSE 1 END
+             LIMIT 1
+            ;
+         END IF;
 
      ELSE
-       vbId := 0;
+         -- не нашли
+         vbId := 0;
      END IF;
-     
+
+
      IF COALESCE (vbId, 0) <> 0 AND vbDescId = zc_Movement_OrderInternal()
      THEN
-     
+
        IF COALESCE((SELECT COUNT(DISTINCT MIFloat_MovementId.ValueData)
-                    FROM MovementItem 
-                    
+                    FROM MovementItem
+
                          INNER JOIN MovementItemFloat AS MIFloat_MovementId
                                                       ON MIFloat_MovementId.MovementItemId = MovementItem.Id
                                                      AND MIFloat_MovementId.DescId         = zc_MIFloat_MovementId()
-                                                     AND COALESCE (MIFloat_MovementId.ValueData, 0) <> 0
-                                     
+                                                     AND MIFloat_MovementId.ValueData      > 0
+
                     WHERE MovementItem.MovementId = vbId
-                      AND MovementItem.DescId     = zc_MI_Master()), 0)  = 1     
+                      AND MovementItem.isErased   = FALSE
+                      AND MovementItem.DescId     = zc_MI_Master()), 0)  = 1
        THEN
-       
+
          SELECT MIFloat_MovementId.ValueData::Integer
-         INTO vbId
-         FROM MovementItem 
-                        
+                INTO vbId
+         FROM MovementItem
               INNER JOIN MovementItemFloat AS MIFloat_MovementId
                                            ON MIFloat_MovementId.MovementItemId = MovementItem.Id
                                           AND MIFloat_MovementId.DescId         = zc_MIFloat_MovementId()
-                                          AND COALESCE (MIFloat_MovementId.ValueData, 0) <> 0
-                                         
+                                          AND MIFloat_MovementId.ValueData      > 0
+
          WHERE MovementItem.MovementId = vbId
+           AND MovementItem.isErased   = FALSE
            AND MovementItem.DescId     = zc_MI_Master()
          LIMIT 1;
-       
+
        ELSEIF COALESCE((SELECT COUNT(DISTINCT MIFloat_MovementId.ValueData)
-                        FROM MovementItem 
-                        
+                        FROM MovementItem
+
                              INNER JOIN MovementItemFloat AS MIFloat_MovementId
                                                           ON MIFloat_MovementId.MovementItemId = MovementItem.Id
                                                          AND MIFloat_MovementId.DescId         = zc_MIFloat_MovementId()
                                                          AND COALESCE (MIFloat_MovementId.ValueData, 0) <> 0
-                                         
+
                         WHERE MovementItem.MovementId = vbId
-                          AND MovementItem.DescId     = zc_MI_Master()), 0)  > 1     
+                          AND MovementItem.isErased   = FALSE
+                          AND MovementItem.DescId     = zc_MI_Master()), 0)  > 1
        THEN
          IF COALESCE(inBarCode, '') = ''
          THEN
            RAISE EXCEPTION 'Ошибка. В заказе на производство c номером <%> более одной связи на заказ покупателя.', inInvNumber;
          ELSE
-           RAISE EXCEPTION 'Ошибка. В заказе на производство по Ш/К <%> более одной связи на заказ покупателя.', inBarCode;       
-         END IF;              
+           RAISE EXCEPTION 'Ошибка. В заказе на производство по Ш/К <%> более одной связи на заказ покупателя.', inBarCode;
+         END IF;
        ELSE
          IF COALESCE(inBarCode, '') = ''
          THEN
            RAISE EXCEPTION 'Ошибка. В заказе на производство c номером <%> нет связей на заказ покупателя.', inInvNumber;
          ELSE
-           RAISE EXCEPTION 'Ошибка. В заказе на производство по Ш/К <%> нет связей на заказ покупателя.', inBarCode;       
-         END IF;       
-       END IF;                                                  
+           RAISE EXCEPTION 'Ошибка. В заказе на производство по Ш/К <%> нет связей на заказ покупателя.', inBarCode;
+         END IF;
+       END IF;
      END IF;
-     
+
      IF COALESCE (vbId, 0) = 0
      THEN
        IF COALESCE(inBarCode, '') = ''
        THEN
          RAISE EXCEPTION 'Ошибка. Заказ клиента c номером <%> не наден.', inInvNumber;
        ELSE
-         RAISE EXCEPTION 'Ошибка. Заказ клиента по Ш/К <%> не наден.', inBarCode;       
+         RAISE EXCEPTION 'Ошибка. Заказ клиента по Ш/К <%> не наден.', inBarCode;
        END IF;
-     ELSEIF vbStatusId = zc_Enum_Status_Erased()  
+     ELSEIF vbStatusId = zc_Enum_Status_Erased()
      THEN
        RAISE EXCEPTION 'Ошибка. Заказ клиента номер <%> удален.', vbInvNumber;
      END IF;
@@ -119,11 +154,11 @@ BEGIN
        SELECT Movement.Id            AS Id
             , Movement.InvNumber     AS InvNumber
             , Movement.InvNumber     AS InvNumberFull
-            
+
        FROM Movement
        WHERE Movement.Id = vbId
       ;
-      
+
 END;
 $BODY$
   LANGUAGE PLPGSQL VOLATILE;
@@ -135,6 +170,6 @@ $BODY$
 */
 
 -- тест
--- 
+--
 
 select * from gpGet_Movement_MobilebyOrderClient(inBarCode := '223000000813' , inInvNumber := '' ,  inSession := '5');
