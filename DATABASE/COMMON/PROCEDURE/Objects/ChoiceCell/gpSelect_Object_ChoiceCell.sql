9d90c3fbc72eae30c1db7f1877976bf05e011702
K0sta <ashtu777@gmail.com>
2024-07-30 13:34:14 +0300

    ok

--- DATABASE/COMMON/PROCEDURE/Objects/ChoiceCell/gpSelect_Object_ChoiceCell.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/ChoiceCell/gpSelect_Object_ChoiceCell.sql
@@ -12,18 +12,103 @@ RETURNS TABLE (Id Integer, Code Integer, Name TVarChar
              , GoodsGroupName TVarChar, GoodsGroupNameFull TVarChar
              , NPP TFloat, BoxCount TFloat
              , Comment TVarChar
+             , PartionGoodsDate_RK   TDateTime
+             , PartionGoodsDate_real TDateTime
              , isErased Boolean
               )
 AS
 $BODY$
    DECLARE vbUserId Integer;
+   DECLARE curPartionCell refcursor;
+   DECLARE vbPartionCellId Integer;
 BEGIN
      -- проверка прав пользователя на вызов процедуры
      -- vbUserId:= lpCheckRight(inSession, zc_Enum_Process_Select_Object_ChoiceCell());
      vbUserId:= lpGetUserBySession (inSession);
   
+
+     --
+     CREATE TEMP TABLE _tmpPartionCell (PartionCellId Integer, GoodsId Integer, GoodsKindId Integer, PartionGoodsDate TDateTime) ON COMMIT DROP;
+
+     --
+     OPEN curPartionCell FOR SELECT Object.Id FROM Object WHERE Object.DescId = zc_Object_PartionCell() /*AND Object.Id = zc_PartionCell_RK()*/ ORDER BY Object.Id;
+     -- начало цикла по курсору
+     LOOP
+          -- данные
+          FETCH curPartionCell INTO vbPartionCellId;
+          -- если данных нет, то мы выходим
+          IF NOT FOUND THEN
+             EXIT;
+          END IF;
+
+          -- Только заполненные ячейки + отбор
+          INSERT INTO _tmpPartionCell (PartionCellId, GoodsId, GoodsKindId, PartionGoodsDate)
+             WITH tmpMILO AS (SELECT * FROM MovementItemLinkObject AS MILO WHERE MILO.ObjectId = vbPartionCellId)
+                  , tmpMI AS (SELECT DISTINCT tmpMILO.ObjectId AS PartionCellId, MovementItem.ObjectId AS GoodsId, MILO_GoodsKind.ObjectId AS GoodsKindId, COALESCE (MIDate_PartionGoods.ValueData, Movement.OperDate) AS PartionGoodsDate
+                              FROM tmpMILO
+                                   LEFT JOIN MovementItem ON MovementItem.Id       = tmpMILO.MovementItemId
+                                                         AND MovementItem.isErased = FALSE
+                                   LEFT JOIN Movement ON Movement.Id = MovementItem.MovementId
+                                   LEFT JOIN MovementItemLinkObject AS MILO_GoodsKind
+                                                                    ON MILO_GoodsKind.MovementItemId = MovementItem.Id
+                                                                   AND MILO_GoodsKind.DescId         = zc_MILinkObject_GoodsKind()
+                                   LEFT JOIN MovementItemDate AS MIDate_PartionGoods
+                                                              ON MIDate_PartionGoods.MovementItemId = MovementItem.Id
+                                                             AND MIDate_PartionGoods.DescId         = zc_MIDate_PartionGoods()
+                              -- Только заполненные
+                              WHERE tmpMILO.ObjectId > 0
+                                AND tmpMILO.DescId IN (zc_MILinkObject_PartionCell_1()
+                                                     , zc_MILinkObject_PartionCell_2()
+                                                     , zc_MILinkObject_PartionCell_3()
+                                                     , zc_MILinkObject_PartionCell_4()
+                                                     , zc_MILinkObject_PartionCell_5()
+                                                     , zc_MILinkObject_PartionCell_6()
+                                                     , zc_MILinkObject_PartionCell_7()
+                                                     , zc_MILinkObject_PartionCell_8()
+                                                     , zc_MILinkObject_PartionCell_9()
+                                                     , zc_MILinkObject_PartionCell_10()
+                                                     , zc_MILinkObject_PartionCell_11()
+                                                     , zc_MILinkObject_PartionCell_12()
+                                                     , zc_MILinkObject_PartionCell_13()
+                                                     , zc_MILinkObject_PartionCell_14()
+                                                     , zc_MILinkObject_PartionCell_15()
+                                                     , zc_MILinkObject_PartionCell_16()
+                                                     , zc_MILinkObject_PartionCell_17()
+                                                     , zc_MILinkObject_PartionCell_18()
+                                                     , zc_MILinkObject_PartionCell_19()
+                                                     , zc_MILinkObject_PartionCell_20()
+                                                     , zc_MILinkObject_PartionCell_21()
+                                                     , zc_MILinkObject_PartionCell_22()
+                                                      )
+                             )
+             -- Результат
+             SELECT tmpMI.PartionCellId, tmpMI.GoodsId, tmpMI.GoodsKindId, tmpMI.PartionGoodsDate
+             FROM tmpMI
+            ;
+          --
+     END LOOP; -- финиш цикла по курсору
+     CLOSE curPartionCell; -- закрыли курсор
+
+
      -- Результат
      RETURN QUERY 
+       WITH tmpPartionCell_RK AS (SELECT tmpMI.GoodsId, tmpMI.GoodsKindId, tmpMI.PartionGoodsDate
+                                         -- № п/п
+                                       , ROW_NUMBER() OVER (PARTITION BY tmpMI.GoodsId, tmpMI.GoodsKindId ORDER BY tmpMI.PartionGoodsDate DESC) AS Ord
+                                  FROM (SELECT DISTINCT _tmpPartionCell.GoodsId, _tmpPartionCell.GoodsKindId, _tmpPartionCell.PartionGoodsDate
+                                        FROM _tmpPartionCell
+                                        WHERE _tmpPartionCell.PartionCellId = zc_PartionCell_RK()
+                                       ) AS tmpMI
+                                 )
+        , tmpPartionCell_real AS (SELECT tmpMI.GoodsId, tmpMI.GoodsKindId, tmpMI.PartionGoodsDate
+                                         -- № п/п
+                                       , ROW_NUMBER() OVER (PARTITION BY tmpMI.GoodsId, tmpMI.GoodsKindId ORDER BY tmpMI.PartionGoodsDate ASC) AS Ord
+                                  FROM (SELECT DISTINCT _tmpPartionCell.GoodsId, _tmpPartionCell.GoodsKindId, _tmpPartionCell.PartionGoodsDate
+                                        FROM _tmpPartionCell
+                                        WHERE _tmpPartionCell.PartionCellId <> zc_PartionCell_RK()
+                                       ) AS tmpMI
+                                 )
+       
 
     -- Результат
     SELECT 
@@ -46,6 +131,9 @@ BEGIN
          , ObjectFloat_BoxCount.ValueData AS BoxCount
 
          , ObjectString_Comment.ValueData  AS Comment
+
+         , tmpPartionCell_RK.PartionGoodsDate   :: TDateTime AS PartionGoodsDate_RK
+         , tmpPartionCell_real.PartionGoodsDate :: TDateTime AS PartionGoodsDate_real
 
          , Object_ChoiceCell.isErased      AS isErased
        
@@ -80,6 +168,14 @@ BEGIN
         LEFT JOIN ObjectString AS ObjectString_Goods_GoodsGroupFull
                                ON ObjectString_Goods_GoodsGroupFull.ObjectId = Object_Goods.Id
                               AND ObjectString_Goods_GoodsGroupFull.DescId = zc_ObjectString_Goods_GroupNameFull()
+
+        LEFT JOIN tmpPartionCell_RK ON tmpPartionCell_RK.GoodsId     = Object_Goods.Id
+                                   AND tmpPartionCell_RK.GoodsKindId = Object_GoodsKind.Id
+                                   AND tmpPartionCell_RK.ord         = 1
+
+        LEFT JOIN tmpPartionCell_real ON tmpPartionCell_real.GoodsId     = Object_Goods.Id
+                                     AND tmpPartionCell_real.GoodsKindId = Object_GoodsKind.Id
+                                     AND tmpPartionCell_real.ord         = 1
 
     WHERE Object_ChoiceCell.DescId = zc_Object_ChoiceCell()  
       AND (Object_ChoiceCell.isErased = FALSE OR inShowAll = TRUE)
