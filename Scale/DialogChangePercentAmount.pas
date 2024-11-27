2afcd156a8a792ba04b4b48c2af9ad7a27228fdd
K0sta <ashtu777@gmail.com>
2024-11-27 09:33:49 +0200

    ok

---
+++ Scale/DialogChangePercentAmount.pas
@@ -0,0 +1,125 @@
+unit DialogChangePercentAmount;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, AncestorDialogScale, StdCtrls, Mask, Buttons,
+  ExtCtrls, cxGraphics, cxLookAndFeels, cxLookAndFeelPainters, Vcl.Menus,
+  dxSkinsCore, dxSkinsDefaultPainters, cxControls, cxContainer, cxEdit,
+  cxTextEdit, cxCurrencyEdit, dsdDB, Vcl.ActnList, dsdAction, cxPropertiesStore,
+  dsdAddOn, cxButtons, dxSkinBlack, dxSkinBlue, dxSkinBlueprint, dxSkinCaramel,
+  dxSkinCoffee, dxSkinDarkRoom, dxSkinDarkSide, dxSkinDevExpressDarkStyle,
+  dxSkinDevExpressStyle, dxSkinFoggy, dxSkinGlassOceans, dxSkinHighContrast,
+  dxSkiniMaginary, dxSkinLilian, dxSkinLiquidSky, dxSkinLondonLiquidSky,
+  dxSkinMcSkin, dxSkinMoneyTwins, dxSkinOffice2007Black, dxSkinOffice2007Blue,
+  dxSkinOffice2007Green, dxSkinOffice2007Pink, dxSkinOffice2007Silver,
+  dxSkinOffice2010Black, dxSkinOffice2010Blue, dxSkinOffice2010Silver,
+  dxSkinPumpkin, dxSkinSeven, dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus,
+  dxSkinSilver, dxSkinSpringTime, dxSkinStardust, dxSkinSummer2008,
+  dxSkinTheAsphaltWorld, dxSkinValentine, dxSkinVS2010, dxSkinWhiteprint,
+  dxSkinXmas2008Blue, Vcl.ComCtrls, dxCore, cxDateUtils, cxMaskEdit,
+  cxDropDownEdit, cxCalendar, dsdCommon;
+
+type
+  TDialogChangePercentAmountForm = class(TAncestorDialogScaleForm)
+    PanelNumberValue: TPanel;
+    rgDiscountAmountPartner: TRadioGroup;
+    Label2: TLabel;
+    DiscountAmountPartnerEdit: TcxCurrencyEdit;
+    btnSaveAll: TBitBtn;
+    Panel1: TPanel;
+    Label1: TLabel;
+    PartnertEdit: TEdit;
+    Panel2: TPanel;
+    Label3: TLabel;
+    InvNumberEdit: TEdit;
+    InvNumberPartnerEdit: TEdit;
+    Label4: TLabel;
+    DateValueEdit: TcxDateEdit;
+    LabelDateValue: TLabel;
+    ActionList: TActionList;
+    actWeighingPartner_ActDiffF: TdsdInsertUpdateAction;
+    FormParams: TdsdFormParams;
+    procedure btnSaveAllClick(Sender: TObject);
+    procedure DiscountAmountPartnerEditExit(Sender: TObject);
+  private
+    function Checked: boolean; override;//Проверка корректного ввода в Edit
+  public
+   MovementId_wp, MovementId_DocPartner : Integer;
+  end;
+
+var
+   DialogChangePercentAmountForm: TDialogChangePercentAmountForm;
+
+implementation
+uses UtilScale, DMMainScale
+//  System.SysUtils, System.Classes
+   , Data.DB // , Datasnap.DBClient, Vcl.Dialogs,Forms,Vcl.StdCtrls
+    ;
+{$R *.dfm}
+{------------------------------------------------------------------------------}
+procedure TDialogChangePercentAmountForm.btnSaveAllClick(Sender: TObject);
+begin
+     if MovementId_DocPartner = 0
+     then begin
+              ShowMessage('Ошибка.Документ Поставщика не установлен.');
+              exit;
+     end;
+     //
+     if not Checked then exit;
+     //
+     FormParams.ParamByName('MovementId_begin').Value:=MovementId_DocPartner;
+     actWeighingPartner_ActDiffF.Execute;
+     //
+     ModalResult:= mrOK;
+end;
+{------------------------------------------------------------------------------}
+function TDialogChangePercentAmountForm.Checked: boolean; //Проверка корректного ввода в Edit
+var DiscountAmountPartner : Double;
+     isDiscount_q:Boolean;
+     isDiscount_t:Boolean;
+     execParams : TParams;
+begin
+     //
+     try DiscountAmountPartner:= StrToFloat(DiscountAmountPartnerEdit.Text)
+     except DiscountAmountPartner:= 0;
+     end;
+     if (rgDiscountAmountPartner.ItemIndex = -1) and (DiscountAmountPartner > 0)
+     then begin
+               ShowMessage('Ошибка.Для скидки по весу '+DiscountAmountPartnerEdit.Text+'% необходимо выбрать вид скидки.');
+               ActiveControl:= rgDiscountAmountPartner;
+               exit;
+     end;
+     if (rgDiscountAmountPartner.ItemIndex >= 0) and (DiscountAmountPartner = 0)
+     then begin
+               ShowMessage('Ошибка.Для скидки '+rgDiscountAmountPartner.Items[rgDiscountAmountPartner.ItemIndex]+' необходимо заполнить % скидки по весу.');
+               ActiveControl:= DiscountAmountPartnerEdit;
+               exit;
+     end;
+     //
+     isDiscount_q:= rgDiscountAmountPartner.ItemIndex = 0;
+     isDiscount_t:= rgDiscountAmountPartner.ItemIndex = 1;
+     //
+     execParams:=nil;
+     ParamAddValue(execParams,'MovementId', ftInteger, MovementId_wp);   //
+     ParamAddValue(execParams,'DiscountAmountPartner', ftFloat, DiscountAmountPartner); //
+     ParamAddValue(execParams,'isDiscount_q', ftBoolean, isDiscount_q);  //
+     ParamAddValue(execParams,'isDiscount_t', ftBoolean, isDiscount_t);  //
+     //
+     if (DiscountAmountPartner > 0) or(rgDiscountAmountPartner.ItemIndex >=0) then
+     begin
+          Result:= DMMainScaleForm.gpUpdate_Scale_Movement_ChangePercentAmount(execParams);
+          execParams.Free;
+          if not Result then exit;
+     end;
+end;
+{------------------------------------------------------------------------------}
+procedure TDialogChangePercentAmountForm.DiscountAmountPartnerEditExit(
+  Sender: TObject);
+begin
+  if (trim(DiscountAmountPartnerEdit.Text) = '') OR (trim(DiscountAmountPartnerEdit.Text) = '0')
+  then rgDiscountAmountPartner.ItemIndex:= -1;
+end;
+{------------------------------------------------------------------------------}
+end.
