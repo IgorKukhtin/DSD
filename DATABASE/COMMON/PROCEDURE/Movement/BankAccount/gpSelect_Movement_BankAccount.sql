de636312a186829a25f902f053df75d267e1daeb
K0sta <ashtu777@gmail.com>
2025-01-21 11:01:12 +0200

    ok

--- DATABASE/COMMON/PROCEDURE/Movement/BankAccount/gpSelect_Movement_BankAccount.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/BankAccount/gpSelect_Movement_BankAccount.sql
@@ -49,7 +49,11 @@ $BODY$
    DECLARE vbMemberId Integer;
    DECLARE vbIsExists Boolean;
    DECLARE vbCount    Integer;
+   DECLARE vbOperDate_Begin1 TDateTime;
 BEGIN
+     -- сразу запомнили время начала выполнения Проц.
+     vbOperDate_Begin1:= CLOCK_TIMESTAMP();
+
      -- проверка прав пользователя на вызов процедуры
      -- vbUserId:= lpCheckRight (inSession, zc_Enum_Process_Select_Movement_BankAccount());
      vbUserId:= lpGetUserBySession (inSession);
@@ -363,6 +367,55 @@ BEGIN
        ;
 
        --zc_Juridical_Irna() = 15512, тогда в селекте COALESCE (zc_Object_BankAccount.zc_ObjectBoolean_Guide_Irna, FALSE) = TRUE
+
+
+
+     -- !!!временно - ПРОТОКОЛ - ЗАХАРДКОДИЛ!!!
+     INSERT INTO ResourseProtocol (UserId
+                                 , OperDate
+                                 , Value1
+                                 , Value2
+                                 , Value3
+                                 , Value4
+                                 , Value5
+                                 , Time1
+                                 , Time2
+                                 , Time3
+                                 , Time4
+                                 , Time5
+                                 , ProcName
+                                 , ProtocolData
+                                  )
+        WITH tmp_pg AS (SELECT * FROM pg_stat_activity WHERE state = 'active')
+        SELECT vbUserId
+               -- во сколько началась
+             , CURRENT_TIMESTAMP
+             , (SELECT COUNT (*) FROM tmp_pg)                                                    AS Value1
+             , (SELECT COUNT (*) FROM tmp_pg WHERE position( 'autovacuum: VACUUM' in query) = 1) AS Value2
+             , NULL AS Value3
+             , NULL AS Value4
+             , NULL AS Value5
+               -- сколько всего выполнялась проц
+             , (CLOCK_TIMESTAMP() - vbOperDate_Begin1) :: INTERVAL AS Time1
+               -- сколько всего выполнялась проц ДО lpSelectMinPrice_List
+             , NULL AS Time2
+               -- сколько всего выполнялась проц lpSelectMinPrice_List
+             , NULL AS Time3
+               -- сколько всего выполнялась проц ПОСЛЕ lpSelectMinPrice_List
+             , NULL AS Time4
+               -- во сколько закончилась
+             , CLOCK_TIMESTAMP() AS Time5
+               -- ProcName
+             , 'gpSelect_Movement_BankAccount'
+               -- ProtocolData
+             , CHR (39) || zfConvert_DateToString (inStartDate) || CHR (39)
+    || ', ' || CHR (39) || zfConvert_DateToString (inEndDate) || CHR (39)
+    || ', ' || inJuridicalBasisId :: TVarChar
+    || ', ' || inAccountId :: TVarChar
+    || ', ' || CASE WHEN inIsErased = TRUE THEN 'TRUE' ELSE 'FALSE' END
+    || ', ' || inSession
+              ;
+
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE;
