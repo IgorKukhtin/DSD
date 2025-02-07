78f8053306d4b7e74e8b599ed01d693a62f4b5d6
K0sta <ashtu777@gmail.com>
2025-02-07 18:02:12 +0200

    ok

--- DATABASE/COMMON/REPORTS/Tara/gpReport_SaleTare_Gofro.sql
+++ DATABASE/COMMON/REPORTS/Tara/gpReport_SaleTare_Gofro.sql
@@ -17,7 +17,7 @@ RETURNS TABLE(OperDate TDateTime, OperDatePartner TDateTime
             , isGoodsBox Boolean
             , GoodsId Integer, GoodsCode Integer, GoodsName TVarChar
             , GoodsGroupNameFull TVarChar
-			, Amount TFloat, BoxCount TFloat, BoxCount_calc TFloat 
+			, Amount TFloat, BoxCount TFloat, BoxCount_calc TFloat
 			, MovementId Integer , InvNumber TVarChar
     )
 AS
@@ -26,38 +26,42 @@ $BODY$
 BEGIN
     -- проверка прав пользователя на вызов процедуры
     vbUserId:= lpGetUserBySession (inSession);
-    
+
      -- !!!Только просмотр Аудитор!!!
      PERFORM lpCheckPeriodClose_auditor (inStartDate, inEndDate, NULL, NULL, NULL, vbUserId);
 
     -- Результат
     RETURN QUERY
-    --данные продаж по товарам       
-    WITH 
+    --данные продаж по товарам
+    WITH
       -- товары из группы Гофротара
       tmpGoods AS (SELECT lfSelect.GoodsId AS GoodsId
                    FROM lfSelect_Object_Goods_byGoodsGroup (inGoodsGroupId) AS lfSelect
                    )
     , tmpMovement AS (SELECT Movement.*
-                      FROM Movement
+                      FROM MovementDate AS MovementDate_OperDatePartner
+                           INNER JOIN Movement
+                                   ON Movement.Id = MovementDate_OperDatePartner.MovementId
+                                  AND Movement.DescId = zc_Movement_Sale()
+                                  AND Movement.StatusId = zc_Enum_Status_Complete()
+
                            INNER JOIN MovementLinkObject AS MovementLinkObject_From
                                                          ON MovementLinkObject_From.MovementId = Movement.Id
                                                         AND MovementLinkObject_From.DescId     = zc_MovementLinkObject_From()
-                                                        AND (MovementLinkObject_From.ObjectId  = inUnitId OR inUnitId = 0) 
-                      WHERE Movement.OperDate BETWEEN inStartDate AND inEndDate
-                        AND Movement.DescId = zc_Movement_Sale()
-                        AND Movement.StatusId = zc_Enum_Status_Complete()
+                                                        AND (MovementLinkObject_From.ObjectId  = inUnitId OR inUnitId = 0)
+                      WHERE MovementDate_OperDatePartner.ValueData BETWEEN inStartDate AND inEndDate
+                        AND MovementDate_OperDatePartner.DescId = zc_MovementDate_OperDatePartner()
                      )
-    
+
    --все строки док. продаж
    , tmpMI_All AS (SELECT MovementItem.*
                    FROM MovementItem
                    WHERE MovementItem.MovementId IN (SELECT DISTINCT tmpMovement.Id FROM tmpMovement)
                      AND MovementItem.DescId = zc_MI_Master()
-                     AND MovementItem.isErased = FALSE 
+                     AND MovementItem.isErased = FALSE
                   )
 
-    -- zc_MILinkObject_Box.ObjectId     
+    -- zc_MILinkObject_Box.ObjectId
    , tmpMILO_Box AS (SELECT MovementItemLinkObject.*
                      FROM MovementItemLinkObject
                      WHERE MovementItemLinkObject.MovementItemId IN (SELECT DISTINCT tmpMI_All.Id FROM tmpMI_All)
@@ -68,13 +72,13 @@ BEGIN
                     WHERE MovementItemFloat.MovementItemId IN (SELECT DISTINCT tmpMI_All.Id FROM tmpMI_All)
                       AND MovementItemFloat.DescId = zc_MIFloat_BoxCount()
                     )
-                     
+
    , tmpMI AS (SELECT MovementItem.MovementId
                     , MovementItem.Id
                     , MovementItem.ObjectId AS GoodsId
                     , COALESCE (MovementItem.Amount,0) AS Amount
                     , 0                     AS BoxCount
-               FROM tmpMI_All AS MovementItem  
+               FROM tmpMI_All AS MovementItem
                     INNER JOIN tmpGoods ON tmpGoods.GoodsId = MovementItem.ObjectId
               UNION
                SELECT MovementItem.MovementId
@@ -111,7 +115,7 @@ BEGIN
                            AND MovementItemLinkObject.DescId = zc_MILinkObject_GoodsKind()
                          )
 
-  
+
    , tmpGoodsPropertyValue AS (SELECT tmpMI.MovementId
                                     , tmpMI.Id
                                     , ObjectLink_GoodsPropertyValue_GoodsBox.ChildObjectId AS GoodsBoxId
@@ -119,19 +123,19 @@ BEGIN
                                      LEFT JOIN tmpMovementLinkObject AS MovementLinkObject_GoodsProperty
                                                                      ON MovementLinkObject_GoodsProperty.MovementId = tmpMI.MovementId
                                                                     AND MovementLinkObject_GoodsProperty.DescId = zc_MovementLinkObject_GoodsProperty()
-                                      
+
                                      LEFT JOIN tmpMLO_GoodsKind AS MILinkObject_GoodsKind
                                                                 ON MILinkObject_GoodsKind.MovementItemId = tmpMI.Id
-     
+
                                      INNER JOIN ObjectLink AS ObjectLink_GoodsPropertyValue_GoodsProperty
                                                            ON ObjectLink_GoodsPropertyValue_GoodsProperty.ChildObjectId =  MovementLinkObject_GoodsProperty.ObjectId
                                                           AND ObjectLink_GoodsPropertyValue_GoodsProperty.DescId = zc_ObjectLink_GoodsPropertyValue_GoodsProperty()
-     
+
                                      INNER JOIN ObjectLink AS ObjectLink_GoodsPropertyValue_GoodsBox
                                                            ON ObjectLink_GoodsPropertyValue_GoodsBox.ObjectId = ObjectLink_GoodsPropertyValue_GoodsProperty.ObjectId
                                                           AND ObjectLink_GoodsPropertyValue_GoodsBox.DescId = zc_ObjectLink_GoodsPropertyValue_GoodsBox()
                                                           and ObjectLink_GoodsPropertyValue_GoodsBox.ChildObjectId > 0
-     
+
                                      INNER JOIN ObjectLink AS ObjectLink_GoodsPropertyValue_Goods
                                                            ON ObjectLink_GoodsPropertyValue_Goods.ObjectId = ObjectLink_GoodsPropertyValue_GoodsProperty.ObjectId
                                                           AND ObjectLink_GoodsPropertyValue_Goods.DescId = zc_ObjectLink_GoodsPropertyValue_Goods()
@@ -142,7 +146,7 @@ BEGIN
                                                           AND ObjectLink_GoodsPropertyValue_GoodsKind.DescId = zc_ObjectLink_GoodsPropertyValue_GoodsKind()
                                                           AND COALESCE (MILinkObject_GoodsKind.ObjectId,0) = COALESCE (ObjectLink_GoodsPropertyValue_GoodsKind.ChildObjectId,0)
                                )
-       
+
            --
            SELECT Movement.OperDate   ::TDateTime        AS OperDate
                 , MovementDate_OperDatePartner.ValueData AS OperDatePartner
@@ -151,7 +155,7 @@ BEGIN
                 , Object_From.ValueData               AS FromName
                 , Object_To.Id                        AS ToId
                 , Object_To.ObjectCode                AS ToCode
-                , Object_To.ValueData                 AS ToName  
+                , Object_To.ValueData                 AS ToName
                 , COALESCE (ObjectBoolean_Partner_GoodsBox.ValueData, FALSE) :: Boolean AS isGoodsBox
 
                 , Object_Goods.Id                     AS GoodsId
@@ -160,12 +164,12 @@ BEGIN
                 , ObjectString_Goods_GoodsGroupFull.ValueData AS GoodsGroupNameFull
 
                 , SUM (COALESCE (tmpMI.Amount,0))     :: TFloat    AS Amount
-                , SUM (COALESCE (tmpMI.BoxCount,0))   :: TFloat    AS BoxCount 
+                , SUM (COALESCE (tmpMI.BoxCount,0))   :: TFloat    AS BoxCount
                 , SUM (CASE WHEN COALESCE (ObjectBoolean_Partner_GoodsBox.ValueData, FALSE) = TRUE
                             THEN COALESCE (tmpMI.BoxCount,0)
-                            ELSE 0 
-                       END)   :: TFloat  AS BoxCount_calc 
-                 
+                            ELSE 0
+                       END)   :: TFloat  AS BoxCount_calc
+
                 , CASE WHEN inisDetail = TRUE THEN Movement.Id ELSE 0 END         ::Integer AS MovementId
                 , CASE WHEN inisDetail = TRUE THEN Movement.InvNumber ELSE '' END ::TVarChar AS InvNumber
            FROM tmpMI
@@ -177,29 +181,29 @@ BEGIN
                 LEFT JOIN tmpMovementLinkObject AS MovementLinkObject_From
                                                 ON MovementLinkObject_From.MovementId = Movement.Id
                                                AND MovementLinkObject_From.DescId = zc_MovementLinkObject_From()
-                LEFT JOIN Object AS Object_From ON Object_From.Id = MovementLinkObject_From.ObjectId  
+                LEFT JOIN Object AS Object_From ON Object_From.Id = MovementLinkObject_From.ObjectId
 
                 LEFT JOIN tmpMovementLinkObject AS MovementLinkObject_To
                                                 ON MovementLinkObject_To.MovementId = Movement.Id
                                                AND MovementLinkObject_To.DescId = zc_MovementLinkObject_To()
-                LEFT JOIN Object AS Object_To ON Object_To.Id = MovementLinkObject_To.ObjectId 
+                LEFT JOIN Object AS Object_To ON Object_To.Id = MovementLinkObject_To.ObjectId
 
                 LEFT JOIN ObjectString AS ObjectString_Goods_GoodsGroupFull
                                        ON ObjectString_Goods_GoodsGroupFull.ObjectId = tmpMI.GoodsId
-                                      AND ObjectString_Goods_GoodsGroupFull.DescId = zc_ObjectString_Goods_GroupNameFull() 
+                                      AND ObjectString_Goods_GoodsGroupFull.DescId = zc_ObjectString_Goods_GroupNameFull()
 
                 LEFT JOIN ObjectBoolean AS ObjectBoolean_Partner_GoodsBox
                                         ON ObjectBoolean_Partner_GoodsBox.ObjectId = MovementLinkObject_To.ObjectId
-                                       AND ObjectBoolean_Partner_GoodsBox.DescId = zc_ObjectBoolean_Partner_GoodsBox()   
+                                       AND ObjectBoolean_Partner_GoodsBox.DescId = zc_ObjectBoolean_Partner_GoodsBox()
 
                 LEFT JOIN tmpGoodsPropertyValue ON tmpGoodsPropertyValue.MovementId = tmpMI.MovementId
                                                AND tmpGoodsPropertyValue.Id = tmpMI.Id
                 --LEFT JOIN Object AS Object_GoodsBox ON Object_GoodsBox.Id = tmpGoodsPropertyValue.GoodsBoxId
-                
+
                 LEFT JOIN Object AS Object_Goods ON Object_Goods.Id = COALESCE (tmpGoodsPropertyValue.GoodsBoxId, tmpMI.GoodsId)
-                               
+
            GROUP BY  Movement.OperDate
-                , MovementDate_OperDatePartner.ValueData                
+                , MovementDate_OperDatePartner.ValueData
                 , Object_From.Id
                 , Object_From.ObjectCode
                 , Object_From.ValueData
@@ -225,4 +229,4 @@ $BODY$
 */
 
 -- тест
--- SELECT * FROM gpReport_SaleTare_Gofro (inStartDate := '08.04.2022'::TDatetime, inEndDate := '08.04.2022'::TDatetime, inUnitId:=0, inGoodsGroupId:= 1960, inSession:='5'::TVarChar);
+-- SELECT * FROM gpReport_SaleTare_Gofro (inStartDate := '08.04.2022'::TDatetime, inEndDate := '08.04.2022'::TDatetime, inUnitId:=0, inGoodsGroupId:= 1960, inisDetail:= FALSE, inSession:='5'::TVarChar);
