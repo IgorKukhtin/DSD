db83e518837f0c51a7fb2314cf9227c680f8279c
K0sta <ashtu777@gmail.com>
2025-02-19 15:08:17 +0200

    okok

--- Forms/Guides/PriceList.pas
+++ Forms/Guides/PriceList.pas
@@ -20,7 +20,8 @@ uses
   dxSkinPumpkin, dxSkinSeven, dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus,
   dxSkinSilver, dxSkinSpringTime, dxSkinStardust, dxSkinSummer2008,
   dxSkinTheAsphaltWorld, dxSkinsDefaultPainters, dxSkinValentine, dxSkinVS2010,
-  dxSkinWhiteprint, dxSkinXmas2008Blue, dxSkinscxPCPainter, dxSkinsdxBarPainter;
+  dxSkinWhiteprint, dxSkinXmas2008Blue, dxSkinscxPCPainter, dxSkinsdxBarPainter,
+  dsdCommon;
 
 type
   TPriceListForm = class(TParentForm)
