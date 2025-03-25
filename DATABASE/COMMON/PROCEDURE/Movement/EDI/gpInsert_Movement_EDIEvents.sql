e0ce02fa6006e918ab197dcdf89057a03aad2fc2
K0sta <ashtu777@gmail.com>
2025-03-25 16:21:34 +0200

    ok

--- DATABASE/COMMON/PROCEDURE/Movement/EDI/gpInsert_Movement_EDIEvents.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/EDI/gpInsert_Movement_EDIEvents.sql
@@ -1,12 +1,15 @@
 -- Function: gpInsertUpdate_MI_EDI()
 
-DROP FUNCTION IF EXISTS gpInsert_Movement_EDIEvents(Integer, TVarChar, TVarChar);
+-- DROP FUNCTION IF EXISTS gpInsert_Movement_EDIEvents(Integer, TVarChar, TVarChar);
+DROP FUNCTION IF EXISTS gpInsert_Movement_EDIEvents (Integer, TVarChar, TVarChar, TVarChar, TVarChar);
 
 CREATE OR REPLACE FUNCTION gpInsert_Movement_EDIEvents(
     IN inMovementId          Integer   , -- Ключ объекта <Документ>
+    IN inDocumentId          TVarChar  , --
+    IN inVchasnoId           TVarChar  , --
     IN inEDIEvent            TVarChar  , -- Описание события
-    IN inSession             TVarChar     -- Пользователь
-)                              
+    IN inSession             TVarChar    -- Пользователь
+)
 RETURNS VOID AS
 $BODY$
    DECLARE vbUserId Integer;
@@ -16,6 +19,25 @@ BEGIN
      -- проверка прав пользователя на вызов процедуры
      -- PERFORM lpCheckRight (inSession, zc_Enum_Process_InsertUpdate_Movement_EDI());
    vbUserId := lpGetUserBySession(inSession);
+
+
+    IF COALESCE (inMovementId, 0) = 0
+    THEN
+        RAISE EXCEPTION 'Ошибка.inMovementId = 0.';
+    END IF;
+
+
+   IF TRIM (COALESCE (inDocumentId, '')) <> ''
+   THEN
+       PERFORM lpInsertUpdate_MovementString (zc_MovementString_DocumentId_vch(), inMovementId, inDocumentId);
+   END IF;
+
+
+   IF TRIM (COALESCE (inVchasnoId, '')) <> ''
+   THEN
+       PERFORM lpInsertUpdate_MovementString (zc_MovementString_VchasnoId(), inMovementId, inVchasnoId);
+   END IF;
+
 
    IF SUBSTRING (inEDIEvent FROM 1 FOR 1) = '{'
    THEN
@@ -28,7 +50,7 @@ BEGIN
         --
         vbCount_in:= zfConvert_StringToFloat (SUBSTRING (inEDIEvent FROM 2 FOR POSITION ('}' IN inEDIEvent) - 2)) :: Integer;
         --
-        PERFORM lpInsert_Movement_EDIEvents (inMovementId 
+        PERFORM lpInsert_Movement_EDIEvents (inMovementId
                                            , CASE WHEN vbCount <> vbCount_in
                                                        THEN 'Ошибка.Загружено {' || (vbCount :: TVarChar) || '} строк из {' || (vbCount_in :: TVarChar) || '}.'
                                                   ELSE ''
@@ -38,7 +60,7 @@ BEGIN
                                                         FOR  LENGTH (inEDIEvent) - POSITION ('}' IN inEDIEvent)
                                                        )
                                            , vbUserId);
-   ELSE 
+   ELSE
         PERFORM lpInsert_Movement_EDIEvents (inMovementId, inEDIEvent, vbUserId);
    END IF;
 
@@ -50,7 +72,7 @@ LANGUAGE PLPGSQL VOLATILE;
 /*
  ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
                Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
- 02.06.14                         * 
+ 02.06.14                         *
 
 */
 
