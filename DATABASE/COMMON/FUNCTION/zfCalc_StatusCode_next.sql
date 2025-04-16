005d9d73569a86c607f91cc510c8e39b741ee7cc
K0sta <ashtu777@gmail.com>
2025-04-16 19:18:57 +0300

    ok

--- DATABASE/COMMON/FUNCTION/zfCalc_StatusCode_next.sql
+++ DATABASE/COMMON/FUNCTION/zfCalc_StatusCode_next.sql
@@ -8,13 +8,18 @@ AS
 $BODY$
 BEGIN
 
-   RETURN CASE inStatusId
-               WHEN zc_Enum_Status_Complete()   THEN 2
-               WHEN zc_Enum_Status_UnComplete() THEN 4 -- 4 or 1 
-               WHEN zc_Enum_Status_Erased()     THEN 3
-               ELSE 1
-          END
-         ;
+   IF inStatusId = zc_Enum_Status_Complete() AND inStatusId_next = zc_Enum_Status_UnComplete()
+   THEN
+       RETURN 1;
+   ELSE
+       RETURN CASE inStatusId
+                   WHEN zc_Enum_Status_Complete()   THEN 2
+                   WHEN zc_Enum_Status_UnComplete() THEN 4 -- 4 or 1 
+                   WHEN zc_Enum_Status_Erased()     THEN 3
+                   ELSE 1
+              END
+             ;
+   END IF;
 
 END;
 $BODY$
@@ -30,3 +35,4 @@ $BODY$
 -- тест
 -- SELECT * FROM gpSelect_Object_Status('2')
 -- SELECT zfCalc_StatusCode_next (zc_Enum_Status_UnComplete(), zc_Enum_Status_UnComplete())
+-- select zfCalc_StatusCode_next (StatusId, StatusId_next), zfCalc_StatusName_next (Object_Status.ValueData, Movement.StatusId, Movement.StatusId_next), * from Movement LEFT JOIN Object AS Object_Status ON Object_Status.Id = Movement.StatusId where Movement .Id = 30990114 
--- DATABASE/COMMON/FUNCTION/zfCalc_StatusName_next.sql
+++ DATABASE/COMMON/FUNCTION/zfCalc_StatusName_next.sql
@@ -8,14 +8,19 @@ AS
 $BODY$
 BEGIN
 
-   RETURN CASE inStatusId
-               WHEN zc_Enum_Status_Complete()   THEN inStatusName
-               WHEN zc_Enum_Status_UnComplete() THEN inStatusName
-             --WHEN zc_Enum_Status_UnComplete() THEN '***' || inStatusName
-               WHEN zc_Enum_Status_Erased()     THEN inStatusName
-               ELSE inStatusName
-          END
-         ;
+   IF inStatusId = zc_Enum_Status_Complete() AND inStatusId_next = zc_Enum_Status_UnComplete()
+   THEN
+       RETURN '*** Не проведен';
+   ELSE
+       RETURN CASE inStatusId
+                   WHEN zc_Enum_Status_Complete()   THEN inStatusName
+                   WHEN zc_Enum_Status_UnComplete() THEN inStatusName
+                 --WHEN zc_Enum_Status_UnComplete() THEN '***' || inStatusName
+                   WHEN zc_Enum_Status_Erased()     THEN inStatusName
+                   ELSE inStatusName
+              END
+             ;
+   END IF;
 
 END;
 $BODY$
@@ -31,3 +36,4 @@ $BODY$
 -- тест
 -- SELECT * FROM gpSelect_Object_Status('2')
 -- SELECT zfCalc_StatusName_next ('Не проведен', zc_Enum_Status_UnComplete(), zc_Enum_Status_UnComplete())
+-- select zfCalc_StatusCode_next (StatusId, StatusId_next), zfCalc_StatusName_next (Object_Status.ValueData, Movement.StatusId, Movement.StatusId_next), * from Movement LEFT JOIN Object AS Object_Status ON Object_Status.Id = Movement.StatusId where Movement .Id = 30990114 
