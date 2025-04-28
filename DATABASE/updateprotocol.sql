88e33f1a5843d0eb76519579ee6b00f12b6f4398
K0sta <ashtu777@gmail.com>
2025-04-28 09:38:10 +0300

    ok

--- DATABASE/updateprotocol.sql
+++
@@ -1,2 +0,0 @@
-UPDATE  objectprotocol set protocoldata = REPLACE(protocoldata, '<Пользователь (корректировка)>', 'Пользователь (корректировка)') 
-WHERE protocoldata LIKE '%<Пользователь (корректировка)>%'
