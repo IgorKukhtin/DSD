83eea0730dafc0dfd333dcacd441c4405a296999
K0sta <ashtu777@gmail.com>
2025-06-29 11:15:34 +0300

    ok

--- DATABASE/COMMON/PROCEDURE/Objects/Sticker/gpInsert_Object_Sticker_ReportName.sql
+++ DATABASE/COMMON/PROCEDURE/Objects/Sticker/gpInsert_Object_Sticker_ReportName.sql
@@ -20,20 +20,42 @@ BEGIN
    
    -- если такого Шаблона еще нет
    IF NOT EXISTS (SELECT 1 FROM Object WHERE Object.DescId = zc_Object_Form() AND TRIM (Object.ValueData) = inReportName)
+      OR (inReportName ILIKE '%ТМ Favore%' AND 1=0)
    THEN
-       -- сохранили <Объект>
-       vbId:= lpInsertUpdate_Object (0, zc_Object_Form(), 0, inReportName);
+
+       IF inReportName ILIKE '%ТМ Favore%'
+       THEN vbId:= (SELECT Object.Id FROM Object WHERE Object.DescId = zc_Object_Form() AND TRIM (Object.ValueData) = inReportName);
+       END IF;
        
-       -- сохранили <Объект>
-       INSERT INTO ObjectBlob (DescId, ObjectId, ValueData )
-         SELECT zc_objectBlob_form_Data(), vbId , ObjectBlob.ValueData
-         FROM Object
-               INNER JOIN ObjectBlob ON ObjectId = Object.Id
-         WHERE Object.DescId    = zc_Object_Form()
-           AND Object.ValueData ILIKE ('%' || CASE WHEN inReportName ILIKE '%_70_70%' THEN '_70_70' ELSE '' END|| '.Sticker%')
-         ORDER BY Object.Id
-         LIMIT 1
-        ;
+       IF vbId > 0
+       THEN
+           -- сохранили <Объект>
+           UPDATE ObjectBlob SET ValueData = tmp_find.ValueData
+           FROM (SELECT ObjectBlob.ValueData
+                 FROM Object
+                       INNER JOIN ObjectBlob ON ObjectId = Object.Id
+                 WHERE Object.DescId    = zc_Object_Form()
+                   AND Object.ValueData ILIKE ('%' || CASE WHEN inReportName ILIKE '%_70_70%' THEN 'ШАБЛОН + ТМ ВАРТО (Варус) - Український_70_70' ELSE '' END|| '.Sticker%')
+                 ORDER BY Object.Id
+                 LIMIT 1
+                ) AS tmp_find
+           WHERE ObjectId = vbId AND DescId =zc_objectBlob_form_Data()
+          ;
+       ELSE
+           -- сохранили <Объект>
+           vbId:= lpInsertUpdate_Object (0, zc_Object_Form(), 0, inReportName);
+           
+           -- сохранили <Объект>
+           INSERT INTO ObjectBlob (DescId, ObjectId, ValueData )
+             SELECT zc_objectBlob_form_Data(), vbId , ObjectBlob.ValueData
+             FROM Object
+                   INNER JOIN ObjectBlob ON ObjectId = Object.Id
+             WHERE Object.DescId    = zc_Object_Form()
+               AND Object.ValueData ILIKE ('%' || CASE WHEN inReportName ILIKE '%_70_70%' THEN 'ШАБЛОН + ТМ ВАРТО (Варус) - Український_70_70' ELSE '' END|| '.Sticker%')
+             ORDER BY Object.Id
+             LIMIT 1
+            ;
+       END IF;
 
    END IF;
     
