01fe45fd5288dc32d8d1f52762a073e80fcd65e1
K0sta <ashtu777@gmail.com>
2025-11-07 20:59:54 +0200

    ok

--- DATABASE/COMMON/REPORTS/Personal/gpReport_Personal_Cash_sms.sql
+++ DATABASE/COMMON/REPORTS/Personal/gpReport_Personal_Cash_sms.sql
@@ -1,12 +1,12 @@
 -- Function: gpReport_Personal_Cash_sms ()
 /*
- в спрв ведомости - 
- новая кнопка + с правами - 
+ в спрв ведомости -
+ новая кнопка + с правами -
  отчет по zc_ObjectBoolean_PersonalServiceList_Sms -
-  открыть диалог (период) 
-  + потом за период  по всем ведомостям где PersonalServiceList_Sms = TRUE 
-  + по документам zc_Movement_Cash + zc_MI_Child - 
-  
+  открыть диалог (период)
+  + потом за период  по всем ведомостям где PersonalServiceList_Sms = TRUE
+  + по документам zc_Movement_Cash + zc_MI_Child -
+
   показать сотрудника (фио + должность + подразделение + ведоость) + + Movement.OperDate + InvNumber  + zc_MIBoolean_SMS + zc_MIDate_SMS + zc_ObjectString_Member_Phone
 
 */
@@ -19,11 +19,12 @@ CREATE OR REPLACE FUNCTION gpReport_Personal_Cash_sms(
     IN inSession            TVarChar   -- сессия пользователя
 )
 RETURNS TABLE (MovementId Integer, OperDate TDateTime, InvNumber TVarChar
-             , PersonalId Integer, PersonalCode Integer, PersonalName TVarChar
+             , MovementItemId Integer, PersonalId Integer, PersonalCode Integer, PersonalName TVarChar
              , PositionId Integer, PositionCode Integer, PositionName TVarChar
              , PositionLevelId Integer, PositionLevelCode Integer, PositionLevelName TVarChar
              , UnitId Integer, UnitCode Integer, UnitName TVarChar
-             , PersonalServiceListCode Integer, PersonalServiceListName TVarChar
+             , PersonalServiceListId Integer, PersonalServiceListName TVarChar
+             , Amount   TFloat
              , Date_SMS TDateTime
              , isSms    Boolean
              , Phone    TVarChar
@@ -48,13 +49,12 @@ BEGIN
                                 FROM Object AS Object_PersonalServiceList
                                      INNER JOIN ObjectBoolean AS ObjectBoolean_Sms
                                                               ON ObjectBoolean_Sms.ObjectId = Object_PersonalServiceList.Id
-                                                             AND ObjectBoolean_Sms.DescId = zc_ObjectBoolean_PersonalServiceList_Sms() 
+                                                             AND ObjectBoolean_Sms.DescId = zc_ObjectBoolean_PersonalServiceList_Sms()
                                                              AND ObjectBoolean_Sms.ValueData = TRUE
                                 WHERE Object_PersonalServiceList.DescId = zc_Object_PersonalServiceList()
                                   AND Object_PersonalServiceList.isErased = FALSE
---and Object_PersonalServiceList.Id = 7775704
-                                )
-     --документы начисления за период по выбранным ведомостям
+                               )
+     -- документы Выплата по выбранным ведомостям за период
    , tmpMovement AS (SELECT Movement.*
                           , MovementLinkObject_PersonalServiceList.ObjectId AS PersonalServiceListId
                      FROM Movement
@@ -72,7 +72,7 @@ BEGIN
                        AND Movement.StatusId = zc_Enum_Status_Complete()
                        AND Movement.ParentId > 0
                      )
-     
+
    , tmpMI_Child AS (WITH
                      tmpMI AS (SELECT MovementItem.*
                                FROM MovementItem
@@ -102,11 +102,12 @@ BEGIN
                      SELECT MovementItem.Id                          AS MovementItemId
                           , MovementItem.MovementId                  AS MovementId
                           , MovementItem.ObjectId                    AS PersonalId
+                          , MovementItem.Amount                      AS Amount
                           , MILinkObject_Unit.ObjectId               AS UnitId
                           , MILinkObject_Position.ObjectId           AS PositionId
                           , MIDate_SMS.ValueData      ::TDateTime    AS Date_SMS
                           , MIBoolean_SMS.ValueData   ::Boolean      AS isSms
-                     FROM tmpMI AS MovementItem 
+                     FROM tmpMI AS MovementItem
                           LEFT JOIN tmpMILO AS MILinkObject_Unit
                                             ON MILinkObject_Unit.MovementItemId = MovementItem.Id
                                            AND MILinkObject_Unit.DescId = zc_MILinkObject_Unit()
@@ -120,12 +121,13 @@ BEGIN
                           LEFT JOIN tmpMIBoolean AS MIBoolean_SMS
                                                  ON MIBoolean_SMS.MovementItemId = MovementItem.Id
                                                 AND MIBoolean_SMS.DescId         = zc_MIBoolean_SMS()
-                    ) 
- 
+                    )
+
     --
     SELECT Movement.Id                     AS MovementId
          , Movement.OperDate               AS OperDate
          , Movement.InvNumber              AS InvNumber
+         , tmpMI_Child.MovementItemId      AS MovementItemId
          , Object_Personal.Id              AS PersonalId
          , Object_Personal.ObjectCode      AS PersonalCode
          , Object_Personal.ValueData       AS PersonalName
@@ -138,11 +140,14 @@ BEGIN
          , Object_Unit.Id                  AS UnitId
          , Object_Unit.ObjectCode          AS UnitCode
          , Object_Unit.ValueData           AS UnitName
-         , Object_PersonalServiceList.Id     AS PersonalServiceListCode
-         , Object_PersonalServiceList.ValueData       AS PersonalServiceListName
+         , Object_PersonalServiceList.Id          AS PersonalServiceListId
+         , Object_PersonalServiceList.ValueData   AS PersonalServiceListName
+
+         , tmpMI_Child.Amount                            AS Amount
          , tmpMI_Child.Date_SMS              ::TDateTime AS Date_SMS
          , COALESCE (tmpMI_Child.isSms,FALSE)::Boolean   AS isSms
          , ObjectString_Phone.ValueData      :: TVarChar AS Phone
+
     FROM tmpMovement AS Movement
          LEFT JOIN tmpMI_Child ON tmpMI_Child.MovementId = Movement.Id
 
@@ -150,7 +155,7 @@ BEGIN
          LEFT JOIN Object AS Object_Position            ON Object_Position.Id            = tmpMI_Child.PositionId
          LEFT JOIN Object AS Object_Unit                ON Object_Unit.Id                = tmpMI_Child.UnitId
          LEFT JOIN Object AS Object_PersonalServiceList ON Object_PersonalServiceList.Id = Movement.PersonalServiceListId
-       
+
          LEFT JOIN ObjectLink AS ObjectLink_Personal_PositionLevel
                               ON ObjectLink_Personal_PositionLevel.ObjectId = tmpMI_Child.PersonalId
                              AND ObjectLink_Personal_PositionLevel.DescId   = zc_ObjectLink_Personal_PositionLevel()
@@ -159,11 +164,10 @@ BEGIN
          LEFT JOIN ObjectLink AS ObjectLink_Personal_Member
                               ON ObjectLink_Personal_Member.ObjectId = tmpMI_Child.PersonalId
                              AND ObjectLink_Personal_Member.DescId = zc_ObjectLink_Personal_Member()
-         
+
          LEFT JOIN ObjectString AS ObjectString_Phone
                                 ON ObjectString_Phone.ObjectId = ObjectLink_Personal_Member.ChildObjectId
-                               AND ObjectString_Phone.DescId = zc_ObjectString_Member_Phone() 
-
+                               AND ObjectString_Phone.DescId = zc_ObjectString_Member_Phone()
    ;
 
 END;
@@ -178,5 +182,4 @@ $BODY$
 
 
 -- тест
---SELECT * FROM gpReport_Personal_Cash_sms (inStartDate:= '20.10.2025'::TDateTime, inEndDate:= '20.10.2025'::TDateTime, inSession:= '9457'::TVarChar);
-
+-- SELECT * FROM gpReport_Personal_Cash_sms (inStartDate:= '20.10.2025'::TDateTime, inEndDate:= '20.10.2025'::TDateTime, inSession:= '9457'::TVarChar);
