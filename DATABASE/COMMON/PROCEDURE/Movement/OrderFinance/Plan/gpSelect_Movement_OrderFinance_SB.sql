32e07a3209aa4dd8b644e192d16726912710f62a
innafelon <innafelon@gmail.com>
2026-01-20 00:11:04 +0200

    +
    Signed-off-by: innafelon <innafelon@gmail.com>

--- DATABASE/COMMON/PROCEDURE/Movement/OrderFinance/Plan/gpSelect_Movement_OrderFinance_SB.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/OrderFinance/Plan/gpSelect_Movement_OrderFinance_SB.sql
@@ -503,42 +503,12 @@ BEGIN
 
                           , Movement.WeekNumber              ::TFloat    AS WeekNumber
                             -- Предварительный План на неделю
-                        /*  , COALESCE (MovementFloat_TotalSumm.Valuedata, 0)    ::TFloat   AS TotalSumm
-                            -- Согласована сумма на неделю
-                          , (COALESCE (MovementFloat_TotalSumm_1.Valuedata, 0) + COALESCE (MovementFloat_TotalSumm_2.Valuedata, 0) + COALESCE (MovementFloat_TotalSumm_3.Valuedata, 0)) ::TFloat AS TotalSumm_all
-                          , COALESCE (MovementFloat_TotalSumm_1.Valuedata, 0)  ::TFloat   AS TotalSumm_1
-                          , COALESCE (MovementFloat_TotalSumm_2.Valuedata, 0)  ::TFloat   AS TotalSumm_2
-                          , COALESCE (MovementFloat_TotalSumm_3.Valuedata, 0)  ::TFloat   AS TotalSumm_3
-
-                          , COALESCE (MovementFloat_AmountPlan_1.Valuedata, 0) ::TFloat   AS AmountPlan_1
-                          , COALESCE (MovementFloat_AmountPlan_2.Valuedata, 0) ::TFloat   AS AmountPlan_2
-                          , COALESCE (MovementFloat_AmountPlan_3.Valuedata, 0) ::TFloat   AS AmountPlan_3
-                          , COALESCE (MovementFloat_AmountPlan_4.Valuedata, 0) ::TFloat   AS AmountPlan_4
-                          , COALESCE (MovementFloat_AmountPlan_5.Valuedata, 0) ::TFloat   AS AmountPlan_5
-                         */
+                       
                           , zfCalc_Week_StartDate (Movement.OperDate, MovementFloat_WeekNumber.ValueData) AS StartDate_WeekNumber
                           , zfCalc_Week_EndDate   (Movement.OperDate, MovementFloat_WeekNumber.ValueData) AS EndDate_WeekNumber
 
-                        /*  , MovementDate_Update_report.ValueData ::TDateTime AS DateUpdate_report
-                          , Object_Update_report.ValueData       ::TVarChar  AS UserUpdate_report
-                          , Object_Member_1.ValueData            ::TVarChar  AS UserMember_1
-                          , Object_Member_2.ValueData            ::TVarChar  AS UserMember_2
-                          */
                           , MovementString_Comment.ValueData       AS Comment
 
-                         /* , Object_Insert.ValueData                AS InsertName
-                          , MovementDate_Insert.ValueData          AS InsertDate
-                          , Object_Update.ValueData                AS UpdateName
-                          , MovementDate_Update.ValueData          AS UpdateDate
-                          
-                          , Object_Unit_insert.ValueData      ::TVarChar AS UnitName_insert
-                          , Object_Position_insert.ValueData  ::TVarChar AS PositionName_insert
-
-                          , COALESCE (MovementDate_SignWait_1.ValueData, NULL)     ::TDateTime AS Date_SignWait_1
-                          , COALESCE (MovementDate_Sign_1.ValueData, NULL)         ::TDateTime AS Date_Sign_1
-                          , COALESCE (MovementBoolean_SignWait_1.ValueData, FALSE) ::Boolean   AS isSignWait_1
-                          , COALESCE (MovementBoolean_Sign_1.ValueData, FALSE)     ::Boolean   AS isSign_1     
-                          */ 
                           , MovementDate_SignSB.ValueData                          ::TDateTime AS Date_SignSB
                           , COALESCE (MovementBoolean_SignSB.ValueData, FALSE)     ::Boolean   AS isSignSB
                       FROM tmpMovement AS Movement
@@ -549,35 +519,6 @@ BEGIN
                                                    ON MovementFloat_WeekNumber.MovementId = Movement.Id
                                                   AND MovementFloat_WeekNumber.DescId = zc_MovementFloat_WeekNumber()
 
-                          /* LEFT JOIN MovementFloat AS MovementFloat_AmountPlan_1
-                                                   ON MovementFloat_AmountPlan_1.MovementId = Movement.Id
-                                                  AND MovementFloat_AmountPlan_1.DescId = zc_MovementFloat_AmountPlan_1()
-                           LEFT JOIN MovementFloat AS MovementFloat_AmountPlan_2
-                                                   ON MovementFloat_AmountPlan_2.MovementId = Movement.Id
-                                                  AND MovementFloat_AmountPlan_2.DescId = zc_MovementFloat_AmountPlan_2()
-                           LEFT JOIN MovementFloat AS MovementFloat_AmountPlan_3
-                                                   ON MovementFloat_AmountPlan_3.MovementId = Movement.Id
-                                                  AND MovementFloat_AmountPlan_3.DescId = zc_MovementFloat_AmountPlan_3()
-                           LEFT JOIN MovementFloat AS MovementFloat_AmountPlan_4
-                                                   ON MovementFloat_AmountPlan_4.MovementId = Movement.Id
-                                                  AND MovementFloat_AmountPlan_4.DescId = zc_MovementFloat_AmountPlan_4()
-                           LEFT JOIN MovementFloat AS MovementFloat_AmountPlan_5
-                                                   ON MovementFloat_AmountPlan_5.MovementId = Movement.Id
-                                                  AND MovementFloat_AmountPlan_5.DescId = zc_MovementFloat_AmountPlan_5()
-
-                           LEFT JOIN MovementFloat AS MovementFloat_TotalSumm
-                                                   ON MovementFloat_TotalSumm.MovementId = Movement.Id
-                                                  AND MovementFloat_TotalSumm.DescId = zc_MovementFloat_TotalSumm()
-                           LEFT JOIN MovementFloat AS MovementFloat_TotalSumm_1
-                                                   ON MovementFloat_TotalSumm_1.MovementId = Movement.Id
-                                                  AND MovementFloat_TotalSumm_1.DescId = zc_MovementFloat_TotalSumm_1()
-                           LEFT JOIN MovementFloat AS MovementFloat_TotalSumm_2
-                                                   ON MovementFloat_TotalSumm_2.MovementId = Movement.Id
-                                                  AND MovementFloat_TotalSumm_2.DescId = zc_MovementFloat_TotalSumm_2()
-                           LEFT JOIN MovementFloat AS MovementFloat_TotalSumm_3
-                                                   ON MovementFloat_TotalSumm_3.MovementId = Movement.Id
-                                                  AND MovementFloat_TotalSumm_3.DescId = zc_MovementFloat_TotalSumm_3()
-                           */
                            LEFT JOIN MovementString AS MovementString_Comment
                                                     ON MovementString_Comment.MovementId = Movement.Id
                                                    AND MovementString_Comment.DescId = zc_MovementString_Comment()
@@ -586,66 +527,6 @@ BEGIN
                                                         ON MovementLinkObject_OrderFinance.MovementId = Movement.Id
                                                        AND MovementLinkObject_OrderFinance.DescId = zc_MovementLinkObject_OrderFinance()
                            LEFT JOIN Object AS Object_OrderFinance ON Object_OrderFinance.Id = MovementLinkObject_OrderFinance.ObjectId
-
-                           /*LEFT JOIN MovementLinkObject AS MovementLinkObject_Update_report
-                                                        ON MovementLinkObject_Update_report.MovementId = Movement.Id
-                                                       AND MovementLinkObject_Update_report.DescId = zc_MovementLinkObject_Update_report()
-                           LEFT JOIN Object AS Object_Update_report ON Object_Update_report.Id = MovementLinkObject_Update_report.ObjectId
-
-                           LEFT JOIN MovementLinkObject AS MovementLinkObject_Member_1
-                                                        ON MovementLinkObject_Member_1.MovementId = Movement.Id
-                                                       AND MovementLinkObject_Member_1.DescId = zc_MovementLinkObject_Member_1()
-                           LEFT JOIN Object AS Object_Member_1 ON Object_Member_1.Id = MovementLinkObject_Member_1.ObjectId
-
-                           LEFT JOIN MovementLinkObject AS MovementLinkObject_Member_2
-                                                        ON MovementLinkObject_Member_2.MovementId = Movement.Id
-                                                       AND MovementLinkObject_Member_2.DescId = zc_MovementLinkObject_Member_2()
-                           LEFT JOIN Object AS Object_Member_2 ON Object_Member_2.Id = MovementLinkObject_Member_2.ObjectId
-
-                           LEFT JOIN MovementLinkObject AS MovementLinkObject_Unit
-                                                        ON MovementLinkObject_Unit.MovementId = Movement.Id
-                                                       AND MovementLinkObject_Unit.DescId = zc_MovementLinkObject_Unit()
-                           LEFT JOIN Object AS Object_Unit_insert ON Object_Unit_insert.Id = MovementLinkObject_Unit.ObjectId
-
-                           LEFT JOIN MovementLinkObject AS MovementLinkObject_Position
-                                                        ON MovementLinkObject_Position.MovementId = Movement.Id
-                                                       AND MovementLinkObject_Position.DescId = zc_MovementLinkObject_Position()
-                           LEFT JOIN Object AS Object_Position_insert ON Object_Position_insert.Id = MovementLinkObject_Position.ObjectId
-
-                           LEFT JOIN MovementDate AS MovementDate_Update_report
-                                                  ON MovementDate_Update_report.MovementId = Movement.Id
-                                                 AND MovementDate_Update_report.DescId = zc_MovementDate_Update_report()
-
-                           LEFT JOIN MovementDate AS MovementDate_Insert
-                                                  ON MovementDate_Insert.MovementId = Movement.Id
-                                                 AND MovementDate_Insert.DescId = zc_MovementDate_Insert()
-                           LEFT JOIN MovementDate AS MovementDate_Update
-                                                  ON MovementDate_Update.MovementId = Movement.Id
-                                                 AND MovementDate_Update.DescId = zc_MovementDate_Update()
-
-                           LEFT JOIN MovementLinkObject AS MovementLinkObject_Insert
-                                                        ON MovementLinkObject_Insert.MovementId = Movement.Id
-                                                       AND MovementLinkObject_Insert.DescId = zc_MovementLinkObject_Insert()
-                           LEFT JOIN Object AS Object_Insert ON Object_Insert.Id = MovementLinkObject_Insert.ObjectId
-
-                           LEFT JOIN MovementLinkObject AS MovementLinkObject_Update
-                                                        ON MovementLinkObject_Update.MovementId = Movement.Id
-                                                       AND MovementLinkObject_Update.DescId = zc_MovementLinkObject_Update()
-                           LEFT JOIN Object AS Object_Update ON Object_Update.Id = MovementLinkObject_Update.ObjectId
-
-                           LEFT JOIN MovementDate AS MovementDate_SignWait_1
-                                                  ON MovementDate_SignWait_1.MovementId = Movement.Id
-                                                 AND MovementDate_SignWait_1.DescId = zc_MovementDate_SignWait_1()
-                           LEFT JOIN MovementDate AS MovementDate_Sign_1
-                                                  ON MovementDate_Sign_1.MovementId = Movement.Id
-                                                 AND MovementDate_Sign_1.DescId = zc_MovementDate_Sign_1()
-                           LEFT JOIN MovementBoolean AS MovementBoolean_SignWait_1
-                                                     ON MovementBoolean_SignWait_1.MovementId = Movement.Id
-                                                    AND MovementBoolean_SignWait_1.DescId = zc_MovementBoolean_SignWait_1()
-                           LEFT JOIN MovementBoolean AS MovementBoolean_Sign_1
-                                                     ON MovementBoolean_Sign_1.MovementId = Movement.Id
-                                                    AND MovementBoolean_Sign_1.DescId = zc_MovementBoolean_Sign_1() 
-                        */
 
                            LEFT JOIN MovementDate AS MovementDate_SignSB
                                                   ON MovementDate_SignSB.MovementId = Movement.Id
@@ -782,13 +663,7 @@ BEGIN
         , tmpMovement.StatusName
         , tmpMovement.OrderFinanceId
         , tmpMovement.OrderFinanceName
-     /*   , COALESCE (tmpJuridicalOrderFinance.BankAccountId_main, tmpJuridicalOrderFinance_last.BankAccountId_main)          ::Integer  AS BankAccountId
-        , COALESCE (tmpJuridicalOrderFinance.BankAccountName_main, tmpJuridicalOrderFinance_last.BankAccountName_main)      ::TVarChar AS BankAccountName
-        , COALESCE (tmpJuridicalOrderFinance.BankId_main, tmpJuridicalOrderFinance_last.BankId_main)                        ::Integer  AS BankId
-        , COALESCE (tmpJuridicalOrderFinance.BankName_main, tmpJuridicalOrderFinance_last.BankName_main)                    ::TVarChar AS BankName
-        , COALESCE (tmpJuridicalOrderFinance.BankAccountNameAll_main, tmpJuridicalOrderFinance_last.BankAccountNameAll_main)::TVarChar AS BankAccountNameAll
-        , COALESCE (tmpJuridicalOrderFinance.MFO_main, tmpJuridicalOrderFinance_last.MFO_main)                              ::TVarChar AS MFO
-          */
+
         , tmpMovement.WeekNumber
         , tmpMovement.StartDate_WeekNumber ::TDateTime
         , tmpMovement.EndDate_WeekNumber   ::TDateTime  --20
@@ -849,29 +724,7 @@ BEGIN
         , tmpMI.isAmountPlan_day ::Boolean
 
         , tmpMI.Comment        ::TVarChar AS Comment
-    /*
-          -- всегда считаем - ФАКТ Назначение платежа
-        , CASE WHEN COALESCE (tmpMI.isAmountPlan_day, TRUE) = TRUE AND tmpMI.AmountPlan_day > 0
-                    THEN REPLACE
-                        (REPLACE
-                        (REPLACE
-                        (REPLACE (COALESCE (tmpJuridicalOrderFinance.Comment, tmpJuridicalOrderFinance_last.Comment)
-                                                                  , 'NOM_DOG', COALESCE (tmpMI.ContractName, ''))
-                                                                  , 'DATA_DOG', zfConvert_DateToString (COALESCE (tmpMI.StartDate, zc_DateStart())))
-                                                                  , 'PDV', '20')
-                                                                  , 'SUMMA_P', zfConvert_FloatToString (ROUND(tmpMI.AmountPlan_day/6, 2)))
-               ELSE ''
-          END :: TVarChar AS Comment_pay
-
-        , COALESCE (tmpJuridicalOrderFinance.JuridicalOrderFinanceId, tmpJuridicalOrderFinance_last.JuridicalOrderFinanceId)  ::Integer  AS JuridicalOrderFinanceId
-        , COALESCE (tmpJuridicalOrderFinance.Comment, tmpJuridicalOrderFinance_last.Comment)                                  ::TVarChar AS Comment_jof          -- JuridicalOrderFinance
-        , COALESCE (tmpJuridicalOrderFinance.BankAccountId, tmpJuridicalOrderFinance_last.BankAccountId)                      ::Integer  AS BankAccountId_jof    -- JuridicalOrderFinance
-        , COALESCE (tmpJuridicalOrderFinance.BankAccountName, tmpJuridicalOrderFinance_last.BankAccountName)                  ::TVarChar AS BankAccountName_jof  -- JuridicalOrderFinance
-        , COALESCE (tmpJuridicalOrderFinance.BankId, tmpJuridicalOrderFinance_last.BankId )                                   ::Integer  AS BankId_jof
-        , COALESCE (tmpJuridicalOrderFinance.BankName, tmpJuridicalOrderFinance_last.BankName)                                ::TVarChar AS BankName_jof
-        , COALESCE (tmpJuridicalOrderFinance.MFO, tmpJuridicalOrderFinance_last.MFO)                                          ::TVarChar AS MFO_jof
-        */
-        
+   
         --строки для согласования СБ
         , tmpMI.MovementItemId_Child
         , tmpMI.Amount
@@ -893,17 +746,7 @@ BEGIN
 
         LEFT JOIN tmpInfoMoney_OFP ON tmpInfoMoney_OFP.InfoMoneyId = tmpMI.InfoMoneyId
                                   AND tmpInfoMoney_OFP.OrderFinanceId = tmpMovement.OrderFinanceId
-      /*  -- привязка  юр.лицо + статья + выбранный банк (плательщик)
-        LEFT JOIN tmpJuridicalOrderFinance ON tmpJuridicalOrderFinance.JuridicalId = tmpMI.JuridicalId
-                                          AND tmpJuridicalOrderFinance.InfoMoneyId = tmpMI.InfoMoneyId
-                                          AND inBankMainId <> 0
-                                          AND tmpJuridicalOrderFinance.Ord = 1
-
-        -- привязка  юр.лицо + статья + последний платеж
-        LEFT JOIN tmpJuridicalOrderFinance_last ON tmpJuridicalOrderFinance_last.JuridicalId = tmpMI.JuridicalId
-                                               AND tmpJuridicalOrderFinance_last.InfoMoneyId = tmpMI.InfoMoneyId
-                                               AND tmpJuridicalOrderFinance_last.Ord = 1
-   */
+   
       ;
 
 END;
