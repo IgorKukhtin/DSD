3fc124fe9e9736ae2f0a2b9422fb1e5ddccf38d6
K0sta <ashtu777@gmail.com>
2026-02-03 13:18:33 +0200

    ok

---
+++ DATABASE/COMMON/FUNCTION/zfCalc_Comment_pay_OrderFinance.sql
@@ -0,0 +1,40 @@
+-- Function: zfCalc_Comment_pay_OrderFinance
+
+DROP FUNCTION IF EXISTS zfCalc_Comment_pay_OrderFinance (TVarChar, TVarChar, TVarChar, TDateTime, TFloat, TFloat);
+
+CREATE OR REPLACE FUNCTION zfCalc_Comment_pay_OrderFinance(
+    IN inComment     Text,
+    IN inNOM_DOG     TVarChar,
+    IN inNOM_IVOICE  TVarChar,
+    IN inDATA_DOG    TDateTime,
+    IN inPDV         TFloat,
+    IN inSUMMA_P     TFloat
+)
+RETURNS Text
+AS
+$BODY$
+BEGIN
+
+     RETURN REPLACE
+           (REPLACE
+           (REPLACE
+           (REPLACE
+           (REPLACE (COALESCE (inComment, ''), 'NOM_DOG',    COALESCE (inNOM_DOG, ''))
+                                             , 'NOM_IVOICE', COALESCE (inNOM_IVOICE, ''))
+                                             , 'DATA_DOG',   zfConvert_DateToString (COALESCE (inDATA_DOG, zc_DateStart())))
+                                             , 'PDV', '20')
+                                             , 'SUMMA_P', zfConvert_FloatToString (ROUND(inSUMMA_P/6, 2)));
+
+END;
+$BODY$
+  LANGUAGE PLPGSQL VOLATILE;
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 11.09.25                                        *
+*/
+
+-- тест
+-- SELECT * FROM zfCalc_Comment_pay_OrderFinance ('text: 1(NOM_DOG) 2(NOM_IVOICE) 3(DATA_DOG) 4(PDV) 5(SUMMA_P)', '11', '22', CURRENT_DATE, 44, 240);
