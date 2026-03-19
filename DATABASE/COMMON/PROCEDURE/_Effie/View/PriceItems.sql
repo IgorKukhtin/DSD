f10d2407cb6a82a27f4f5ff35061d2473a72f2b2
K0sta <ashtu777@gmail.com>
2026-03-19 13:40:31 +0200

    ok

--- DATABASE/COMMON/PROCEDURE/_Effie/View/PriceItems.sql
+++ DATABASE/COMMON/PROCEDURE/_Effie/View/PriceItems.sql
@@ -9,7 +9,7 @@ AS
                            , productExtId
                            , price
                            , isDeleted
-                      FROM dblink ('host=192.168.0.228 dbname=project port=5432 user=project password=sqoII5szOnrcZxJVF1BL'::text
+                      FROM dblink ('host=192.168.0.219 dbname=project port=5432 user=project password=sqoII5szOnrcZxJVF1BL'::text
                                  , ('SELECT *
                                     FROM gpSelect_Object_PriceItems_effie(zfCalc_UserAdmin())'
                                     ) :: Text
