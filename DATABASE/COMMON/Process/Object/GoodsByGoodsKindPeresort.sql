13d6bb7edc071cec20ecafb1af45be1db5a9b6d6
K0sta <ashtu777@gmail.com>
2026-04-08 22:04:15 +0300

    ok

--- DATABASE/COMMON/Process/Object/GoodsByGoodsKindPeresort.sql
+++ DATABASE/COMMON/Process/Object/GoodsByGoodsKindPeresort.sql
@@ -1,21 +1,23 @@
-CREATE OR REPLACE FUNCTION zc_Enum_Process_InsertUpdate_Object_GoodsByGoodsKindPeresort() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_InsertUpdate_Object_GoodsByGoodsKindPeresort' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
-CREATE OR REPLACE FUNCTION zc_Enum_Process_Update_Object_isErased_GoodsByGoodsKindPeresort() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_InsertUpdate_Object_GoodsByGoodsKindPeresort' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
+-- CREATE OR REPLACE FUNCTION zc_Enum_Process_InsertUpdate_Object_GoodsByGoodsKindPeresort() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_InsertUpdate_Object_GoodsByGoodsKindPeresort' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
+-- CREATE OR REPLACE FUNCTION zc_Enum_Process_Update_Object_isErased_GoodsByGoodsKindPeresort() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_InsertUpdate_Object_GoodsByGoodsKindPeresort' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
  
+CREATE OR REPLACE FUNCTION zc_Enum_Process_InsertUpdate_Object_GK_Peresort() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_InsertUpdate_Object_GK_Peresort' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION zc_Enum_Process_Update_Object_isErased_GK_Peresort() RETURNS Integer AS $BODY$BEGIN RETURN (SELECT ObjectId AS Id FROM ObjectString WHERE ValueData = 'zc_Enum_Process_Update_Object_isErased_GK_Peresort' AND DescId = zc_ObjectString_Enum()); END; $BODY$ LANGUAGE plpgsql;
 
 DO $$
 BEGIN
 
-PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_InsertUpdate_Object_GoodsByGoodsKindPeresort()
+PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_InsertUpdate_Object_GK_Peresort()
                                   , inDescId:= zc_Object_Process()
                                   , inCode:= 1
                                   , inName:= 'Справочник <'||(SELECT ItemName FROM ObjectDesc WHERE Id = zc_Object_GoodsByGoodsKindPeresort())||'> - сохранение данных.'
-                                  , inEnumName:= 'zc_Enum_Process_InsertUpdate_Object_GoodsByGoodsKindPeresort');
+                                  , inEnumName:= 'zc_Enum_Process_InsertUpdate_Object_GK_Peresort');
 
-PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_Update_Object_isErased_GoodsByGoodsKindPeresort()
+PERFORM lpInsertUpdate_Object_Enum (inId:= zc_Enum_Process_Update_Object_isErased_GK_Peresort()
                                   , inDescId:= zc_Object_Process()
                                   , inCode:= 2
                                   , inName:= 'Справочник <'||(SELECT ItemName FROM ObjectDesc WHERE Id = zc_Object_GoodsByGoodsKindPeresort())||'> - удаление данных.'
-                                  , inEnumName:= 'zc_Enum_Process_Update_Object_isErased_GoodsByGoodsKindPeresort');
+                                  , inEnumName:= 'zc_Enum_Process_Update_Object_isErased_GK_Peresort');
 END $$;
 
 /*-------------------------------------------------------------------------------*/
