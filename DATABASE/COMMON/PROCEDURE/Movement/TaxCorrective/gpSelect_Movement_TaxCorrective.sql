451a75bdd368b861c3a52ea0cba8765c81f16eac
innafelon <innafelon@gmail.com>
2026-06-04 16:25:50 +0300

    +
    Signed-off-by: innafelon <innafelon@gmail.com>

--- DATABASE/COMMON/PROCEDURE/Movement/TaxCorrective/gpSelect_Movement_TaxCorrective.sql
+++ DATABASE/COMMON/PROCEDURE/Movement/TaxCorrective/gpSelect_Movement_TaxCorrective.sql
@@ -223,7 +223,7 @@ BEGIN
            , CASE WHEN MovementBoolean_Document.ValueData = TRUE THEN 'V' ELSE '-' END :: TVarChar AS DocumentValue
            , MovementDate_DateRegistered.ValueData      AS DateRegistered
            , COALESCE (MovementDate_DateRegistered.ValueData, inEndDate) :: TDateTime AS DateRegistered_notNull
-           , CASE WHEN COALESCE (MovementDate_DateRegistered.ValueData, zc_DateEnd()) = zc_DateEnd() THEN EXTRACT ( 'DAY' from ((Movement.OperDate + INTERVAL '18 DAY') - Current_Date)) ELSE 0 END ::Integer AS DayForRegister
+           , CASE WHEN EXTRACT ( 'DAY' from ((Movement.OperDate + INTERVAL '18 DAY') - Current_Date)) > 0 THEN EXTRACT ( 'DAY' from ((Movement.OperDate + INTERVAL '18 DAY') - Current_Date)) ELSE 0 END ::Integer AS DayForRegister
            , MovementString_InvNumberRegistered.ValueData   AS InvNumberRegistered
            , MovementBoolean_PriceWithVAT.ValueData     AS PriceWithVAT
            , MovementFloat_VATPercent.ValueData         AS VATPercent
