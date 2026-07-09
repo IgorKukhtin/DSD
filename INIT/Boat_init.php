9946a3c9f2cea52252e6ff1f967ee862ebce41d6
K0sta <ashtu777@gmail.com>
2026-07-09 16:15:09 +0300

    ok

--- INIT/Boat_init.php
+++ INIT/Boat_init.php
@@ -1,5 +1,5 @@
 <?php
-  $connectstring = "port=5432 dbname=projectboat_real host=172.20.0.2 user=postgres password=ET862tira";
-  $host = "http://in.mer-lin.org.ua/projectboat_real/index.php";
+  $connectstring = "port=5432 dbname=projectboat_test host=172.20.0.2 user=postgres password=ET862tira";
+  $host = "http://in.mer-lin.org.ua/projectboat_test/index.php";
   $isArchive = true;
 ?>
