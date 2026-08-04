4f26e1938c2c0ad9275bfa446b503686baf0d36a
K0sta <ashtu777@gmail.com>
2026-08-04 20:33:01 +0300

    ok

---
+++ DATABASE/COMMON/PROCEDURE/Container/Get/lpInsertUpdate_ContainerCount_Virt.sql
@@ -0,0 +1,51 @@
+-- Function: lpInsertUpdate_ContainerCount_Virt (TDateTime, Integer, Integer, Integer, Integer, Integer, Boolean, Integer, Integer)
+
+DROP FUNCTION IF EXISTS lpInsertUpdate_ContainerCount_Virt (TDateTime, Integer, Integer, Integer, Integer, Integer, Integer, Boolean, Integer, Integer, Integer, Integer, Integer);
+
+CREATE OR REPLACE FUNCTION lpInsertUpdate_ContainerCount_Virt (
+    IN inOperDate               TDateTime,
+    IN inUnitId                 Integer ,
+    IN inInfoMoneyDestinationId Integer ,
+    IN inInfoMoneyId            Integer ,
+    IN inGoodsId                Integer ,
+    IN inGoodsKindId            Integer ,
+)
+  RETURNS Integer
+AS
+$BODY$
+   DECLARE vbContainerId Integer;
+BEGIN
+
+     -- 1. - 20700 Товары + 20900 Ирна + 30100 Продукция
+     IF 1=1 -- inInfoMoneyDestinationId IN (zc_Enum_InfoMoneyDestination_20700(), zc_Enum_InfoMoneyDestination_20900(), zc_Enum_InfoMoneyDestination_30100(), zc_Enum_InfoMoneyDestination_30200())
+     THEN vbContainerId := lpInsertFind_Container (inContainerDescId   := zc_Container_CountVirt()
+                                                 , inParentId          := NULL
+                                                 , inObjectId          := inGoodsId
+                                                 , inJuridicalId_basis := NULL
+                                                 , inBusinessId        := NULL
+                                                 , inObjectCostDescId  := NULL
+                                                 , inObjectCostId      := NULL
+                                                 , inDescId_1          := zc_ContainerLinkObject_Unit()
+                                                 , inObjectId_1        := inUnitId
+                                                 , inDescId_2          := zc_ContainerLinkObject_GoodsKind()
+                                                 , inObjectId_2        := CASE inGoodsKindId
+                                                  );
+     -- 2.1. !!!Other!!!
+     END IF;
+
+     -- Возвращаем значение
+     RETURN (vbContainerId);
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+/*-------------------------------------------------------------------------------*/
+/*
+ ИСТОРИЯ РАЗРАБОТКИ: ДАТА, АВТОР
+               Фелонюк И.В.   Кухтин И.В.   Климентьев К.И.
+ 20.07.26                                        * add inAccountId
+*/
+
+-- тест
+-- SELECT * FROM lpInsertUpdate_ContainerCount_Virt ()
